60431239122164bcb4f8836280453d33792b4b73
Michael Wiegand <michael.wiegand@greenbone.net>
2009-12-10 11:39:26 +0000

    * src/gsad.c (exec_omp_get): Increased maximum result count.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@6122 423fd1db-d629-0410-8442-d21db03e70f4

--- src/gsad.c
+++ src/gsad.c
@@ -1497,7 +1497,7 @@ exec_omp_get (struct MHD_Connection *connection)
       if (!first_result || sscanf (first_result, "%u", &first) != 1)
         first = 1;
       if (!max_results || sscanf (max_results, "%u", &max) != 1)
-        max = 1000;
+        max = 10000;
 
       if (format != NULL)
         {
