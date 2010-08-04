9c5e342f56a9af4db1a51f7915275f8e3d22db72
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2010-08-04 09:14:11 +0000

    * src/gsad_omp.c: Reverted unrelated changes of recent backport.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/branches/gsa-1-0@8663 423fd1db-d629-0410-8442-d21db03e70f4

--- src/gsad_omp.c
+++ src/gsad_omp.c
@@ -4593,14 +4593,11 @@ get_report_omp (credentials_t * credentials, const char *report_id,
     }
   else
     {
-      gchar *task_id = NULL;
-
       /* Format is NULL, send XSL transformed XML. */
 
-      xml = g_string_new ("<get_report>");
+      xml = g_string_new ("<commands_response>");
 
-      entity = NULL;
-      if (read_entity_and_string (&session, &entity, &xml))
+      if (read_string (&session, &xml))
         {
           openvas_server_close (socket, session);
           return gsad_message ("Internal error", __FUNCTION__, __LINE__,
@@ -4655,7 +4652,7 @@ get_report_omp (credentials_t * credentials, const char *report_id,
         g_string_append (xml, "</all>");
       }
 
-      g_string_append (xml, "</get_report>");
+      g_string_append (xml, "</commands_response>");
       openvas_server_close (socket, session);
       return xsl_transform_omp (credentials, g_string_free (xml, FALSE));
     }
