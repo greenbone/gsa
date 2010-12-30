5faa64ef4d32943c5cf567222f9f08bb31968fa7
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2010-12-30 20:23:44 +0000

    * src/gsad.c (main): Adapted --version output to comply with GNU Coding Standards (http://www.gnu.org/prep/standards/standards.html#g_t_002d_002dversion)
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@9837 423fd1db-d629-0410-8442-d21db03e70f4

--- src/gsad.c
+++ src/gsad.c
@@ -4673,8 +4673,12 @@ main (int argc, char **argv)
 
   if (print_version)
     {
-      printf ("gsad %s\n", GSAD_VERSION);
-      printf ("Copyright (C) 2009 Greenbone Networks GmbH\n\n");
+      printf ("Greenbone Security Assistant %s\n", GSAD_VERSION);
+      printf ("Copyright (C) 2010 Greenbone Networks GmbH\n");
+      printf ("License GPLv2+: GNU GPL version 2 or later\n");
+      printf
+        ("This is free software: you are free to change and redistribute it.\n"
+         "There is NO WARRANTY, to the extent permitted by law.\n\n");
       exit (EXIT_SUCCESS);
     }
 
