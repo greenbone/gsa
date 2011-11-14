015770ed094b32908c705f611d2e3f4877b83767
Matthew Mundell <matthew.mundell@greenbone.net>
2011-11-14 15:01:04 +0000

    * src/gsad.c (init_validator): Add - to hostport.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@12113 423fd1db-d629-0410-8442-d21db03e70f4

--- src/gsad.c
+++ src/gsad.c
@@ -652,7 +652,7 @@ init_validator ()
   openvas_validator_add (validator, "max_results",  "^[0-9]+$");
   openvas_validator_add (validator, "format",     "^[-[:alnum:]]{1,15}$");
   openvas_validator_add (validator, "host",       "^[[:alnum:]\\.]{1,80}$");
-  openvas_validator_add (validator, "hostport",   "^[[:alnum:]\\. :]{1,80}$");
+  openvas_validator_add (validator, "hostport",   "^[-[:alnum:]\\. :]{1,80}$");
   openvas_validator_add (validator, "hosts",      "^[-[:alnum:],: \\./]{1,2000}$");
   openvas_validator_add (validator, "hosts_allow", "^0|1|2$");
   openvas_validator_add (validator, "hosts_opt",  "^[[:alnum:], \\./]{0,80}$");
