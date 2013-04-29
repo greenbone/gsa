bcc76edf426cabf4ce8e451b748e3dce2ed500b7
Matthew Mundell <matthew.mundell@greenbone.net>
2013-04-29 20:18:54 +0000

    * src/gsad_omp.c (get_many): Set rows in filter for GET_FILTERS to get 	all rows, otherwise Rows Per Page is used.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@16184 423fd1db-d629-0410-8442-d21db03e70f4

--- src/gsad_omp.c
+++ src/gsad_omp.c
@@ -939,7 +939,7 @@ get_many (const char *type, credentials_t * credentials, params_t *params,
 
   if (openvas_server_sendf_xml (&session,
                                 "<get_filters"
-                                " filter=\"type=%s or type=\"/>",
+                                " filter=\"rows=-1 type=%s or type=\"/>",
                                 type)
       == -1)
     {
