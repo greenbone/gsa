2782e76a9a51968838dadda57fdde902bbb2584e
Matthew Mundell <matthew.mundell@greenbone.net>
2013-07-30 12:25:35 +0000

    Backport r17276.
    	* src/gsad.c (init_validator): Add parentheses to comment chars.
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/branches/gsa-4.0@17277 423fd1db-d629-0410-8442-d21db03e70f4

--- src/gsad.c
+++ src/gsad.c
@@ -710,7 +710,7 @@ init_validator ()
   openvas_validator_add (validator, "autofp_value", "^(1|2)$");
   openvas_validator_add (validator, "boolean",    "^0|1$");
   openvas_validator_add (validator, "caller",     "^.*$");
-  openvas_validator_add (validator, "comment",    "^[-_;'[:alnum:]äüöÄÜÖß, \\./]{0,400}$");
+  openvas_validator_add (validator, "comment",    "^[-_;'()[:alnum:]äüöÄÜÖß, \\./]{0,400}$");
   openvas_validator_add (validator, "config_id",  "^[a-z0-9\\-]+$");
   openvas_validator_add (validator, "config_id_optional", "^(--|[a-z0-9\\-]+)$");
   openvas_validator_add (validator, "condition",  "^[[:alnum:] ]{0,100}$");
