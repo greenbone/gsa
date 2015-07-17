3e246bfc66f2b9b17ea72455dd175efb8dce2e7f
Matthew Mundell <matthew.mundell@greenbone.net>
2015-07-17 10:32:10 +0000

    * src/gsad_omp.c (manager_connect): Init html to keep compiler quiet.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@22837 423fd1db-d629-0410-8442-d21db03e70f4

--- src/gsad_omp.c
+++ src/gsad_omp.c
@@ -23139,6 +23139,8 @@ int
 manager_connect (credentials_t *credentials, int *socket,
                  gnutls_session_t *session, gchar **html)
 {
+  *html = NULL;  /* Keep compiler quiet. */
+
   *socket = openvas_server_open (session,
                                  manager_address
                                   ? manager_address
