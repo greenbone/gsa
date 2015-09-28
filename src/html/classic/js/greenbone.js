39f2e0127d29e52ca302d181b8e74a7251f72ad5
Benoît Allard <benoit.allard@greenbone.net>
2015-09-28 09:43:34 +0000

    Move the error handling to a separate callback
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@23443 423fd1db-d629-0410-8442-d21db03e70f4

--- src/html/classic/js/greenbone.js
+++ src/html/classic/js/greenbone.js
@@ -130,12 +130,12 @@
       contentType: false,
       type: 'POST',
       dataType: 'xml',
-    }).done(function(xml){
-      xml = $(xml);
-      var response = xml.find(RESPONSE_SELECTORS[self.command]),
-          gsad_msg = xml.find('gsad_msg');
-      if ((gsad_msg.length > 0) || (! isStatusOk(response.attr('status')))){
-        var error = gsad_msg.attr("status_text");
+    })
+      .fail(function(jqXHR){
+        var xml = $(jqXHR.responseXML),
+            response = xml.find(RESPONSE_SELECTORS[self.command]),
+            gsad_msg = xml.find('gsad_msg'),
+            error = gsad_msg.attr("status_text");
         if (gsad_msg.length == 0){
           error = response.attr('status_text');
         }
@@ -143,33 +143,35 @@
 
         // restore the original button.
         self.done();
+      })
+      .done(function(xml){
+        xml = $(xml);
+        var response = xml.find(RESPONSE_SELECTORS[self.command]),
+            gsad_msg = xml.find('gsad_msg');
+        if (self.reload === true){
+          window.document.location.reload();
+          // a bit overkill, but better-safe-than-sorry.
+          return;
+        }
+        if (self.element === undefined){
+          // No element to update, exit early.
+          self.dialog.dialog("close");
+          return;
+        }
+        // get the uuid of the created resource
+        var uuid = response.attr("id");
+        // get its name
+        var name = NAME_SELECTORS[self.command](xml, uuid);
+        // fill in the new information in the $element and make it selected
+        self.element.append($("<option/>", {value: uuid, html: name, selected: true}));
+        /// make it the selected option.
+        self.element.select2("destroy");
+        self.element.select2();
 
-        return;
-      }
-      if (self.reload === true){
-        window.document.location.reload();
-        // a bit overkill, but better-safe-than-sorry.
-        return;
-      }
-      if (self.element === undefined){
-        // No element to update, exit early.
+        // And finally, close our dialog.
         self.dialog.dialog("close");
-        return;
-      }
-      // get the uuid of the created resource
-      var uuid = response.attr("id");
-      // get its name
-      var name = NAME_SELECTORS[self.command](xml, uuid);
-      // fill in the new information in the $element and make it selected
-      self.element.append($("<option/>", {value: uuid, html: name, selected: true}));
-      /// make it the selected option.
-      self.element.select2("destroy");
-      self.element.select2();
-
-      // And finally, close our dialog.
-      self.dialog.dialog("close");
-      self.dialog = undefined;
-    });
+        self.dialog = undefined;
+      });
   };
 
   OMPDialog.prototype.show = function(button){
