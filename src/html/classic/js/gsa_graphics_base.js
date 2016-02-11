9822d0b43a4e06a9f71c07e7a22fbe5505e31749
Björn Ricks <bjoern.ricks@greenbone.net>
2016-02-11 16:32:08 +0000

    * src/html/classic/js/gsa_graphics_base.js: Update Copyright and add me as an author.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@24360 423fd1db-d629-0410-8442-d21db03e70f4

--- src/html/classic/js/gsa_graphics_base.js
+++ src/html/classic/js/gsa_graphics_base.js
@@ -5,9 +5,10 @@
  *
  * Authors:
  * Timo Pollmeier <timo.pollmeier@greenbone.net>
+ * Björn Ricks <bjoern.ricks@greenbone.net>
  *
  * Copyright:
- * Copyright (C) 2014 Greenbone Networks GmbH
+ * Copyright (C) 2014 - 2006 Greenbone Networks GmbH
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
