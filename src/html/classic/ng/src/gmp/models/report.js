ee6e75afbb7c752e2beee474119c88001db6b70e
Björn Ricks <bjoern.ricks@greenbone.net>
2017-02-03 10:32:00 +0000

    * src/html/classic/ng/src/gmp/models/report.js: Extend the parsing of the Report model. Add a ReportReport model (The report response is really mixed up and strange).
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@27449 423fd1db-d629-0410-8442-d21db03e70f4

--- src/html/classic/ng/src/gmp/models/report.js
+++ src/html/classic/ng/src/gmp/models/report.js
@@ -21,9 +21,45 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+import {parse_float} from '../../utils.js';
+
 import Model from '../model.js';
 
+class ReportReport extends Model {
+
+  parseProperties(elem) {
+    let ret = super.parseProperties(elem);
+    let {task, severity} = ret;
+
+    ret.severity = {
+      filtered: parse_float(severity.filtered),
+      full: parse_float(severity.full),
+    };
+
+    ret.severity_class = new Model(ret.severity_class);
+
+    ret.task = new Model(task);
+    ret.task.target = new Model(task.target);
+
+    return ret;
+  }
+
+}
+
 export class Report extends Model {
+
+  parseProperties(elem) {
+    let ret = super.parseProperties(elem);
+
+    ret.report = new ReportReport(ret.report);
+    ret.report_format = new Model(ret.report_format);
+    ret.task = new Model(ret.task);
+
+    ret.type = ret._type;
+    ret.content_type = ret._content_type;
+
+    return ret;
+  }
 }
 
 export default Report;
