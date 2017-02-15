e5612b6a10a7f3d282e994aa0eca22f9ea35507a
Björn Ricks <bjoern.ricks@greenbone.net>
2017-02-15 12:09:06 +0000

    * src/html/classic/ng/CMakeLists.txt, src/html/classic/ng/src/cpe.js, src/html/classic/ng/src/web/icons/cpeicon.js: Add a CpeIcon component. It will be required for os page.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@27656 423fd1db-d629-0410-8442-d21db03e70f4

---
+++ src/html/classic/ng/src/cpe.js
@@ -0,0 +1,193 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2017 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+export const cpes = [
+  {
+    pattern: 'cpe:/a:apache:http_server',
+    icon: 'cpe/a:apache:http_server.svg',
+  },
+  {
+    pattern: 'cpe:/a:drupal:drupal',
+    icon: 'cpe/a:drupal:drupal.svg',
+  },
+  {
+    pattern: 'cpe:/a:gnu',
+    icon: 'cpe/a:gnu.svg',
+  },
+  {
+    pattern: 'cpe:/a:google',
+    icon: 'cpe/a:google.svg',
+  },
+  {
+    pattern: 'cpe:/a:mysql:mysql',
+    icon: 'cpe/a:mysql:mysql.svg',
+  },
+  {
+    pattern: 'cpe:/a:openbsd:openssh',
+    icon: 'cpe/a:openbsd:openssh.svg',
+  },
+  {
+    pattern: 'cpe:/a:otrs:otrs',
+    icon: 'cpe/a:otrs:otrs.svg',
+  },
+  {
+    pattern: 'cpe:/a:php:php',
+    icon: 'cpe/a:php:php.svg',
+  },
+  {
+    pattern: 'cpe:/a:postgresql:postgresql',
+    icon: 'cpe/a:postgresql:postgresql.svg',
+  },
+  {
+    pattern: 'cpe:/a:snort:snort',
+    icon: 'cpe/a:snort:snort.svg',
+  },
+  {
+    pattern: 'cpe:/a:sourcefire',
+    icon: 'cpe/a:sourcefire.svg',
+  },
+  {
+    pattern: 'cpe:/a:typo3:typo3',
+    icon: 'cpe/a:typo3:typo3.svg',
+  },
+  {
+    pattern: 'cpe:/a:wordpress:wordpress',
+    icon: 'cpe/a:wordpress:wordpress.svg',
+  },
+  {
+    pattern: 'cpe:/o:microsoft:windows',
+    icon: 'os_windows.svg',
+  },
+  {
+    pattern: 'cpe:/o:redhat:',
+    icon: 'os_redhat.svg',
+  },
+  {
+    pattern: 'cpe:/o:fedoraproject:',
+    icon: 'os_fedora.svg',
+  },
+  {
+    pattern: 'cpe:/o:mandriva:',
+    icon: 'os_mandriva.svg',
+  },
+  {
+    pattern: 'cpe:/o:mandrakesoft:',
+    icon: 'os_mandriva.svg',
+  },
+  {
+    pattern: 'cpe:/o:centos:',
+    icon: 'os_centos.svg',
+  },
+  {
+    pattern: 'cpe:/o:debian:',
+    icon: 'os_debian.svg',
+  },
+  {
+    pattern: 'cpe:/o:suse:',
+    icon: 'os_suse.svg',
+  },
+  {
+    pattern: 'cpe:/o:novell:',
+    icon: 'os_novell.svg',
+  },
+  {
+    pattern: 'cpe:/o:novell:',
+    icon: 'os_novell.svg',
+  },
+  {
+    pattern: 'cpe:/o:trustix:',
+    icon: 'os_trustix.svg',
+  },
+  {
+    pattern: 'cpe:/o:gentoo:',
+    icon: 'os_gentoo.svg',
+  },
+  {
+    pattern: 'cpe:/o:hp:',
+    icon: 'os_hp.svg',
+  },
+  {
+    pattern: 'cpe:/o:sun:',
+    icon: 'os_sun.svg',
+  },
+  {
+    pattern: 'cpe:/o:apple:',
+    icon: 'os_apple.svg',
+  },
+  {
+    pattern: 'cpe:/o:ibm:aix',
+    icon: 'os_aix.svg',
+  },
+  {
+    pattern: 'cpe:/o:cisco:',
+    icon: 'os_cisco.svg',
+  },
+  {
+    pattern: 'cpe:/o:juniper:',
+    icon: 'os_junos.svg',
+  },
+  {
+    pattern: 'cpe:/o:freebsd:',
+    icon: 'os_freebsd.svg',
+  },
+  {
+    pattern: 'cpe:/o:linux',
+    icon: 'os_linux.svg',
+  },
+  {
+    pattern: 'cpe:/o:netbsd:',
+    icon: 'os_netbsd.svg',
+  },
+  {
+    pattern: 'cpe:/o:openbsd:',
+    icon: 'os_openbsd.svg',
+  },
+  {
+    pattern: 'cpe:/o:univention:univention_corporate_server:',
+    icon: 'os_ucs.svg',
+  },
+  {
+    pattern: 'cpe:/o:canonical:ubuntu_linux',
+    icon: 'os_ubuntu.svg',
+  },
+  {
+    pattern: 'cpe:/o:greenbone:',
+    icon: 'os_gos.svg',
+  },
+  {
+    pattern: 'cpe:/o:slackware:',
+    icon: 'os_slackware.svg',
+  },
+  {
+    pattern: 'cpe:/o:vmware:',
+    icon: 'os_vmware.svg',
+  },
+  {
+    pattern: 'cpe:/h:hp:',
+    icon: 'os_hp.svg',
+  },
+];
+
+export default {
+  find: name => cpes.find(cpe => name.includes(cpe.pattern)),
+};
---
+++ src/html/classic/ng/src/web/icons/cpeicon.js
@@ -0,0 +1,50 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2017 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+import React from 'react';
+
+import _ from '../../locale.js';
+import Cpe from '../../cpe.js';
+import {is_defined} from '../../utils.js';
+
+import Icon from './icon.js';
+
+export const CpeIcon = ({name, ...props}) => {
+  let icon;
+  let cpe = Cpe.find(name);
+
+  if (is_defined(cpe)) {
+    icon = cpe.icon;
+  }
+  else {
+    icon = 'cpe/other.svg';
+  }
+  return <Icon {...props} img={icon}/>;
+};
+
+CpeIcon.propTypes = {
+  name: React.PropTypes.string,
+};
+
+
+export default CpeIcon;
