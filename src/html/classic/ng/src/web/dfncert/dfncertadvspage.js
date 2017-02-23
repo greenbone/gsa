29af0cb22e39969144ba80142a9b114670e71acc
Björn Ricks <bjoern.ricks@greenbone.net>
2017-02-23 11:10:05 +0000

    * src/html/classic/ng/CMakeLists.txt, src/html/classic/ng/src/web/dfncert/dfncertadvspage.js, src/html/classic/ng/src/web/dfncert/row.js, src/html/classic/ng/src/web/dfncert/table.js: Add components for dfn cert advisories list page.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@27797 423fd1db-d629-0410-8442-d21db03e70f4

---
+++ src/html/classic/ng/src/web/dfncert/dfncertadvspage.js
@@ -0,0 +1,66 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2017 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+import React from 'react';
+
+import _ from '../../locale.js';
+
+ // DFN-CERT uses same filter dialog as CERT-Bund
+import FilterDialog from '../certbund/filterdialog.js';
+
+import {withDashboard} from '../dashboard/dashboard.js';
+
+import EntitiesPage from '../entities/page.js';
+import {withEntitiesContainer} from '../entities/container.js';
+
+import HelpIcon from '../icons/helpicon.js';
+
+import DfnCertCharts from './charts.js';
+import DfnCertTable from './table.js';
+
+const ToolBarIcons = props => {
+  return (
+    <HelpIcon
+      page="dfn_cert_advs"
+      title={_('Help: DFN-CERT Advisories')}/>
+  );
+};
+
+const Dashboard = withDashboard(DfnCertCharts, {
+  hideFilterSelect: true,
+  configPrefId: '9812ea49-682d-4f99-b3cc-eca051d1ce59',
+  defaultControllersString: 'dfn_cert_adv-by-severity-class|' +
+    'dfn_cert_adv-by-created|dfn_cert_adv-by-cvss',
+  defaultControllerString: 'dfn_cert_adv-by-cvss',
+});
+
+export default withEntitiesContainer(EntitiesPage, 'dfncertadv', {
+  dashboard: Dashboard,
+  filterEditDialog: FilterDialog,
+  sectionIcon: 'dfn_cert_adv.svg',
+  table: DfnCertTable,
+  title: _('DFN-CERT Advisories'),
+  toolBarIcons: ToolBarIcons,
+});
+
+// vim: set ts=2 sw=2 tw=80:
---
+++ src/html/classic/ng/src/web/dfncert/row.js
@@ -0,0 +1,83 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2017 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+import React from 'react';
+
+import _, {datetime} from '../../locale.js';
+
+import Comment from '../comment.js';
+import LegacyLink from '../legacylink.js';
+import PropTypes from '../proptypes.js';
+import SeverityBar from '../severitybar.js';
+import {na, render_component} from '../render.js';
+
+import {withEntityRow} from '../entities/row.js';
+
+import TableBody from '../table/body.js';
+import TableRow from '../table/row.js';
+import TableData from '../table/data.js';
+
+const Row = ({entity, links = true, actions, ...other}) => {
+  return (
+    <TableBody>
+      <TableRow>
+        <TableData>
+          {links ?
+            <LegacyLink
+              cmd="get_info"
+              details="1"
+              info_type="dfn_cert_adv"
+              info_id={entity.id}>
+              {entity.name}
+            </LegacyLink> :
+              entity.name
+          }
+          <Comment text={entity.comment}/>
+        </TableData>
+        <TableData>
+          {na(entity.title)}
+        </TableData>
+        <TableData>
+          {datetime(entity.creation_time)}
+        </TableData>
+        <TableData flex align="end">
+          {entity.cve_refs}
+        </TableData>
+        <TableData flex align="center">
+          <SeverityBar severity={entity.severity}/>
+        </TableData>
+        {render_component(actions, {...other, entity})}
+      </TableRow>
+    </TableBody>
+  );
+};
+
+Row.propTypes = {
+  actions: PropTypes.componentOrFalse,
+  entity: React.PropTypes.object,
+  links: React.PropTypes.bool,
+};
+
+export default withEntityRow(Row);
+
+// vim: set ts=2 sw=2 tw=80:
