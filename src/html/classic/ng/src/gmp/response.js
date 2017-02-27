467c85a438041654010b04caa0831cb7490972db
Björn Ricks <bjoern.ricks@greenbone.net>
2017-02-27 14:49:00 +0000

    * src/html/classic/ng/src/gmp/response.js: Add missing response class.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@27829 423fd1db-d629-0410-8442-d21db03e70f4

---
+++ src/html/classic/ng/src/gmp/response.js
@@ -0,0 +1,56 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2017 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+import {extend} from '../utils.js';
+
+export class Response {
+
+  constructor(data, meta = {}) {
+    this._data = data;
+    this._meta = meta;
+  }
+
+  set(data, meta) {
+    return new Response(data, extend({}, this._meta, meta));
+  }
+
+  setMeta(meta) {
+    return new Response(this._data, extend({}, this._meta, meta));
+  }
+
+  setData(data) {
+    return new Response(data, this._meta);
+  }
+
+  get meta() {
+    return this._meta;
+  }
+
+  get data() {
+    return this._data;
+  }
+}
+
+export default Response;
+
+// vim: set ts=2 sw=2 tw=80:
