61f2bb20682d8c9a7faf4033f8682a5f85316479
Björn Ricks <bjoern.ricks@greenbone.net>
2017-03-15 16:02:44 +0000

    * src/html/classic/ng/src/gmp/models/schedule.js: Add parsing of Schedule models.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@28015 423fd1db-d629-0410-8442-d21db03e70f4

--- src/html/classic/ng/src/gmp/models/schedule.js
+++ src/html/classic/ng/src/gmp/models/schedule.js
@@ -21,9 +21,51 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+import moment from 'moment';
+
+import {is_defined, is_empty, parse_int} from '../../utils.js';
+
 import Model from '../model.js';
 
 export class Schedule extends Model {
+
+  parseProperties(elem) {
+    let ret = super.parseProperties(elem);
+
+    ret.period = parse_int(ret.period);
+    ret.period_months = parse_int(ret.period_months);
+    ret.duration = parse_int(ret.duration);
+
+    if (is_defined(ret.simple_duration)) {
+      ret.simple_duration = {
+        value: parse_int(ret.simple_duration.__text),
+        unit: is_empty(ret.simple_duration.unit) ? undefined :
+          ret.simple_duration.unit,
+      };
+    }
+    else {
+      ret.simple_duration = {
+      };
+    }
+
+    if (is_defined(ret.simple_period)) {
+      ret.simple_period = {
+        value: parse_int(ret.simple_period.__text),
+        unit: is_empty(ret.simple_period.unit) ? undefined :
+          ret.simple_period.unit,
+      };
+    }
+    else {
+      ret.simple_period = {
+      };
+    }
+
+    ret.first_time = moment(ret.first_time).tz(ret.timezone);
+    ret.next_time = ret.next_time === 'over' ? ret.next_time :
+      moment(ret.next_time).tz(ret.timezone);
+
+    return ret;
+  }
 }
 
 export default Schedule;
