9e3b9638f33a79b538c8fb05893908013f5f5b08
Björn Ricks <bjoern.ricks@greenbone.net>
2017-03-15 16:02:53 +0000

    * src/html/classic/ng/CMakeLists.txt, src/html/classic/ng/src/web/schedules/row.js, src/html/classic/ng/src/web/schedules/schedulespage.js, src/html/classic/ng/src/web/schedules/table.js: Add react components for schedules list page.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@28020 423fd1db-d629-0410-8442-d21db03e70f4

---
+++ src/html/classic/ng/src/web/schedules/row.js
@@ -0,0 +1,174 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2017 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+import React from 'react';
+
+import _, {long_date, interval} from '../../locale.js';
+
+import Comment from '../comment.js';
+import Layout from '../layout.js';
+import LegacyLink from '../legacylink.js';
+import PropTypes from '../proptypes.js';
+import {render_component} from '../render.js';
+
+import {withEntityActions} from '../entities/actions.js';
+import {withEntityRow} from '../entities/row.js';
+
+import CloneIcon from '../entities/icons/entitycloneicon.js';
+import EditIcon from '../entities/icons/entityediticon.js';
+import ObserverIcon from '../entities/icons/entityobservericon.js';
+import TrashIcon from '../entities/icons/entitytrashicon.js';
+
+import ExportIcon from '../icons/exporticon.js';
+
+import TableData from '../table/data.js';
+import TableRow from '../table/row.js';
+
+const Actions = ({
+    entity,
+    onEntityDelete,
+    onEntityDownload,
+    onEntityClone,
+    onEntityEdit,
+  }) => {
+  return (
+    <Layout flex align={['center', 'center']}>
+      <TrashIcon
+        displayName={_('Schedule')}
+        name="schedule"
+        entity={entity}
+        onClick={onEntityDelete}/>
+      <EditIcon
+        displayName={_('Schedule')}
+        name="schedule"
+        entity={entity}
+        onClick={onEntityEdit}/>
+      <CloneIcon
+        displayName={_('Schedule')}
+        name="schedule"
+        entity={entity}
+        title={_('Clone Schedule')}
+        value={entity}
+        onClick={onEntityClone}/>
+      <ExportIcon
+        value={entity}
+        title={_('Export Schedule')}
+        onClick={onEntityEdit}
+      />
+    </Layout>
+  );
+};
+
+Actions.propTypes = {
+  entity: PropTypes.model,
+  onEntityEdit: React.PropTypes.func,
+  onEntityClone: React.PropTypes.func,
+  onEntityDelete: React.PropTypes.func,
+  onEntityDownload: React.PropTypes.func,
+  onTestSchedule: React.PropTypes.func,
+};
+
+const render_period = entity => {
+  if (entity.period === 0 && entity.period_months === 0) {
+    return _('Once');
+  }
+  if (entity.period === 0 && entity.period_months === 1) {
+    return _('One month');
+  }
+  if (entity.period === 0) {
+    return _('{{number}} months', {number: entity.period_months});
+  }
+  return interval(entity.period);
+};
+
+const render_duration = entity => {
+  if (entity.duration === 0) {
+    return _('Entire Operation');
+  }
+  return interval(entity.duration);
+};
+
+const Row = ({
+    actions,
+    entity,
+    links = true,
+    ...props
+  }, {
+    capabilities,
+    username,
+  }) => {
+  let next_time = entity.next_time === 'over' ? '-' :
+    long_date(entity.next_time);
+  return (
+    <TableRow>
+      <TableData flex="column">
+        <Layout flex align="space-between">
+          {links ?
+            <LegacyLink
+              cmd="get_schedule"
+              schedule_id={entity.id}>
+              {entity.name}
+            </LegacyLink> :
+            entity.name
+          }
+          <ObserverIcon
+            displayName={_('Schedule')}
+            entity={entity}
+            userName={username}
+          />
+        </Layout>
+        {entity.comment &&
+          <Comment>({entity.comment})</Comment>
+        }
+      </TableData>
+      <TableData>
+        {long_date(entity.first_time)}
+      </TableData>
+      <TableData>
+        {next_time}
+      </TableData>
+      <TableData>
+        {render_period(entity)}
+      </TableData>
+      <TableData>
+        {render_duration(entity)}
+      </TableData>
+      {render_component(actions, {...props, entity})}
+    </TableRow>
+  );
+};
+
+Row.propTypes = {
+  actions: PropTypes.componentOrFalse,
+  entity: PropTypes.model.isRequired,
+  links: React.PropTypes.bool,
+};
+
+Row.contextTypes = {
+  capabilities: React.PropTypes.object.isRequired,
+  username: React.PropTypes.string.isRequired,
+};
+
+export default withEntityRow(Row, withEntityActions(Actions));
+
+// vim: set ts=2 sw=2 tw=80:
---
+++ src/html/classic/ng/src/web/schedules/schedulespage.js
@@ -0,0 +1,177 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2017 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+import React from 'react';
+
+import moment from 'moment-timezone';
+
+import _ from '../../locale.js';
+import {is_defined} from '../../utils.js';
+
+import Layout from '../layout.js';
+import PropTypes from '../proptypes.js';
+
+import EntitiesPage from '../entities/page.js';
+import {withEntitiesContainer} from '../entities/container.js';
+
+import HelpIcon from '../icons/helpicon.js';
+import NewIcon from '../icons/newicon.js';
+
+import {createFilterDialog} from '../powerfilter/dialog.js';
+
+import ScheduleDialog from './dialog.js';
+import Table from './table.js';
+
+const SORT_FIELDS = [
+  ['name', _('Name')],
+  ['first_run', _('First Run')],
+  ['next_run', _('Next Run')],
+  ['period', _('Period')],
+  ['duration', _('Duration')],
+];
+
+const ToolBarIcons = ({
+    onNewScheduleClick
+  }, {capabilities}) => {
+  return (
+    <Layout flex>
+      <HelpIcon
+        page="schedules"
+        title={_('Help: Schedules')}/>
+      {capabilities.mayCreate('schedule') &&
+        <NewIcon
+          title={_('New Schedule')}
+          onClick={onNewScheduleClick}/>
+      }
+    </Layout>
+  );
+};
+
+ToolBarIcons.propTypes = {
+  onNewScheduleClick: React.PropTypes.func,
+};
+
+ToolBarIcons.contextTypes = {
+  capabilities: PropTypes.capabilities.isRequired,
+};
+
+class Page extends React.Component {
+
+  constructor(...args) {
+    super(...args);
+
+    this.handleSaveSchedule = this.handleSaveSchedule.bind(this);
+    this.openScheduleDialog = this.openScheduleDialog.bind(this);
+  }
+
+  handleSaveSchedule(data) {
+    const {onChanged, entityCommand} = this.props;
+    let promise;
+
+    if (is_defined(data.schedule)) {
+      promise = entityCommand.save(data);
+    }
+    else {
+      promise = entityCommand.create(data);
+    }
+
+    return promise.then(() => onChanged());
+  }
+
+  openScheduleDialog(schedule) {
+    let {gmp} = this.context;
+
+    if (is_defined(schedule)) {
+      let date = schedule.first_time;
+      this.schedule_dialog.show({
+        comment: schedule.comment,
+        date,
+        duration: schedule.simple_duration.value,
+        duration_unit: is_defined(schedule.simple_duration.unit) ?
+          schedule.simple_duration.unit : 'hour',
+        hour: date.hours(),
+        id: schedule.id,
+        minute: date.minutes(),
+        name: schedule.name,
+        period: schedule.simple_period.value,
+        period_unit: is_defined(schedule.simple_period.unit) ?
+          schedule.simple_period.unit : 'hour',
+        schedule,
+        timezone: schedule.timezone,
+
+      }, {
+        title: _('Edit Schedule {{name}}', {name: schedule.name}),
+      });
+    }
+    else {
+      let timezone = gmp.globals.timezone;
+      let now = moment().tz(timezone);
+
+      this.schedule_dialog.show({
+        timezone,
+        minute: now.minutes(),
+        hour: now.hours(),
+        date: now,
+      });
+    }
+  }
+
+  render() {
+    return (
+      <Layout>
+        <EntitiesPage
+          {...this.props}
+          onEntityEdit={this.openScheduleDialog}
+          onNewScheduleClick={this.openScheduleDialog}
+        />
+        <ScheduleDialog
+          ref={ref => this.schedule_dialog = ref}
+          onSave={this.handleSaveSchedule}
+        />
+      </Layout>
+    );
+  }
+}
+
+Page.propTypes = {
+  entityCommand: PropTypes.entitycommand,
+  onChanged: React.PropTypes.func,
+  showError: React.PropTypes.func.isRequired,
+  showSuccess: React.PropTypes.func.isRequired,
+};
+
+Page.contextTypes = {
+  gmp: React.PropTypes.object.isRequired,
+};
+
+export default withEntitiesContainer(Page, 'schedule', {
+  filterEditDialog: createFilterDialog({
+    sortFields: SORT_FIELDS,
+  }),
+  sectionIcon: 'schedule.svg',
+  table: Table,
+  title: _('Schedules'),
+  toolBarIcons: ToolBarIcons,
+});
+
+// vim: set ts=2 sw=2 tw=80:
