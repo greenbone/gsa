e0f9ef56f9d59a9b9299d1bee588633ba00a80ad
Björn Ricks <bjoern.ricks@greenbone.net>
2017-05-22 12:48:02 +0000

    * src/html/classic/ng/src/gmp/models/user.js: Update user model to parse several properties.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@28449 423fd1db-d629-0410-8442-d21db03e70f4

--- src/html/classic/ng/src/gmp/models/user.js
+++ src/html/classic/ng/src/gmp/models/user.js
@@ -4,7 +4,7 @@
  * Björn Ricks <bjoern.ricks@greenbone.net>
  *
  * Copyright:
- * Copyright (C) 2016 Greenbone Networks GmbH
+ * Copyright (C) 2016 - 2017 Greenbone Networks GmbH
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -21,10 +21,55 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+import {is_defined, is_empty, map} from '../../utils.js';
+
 import Model from '../model.js';
 
 export class User extends Model {
 
+  parseProperties(elem) {
+    let ret = super.parseProperties(elem);
+
+    ret.roles = map(elem.role, role => {
+      return new Model(role);
+    });
+
+    delete ret.role;
+
+    if (is_empty(elem.groups)) {
+      ret.groups = [];
+    }
+    else {
+      ret.groups = map(elem.groups.group, group => {
+        return new Model(group);
+      });
+    }
+
+    if (is_defined(elem.hosts)) {
+      ret.hosts = {
+        addresses: elem.hosts.__text,
+        allow: elem.hosts._allow,
+      };
+    }
+    else {
+      ret.hosts = {};
+    }
+
+    if (is_defined(elem.ifaces)) {
+      ret.ifaces = {
+        addresses: elem.ifaces.__text,
+        allow: elem.ifaces._allow,
+      };
+    }
+    else {
+      ret.ifaces = {};
+    }
+
+    ret.auth_method = elem.sources.source;
+    delete ret.sources;
+
+    return ret;
+  }
 }
 
 export default User;
