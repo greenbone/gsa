2fde059c6aacf1e3a94050b592f0c053dc96104c
Björn Ricks <bjoern.ricks@greenbone.net>
2017-06-09 10:37:27 +0000

    * src/html/classic/ng/src/gmp/commands/roles.js: Implement gmp api for handling a role.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@28622 423fd1db-d629-0410-8442-d21db03e70f4

--- src/html/classic/ng/src/gmp/commands/roles.js
+++ src/html/classic/ng/src/gmp/commands/roles.js
@@ -21,9 +21,108 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-import {EntitiesCommand, register_command} from '../command.js';
+import {EntityCommand, EntitiesCommand, register_command} from '../command.js';
 
+import logger from '../../log.js';
+import {is_array, is_empty, map} from '../../utils.js';
+
+import Model from '../model.js';
+
+import Permission from '../models/permission.js';
 import Role from '../models/role.js';
+
+const log = logger.getLogger('gmp.commands.roles');
+
+export class RoleCommand extends EntityCommand {
+
+  constructor(http) {
+    super(http, 'role', Role);
+  }
+
+  create({
+    name,
+    comment = '',
+    users = [],
+  }) {
+    const data = {
+      cmd: 'create_role',
+      next: 'get_role',
+      name,
+      comment,
+      users: is_array(users) ? users.join(',') : '',
+    };
+    log.debug('Creating new role', data);
+    return this.httpPost(data).then(this.transformResponse);
+  }
+
+  save({
+    id,
+    name,
+    comment = '',
+    users = [],
+  }) {
+    const data = {
+      cmd: 'save_role',
+      next: 'get_role',
+      id,
+      name,
+      comment,
+      users: is_array(users) ? users.join(',') : '',
+    };
+    log.debug('Saving role', data);
+    return this.httpPost(data).then(this.transformResponse);
+  }
+
+  editRoleSettings({id}) {
+    return this.httpGet({
+      cmd: 'edit_role',
+      id,
+    }).then(response => {
+      let {capabilities, edit_role} = response.data;
+
+      edit_role.role = new Role(
+      edit_role.commands_response.get_roles_response.role);
+
+      delete edit_role.commands_response;
+
+      const perm_names = new Set();
+
+      edit_role.permissions = map(
+        edit_role.get_permissions_response.permission, permission => {
+          let perm = new Permission(permission);
+          if (is_empty(perm.resource.id)) {
+            perm_names.add(permission.name);
+          }
+          return perm;
+        }
+      );
+
+      delete edit_role.get_permissions_response;
+
+      const caps = map(capabilities.help_response.schema.command, cap => {
+        cap.name = cap.name.toLowerCase();
+        return cap;
+      })
+        .filter(cap => {
+          return cap.name !== 'get_version' && !perm_names.has(cap.name);
+        });
+
+      edit_role.all_permissions = caps;
+
+      edit_role.groups = map(edit_role.get_groups_response.group, group => {
+        return new Model(group);
+      });
+
+      delete edit_role.get_groups_response;
+
+      return response.setData(edit_role);
+    });
+  }
+
+  getElementFromRoot(root) {
+    return root.get_role.get_roles_response.role;
+  }
+}
 
 export class RolesCommand extends EntitiesCommand {
 
@@ -36,6 +135,7 @@ export class RolesCommand extends EntitiesCommand {
   }
 }
 
+register_command('role', RoleCommand);
 register_command('roles', RolesCommand);
 
 // vim: set ts=2 sw=2 tw=80:
