40dd9d1d2bafb658c1d9431d325ead2bd102fc64
Björn Ricks <bjoern.ricks@greenbone.net>
2017-06-15 14:44:23 +0000

    * src/html/classic/ng/CMakeLists.txt, src/html/classic/ng/src/web/link/detailslink.js, src/html/classic/ng/src/web/link/external.js, src/html/classic/ng/src/web/link/infolink.js: Add additional link components. Using this components will make it easier to change how they are displayed in future e.g. when all detail pages have been converted.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@28678 423fd1db-d629-0410-8442-d21db03e70f4

---
+++ src/html/classic/ng/src/web/link/external.js
@@ -0,0 +1,56 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2017 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+import React from 'react';
+
+import {classes} from '../../utils.js';
+
+import PropTypes from '../proptypes.js';
+
+import {withTextOnly} from './link.js';
+
+const ExternalLink = ({
+  className,
+  children,
+  ...props,
+}) => {
+
+  className = classes(className, 'external-link');
+
+  return (
+    <a {...props}
+      className={className}
+      rel="noopener noreferrer"
+      target="_blank">
+      {children}
+    </a>
+  );
+};
+
+ExternalLink.propTypes = {
+  className: PropTypes.string,
+};
+
+export default withTextOnly(ExternalLink);
+
+// vim: set ts=2 sw=2 tw=80:
