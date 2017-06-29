0b2344175268dd528854c28f77f089b53e38ee1a
Björn Ricks <bjoern.ricks@greenbone.net>
2017-06-29 14:20:38 +0000

    * src/html/classic/ng/src/web/components/icon/listicon.js: Fix import of Link component.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@28880 423fd1db-d629-0410-8442-d21db03e70f4

--- src/html/classic/ng/src/web/components/icon/listicon.js
+++ src/html/classic/ng/src/web/components/icon/listicon.js
@@ -25,7 +25,7 @@ import React from 'react';
 
 import PropTypes from '../../utils/proptypes.js';
 
-import Link from '../../link/link.js';
+import Link from '../link/link.js';
 
 import Icon from './icon.js';
 
