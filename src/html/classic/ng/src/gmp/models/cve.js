f14a59b4d4a020b6f5cd4f3fc459d676baf18efb
Björn Ricks <bjoern.ricks@greenbone.net>
2017-08-02 17:22:01 +0000

    * src/html/classic/ng/src/gmp/models/cve.js: Parse additional cve details data.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@29289 423fd1db-d629-0410-8442-d21db03e70f4

--- src/html/classic/ng/src/gmp/models/cve.js
+++ src/html/classic/ng/src/gmp/models/cve.js
@@ -21,9 +21,34 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-import {extend} from '../utils.js';
+import moment from 'moment';
+
+import {is_empty, is_defined, extend, map} from '../utils.js';
+
+import {
+  parse_severity,
+  parse_cvss_base_vector,
+  set_properties,
+} from '../parser.js';
 
 import Info from './info.js';
+
+const delete_empty = (obj, props) => {
+  for (const prop of props) {
+    if (is_empty(obj[prop])) {
+      delete obj[prop];
+    }
+  }
+};
+
+const rename_props = (obj, rename = {}) => {
+  for (const [oldname, newname] of Object.entries(rename)) {
+    if (is_defined(obj[oldname])) {
+      obj[newname] = obj[oldname];
+      delete obj[oldname];
+    }
+  }
+};
 
 class Cve extends Info {
 
@@ -37,8 +62,114 @@ class Cve extends Info {
       delete ret.cve;
     }
 
-    ret.severity = ret.cvss;
+    if (is_defined(ret.update_time)) {
+      ret.update_time = moment(ret.update_time);
+    }
+
+    ret.severity = parse_severity(ret.cvss);
     delete ret.cvss;
+
+    if (is_defined(ret.nvts)) {
+      ret.nvts = map(ret.nvts.nvt, nvt => {
+        return set_properties({
+          ...nvt,
+          id: nvt._oid,
+          oid: nvt._oid,
+        });
+      });
+    }
+
+    if (is_defined(ret.cert)) {
+      ret.certs = map(ret.cert.cert_ref, ref => {
+        return {
+          name: ref.name,
+          title: ref.title,
+          cert_type: ref._type,
+        };
+      });
+
+      delete ret.cert;
+    }
+    else {
+      ret.certs = [];
+    }
+
+    delete_empty(ret, [
+      'vector',
+      'complexity',
+      'authentication',
+      'confidentiality_impact',
+      'integrity_impact',
+      'availability_impact',
+      'cert',
+    ]);
+
+    ret.cvss_base_vector = parse_cvss_base_vector({
+      access_complexity: ret.complexity,
+      access_vector: ret.vector,
+      authentication: ret.authentication,
+      availability_impact: ret.confidentiality_impact,
+      confidentiality_impact: ret.confidentiality_impact,
+      integrity_impact: ret.integrity_impact,
+    });
+
+    // use consistent names for cvss values
+    rename_props(ret, {
+      vector: 'cvss_access_vector',
+      complexity: 'cvss_access_complexity',
+      authentication: 'cvss_authentication',
+      confidentiality_impact: 'cvss_confidentiality_impact',
+      integrity_impact: 'cvss_integrity_impact',
+      availability_impact: 'cvss_availability_impact',
+    });
+
+    if (is_empty(ret.products)) {
+      ret.products = [];
+    }
+    else {
+      ret.products = ret.products.split(' ');
+    }
+
+    if (is_defined(ret.raw_data) && is_defined(ret.raw_data.entry)) {
+      const {entry} = ret.raw_data;
+
+      if (is_defined(ret.cwe)) {
+        ret.cwe_id = entry.cwe._id;
+      }
+
+      ret.published_time = moment(entry['published-datetime'].__text);
+      ret.last_modified_time = moment(entry['last-modified-datetime'].__text);
+
+      ret.references = map(entry.references, ref => ({
+        name: ref.reference.__text,
+        href: ref.reference._href,
+        source: ref.source.__text,
+        reference_type: ref._reference_type,
+      }));
+
+      if (is_defined(entry.cvss) && is_defined(entry.cvss.base_metrics) &&
+        is_defined(entry.cvss.base_metrics.source)) {
+        ret.source = entry.cvss.base_metrics.source.__text;
+      }
+
+      if (is_defined(entry.summary) && !is_empty(entry.summary.__text)) {
+        // really don't know why entry.summary and ret.description can differ
+        // but xslt did use the summary and and e.g. the description of
+        // CVE-2017-2988 was empty but summary not
+        ret.description = entry.summary.__text;
+      }
+
+      const products = entry['vulnerable-software-list'];
+      if (is_defined(products)) {
+        ret.products = map(products.product, product => product.__text);
+      }
+
+      delete ret.raw_data;
+    }
+    else {
+      ret.references = [];
+    }
+
 
     return ret;
   }
