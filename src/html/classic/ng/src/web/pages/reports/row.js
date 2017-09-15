f0b23e299e80efd8559f4cd2c9a0e322d784be27
Björn Ricks <bjoern.ricks@greenbone.net>
2017-09-15 12:34:43 +0000

    * src/html/classic/ng/src/web/pages/reports/row.js: Link to ng report details.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@29601 423fd1db-d629-0410-8442-d21db03e70f4

--- src/html/classic/ng/src/web/pages/reports/row.js
+++ src/html/classic/ng/src/web/pages/reports/row.js
@@ -95,7 +95,6 @@ const Row = ({entity, links = true, actions, ...other}) => {
     <TableRow>
       <TableData>
         <DetailsLink
-          legacy
           type="report"
           id={entity.id}
           textOnly={!links}>
