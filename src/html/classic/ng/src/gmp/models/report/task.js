a435cf33f32146cdbb024d1356ea38a78cf1c879
Björn Ricks <bjoern.ricks@greenbone.net>
2017-09-21 14:33:22 +0000

    * src/html/classic/ng/src/gmp/models/report/task.js: Parse progress also in Report Task model.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@29636 423fd1db-d629-0410-8442-d21db03e70f4

--- src/html/classic/ng/src/gmp/models/report/task.js
+++ src/html/classic/ng/src/gmp/models/report/task.js
@@ -4,7 +4,7 @@
  * Björn Ricks <bjoern.ricks@greenbone.net>
  *
  * Copyright:
- * Copyright (C) 2016 - 2017 Greenbone Networks GmbH
+ * Copyright (C) 2017 Greenbone Networks GmbH
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -21,6 +21,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 import {is_defined, is_empty} from '../../utils.js';
+
+import {parse_progress} from '../../parser.js';
 
 import Model from '../../model.js';
 
@@ -42,6 +44,8 @@ class ReportTask extends Model {
     else {
       delete copy.target;
     }
+
+    copy.progress = parse_progress(elem.progress);
 
     return copy;
   }
