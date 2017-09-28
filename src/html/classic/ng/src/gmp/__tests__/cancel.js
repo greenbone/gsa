a9221a1e4e62bb2229f439002fc98d9e66b3820f
Björn Ricks <bjoern.ricks@greenbone.net>
2017-09-28 09:07:46 +0000

    * src/html/classic/ng/CMakeLists.txt, src/html/classic/ng/src/gmp/__tests__/cancel.js, src/html/classic/ng/src/gmp/cancel.js: Introduce a CancelToken class to be able to cancel promises at the end.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@29696 423fd1db-d629-0410-8442-d21db03e70f4

---
+++ src/html/classic/ng/src/gmp/__tests__/cancel.js
@@ -0,0 +1,90 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2017 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+import CancelToken from '../cancel.js';
+
+describe('CancelToken tests', () => {
+
+  test('new CancelToken resolve', () => {
+    const token = new CancelToken(cancel => {
+      cancel('because of a test');
+    });
+    return expect(token.promise).resolves.toBe('because of a test');
+  });
+
+  test('CancelToken.source resolve', () => {
+    const {token, cancel} = CancelToken.source();
+
+    cancel('because of a test');
+
+    return expect(token.promise).resolves.toBe('because of a test');
+  });
+
+  test('new CancelToken canceled', () => {
+    const token = new CancelToken(cancel => {
+      cancel('because of a test');
+    });
+
+    expect(token.canceled).toBe(true);
+    expect(token.reason).toBe('because of a test');
+  });
+
+  test('new CancelToken not canceled', () => {
+    const token = new CancelToken(() => {});
+
+    expect(token.canceled).toBe(false);
+    expect(token.reason).toBeUndefined();
+  });
+
+  test('CancelToken.source canceled', () => {
+    const {token, cancel} = CancelToken.source();
+
+    expect(token.reason).toBeUndefined();
+    expect(token.canceled).toBe(false);
+
+    cancel('because of a test');
+
+    expect(token.reason).toBe('because of a test');
+    expect(token.canceled).toBe(true);
+  });
+
+  test('CancelToken cancel twice', async () => {
+    const {token, cancel} = CancelToken.source();
+
+    cancel('because of a test');
+
+    expect(token.reason).toBe('because of a test');
+    expect(token.canceled).toBe(true);
+
+    await expect(token.promise).resolves.toBe('because of a test');
+
+    cancel('because of a second test');
+
+    expect(token.reason).toBe('because of a second test');
+    expect(token.canceled).toBe(true);
+
+    await expect(token.promise).resolves.toBe('because of a test');
+  });
+});
+
+// vim: set ts=2 sw=2 tw=80:
---
+++ src/html/classic/ng/src/gmp/cancel.js
@@ -0,0 +1,94 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2017 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+import Promise from './promise.js';
+
+/*
+ * The CancelToken class is based on the cancelable-promises tc39 proposal
+ *
+ * https://github.com/tc39/proposal-cancelable-promises/blob/0e769fda8e16bff0feffe964fddc43dcd86668ba/Cancel%20Tokens.md
+ *
+ * Usage:
+ *
+ * const cancel_token = new CancelToken(cancel => {
+ *   // store `cancel` for later
+ *   // e.g. do_cancel = cancel;
+ *   // or in a class this.doCancel = cancel
+ * });
+ *
+ * // pass it to a cancelable function that returns a promise
+ * doCancelableThing(cancelToken);
+ *
+ * // ... or another example
+ * doOtherCancelableThing({token: cancel_token});
+ *
+ * // call cancel later
+ * do_cancel('my reason');
+ *
+ * // or in a class
+ * this.doCancel('my reason');
+ *
+ * In a consumer implement something like
+ *
+ * const delay = (ms, cancel_token) => {
+ *   return new Promise((resolve, reject) => {
+ *     const id = setTimeout(resolve, ms);
+ *
+ *     if (cancel_token) {
+ *       cancelToken.promise.then(reason => {
+ *         reject(reason);
+ *         clearTimeout(id);
+ *       });
+ *     }
+ *   });
+ * };
+ */
+
+class CancelToken {
+
+  constructor(executor) {
+    this.canceled = false;
+
+    this.promise = Promise.create(resolve => {
+      executor(reason => {
+        this.reason = reason;
+        this.canceled = true;
+
+        resolve(reason);
+      });
+    });
+  }
+
+  static source() {
+    let cancel;
+    const token = new CancelToken(func => cancel = func);
+    return {
+      cancel,
+      token,
+    };
+  }
+}
+
+export default CancelToken;
+
+// vim: set ts=2 sw=2 tw=80:
