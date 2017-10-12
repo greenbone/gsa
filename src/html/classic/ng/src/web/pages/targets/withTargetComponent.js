3b834e94473bb904e7901d9616a5bb902fb452ba
Björn Ricks <bjoern.ricks@greenbone.net>
2017-10-12 12:21:55 +0000

    * src/html/classic/ng/src/web/pages/permissions/withPermissionsComponent.js, src/html/classic/ng/src/web/pages/targets/withTargetComponent.js: Use withComponent function to implement withTargetComponent and withPermissionsComponent HOCs. Implementing both is now an one liner.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@29835 423fd1db-d629-0410-8442-d21db03e70f4

--- src/html/classic/ng/src/web/pages/targets/withTargetComponent.js
+++ src/html/classic/ng/src/web/pages/targets/withTargetComponent.js
@@ -21,9 +21,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-import React from 'react';
-
-import {get_handler} from '../../entity/withEntityComponent.js';
+import withComponent from '../../entity/withComponent.js';
 
 import TargetComponent from './component.js';
 
@@ -41,67 +39,6 @@ const DEFAULT_MAPPING = {
   edit: 'onTargetEditClick',
 };
 
-const withTargetComponent = (mapping = {}) => Component => {
-
-  mapping = {
-    ...DEFAULT_MAPPING,
-    ...mapping,
-  };
-
-  const TargetComponentWrapper = props => {
-    const {
-      create: create_name,
-      clone: clone_name,
-      delete: delete_name,
-      download: download_name,
-      save: save_name,
-      onError,
-      onCreated,
-      onCreateError,
-      onDeleted,
-      onDeleteError,
-      onSaved,
-      onSaveError,
-      onDownloaded,
-      onDownloadError,
-    } = mapping;
-    return (
-      <TargetComponent
-        onError={get_handler(props, onError)}
-        onCreated={get_handler(props, onCreated)}
-        onCreateError={get_handler(props, onCreateError)}
-        onDeleted={get_handler(props, onDeleted)}
-        onDeleteError={get_handler(props, onDeleteError)}
-        onSaved={get_handler(props, onSaved)}
-        onSaveError={get_handler(props, onSaveError)}
-        onDownloaded={get_handler(props, onDownloaded)}
-        onDownloadError={get_handler(props, onDownloadError)}
-      >
-        {({
-          create,
-          clone,
-          delete: delete_func,
-          save,
-          download,
-        }) => {
-          const cprops = {
-            [create_name]: create,
-            [clone_name]: clone,
-            [delete_name]: delete_func,
-            [download_name]: download,
-            [save_name]: save,
-          };
-          return (
-            <Component {...props} {...cprops} />
-          );
-        }}
-      </TargetComponent>
-    );
-  };
-
-  return TargetComponentWrapper;
-};
-
-export default withTargetComponent;
+export default withComponent(DEFAULT_MAPPING, TargetComponent);
 
 // vim: set ts=2 sw=2 tw=80:
