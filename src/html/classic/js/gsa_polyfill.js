d73d1f88372e15f40f41e4d6206c1c91b79ebdc1
Timo Pollmeier <timo.pollmeier@greenbone.net>
2017-10-17 14:36:03 +0000

    Backport r29876.
    	* src/html/classic/js/gsa_polyfill.js: Add polyfills for the string
    	functions endsWith, includes and startsWith.
    
    	* COPYING: Add copyright info for new polyfills.
    
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/branches/gsa-7.0@29877 423fd1db-d629-0410-8442-d21db03e70f4

--- src/html/classic/js/gsa_polyfill.js
+++ src/html/classic/js/gsa_polyfill.js
@@ -59,4 +59,179 @@
     });
   }
 
+  /*
+   * Polyfills for string functions
+   *  by Mathias Bynens <https://mathiasbynens.be/>
+   *  released under the MIT license
+   *
+   * https://github.com/mathiasbynens/String.prototype.endsWith
+   * https://github.com/mathiasbynens/String.prototype.includes
+   * https://github.com/mathiasbynens/String.prototype.startsWith
+   */
+
+  if (!String.prototype.endsWith) {
+    (function() {
+      'use strict'; // needed to support `apply`/`call` with `undefined`/`null`
+      var defineProperty = (function() {
+        // IE 8 only supports `Object.defineProperty` on DOM elements
+        try {
+          var object = {};
+          var $defineProperty = Object.defineProperty;
+          var result = $defineProperty(object, object, object) && $defineProperty;
+        } catch(error) {}
+        return result;
+      }());
+      var toString = {}.toString;
+      var endsWith = function(search) {
+        if (this == null) {
+          throw TypeError();
+        }
+        var string = String(this);
+        if (search && toString.call(search) == '[object RegExp]') {
+          throw TypeError();
+        }
+        var stringLength = string.length;
+        var searchString = String(search);
+        var searchLength = searchString.length;
+        var pos = stringLength;
+        if (arguments.length > 1) {
+          var position = arguments[1];
+          if (position !== undefined) {
+            // `ToInteger`
+            pos = position ? Number(position) : 0;
+            if (pos != pos) { // better `isNaN`
+              pos = 0;
+            }
+          }
+        }
+        var end = Math.min(Math.max(pos, 0), stringLength);
+        var start = end - searchLength;
+        if (start < 0) {
+          return false;
+        }
+        var index = -1;
+        while (++index < searchLength) {
+          if (string.charCodeAt(start + index) != searchString.charCodeAt(index)) {
+            return false;
+          }
+        }
+        return true;
+      };
+      if (defineProperty) {
+        defineProperty(String.prototype, 'endsWith', {
+          'value': endsWith,
+          'configurable': true,
+          'writable': true
+        });
+      } else {
+        String.prototype.endsWith = endsWith;
+      }
+    }());
+  }
+
+  if (!String.prototype.includes) {
+    (function() {
+      'use strict'; // needed to support `apply`/`call` with `undefined`/`null`
+      var toString = {}.toString;
+      var defineProperty = (function() {
+        // IE 8 only supports `Object.defineProperty` on DOM elements
+        try {
+          var object = {};
+          var $defineProperty = Object.defineProperty;
+          var result = $defineProperty(object, object, object) && $defineProperty;
+        } catch(error) {}
+        return result;
+      }());
+      var indexOf = ''.indexOf;
+      var includes = function(search) {
+        if (this == null) {
+          throw TypeError();
+        }
+        var string = String(this);
+        if (search && toString.call(search) == '[object RegExp]') {
+          throw TypeError();
+        }
+        var stringLength = string.length;
+        var searchString = String(search);
+        var searchLength = searchString.length;
+        var position = arguments.length > 1 ? arguments[1] : undefined;
+        // `ToInteger`
+        var pos = position ? Number(position) : 0;
+        if (pos != pos) { // better `isNaN`
+          pos = 0;
+        }
+        var start = Math.min(Math.max(pos, 0), stringLength);
+        // Avoid the `indexOf` call if no match is possible
+        if (searchLength + start > stringLength) {
+          return false;
+        }
+        return indexOf.call(string, searchString, pos) != -1;
+      };
+      if (defineProperty) {
+        defineProperty(String.prototype, 'includes', {
+          'value': includes,
+          'configurable': true,
+          'writable': true
+        });
+      } else {
+        String.prototype.includes = includes;
+      }
+    }());
+  }
+
+  if (!String.prototype.startsWith) {
+    (function() {
+      'use strict'; // needed to support `apply`/`call` with `undefined`/`null`
+      var defineProperty = (function() {
+        // IE 8 only supports `Object.defineProperty` on DOM elements
+        try {
+          var object = {};
+          var $defineProperty = Object.defineProperty;
+          var result = $defineProperty(object, object, object) && $defineProperty;
+        } catch(error) {}
+        return result;
+      }());
+      var toString = {}.toString;
+      var startsWith = function(search) {
+        if (this == null) {
+          throw TypeError();
+        }
+        var string = String(this);
+        if (search && toString.call(search) == '[object RegExp]') {
+          throw TypeError();
+        }
+        var stringLength = string.length;
+        var searchString = String(search);
+        var searchLength = searchString.length;
+        var position = arguments.length > 1 ? arguments[1] : undefined;
+        // `ToInteger`
+        var pos = position ? Number(position) : 0;
+        if (pos != pos) { // better `isNaN`
+          pos = 0;
+        }
+        var start = Math.min(Math.max(pos, 0), stringLength);
+        // Avoid the `indexOf` call if no match is possible
+        if (searchLength + start > stringLength) {
+          return false;
+        }
+        var index = -1;
+        while (++index < searchLength) {
+          if (string.charCodeAt(start + index) != searchString.charCodeAt(index)) {
+            return false;
+          }
+        }
+        return true;
+      };
+      if (defineProperty) {
+        defineProperty(String.prototype, 'startsWith', {
+          'value': startsWith,
+          'configurable': true,
+          'writable': true
+        });
+      } else {
+        String.prototype.startsWith = startsWith;
+      }
+    }());
+  }
+
 })(window, window.$, window.console);
