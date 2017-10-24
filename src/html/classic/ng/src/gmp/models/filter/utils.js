525ddbce30f1371b25bcaa2eee020f84aed95358
Björn Ricks <bjoern.ricks@greenbone.net>
2017-10-24 10:34:41 +0000

    * src/html/classic/ng/src/gmp/models/filter/utils.js: The utils.js file got committed accidentally. Remove its content and use it for real filter utility functions now.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@29908 423fd1db-d629-0410-8442-d21db03e70f4

--- src/html/classic/ng/src/gmp/models/filter/utils.js
+++ src/html/classic/ng/src/gmp/models/filter/utils.js
@@ -21,72 +21,12 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-import {is_defined, is_empty} from '../../utils.js';
+import {is_defined} from '../../utils.js';
 
-import {parse_int} from '../../parser.js';
+export const filter_string = filter =>
+  is_defined(filter) && is_defined(filter.toFilterString) ?
+    filter.toFilterString() :
+    filter;
 
-const convert_boolean_int = (keyword, value, relation) => {
-  return {
-    keyword,
-    value:
-    parse_int(value) >= 1 ? 1 : 0,
-    relation,
-  };
-};
-
-function convert_int(keyword, value, relation) {
-  return {
-    keyword,
-    value: parse_int(value),
-    relation,
-  };
-}
-
-function convert_no_relation(keyword, value, relation) {
-  return {
-    keyword,
-    value,
-    relation: '',
-  };
-}
-
-const KEYWORD_CONVERTERS = {
-  min_qod: convert_int,
-  apply_overrides: convert_boolean_int,
-  rows: convert_int,
-  first: convert_int,
-  autofp: convert_int,
-};
-
-const VALUE_CONVERTERS = {
-  and: convert_no_relation,
-  or: convert_no_relation,
-  not: convert_no_relation,
-  re: convert_no_relation,
-  regexp: convert_no_relation,
-  '': convert_no_relation,
-};
-
-export const convert = (keyword, value, relation) => {
-  let converter = KEYWORD_CONVERTERS[keyword];
-  if (is_defined(converter)) {
-    return converter(keyword, value, relation);
-  }
-
-  converter = VALUE_CONVERTERS[value];
-  if (is_defined(converter)) {
-    return converter(keyword, value, relation);
-  }
-
-  if (is_empty(keyword)) {
-    return {value, relation};
-  }
-
-  return {
-    value,
-    keyword,
-    relation,
-  };
-};
 
 // vim: set ts=2 sw=2 tw=80:
