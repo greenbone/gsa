4d2b018ff3374129094a5a8411b8f62c48ae520b
Björn Ricks <bjoern.ricks@greenbone.net>
2017-10-24 10:34:46 +0000

    * src/html/classic/ng/CMakeLists.txt, src/html/classic/ng/src/gmp/commands/infoentity.js: Add an InfoEntityCommand base class.
    git-svn-id: svn+ssh://scm.wald.intevation.org/openvas/trunk/gsa@29910 423fd1db-d629-0410-8442-d21db03e70f4

---
+++ src/html/classic/ng/src/gmp/commands/infoentity.js
@@ -0,0 +1,43 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2017 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+import {EntityCommand} from '../command.js';
+
+class InfoEntityCommand extends EntityCommand {
+
+  constructor(http, info_type, model) {
+    super(http, 'info', model);
+    this.setParam('info_type', info_type);
+    this.setParam('details', '1');
+  }
+
+  getElementFromRoot(root) {
+    /* return the first info element from the response
+     * the second info element is for the counts */
+    return root.get_info.get_info_response.info[0];
+  }
+}
+
+export default InfoEntityCommand;
+
+// vim: set ts=2 sw=2 tw=80:
