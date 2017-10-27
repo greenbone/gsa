5e5119286dea385c0d46e75418af2ea7a3525a17
Steffen Waterkamp <steffen.waterkamp@greenbone.net>
2017-10-27 10:47:17 +0200

    Add new component for inner link anchors
    Anchors will not be hidden behind the top menu bar.

---
+++ src/html/classic/ng/src/web/components/link/target.js
@@ -0,0 +1,43 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Steffen Waterkamp <steffen.waterkamp@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2017 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+import glamorous from 'glamorous';
+
+/**
+* Can be used as link anchor. Offsets the target so that it doesn't hide
+* behind the top menu bar.
+*
+* @module components/link/target.js
+*
+* @exports {glamorous}
+*/
+
+const Target = glamorous.div({
+  content: '',
+  display: 'block',
+  height: '35px',
+  margin: '-35px 0 0',
+  zIndex: '-100',
+});
+
+export default Target;
