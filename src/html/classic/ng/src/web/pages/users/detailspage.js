b95738e9de460c56772989605e2477d1c1fa2dce
Björn Ricks <bjoern.ricks@gmail.com>
2017-11-01 15:56:32 +0100

    Add components for displaying the user details
    Add new components for displaying the details of a user.

---
+++ src/html/classic/ng/src/web/pages/users/detailspage.js
@@ -0,0 +1,184 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2017 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import React from 'react';
+
+import _ from 'gmp/locale.js';
+
+import PropTypes from '../../utils/proptypes.js';
+
+import EntityPage from '../../entity/page.js';
+import EntityContainer from '../../entity/container.js';
+import {goto_details, goto_list} from '../../entity/component.js';
+
+import ExportIcon from '../../components/icon/exporticon.js';
+import CloneIcon from '../../entity/icon/cloneicon.js';
+import CreateIcon from '../../entity/icon/createicon.js';
+import EditIcon from '../../entity/icon/editicon.js';
+import DeleteIcon from '../../entity/icon/deleteicon.js';
+
+import HelpIcon from '../../components/icon/helpicon.js';
+import ListIcon from '../../components/icon/listicon.js';
+
+import Divider from '../../components/layout/divider.js';
+import IconDivider from '../../components/layout/icondivider.js';
+import Layout from '../../components/layout/layout.js';
+
+import UserComponent from './component.js';
+import UserDetails from './details.js';
+
+const ToolBarIcons = ({
+  entity,
+  onUserCloneClick,
+  onUserCreateClick,
+  onUserDeleteClick,
+  onUserDownloadClick,
+  onUserEditClick,
+}) => (
+  <Divider margin="10px">
+    <IconDivider>
+      <HelpIcon
+        page="user_details"
+        title={_('Help: User Details')}
+      />
+      <ListIcon
+        title={_('Users List')}
+        page="users"
+      />
+    </IconDivider>
+    <IconDivider>
+      <CreateIcon
+        entity={entity}
+        onClick={onUserCreateClick}
+      />
+      <CloneIcon
+        entity={entity}
+        onClick={onUserCloneClick}
+      />
+      <EditIcon
+        entity={entity}
+        onClick={onUserEditClick}
+      />
+      <DeleteIcon
+        entity={entity}
+        onClick={onUserDeleteClick}
+      />
+      <ExportIcon
+        value={entity}
+        title={_('Export User as XML')}
+        onClick={onUserDownloadClick}
+      />
+    </IconDivider>
+  </Divider>
+);
+
+ToolBarIcons.propTypes = {
+  entity: PropTypes.model.isRequired,
+  onUserCloneClick: PropTypes.func.isRequired,
+  onUserCreateClick: PropTypes.func.isRequired,
+  onUserDeleteClick: PropTypes.func.isRequired,
+  onUserDownloadClick: PropTypes.func.isRequired,
+  onUserEditClick: PropTypes.func.isRequired,
+};
+
+const Details = ({
+  entity,
+  links = true,
+  ...props
+}) => {
+  return (
+    <Layout flex="column">
+      <UserDetails
+        entity={entity}
+        links={links}
+      />
+    </Layout>
+  );
+};
+
+Details.propTypes = {
+  entity: PropTypes.model.isRequired,
+  links: PropTypes.bool,
+};
+
+const Page = ({
+  onChanged,
+  onDownloaded,
+  onError,
+  ...props
+}) => (
+  <UserComponent
+    onCloned={goto_details('user', props)}
+    onCloneError={onError}
+    onCreated={goto_details('user', props)}
+    onDeleted={goto_list('users', props)}
+    onDeleteError={onError}
+    onDownloaded={onDownloaded}
+    onDownloadError={onError}
+    onSaved={onChanged}
+  >
+    {({
+      clone,
+      create,
+      delete: delete_func,
+      download,
+      edit,
+      save,
+    }) => (
+      <EntityPage
+        {...props}
+        sectionIcon="user.svg"
+        title={_('User')}
+        detailsComponent={Details}
+        toolBarIcons={ToolBarIcons}
+        onUserCloneClick={clone}
+        onUserCreateClick={create}
+        onUserDeleteClick={delete_func}
+        onUserDownloadClick={download}
+        onUserEditClick={edit}
+        onUserSaveClick={save}
+        onPermissionChanged={onChanged}
+        onPermissionDownloaded={onDownloaded}
+        onPermissionDownloadError={onError}
+      />
+    )}
+  </UserComponent>
+);
+
+Page.propTypes = {
+  onChanged: PropTypes.func.isRequired,
+  onDownloaded: PropTypes.func.isRequired,
+  onError: PropTypes.func.isRequired,
+};
+
+const UserPage = props => (
+  <EntityContainer
+    {...props}
+    name="user"
+  >
+    {cprops => <Page {...props} {...cprops} />}
+  </EntityContainer>
+);
+
+export default UserPage;
+
+// vim: set ts=2 sw=2 tw=80:
