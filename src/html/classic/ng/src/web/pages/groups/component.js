62c98391bb9ae2d8fb64bbc01540b27132651000
Björn Ricks <bjoern.ricks@gmail.com>
2017-11-02 16:11:17 +0100

    Add components for displaying group details
    Add a GroupComponent react component that handles CRUD related parts
    including opening the edit and create dialogs.
    Also add the details and a details page.

---
+++ src/html/classic/ng/src/web/pages/groups/component.js
@@ -0,0 +1,138 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2017 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import React from 'react';
+
+import _ from 'gmp/locale.js';
+import {is_defined} from 'gmp/utils.js';
+
+import PropTypes from '../../utils/proptypes.js';
+import withGmp from '../../utils/withGmp.js';
+
+import EntityComponent from '../../entity/component.js';
+
+import Wrapper from '../../components/layout/wrapper.js';
+
+import GroupDialog from './dialog.js';
+
+class GroupComponent extends React.Component {
+
+  constructor(...args) {
+    super(...args);
+
+    this.openGroupDialog = this.openGroupDialog.bind(this);
+  }
+
+  openGroupDialog(group) {
+    const {gmp} = this.props;
+
+    if (is_defined(group)) {
+      const {
+        id,
+        name,
+        comment,
+        users,
+      } = group;
+
+      this.dialog.show({
+        id,
+        name,
+        comment,
+        users,
+      }, {
+        title: _('Edit Group {{name}}', group),
+      });
+    }
+    else {
+      this.dialog.show({});
+    }
+
+    gmp.users.getAll().then(users => {
+      this.dialog.setValue('all_users', users);
+    });
+  }
+
+  render() {
+    const {
+      children,
+      onCloned,
+      onCloneError,
+      onCreated,
+      onCreateError,
+      onDeleted,
+      onDeleteError,
+      onDownloaded,
+      onDownloadError,
+      onSaved,
+      onSaveError,
+    } = this.props;
+    return (
+      <EntityComponent
+        name="group"
+        onCreated={onCreated}
+        onCreateError={onCreateError}
+        onCloned={onCloned}
+        onCloneError={onCloneError}
+        onDeleted={onDeleted}
+        onDeleteError={onDeleteError}
+        onDownloaded={onDownloaded}
+        onDownloadError={onDownloadError}
+        onSaved={onSaved}
+        onSaveError={onSaveError}
+      >
+        {({
+          save,
+          ...other
+        }) => (
+          <Wrapper>
+            {children({
+              ...other,
+              create: this.openGroupDialog,
+              edit: this.openGroupDialog,
+            })}
+            <GroupDialog
+              ref={ref => this.dialog = ref}
+              onSave={save}
+            />
+          </Wrapper>
+        )}
+      </EntityComponent>
+    );
+  }
+}
+
+GroupComponent.propTypes = {
+  children: PropTypes.func.isRequired,
+  gmp: PropTypes.gmp.isRequired,
+  onCloneError: PropTypes.func,
+  onCloned: PropTypes.func,
+  onCreateError: PropTypes.func,
+  onCreated: PropTypes.func,
+  onDeleteError: PropTypes.func,
+  onDeleted: PropTypes.func,
+  onDownloadError: PropTypes.func,
+  onDownloaded: PropTypes.func,
+  onSaveError: PropTypes.func,
+  onSaved: PropTypes.func,
+};
+
+export default withGmp(GroupComponent);
---
+++ src/html/classic/ng/src/web/pages/groups/detailspage.js
@@ -0,0 +1,163 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2017 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import React from 'react';
+
+import _ from 'gmp/locale.js';
+
+import PropTypes from '../../utils/proptypes.js';
+
+import EntityPage from '../../entity/page.js';
+import EntityContainer from '../../entity/container.js';
+import {goto_details, goto_list} from '../../entity/component.js';
+
+import ExportIcon from '../../components/icon/exporticon.js';
+import CloneIcon from '../../entity/icon/cloneicon.js';
+import CreateIcon from '../../entity/icon/createicon.js';
+import EditIcon from '../../entity/icon/editicon.js';
+import TrashIcon from '../../entity/icon/trashicon.js';
+
+import HelpIcon from '../../components/icon/helpicon.js';
+import ListIcon from '../../components/icon/listicon.js';
+
+import Divider from '../../components/layout/divider.js';
+import IconDivider from '../../components/layout/icondivider.js';
+
+import GroupComponent from './component.js';
+import GroupDetails from './details.js';
+
+const ToolBarIcons = ({
+  entity,
+  onGroupCloneClick,
+  onGroupCreateClick,
+  onGroupDeleteClick,
+  onGroupDownloadClick,
+  onGroupEditClick,
+}) => (
+  <Divider margin="10px">
+    <IconDivider>
+      <HelpIcon
+        page="group_details"
+        title={_('Help: Group Details')}
+      />
+      <ListIcon
+        title={_('Groups List')}
+        page="groups"
+      />
+    </IconDivider>
+    <IconDivider>
+      <CreateIcon
+        entity={entity}
+        onClick={onGroupCreateClick}
+      />
+      <CloneIcon
+        entity={entity}
+        onClick={onGroupCloneClick}
+      />
+      <EditIcon
+        entity={entity}
+        onClick={onGroupEditClick}
+      />
+      <TrashIcon
+        entity={entity}
+        onClick={onGroupDeleteClick}
+      />
+      <ExportIcon
+        value={entity}
+        title={_('Export Group as XML')}
+        onClick={onGroupDownloadClick}
+      />
+    </IconDivider>
+  </Divider>
+);
+
+ToolBarIcons.propTypes = {
+  entity: PropTypes.model.isRequired,
+  onGroupCloneClick: PropTypes.func.isRequired,
+  onGroupCreateClick: PropTypes.func.isRequired,
+  onGroupDeleteClick: PropTypes.func.isRequired,
+  onGroupDownloadClick: PropTypes.func.isRequired,
+  onGroupEditClick: PropTypes.func.isRequired,
+};
+
+const Page = ({
+  onChanged,
+  onDownloaded,
+  onError,
+  ...props
+}) => (
+  <GroupComponent
+    onCloned={goto_details('group', props)}
+    onCloneError={onError}
+    onCreated={goto_details('group', props)}
+    onDeleted={goto_list('groups', props)}
+    onDeleteError={onError}
+    onDownloaded={onDownloaded}
+    onDownloadError={onError}
+    onSaved={onChanged}
+  >
+    {({
+      clone,
+      create,
+      delete: delete_func,
+      download,
+      edit,
+      save,
+    }) => (
+      <EntityPage
+        {...props}
+        sectionIcon="group.svg"
+        title={_('Group')}
+        detailsComponent={GroupDetails}
+        toolBarIcons={ToolBarIcons}
+        onGroupCloneClick={clone}
+        onGroupCreateClick={create}
+        onGroupDeleteClick={delete_func}
+        onGroupDownloadClick={download}
+        onGroupEditClick={edit}
+        onGroupSaveClick={save}
+        onPermissionChanged={onChanged}
+        onPermissionDownloaded={onDownloaded}
+        onPermissionDownloadError={onError}
+      />
+    )}
+  </GroupComponent>
+);
+
+Page.propTypes = {
+  onChanged: PropTypes.func.isRequired,
+  onDownloaded: PropTypes.func.isRequired,
+  onError: PropTypes.func.isRequired,
+};
+
+const GroupPage = props => (
+  <EntityContainer
+    {...props}
+    name="group"
+  >
+    {cprops => <Page {...props} {...cprops} />}
+  </EntityContainer>
+);
+
+export default GroupPage;
+
+// vim: set ts=2 sw=2 tw=80:
