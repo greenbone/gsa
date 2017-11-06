8801b9f8038e0d1f4e1f5a736f6f854105632b12
Björn Ricks <bjoern.ricks@gmail.com>
2017-11-06 15:31:55 +0100

    Allow toggling the details of a port list
    At the port lists page allow to toggle details when clicking on the port
    list name. Also update the coding style of the portlists/row js module.

--- ng/src/web/pages/portlists/row.js
+++ ng/src/web/pages/portlists/row.js
@@ -44,82 +44,80 @@ import TableData from '../../components/table/data.js';
 import TableRow from '../../components/table/row.js';
 
 const IconActions = ({
-    entity,
-    onEntityDelete,
-    onEntityDownload,
-    onEntityClone,
-    onEditPortList,
-  }) => {
-  return (
-    <Layout flex align={['center', 'center']}>
-      <TrashIcon
-        entity={entity}
-        displayName={_('Port List')}
-        name="port_list"
-        onClick={onEntityDelete}/>
-      <EditIcon
-        entity={entity}
-        displayName={_('Port List')}
-        name="port_list"
-        onClick={onEditPortList}/>
-      <CloneIcon
-        entity={entity}
-        displayName={_('Port List')}
-        name="port_list"
-        onClick={onEntityClone}/>
-      <ExportIcon
-        value={entity}
-        title={_('Export Port List')}
-        onClick={onEntityDownload}
-      />
-    </Layout>
-  );
-};
+  entity,
+  onPortListDeleteClick,
+  onPortListDownloadClick,
+  onPortListCloneClick,
+  onPortListEditClick,
+}) => (
+  <Layout flex align={['center', 'center']}>
+    <TrashIcon
+      entity={entity}
+      displayName={_('Port List')}
+      name="portlist"
+      onClick={onPortListDeleteClick}/>
+    <EditIcon
+      entity={entity}
+      displayName={_('Port List')}
+      name="port_list"
+      onClick={onPortListEditClick}/>
+    <CloneIcon
+      entity={entity}
+      displayName={_('Port List')}
+      name="port_list"
+      onClick={onPortListCloneClick}/>
+    <ExportIcon
+      value={entity}
+      title={_('Export Port List')}
+      onClick={onPortListDownloadClick}
+    />
+  </Layout>
+);
 
 IconActions.propTypes = {
   entity: PropTypes.model,
-  onEntityDelete: PropTypes.func,
-  onEntityDownload: PropTypes.func,
-  onEditPortList: PropTypes.func,
-  onEntityClone: PropTypes.func,
+  onPortListCloneClick: PropTypes.func.isRequired,
+  onPortListDeleteClick: PropTypes.func.isRequired,
+  onPortListDownloadClick: PropTypes.func.isRequired,
+  onPortListEditClick: PropTypes.func.isRequired,
 };
 
 const Row = ({
-    entity,
-    links = true,
-    actions,
-    ...props,
-  }) => {
-  return (
-    <TableRow>
-      <EntityNameTableData
-        legacy
-        entity={entity}
-        link={links}
-        type="port_list"
-        displayName={_('Port List')}
-      />
-      <TableData
-        flex align="end">
-        {entity.port_count.all}
-      </TableData>
-      <TableData
-        flex align="end">
-        {entity.port_count.tcp}
-      </TableData>
-      <TableData
-        flex align="end">
-        {entity.port_count.udp}
-      </TableData>
-      {render_component(actions, {...props, entity})}
-    </TableRow>
-  );
-};
+  entity,
+  links = true,
+  actions,
+  onToggleDetailsClick,
+  ...props
+}) => (
+  <TableRow>
+    <EntityNameTableData
+      entity={entity}
+      link={links}
+      type="portlist"
+      displayName={_('Port List')}
+      onToggleDetailsClick={onToggleDetailsClick}
+    />
+    <TableData
+      flex align="end">
+      {entity.port_count.all}
+    </TableData>
+    <TableData
+      flex align="end">
+      {entity.port_count.tcp}
+    </TableData>
+    <TableData
+      flex align="end">
+      {entity.port_count.udp}
+    </TableData>
+    {render_component(actions, {...props, entity})}
+  </TableRow>
+);
 
 Row.propTypes = {
   actions: PropTypes.componentOrFalse,
   entity: PropTypes.model.isRequired,
   links: PropTypes.bool,
+  onToggleDetailsClick: PropTypes.func.isRequired,
 };
 
 export default withEntityRow(withEntityActions(IconActions))(Row);
