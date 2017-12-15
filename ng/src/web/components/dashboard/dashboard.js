000e42521fecab31b3debd8dc569ac0cbc6eb238
Björn Ricks <bjoern.ricks@greenbone.net>
2017-12-15 11:26:29 +0100

    Change dashboards to use new ng charts
    Use new ng chart code for the dashboards. The new code is loaded via
    import statements instead of using script tags.

--- ng/src/web/components/dashboard/dashboard.js
+++ ng/src/web/components/dashboard/dashboard.js
@@ -32,6 +32,8 @@ import compose from '../../utils/compose.js';
 import withCache from '../../utils/withCache.js';
 import withGmp from '../../utils/withGmp.js';
 
+import DashboardClass from './legacy/dashboard.js';
+
 import './css/dashboard.css';
 
 const log = logger.getLogger('web.dashboard.dashboard');
@@ -43,7 +45,7 @@ class Dashboard extends React.Component {
 
     const {id = 'dashboard'} = this.props;
 
-    this.dashboard = new window.gsa.charts.Dashboard(id, undefined, {
+    this.dashboard = new DashboardClass(id, undefined, {
       config_pref_id: this.props.configPrefId,
       default_heights_string: '280',
       default_controllers_string: this.props.defaultControllersString,
@@ -68,9 +70,8 @@ class Dashboard extends React.Component {
   }
 
   componentDidMount() {
-    const {gmp, usersettingsCache: cache} = this.props;
+    const {gmp, usersettingsCache: cache, configPrefId: pref_id} = this.props;
     const {dashboard} = this;
-    const pref_id = this.props.configPrefId;
 
     const promises = [
       gmp.user.currentChartPreferences({cache}),
@@ -82,6 +83,7 @@ class Dashboard extends React.Component {
 
     PromiseFactory.all(promises).then(([prefresp, filtersresp]) => {
       const filters = is_defined(filtersresp) ? filtersresp.data : [];
+
       const prefs = prefresp.data;
       const pref = prefs.get(pref_id);
 
