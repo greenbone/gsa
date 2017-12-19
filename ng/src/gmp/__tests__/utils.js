c9d646aa06dd7b90ab09fccc386a488eb2d3553c
Björn Ricks <bjoern.ricks@greenbone.net>
2017-12-19 15:28:05 +0100

    Add tests for sum and avg functions

---
+++ ng/src/gmp/__tests__/utils.js
@@ -0,0 +1,82 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2017 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import {
+  avg,
+  sum,
+} from '../utils.js';
+
+describe('sum function tests', () => {
+
+  test('should calculate the array sum', () => {
+    expect(sum([1, 2, 3])).toBe(6);
+  });
+
+  test('should calculate sum from object props', () => {
+    const array = [
+      {value: 1},
+      {value: 2},
+      {value: 3},
+    ];
+    expect(sum(array, entry => entry.value)).toBe(6);
+  });
+
+  test('should expect zero for undefined values', () => {
+    expect(sum([1, 2, undefined, 3])).toBe(6);
+
+    const array = [
+      {value: 1},
+      {},
+      {value: 2},
+      {value: 3},
+    ];
+    expect(sum(array, entry => entry.value)).toBe(6);
+  });
+});
+
+describe('avg function tests', () => {
+
+  test('should calculate the array average', () => {
+    expect(avg([3, 3, 3])).toBe(3);
+  });
+
+  test('should calculate average from object props', () => {
+    const array = [
+      {value: 3},
+      {value: 3},
+      {value: 3},
+    ];
+    expect(avg(array, entry => entry.value)).toBe(3);
+  });
+
+  test('should expect zero for undefined values', () => {
+    expect(avg([5, undefined, 4])).toBe(3);
+
+    const array = [
+      {value: 5},
+      {},
+      {value: 4},
+    ];
+    expect(avg(array, entry => entry.value)).toBe(3);
+  });
+});
+// vim: set ts=2 sw=2 tw=80:
