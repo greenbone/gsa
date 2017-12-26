greenbone/gsa

commit 050ca75883544367eb98fa78571d730dd9358eb8

Improve code for EntitesTable rendering
Use is_defined for checking if a component is set. Use ternary operator
if possible.

ng/src/web/entities/table.js
@@ -166,16 +166,17 @@ class EntitiesTable extends React.Component {
     }
 
     const rows = [];
-    if (RowComponent) {
+    if (is_defined(RowComponent)) {
       for_each(entities, entity => {
         rows.push(
           <RowComponent
             {...other}
             onToggleDetailsClick={this.handleToggleShowDetails}
             key={entity.id}
-            entity={entity}/>
+            entity={entity}
+          />
         );
-        if (RowDetailsComponent && details[entity.id]) {
+        if (is_defined(RowDetailsComponent) && details[entity.id]) {
           rows.push(
             <RowDetailsComponent
               links={props.links}
@@ -187,49 +188,34 @@ class EntitiesTable extends React.Component {
       });
     }
 
-    let pagination;
-    if (PaginationComponent) {
-      pagination = (
-        <PaginationComponent
-          {...other}
-          onFirstClick={this.handleFirst}
-          onLastClick={this.handleLast}
-          onNextClick={this.handleNext}
-          onPreviousClick={this.handlePrevious}
-          counts={entitiesCounts}
-        />
-      );
-    }
+    const pagination = is_defined(PaginationComponent) ? (
+      <PaginationComponent
+        {...other}
+        onFirstClick={this.handleFirst}
+        onLastClick={this.handleLast}
+        onNextClick={this.handleNext}
+        onPreviousClick={this.handlePrevious}
+        counts={entitiesCounts}
+      />
+    ) : undefined;
 
-    let header;
-    if (HeaderComponent) {
-      header = (
-        <HeaderComponent
-          currentSortBy={currentSortBy}
-          currentSortDir={currentSortDir}
-          {...other}
-        />
-      );
-    }
+    const header = is_defined(HeaderComponent) ? (
+      <HeaderComponent
+        currentSortBy={currentSortBy}
+        currentSortDir={currentSortDir}
+        {...other}
+      />
+    ) : undefined;
 
-    let footer;
-    if (FooterComponent) {
-      footer = (
-        <FooterComponent {...other}/>
-      );
-    }
+    const footer = is_defined(FooterComponent) ? (
+      <FooterComponent {...other} />
+    ) : undefined;
 
-    let body;
-    if (BodyComponent) {
-      body = (
-        <BodyComponent>
-          {rows}
-        </BodyComponent>
-      );
-    }
-    else {
-      body = rows;
-    }
+    const body = is_defined(BodyComponent) ? (
+      <BodyComponent>
+        {rows}
+      </BodyComponent>
+    ) : rows;
 
     const foldState = this.state.allToggled ? 'UNFOLDED' : 'FOLDED';
     const detailsIcon = (

commit ad82aaf1d72e4a61c3d48841ed7a38c56eba046a

Fix rendering of entities tables
Cve, Ovaldef, and Allsecinfo tables got broken by
050ca75883544367eb98fa78571d730dd9358eb8

Allow again to deactivate parts of the EntitiesTable by setting
the corresponding properties to false.

ng/src/web/entities/table.js
@@ -186,34 +186,40 @@ class EntitiesTable extends React.Component {
       });
     }
 
-    const pagination = is_defined(PaginationComponent) ? (
-      <PaginationComponent
-        {...other}
-        onFirstClick={this.handleFirst}
-        onLastClick={this.handleLast}
-        onNextClick={this.handleNext}
-        onPreviousClick={this.handlePrevious}
-        counts={entitiesCounts}
-      />
-    ) : undefined;
+    const pagination = PaginationComponent === false ?
+      undefined :
+      (
+        <PaginationComponent
+          {...other}
+          onFirstClick={this.handleFirst}
+          onLastClick={this.handleLast}
+          onNextClick={this.handleNext}
+          onPreviousClick={this.handlePrevious}
+          counts={entitiesCounts}
+        />
+      );
 
-    const header = is_defined(HeaderComponent) ? (
-      <HeaderComponent
-        currentSortBy={currentSortBy}
-        currentSortDir={currentSortDir}
-        {...other}
-      />
-    ) : undefined;
+    const header = HeaderComponent === false ?
+      undefined :
+      (
+        <HeaderComponent
+          currentSortBy={currentSortBy}
+          currentSortDir={currentSortDir}
+          {...other}
+        />
+      );
 
-    const footer = is_defined(FooterComponent) ? (
-      <FooterComponent {...other} />
-    ) : undefined;
+    const footer = FooterComponent === false ?
+      undefined :
+      <FooterComponent {...other} />;
 
-    const body = is_defined(BodyComponent) ? (
-      <BodyComponent>
-        {rows}
-      </BodyComponent>
-    ) : rows;
+    const body = BodyComponent === false ?
+      rows :
+      (
+        <BodyComponent>
+          {rows}
+        </BodyComponent>
+      );
 
     const foldState = this.state.allToggled ? 'UNFOLDED' : 'FOLDED';
     const detailsIcon = (