7f8f11e5070fe82dec19df4d7a9efddb88175d3b
Björn Ricks <bjoern.ricks@greenbone.net>
2018-01-04 16:21:57 +0100

    Don't set size on WizardIcon
    Use default size for the WizardIcon.

--- ng/src/web/pages/tasks/listpage.js
+++ ng/src/web/pages/tasks/listpage.js
@@ -73,7 +73,6 @@ const ToolBarIcons = ({
       {capabilities.mayOp('run_wizard') &&
         <IconMenu
           img="wizard.svg"
-          size="small"
           onClick={onTaskWizardClick}>
           {capabilities.mayCreate('task') &&
             <MenuEntry
