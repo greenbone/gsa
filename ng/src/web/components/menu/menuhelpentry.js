d07b59357c7933de3f306d0fd01695361f4de538
Björn Ricks <bjoern.ricks@greenbone.net>
2018-01-04 09:52:39 +0100

    Use ManualLink to render MenuHelpEntry
    Remove code duplication by using MenualLink for creating the anchor to
    the manual.

--- ng/src/web/components/menu/menuhelpentry.js
+++ ng/src/web/components/menu/menuhelpentry.js
@@ -23,58 +23,33 @@
 
 import React from 'react';
 
-import {get_language} from 'gmp/locale.js';
-import {is_defined} from 'gmp/utils.js';
-
 import PropTypes from '../../utils/proptypes.js';
-import withGmp from '../../utils/withGmp.js';
+
+import ManualLink from '../link/manuallink.js';
 
 import MenuEntry from './menuentry.js';
 
-const LANGUAGE_MAPPING = {};
-const DEFAULT_LANGUAGE_PATH = 'en';
-
-const get_language_path = () => {
-  const lang = get_language();
-
-  if (!is_defined(lang)) {
-    return DEFAULT_LANGUAGE_PATH;
-  }
-
-  const code = lang.slice(0, 1);
-  const path = LANGUAGE_MAPPING[code];
-
-  return is_defined(path) ? path : DEFAULT_LANGUAGE_PATH;
-};
-
 const MenuHelpEntry = ({
-    gmp,
-    title,
-    ...other
-  }) => {
-
-    const {manualurl} = gmp.globals;
-    let url = manualurl;
-    if (!url.endsWith('/')) {
-      url += '/';
-    }
-
-    url += get_language_path() + '/index.html';
-
-    return (
-      <MenuEntry
-        to={url}
-        title={title}
-        manualLink={true}
-      />
-    );
-};
+  title,
+  ...props
+}) => (
+  <MenuEntry
+    {...props}
+    caps="help"
+  >
+    <ManualLink
+      page="index"
+      title={title}
+    >
+      {title}
+    </ManualLink>
+  </MenuEntry>
+);
 
 MenuHelpEntry.propTypes = {
-  gmp: PropTypes.gmp.isRequired,
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
 };
 
-export default withGmp(MenuHelpEntry);
+export default MenuHelpEntry;
 
 // vim: set ts=2 sw=2 tw=80:
