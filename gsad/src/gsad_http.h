00466665ad63ac19ff19429e891b5b0b3654e6ec
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2018-01-07 21:52:38 +0100

    Reduce gsad_message_new to minimum needed.
    Now that only a single call of gsad_message_new happens,
    all parts of that function can be removed which are not
    used by this call (credential, function, line, xml_flag).
    
    Also remove the proto from the header file which is not
    needed anymore and due to this move up the function before
    first use.

--- gsad/src/gsad_http.h
+++ gsad/src/gsad_http.h
@@ -241,8 +241,6 @@ int params_append_mhd (params_t *params, const char *name, const char *filename,
 char * gsad_message (credentials_t *, const char *, const char *, int,
                      const char *, cmd_response_data_t *);
 
-char * gsad_message_new (credentials_t *, const char *, const char *, int,
-                         const char *, gboolean, cmd_response_data_t *);
 gchar *login_xml (const gchar *, const gchar *, const gchar *, const gchar *,
                   const gchar *, const gchar *);
 
