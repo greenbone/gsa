fdb6563e05531730f00ffea790bb1f5b8f8dca43
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2018-01-15 23:05:29 +0100

    Resolve one of the last calls of xsl_transform.
    * gsad/src/gsad_http.c (gsad_message_new): Instead of calling
      xslt_transform, use a html template instead of filling a xml
      template. The content was determined by title, version and msg
      only anyway.

--- gsad/src/gsad_http.c
+++ gsad/src/gsad_http.c
@@ -465,38 +465,78 @@ char *
 gsad_message_new (const char *title, const char *msg,
                   cmd_response_data_t *response_data)
 {
-  gchar *xml, *resp;
+  gchar *html;
 
-  xml = g_strdup_printf ("<gsad_response>"
-                         "<title>%s (GSA %s)</title>"
-                         "<message>%s</message>"
-                         "<token>%s</token>"
-                         "</gsad_response>",
-                         title,
-                         GSAD_VERSION,
-                         msg,
-                         "");
+  html = g_strdup_printf (
+    "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">"
+    "<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"\">"
+    "<head>"
+      "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"></meta>"
+      "<link rel=\"icon\" href=\"/img/favicon.gif\" type=\"image/gif\"></link>"
+      "<title>Greenbone Security Assistant</title>"
+      "<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/gsa-base.css\"></link>"
+      "<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/select2.min.css\"></link>"
+      "<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/jquery-ui.structure.min.css\"></link>"
+      "<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/jquery-ui.theme.min.css\"></link>"
+      "<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/gsa-style.css\"></link>"
+      "<script src=\"/js/jquery-2.1.4.js\" type=\"text/javascript\"></script>"
+      "<script src=\"/js/jquery-ui.js\" type=\"text/javascript\"></script>"
+      "<script src=\"/js/select2.js\" type=\"text/javascript\"></script>"
+      "<script src=\"/js/i18next-2.3.4.js\" type=\"text/javascript\"></script>"
+      "<script src=\"/js/i18next-xhr-0.5.3.js\" type=\"text/javascript\"></script>"
+      "<script src=\"/js/i18next-languagedetector-0.2.2.js\" type=\"text/javascript\"></script>"
+      "<script src=\"/js/moment.js\" type=\"text/javascript\"></script>"
+      "<script src=\"/js/moment-timezone-with-data.js\" type=\"text/javascript\"></script>"
+      "<script src=\"/js/greenbone-ui.js\" type=\"text/javascript\"></script>"
+      "<script src=\"/js/greenbone.js\" type=\"text/javascript\"></script>"
+      "<script src=\"/js/gsa_polyfill.js\" type=\"text/javascript\"></script>"
+    "</head>"
+    "<body>"
+    "<div class=\"gsa-main\"><div>"
+    "<div class=\"panel panel-error\">"
+      "<div class=\"panel-heading\">"
+        "<h3 class=\"panel-title\">Error Message</h3>"
+      "</div>"
+      "<div class=\"panel-body\">"
+        "<div class=\"row\">"
+	  "<div class=\"pull-right\">"
+	    "<a href=\"/help/error_messages.html?token=\" class=\"icon icon-sm\" title=\"Help: Error Message\">"
+	      "<img src=\"/img/help.svg\" class=\"icon icon-sm\"></img>"
+	    "</a>"
+	  "</div>"
+	  "<img src=\"/img/alert_sign.svg\" alt=\"\" title=\"%s (GSA %s)\" class=\"pull-left icon icon-lg\"></img>"
+	  "<h4>%s (GSA %s)</h4>"
+        "</div>"
+	"<p style=\"margin-top: 10px; font-size: 16px;\">%s</p>"
+      "</div>"
+      "<div class=\"panel-footer\">"
+      "    Your options (not all may work):"
+      "    'Back' button of browser"
+      "    "
+      "        | <a href=\"/logout\">Logout</a></div>"
+      "</div>"
+      "<div class=\"gsa-footer\">"
+        "<div class=\"pull-left\">"
+	  "<span id=\"gsa-token\" style=\"display: none;\"></span>"
+        "</div>"
+	"Greenbone Security Assistant (GSA) Copyright 2009-2017 by Greenbone Networks"
+    " GmbH, <a href=\"http://www.greenbone.net\" target=\"_blank\">www.greenbone.net</a>"
+      "</div>"
+    "</div>"
+  "</div>"
+  "</body></html>",
+                          title,
+                          GSAD_VERSION,
+                          title,
+                          GSAD_VERSION,
+                          msg);
 
   cmd_response_data_set_content_type (response_data,
                                       GSAD_CONTENT_TYPE_TEXT_HTML);
 
-  resp = xsl_transform (xml, response_data);
-  if (resp == NULL)
-    {
-      resp = g_strdup ("<html>"
-                       "<body>"
-                       "An internal server error has occurred during XSL"
-                       " transformation."
-                       "</body>"
-                       "</html>");
-      cmd_response_data_set_status_code (response_data,
-                                         MHD_HTTP_INTERNAL_SERVER_ERROR);
-    }
+  cmd_response_data_set_content_length (response_data, strlen (html));
 
-  cmd_response_data_set_content_length (response_data, strlen (resp));
-
-  g_free (xml);
-  return resp;
+  return html;
 }
 
 /**
