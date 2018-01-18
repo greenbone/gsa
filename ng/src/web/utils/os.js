79a7cb2b9d0e6000bc05297aefe063d942cfa94e
Björn Ricks <bjoern.ricks@greenbone.net>
2018-01-18 11:54:00 +0100

    Remove duplicate os cpe entry for hp-ux 11.23

--- ng/src/web/utils/os.js
+++ ng/src/web/utils/os.js
@@ -804,11 +804,6 @@ const operating_systems = [
     icon: 'os_hp.svg'
   },
   {
-    pattern: 'cpe:/o:hp:hp-ux:11.23',
-    title: 'HP-UX 11i v2',
-    icon: 'os_hp.svg'
-  },
-  {
     pattern: 'cpe:/o:hp:hp-ux',
     title: 'HP-UX',
     icon: 'os_hp.svg'
