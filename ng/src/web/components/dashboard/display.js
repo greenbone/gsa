a1fb687ad6c65712cb0bc779d15f68dd53f04147
Björn Ricks <bjoern.ricks@greenbone.net>
2018-02-01 14:17:18 +0100

    Add new dashboard Display component
    Add new dashbaord display component written in react. This will replace
    the legacy display at the end.

---
+++ ng/src/web/components/dashboard/display.js
@@ -0,0 +1,103 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2018 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+import React from 'react';
+
+import glamorous from 'glamorous';
+
+import _ from 'gmp/locale';
+
+import Theme from '../../utils/theme';
+import PropTypes from '../../utils/proptypes';
+
+import CloseButton from '../dialog/closebutton';
+
+const Header = glamorous.div('display-header', {
+  display: 'flex',
+  flexGrow: 0,
+  flexShrink: 0,
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  backgroundColor: Theme.main.green,
+  border: '1px solid ' + Theme.extra.darkGreen,
+  color: 'white',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  padding: '1px 5px 1px 10px',
+  fontWeight: 'bold',
+  userSelect: 'none',
+});
+
+const DisplayView = glamorous.div('display-view', {
+  display: 'flex',
+  flexDirection: 'column',
+  flexGrow: 1,
+  backgroundColor: '#EEEEEE',
+});
+
+const DisplayContent = glamorous.div('display-content', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexGrow: 1,
+});
+
+const DisplayTitle = glamorous.div('display-title', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexGrow: 1,
+});
+
+const Display = ({
+  children,
+  title,
+  onRemoveClick,
+}) => {
+  return (
+    <DisplayView>
+      <Header>
+        <DisplayTitle>
+          {title}
+        </DisplayTitle>
+        <CloseButton
+          size="small"
+          title={_('Remove')}
+          onClick={onRemoveClick}
+        />
+      </Header>
+      <DisplayContent>
+        {children}
+      </DisplayContent>
+    </DisplayView>
+  );
+};
+
+Display.propTypes = {
+  title: PropTypes.string,
+  onRemoveClick: PropTypes.func.isRequired,
+};
+
+export default Display;
+
+// vim: set ts=2 sw=2 tw=80:
