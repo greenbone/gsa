249309fcf087a51384da8ae372ddef897cd01f2a
Steffen Waterkamp <steffen.waterkamp@greenbone.net>
2018-03-07 13:56:02 +0100

    Add new colors to Theme
    Added are inputBorderGray and mediumBlue.

--- ng/src/web/utils/theme.js
+++ ng/src/web/utils/theme.js
@@ -42,6 +42,8 @@ const Theme = {
   darkRed: '#a94442', // used by: dialog errors font
   blue: '#0000ff', // used by: links
   dialogGray: '#eeeeee', // used by: dialog and dashboard display backgrounds
+  inputBorderGray: '#aaaaaa', // used by: form field, (Multi-)Select
+  mediumBlue: '#5897fb', // used by active/hovered items in Select
 
   /* source ? */
   darkGreen: '#519032',
