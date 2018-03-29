346e8a01f269db688fd85563ce585c33398d72f5
Björn Ricks <bjoern.ricks@greenbone.net>
2018-03-29 14:25:12 +0200

    Add first draft for a react based Dashboard component

---
+++ ng/src/web/components/dashboard2/dashboard.js
@@ -0,0 +1,131 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2018 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import 'core-js/fn/object/entries';
+
+import React from 'react';
+
+import {connect} from 'react-redux';
+
+import {is_defined} from 'gmp/utils/identity';
+
+import Grid, {createRow, createItem} from '../sortable/grid.js';
+
+import compose from '../../utils/compose.js';
+import PropTypes from '../../utils/proptypes.js';
+import withGmp from '../../utils/withGmp.js';
+
+class Dashboard extends React.Component {
+
+  static propTypes = {
+    components: PropTypes.object,
+    defaultContent: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string)),
+    dispatch: PropTypes.func.isRequired,
+    gmp: PropTypes.gmp.isRequired,
+    loaders: PropTypes.arrayOf(PropTypes.func),
+    maxItemsPerRow: PropTypes.number,
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+    };
+
+    this.handleItemsChange = this.handleItemsChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.load();
+    this.createItems();
+  }
+
+  load() {
+    const {loaders = [], dispatch, gmp} = this.props;
+
+    for (const loader of loaders) {
+      dispatch(loader({gmp}));
+    }
+  }
+
+  createItems() {
+    const {components = {}, defaultContent = [[]]} = this.props;
+
+    const items = defaultContent.map(row => {
+      const rowItems = row.map(id => {
+
+        const component = components[id];
+        if (!is_defined(component)) {
+          return undefined;
+        }
+
+        return createItem({id});
+      }).filter(is_defined);
+
+      if (rowItems.length > 0) {
+        return createRow(rowItems);
+      }
+      return undefined;
+    }).filter(is_defined);
+
+    this.setState({items});
+  }
+
+  handleItemsChange(items) {
+    this.setState({items});
+  }
+
+  render() {
+    const {items} = this.state;
+    const {maxItemsPerRow, components = {}} = this.props;
+
+    return (
+      <Grid
+        items={items}
+        maxItemsPerRow={maxItemsPerRow}
+        onChange={this.handleItemsChange}
+      >
+        {({id, props, height, width, remove}) => {
+          const {id: elementId} = props;
+          const Component = components[elementId];
+          return is_defined(Component) ? (
+            <Component
+              height={height}
+              width={width}
+              id={id}
+              onRemoveClick={remove}
+            />
+          ) : null;
+        }}
+      </Grid>
+    );
+  }
+}
+
+export default compose(
+  withGmp,
+  connect(),
+)(Dashboard);
+
+// vim: set ts=2 sw=2 tw=80:
+
