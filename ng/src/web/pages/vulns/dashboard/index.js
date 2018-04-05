1f2bbbe806fb20fa65266e5cad6a026a82d1334e
Steffen Waterkamp <steffen.waterkamp@greenbone.net>
2018-04-05 16:58:33 +0200

    Add new dashboard for vulnerabilities

---
+++ ng/src/web/pages/vulns/dashboard/index.js
@@ -0,0 +1,61 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Steffen Waterkamp <steffen.waterkamp@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2018 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+import React from 'react';
+
+import PropTypes from '../../../utils/proptypes';
+
+import Dashboard from '../../../components/dashboard2/dashboard';
+
+import {vulnsSeverityLoader} from './loaders';
+
+import VulnsCvssDisplay from './cvssdisplay';
+import VulnsSeverityDisplay from './severityclassdisplay';
+
+const VulnerabilitiesDashboard = ({
+  filter,
+}) => (
+  <Dashboard
+    loaders={[
+      vulnsSeverityLoader,
+    ]}
+    components={{
+      'vulns-cvss': VulnsCvssDisplay,
+      'vulns-severity-class': VulnsSeverityDisplay,
+    }}
+    defaultContent={[
+      [
+        'vulns-cvss',
+        'vulns-severity-class',
+      ],
+    ]}
+  />
+);
+
+VulnerabilitiesDashboard.propTypes = {
+  filter: PropTypes.filter,
+};
+
+export default VulnerabilitiesDashboard;
+
+// vim: set ts=2 sw=2 tw=80:
