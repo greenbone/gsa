31a5fef44173c3e01f3f0989ab003a38cd859b9f
Steffen Waterkamp <steffen.waterkamp@greenbone.net>
2018-04-26 11:23:09 +0200

    Add VulnScore display for hosts

---
+++ ng/src/web/pages/hosts/dashboard/vulnscoredisplay.js
@@ -0,0 +1,163 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Steffen Waterkamp <steffen.waterkamp@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2018 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import React from 'react';
+import {withRouter} from 'react-router';
+
+import glamorous from 'glamorous';
+import moment from 'moment';
+
+import _ from 'gmp/locale';
+
+import {parse_float, parse_severity} from 'gmp/parser';
+
+import {resultSeverityRiskFactor} from '../../../utils/severity';
+import PropTypes from '../../../utils/proptypes';
+
+import BarChart from '../../../components/chart/bar';
+
+import DataDisplay from '../../../components/dashboard2/display/datadisplay';
+import {
+  riskFactorColorScale,
+} from '../../../components/dashboard2/display/utils';
+
+import {HostsVulnScoreLoader} from './loaders';
+import {registerDisplay} from '../../../components/dashboard2/registry';
+
+const ToolTip = glamorous.div({
+    fontWeight: 'normal',
+    textAlign: 'center',
+    lineHeight: '1.2em',
+  });
+
+const transformVulnScoreData = (data = {}, {severityClass}) => {
+  const {groups = []} = data;
+  const tdata = groups
+    .filter(group => {
+      const {stats = {}} = group;
+      const {severity = 0} = stats;
+      return parse_float(severity.max) > 0;
+    })
+    .map(group => {
+      const {stats, text, value: id} = group;
+      const {modified, name} = text;
+      const {severity} = stats;
+      const averageSeverity = parse_severity(severity.mean);
+      const riskFactor = resultSeverityRiskFactor(averageSeverity);
+      const modifiedDate = moment(modified).format('lll');
+      const toolTip = (
+        <ToolTip>
+          <b>{name}:</b><br/>
+          {_('{{sevMax}}: ({{riskFactor}})',
+            {
+              sevMax: severity.max,
+              riskFactor,
+            })}<br/>
+          <b>{_('Updated: ')}</b>{modifiedDate}
+        </ToolTip>
+      );
+
+      return {
+        y: parse_float(severity.max),
+        x: name,
+        label: name,
+        color: riskFactorColorScale(riskFactor),
+        toolTip,
+        id,
+      };
+    });
+  return tdata.reverse();
+};
+
+class HostsVulnScoreDisplay extends React.Component {
+
+  constructor(...args) {
+    super(...args);
+
+    this.handleDataClick = this.handleDataClick.bind(this);
+  }
+
+  handleDataClick(data) {
+    const {router} = this.props;
+
+    router.push(`/ng/host/${data.id}`);
+  }
+
+  render() {
+    const {
+      filter,
+      ...props
+    } = this.props;
+    return (
+      <HostsVulnScoreLoader
+        filter={filter}
+      >
+        {loaderProps => (
+          <DataDisplay
+            {...props}
+            {...loaderProps}
+            dataTransform={transformVulnScoreData}
+            dataTitles={[
+              _('Host Name'),
+              _('Max. average Severity Score'),
+            ]}
+            dataRow={({row}) => [row.x, row.y]}
+            title={() => _('Most Vulnerable Hosts')}
+          >
+            {({width, height, data: tdata, svgRef}) => (
+              <BarChart
+                svgRef={svgRef}
+                horizontal
+                displayLegend={false}
+                width={width}
+                height={height}
+                data={tdata}
+                xLabel={_('Vulnerability (Severity) Score')}
+                onDataClick={this.handleDataClick}
+              />
+            )}
+          </DataDisplay>
+        )}
+      </HostsVulnScoreLoader>
+    );
+  }
+}
+
+HostsVulnScoreDisplay.propTypes = {
+  filter: PropTypes.filter,
+  router: PropTypes.object.isRequired,
+  onFilterChanged: PropTypes.func.isRequired,
+};
+
+const DISPLAY_ID = 'host-by-most-vulnerable';
+
+const HostsVulnScoreDisplayWithRouter = withRouter(HostsVulnScoreDisplay);
+
+HostsVulnScoreDisplayWithRouter.displayId = DISPLAY_ID;
+
+registerDisplay(DISPLAY_ID, HostsVulnScoreDisplayWithRouter, {
+  title: _('Hosts by Vulnerability Score'),
+});
+
+export default HostsVulnScoreDisplayWithRouter;
+
+// vim: set ts=2 sw=2 tw=80:
