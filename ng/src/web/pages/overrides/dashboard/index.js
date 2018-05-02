754ef55c71e9fc6bfb223dbb399e2d0250443df1
Steffen Waterkamp <steffen.waterkamp@greenbone.net>
2018-05-02 15:43:27 +0200

    Add Word Cloud display to overrides dashboard

--- ng/src/web/pages/overrides/dashboard/index.js
+++ ng/src/web/pages/overrides/dashboard/index.js
@@ -28,6 +28,7 @@ import PropTypes from '../../../utils/proptypes';
 import Dashboard from '../../../components/dashboard2/dashboard';
 
 import OverridesActiveDaysDisplay from './activedaysdisplay';
+import OverridesWordCloudDisplay from './wordclouddisplay';
 
 export const OVERRIDES_DASHBOARD_ID = '054862fe-0781-4527-b1aa-2113bcd16ce7';
 
@@ -40,10 +41,12 @@ const OverridesDashboard = ({
     filter={filter}
     permittedDisplays={[
       OverridesActiveDaysDisplay.displayId,
+      OverridesWordCloudDisplay.displayId,
     ]}
     defaultContent={[
       [
         OverridesActiveDaysDisplay.displayId,
+        OverridesWordCloudDisplay.displayId,
       ],
     ]}
     maxItemsPerRow={4}
