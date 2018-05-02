ce4e65692bbde77e0e346702eefa8af9641c99ae
Steffen Waterkamp <steffen.waterkamp@greenbone.net>
2018-05-02 15:32:25 +0200

    Add Active Days display for overrides

---
+++ ng/src/web/pages/overrides/dashboard/activedaysdisplay.js
@@ -0,0 +1,199 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Steffen Waterkamp <steffen.waterkamp@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2018 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+import React from 'react';
+
+import _ from 'gmp/locale';
+
+import FilterTerm from 'gmp/models/filter/filterterm';
+import Filter from 'gmp/models/filter';
+
+import {parse_float} from 'gmp/parser';
+
+import {is_defined} from 'gmp/utils/identity';
+
+import PropTypes from '../../../utils/proptypes';
+
+import DonutChart from '../../../components/chart/donut3d';
+import DataDisplay from '../../../components/dashboard2/display/datadisplay';
+import {
+  totalCount,
+  percent,
+  activeDaysColorScale,
+} from '../../../components/dashboard2/display/utils';
+import {registerDisplay} from '../../../components/dashboard2/registry';
+
+import {OverridesActiveDaysLoader} from './loaders';
+
+const MAX_BINS = 10; // if this is changed, activeDaysColorScale needs adjustment
+
+const ACTIVE_YES_ALWAYS_VALUE = '-2';
+const ACTIVE_NO_VALUE = '-1';
+
+const transformActiveDaysData = (data = {}) => {
+  const {groups = []} = data;
+  const sum = totalCount(groups);
+
+  // if more than MAX_BINS groups are loaded, bulk the last ones (those
+  // overrides that are active the longest) into one bin
+  if (groups.length > MAX_BINS) {
+    // cut off array after MAX_BINS
+    const mostActiveDaysBin = groups.splice(MAX_BINS - 1);
+    // get last value of the shortened group-array, as it is the largest value
+    // to be shown separately. The bulked group will be labeled as bigger than
+    // this value
+    const {value} = groups[groups.length - 1];
+
+    const count = mostActiveDaysBin.reduce((prev, current) =>
+      prev + parse_float(current.count), 0);
+    const reducedMostActiveDaysBin = {
+      value,
+      count,
+      bulked: true, // used as flag for special label
+    };
+
+    groups.push(reducedMostActiveDaysBin);
+  }
+
+  let colorCounter = 1;
+  const tdata = groups.map(group => {
+    const {bulked, count, value} = group;
+    const perc = percent(count, sum);
+    let label = '';
+    switch (value) {
+      case ACTIVE_YES_ALWAYS_VALUE:
+        label = _('Active (unlimited)');
+        break;
+      case ACTIVE_NO_VALUE:
+        label = _('Inactive');
+        break;
+      default:
+        if (group.bulked) {
+          label = _('Active for > {{value}} days', {value});
+        }
+        else {
+          label = _('Active for the next {{value}} days', {value});
+        }
+        break;
+    }
+    return {
+      value: count,
+      label,
+      bulked,
+      toolTip: `${label}: ${perc}% (${count})`,
+      color: activeDaysColorScale(colorCounter++),
+      filterValue: value,
+    };
+  });
+
+  tdata.total = sum;
+  return tdata;
+};
+
+class OverridesActiveDaysDisplay extends React.Component {
+
+  constructor(...args) {
+    super(...args);
+
+    this.handleDataClick = this.handleDataClick.bind(this);
+  }
+
+  handleDataClick(data) {
+    const {onFilterChanged, filter} = this.props;
+    const {filterValue, bulked = false} = data;
+
+    if (!is_defined(onFilterChanged)) {
+      return;
+    }
+
+    let activeDaysTerm;
+    if (bulked) {
+      activeDaysTerm =
+        FilterTerm.fromString(`active_days>"${filterValue}"`);
+    }
+    else {
+      activeDaysTerm =
+        FilterTerm.fromString(`active_days="${filterValue}"`);
+    }
+
+    if (is_defined(filter) && filter.hasTerm(activeDaysTerm)) {
+      return;
+    }
+    const activeDaysFilter = Filter.fromTerm(activeDaysTerm);
+
+    const newFilter = is_defined(filter) ? filter.copy().and(activeDaysFilter) :
+      activeDaysFilter;
+
+    onFilterChanged(newFilter);
+  }
+
+  render() {
+    const {
+      filter,
+      ...props
+    } = this.props;
+
+    return (
+      <OverridesActiveDaysLoader
+        filter={filter}
+      >
+        {loaderProps => (
+          <DataDisplay
+            {...props}
+            {...loaderProps}
+            dataTransform={transformActiveDaysData}
+            title={({data: tdata}) => _('Overrides by Active Days (Total: ' +
+              '{{count}})', {count: tdata.total})}
+          >
+            {({width, height, data: tdata, svgRef}) => (
+              <DonutChart
+                svgRef={svgRef}
+                data={tdata}
+                height={height}
+                width={width}
+                onDataClick={this.handleDataClick}
+              />
+            )}
+          </DataDisplay>
+        )}
+      </OverridesActiveDaysLoader>
+    );
+  }
+}
+
+OverridesActiveDaysDisplay.propTypes = {
+  filter: PropTypes.filter,
+  onFilterChanged: PropTypes.func.isRequired,
+};
+
+const DISPLAY_ID = 'override-by-active-days';
+
+OverridesActiveDaysDisplay.displayId = DISPLAY_ID;
+
+registerDisplay(DISPLAY_ID, OverridesActiveDaysDisplay, {
+  title: _('Overrides by Active Days'),
+});
+
+export default OverridesActiveDaysDisplay;
+
+// vim: set ts=2 sw=2 tw=80:
