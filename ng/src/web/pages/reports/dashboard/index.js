b469db5a104a45cd6ec21886b2821bbf707ce770
Björn Ricks <bjoern.ricks@greenbone.net>
2018-05-03 15:46:36 +0200

    Add reports high results table display

--- ng/src/web/pages/reports/dashboard/index.js
+++ ng/src/web/pages/reports/dashboard/index.js
@@ -29,7 +29,10 @@ import Dashboard from '../../../components/dashboard2/dashboard';
 
 import ReportsCvssDisplay from './cvssdisplay';
 import ReportsSeverityDisplay from './severityclassdisplay';
-import {ReportsHighResultsDisplay} from './highresultsdisplay';
+import {
+  ReportsHighResultsDisplay,
+  ReportsHighResultsTableDisplay,
+} from './highresultsdisplay';
 
 export const REPORTS_DASHBOARD_ID = 'e599bb6b-b95a-4bb2-a6bb-fe8ac69bc071';
 
@@ -44,6 +47,7 @@ const ReportsDashboard = ({
       ReportsCvssDisplay.displayId,
       ReportsHighResultsDisplay.displayId,
       ReportsSeverityDisplay.displayId,
+      ReportsHighResultsTableDisplay.displayId,
     ]}
     defaultContent={[
       [
