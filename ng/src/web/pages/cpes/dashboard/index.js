b25d809d95123d6f5438eaf1212b2d51dfeee9a4
Steffen Waterkamp <steffen.waterkamp@greenbone.net>
2018-05-04 16:13:41 +0200

    Add CPEs by Severity Class table to dashboard

--- ng/src/web/pages/cpes/dashboard/index.js
+++ ng/src/web/pages/cpes/dashboard/index.js
@@ -29,7 +29,7 @@ import Dashboard from '../../../components/dashboard2/dashboard';
 
 import {CpesCreatedDisplay, CpesCreatedTableDisplay} from './createddisplay';
 import {CpesCvssDisplay, CpesCvssTableDisplay} from './cvssdisplay';
-import CpesSeverityClassDisplay from './severityclassdisplay';
+import {CpesSeverityClassDisplay, CpesSeverityClassTableDisplay} from './severityclassdisplay'; // eslint-disable-line max-len
 
 export const CPES_DASHBOARD_ID = '9cff9b4d-b164-43ce-8687-f2360afc7500';
 
@@ -46,6 +46,7 @@ const CpesDashboard = ({
       CpesCvssDisplay.displayId,
       CpesCvssTableDisplay.displayId,
       CpesSeverityClassDisplay.displayId,
+      CpesSeverityClassTableDisplay.displayId,
     ]}
     defaultContent={[
       [
