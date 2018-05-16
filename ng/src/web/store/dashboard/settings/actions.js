cb1b1ea5f4ce64b405c35191c08a0c20ce6dcec8
Björn Ricks <bjoern.ricks@greenbone.net>
2018-05-16 16:31:51 +0200

    Import core-js for Object.entries

--- ng/src/web/store/dashboard/settings/actions.js
+++ ng/src/web/store/dashboard/settings/actions.js
@@ -20,6 +20,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+import 'core-js/fn/object/entries';
+
 import {is_defined, is_array} from 'gmp/utils/identity';
 
 import {createRow, createItem} from 'web/components/sortable/grid';
