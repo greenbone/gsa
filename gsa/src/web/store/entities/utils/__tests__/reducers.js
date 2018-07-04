f79f1379b20bc45ef3e14098cd0f872577ad9ec6
Björn Ricks <bjoern.ricks@greenbone.net>
2018-07-04 15:44:39 +0200

    Rearrange entities reducer tests
    Just reorder the tests and remove one description.

--- gsa/src/web/store/entities/utils/__tests__/reducers.js
+++ gsa/src/web/store/entities/utils/__tests__/reducers.js
@@ -29,379 +29,375 @@ import {createReducer, filterIdentifier} from '../reducers';
 
 describe('entities reducers test', () => {
 
-  describe('createReducers tests', () => {
+  test('should create a reducer function', () => {
+    const reducer = createReducer('foo');
 
-    test('should create a reducer function', () => {
-      const reducer = createReducer('foo');
+    expect(is_function(reducer)).toBe(true);
+  });
 
-      expect(is_function(reducer)).toBe(true);
+  test('Should create initial state', () => {
+    const reducer = createReducer('foo');
+
+    expect(reducer(undefined, {})).toEqual({
+      byId: {},
+      isLoading: {},
+      errors: {},
+      default: [],
     });
+  });
 
-    test('Should create initial state', () => {
+  test('should not override byId accidentially', () => {
+    const actions = createEntitiesActions('foo');
+    const reducer = createReducer('foo');
+    const filter = Filter.fromString('byId');
+    const filterId = filterIdentifier(filter);
+    const action = actions.success([{id: 'foo'}], filter);
+    const state = {
+      byId: {
+        bar: {
+          id: 'bar',
+        },
+      },
+      isLoading: {
+        default: true,
+      },
+      default: ['bar'],
+    };
+
+    expect(reducer(state, action)).toEqual({
+      byId: {
+        bar: {
+          id: 'bar',
+        },
+        foo: {
+          id: 'foo',
+        },
+      },
+      errors: {},
+      isLoading: {
+        default: true,
+        [filterId]: false,
+      },
+      default: ['bar'],
+      [filterId]: ['foo'],
+    });
+  });
+
+  test('should not override default accidentially', () => {
+    const actions = createEntitiesActions('foo');
+    const reducer = createReducer('foo');
+    const filter = Filter.fromString('default');
+    const filterId = filterIdentifier(filter);
+    const action = actions.success([{id: 'foo'}], filter);
+    const state = {
+      byId: {
+        bar: {
+          id: 'bar',
+        },
+      },
+      isLoading: {
+        default: true,
+      },
+      default: ['bar'],
+    };
+
+    expect(reducer(state, action)).toEqual({
+      byId: {
+        bar: {
+          id: 'bar',
+        },
+        foo: {
+          id: 'foo',
+        },
+      },
+      errors: {},
+      isLoading: {
+        default: true,
+        [filterId]: false,
+      },
+      default: ['bar'],
+      [filterId]: ['foo'],
+    });
+  });
+
+  describe('reducing entities loading request actions', () => {
+
+    test('should set isLoading with default filter', () => {
+      const actions = createEntitiesActions('foo');
+      const action = actions.request();
       const reducer = createReducer('foo');
 
-      expect(reducer(undefined, {})).toEqual({
+      expect(reducer(undefined, action)).toEqual({
         byId: {},
-        isLoading: {},
+        isLoading: {
+          default: true,
+        },
         errors: {},
         default: [],
       });
     });
 
-    describe('reducing entities loading request actions', () => {
-
-      test('should set isLoading with default filter', () => {
-        const actions = createEntitiesActions('foo');
-        const action = actions.request();
-        const reducer = createReducer('foo');
-
-        expect(reducer(undefined, action)).toEqual({
-          byId: {},
-          isLoading: {
-            default: true,
-          },
-          errors: {},
-          default: [],
-        });
-      });
-
-      test('should set isLoading for filter', () => {
-        const actions = createEntitiesActions('foo');
-        const reducer = createReducer('foo');
-        const filter = Filter.fromString('name=foo');
-        const filterId = filterIdentifier(filter);
-        const action = actions.request(filter);
-
-        expect(reducer(undefined, action)).toEqual({
-          byId: {},
-          isLoading: {
-            [filterId]: true,
-          },
-          errors: {},
-          default: [],
-          [filterId]: [],
-        });
-      });
-
-      test('should set isLoading and not override existing state', () => {
-        const actions = createEntitiesActions('foo');
-        const reducer = createReducer('foo');
-        const filter = Filter.fromString('name=foo');
-        const filterId = filterIdentifier(filter);
-        const otherFilter = Filter.fromString('name=bar');
-        const otherFilterId = filterIdentifier(otherFilter);
-        const action = actions.request(filter);
-        const state = {
-          isLoading: {
-            [otherFilterId]: false,
-          },
-          [otherFilterId]: [],
-        };
-
-        expect(reducer(state, action)).toEqual({
-          byId: {},
-          errors: {},
-          isLoading: {
-            [otherFilterId]: false,
-            [filterId]: true,
-          },
-          [otherFilterId]: [],
-          [filterId]: [],
-        });
-      });
-
-      test('should set isLoading and not override other properties', () => {
-        const actions = createEntitiesActions('foo');
-        const reducer = createReducer('foo');
-        const filter = Filter.fromString('name=foo');
-        const filterId = filterIdentifier(filter);
-        const action = actions.request(filter);
-        const state = {
-          errors: {
-            [filterId]: 'An Error',
-          },
-          isLoading: {
-            [filterId]: false,
-          },
-          [filterId]: ['foo', 'bar'],
-        };
-
-        expect(reducer(state, action)).toEqual({
-          byId: {},
-          errors: {
-            [filterId]: 'An Error',
-          },
-          isLoading: {
-            [filterId]: true,
-          },
-          [filterId]: ['foo', 'bar'],
-        });
-      });
-
-    });
-
-    describe('reducing entities loading success actions', () => {
-
-      test('should set isLoading with default filter', () => {
-        const actions = createEntitiesActions('foo');
-        const reducer = createReducer('foo');
-        const action = actions.success([{id: 'foo'}, {id: 'bar'}]);
-
-        expect(reducer(undefined, action)).toEqual({
-          byId: {
-            foo: {
-              id: 'foo',
-            },
-            bar: {
-              id: 'bar',
-            },
-          },
-          errors: {},
-          isLoading: {
-            default: false,
-          },
-          default: ['foo', 'bar'],
-        });
-      });
-
-      test('should reset other properties with default filter', () => {
-        const actions = createEntitiesActions('foo');
-        const reducer = createReducer('foo');
-        const action = actions.success([{id: 'foo'}, {id: 'bar'}]);
-        const state = {
-          errors: {
-            default: 'An error',
-          },
-          isLoading: {
-            default: true,
-          },
-          default: [],
-        };
-
-        expect(reducer(state, action)).toEqual({
-          byId: {
-            foo: {
-              id: 'foo',
-            },
-            bar: {
-              id: 'bar',
-            },
-          },
-          errors: {},
-          isLoading: {
-            default: false,
-          },
-          default: ['foo', 'bar'],
-        });
-      });
-
-      test('should not override other filters', () => {
-        const actions = createEntitiesActions('foo');
-        const reducer = createReducer('foo');
-        const filter = Filter.fromString('name=bar');
-        const filterId = filterIdentifier(filter);
-        const otherFilter = Filter.fromString('name=foo');
-        const otherFilterId = filterIdentifier(otherFilter);
-        const action = actions.success([{id: 'foo'}, {id: 'bar'}], filter);
-        const state = {
-          errors: {
-            [otherFilterId]: 'An error',
-          },
-          isLoading: {
-            [otherFilterId]: true,
-          },
-          [otherFilterId]: ['lorem', 'ipsum'],
-        };
-
-        expect(reducer(state, action)).toEqual({
-          byId: {
-            foo: {
-              id: 'foo',
-            },
-            bar: {
-              id: 'bar',
-            },
-          },
-          errors: {
-            [otherFilterId]: 'An error',
-          },
-          isLoading: {
-            [otherFilterId]: true,
-            [filterId]: false,
-          },
-          [otherFilterId]: ['lorem', 'ipsum'],
-          [filterId]: ['foo', 'bar'],
-        });
-      });
-
-    });
-
-    describe('reducing entities loading error actions', () => {
-
-      test('should set isLoading and error with default filter', () => {
-        const actions = createEntitiesActions('foo');
-        const reducer = createReducer('foo');
-        const action = actions.error('An error');
-
-        expect(reducer(undefined, action)).toEqual({
-          byId: {},
-          errors: {
-            default: 'An error',
-          },
-          isLoading: {
-            default: false,
-          },
-          default: [],
-        });
-      });
-
-      test('should reset isLoading and error with default filter', () => {
-        const actions = createEntitiesActions('foo');
-        const reducer = createReducer('foo');
-        const action = actions.error('An error');
-        const state = {
-          errors: {
-            default: 'Another error',
-          },
-          isLoading: {
-            default: true,
-          },
-          default: ['foo', 'bar'],
-        };
-
-        expect(reducer(state, action)).toEqual({
-          byId: {},
-          errors: {
-            default: 'An error',
-          },
-          isLoading: {
-            default: false,
-          },
-          default: ['foo', 'bar'],
-        });
-      });
-
-      test('should not override other filters', () => {
-        const actions = createEntitiesActions('foo');
-        const reducer = createReducer('foo');
-        const filter = Filter.fromString('name=bar');
-        const filterId = filterIdentifier(filter);
-        const otherFilter = Filter.fromString('name=foo');
-        const otherFilterId = filterIdentifier(otherFilter);
-        const action = actions.error('An error', filter);
-        const state = {
-          byId: {
-            lorem: {
-              id: 'lorem',
-            },
-            ipsum: {
-              id: 'ipsum',
-            },
-          },
-          errors: {
-            [otherFilterId]: 'Another error',
-          },
-          isLoading: {
-            [otherFilterId]: true,
-          },
-          [otherFilterId]: ['lorem', 'ipsum'],
-        };
-
-        expect(reducer(state, action)).toEqual({
-          byId: {
-            lorem: {
-              id: 'lorem',
-            },
-            ipsum: {
-              id: 'ipsum',
-            },
-          },
-          errors: {
-            [otherFilterId]: 'Another error',
-            [filterId]: 'An error',
-          },
-          isLoading: {
-            [otherFilterId]: true,
-            [filterId]: false,
-          },
-          [otherFilterId]: ['lorem', 'ipsum'],
-          [filterId]: [],
-        });
-      });
-
-    });
-
-    test('should not override byId accidentially', () => {
+    test('should set isLoading for filter', () => {
       const actions = createEntitiesActions('foo');
       const reducer = createReducer('foo');
-      const filter = Filter.fromString('byId');
+      const filter = Filter.fromString('name=foo');
       const filterId = filterIdentifier(filter);
-      const action = actions.success([{id: 'foo'}], filter);
-      const state = {
-        byId: {
-          bar: {
-            id: 'bar',
-          },
-        },
+      const action = actions.request(filter);
+
+      expect(reducer(undefined, action)).toEqual({
+        byId: {},
         isLoading: {
-          default: true,
+          [filterId]: true,
         },
-        default: ['bar'],
+        errors: {},
+        default: [],
+        [filterId]: [],
+      });
+    });
+
+    test('should set isLoading and not override existing state', () => {
+      const actions = createEntitiesActions('foo');
+      const reducer = createReducer('foo');
+      const filter = Filter.fromString('name=foo');
+      const filterId = filterIdentifier(filter);
+      const otherFilter = Filter.fromString('name=bar');
+      const otherFilterId = filterIdentifier(otherFilter);
+      const action = actions.request(filter);
+      const state = {
+        isLoading: {
+          [otherFilterId]: false,
+        },
+        [otherFilterId]: [],
       };
 
       expect(reducer(state, action)).toEqual({
+        byId: {},
+        errors: {},
+        isLoading: {
+          [otherFilterId]: false,
+          [filterId]: true,
+        },
+        [otherFilterId]: [],
+        [filterId]: [],
+      });
+    });
+
+    test('should set isLoading and not override other properties', () => {
+      const actions = createEntitiesActions('foo');
+      const reducer = createReducer('foo');
+      const filter = Filter.fromString('name=foo');
+      const filterId = filterIdentifier(filter);
+      const action = actions.request(filter);
+      const state = {
+        errors: {
+          [filterId]: 'An Error',
+        },
+        isLoading: {
+          [filterId]: false,
+        },
+        [filterId]: ['foo', 'bar'],
+      };
+
+      expect(reducer(state, action)).toEqual({
+        byId: {},
+        errors: {
+          [filterId]: 'An Error',
+        },
+        isLoading: {
+          [filterId]: true,
+        },
+        [filterId]: ['foo', 'bar'],
+      });
+    });
+
+  });
+
+  describe('reducing entities loading success actions', () => {
+
+    test('should set isLoading with default filter', () => {
+      const actions = createEntitiesActions('foo');
+      const reducer = createReducer('foo');
+      const action = actions.success([{id: 'foo'}, {id: 'bar'}]);
+
+      expect(reducer(undefined, action)).toEqual({
         byId: {
-          bar: {
-            id: 'bar',
-          },
           foo: {
             id: 'foo',
+          },
+          bar: {
+            id: 'bar',
           },
         },
         errors: {},
         isLoading: {
-          default: true,
-          [filterId]: false,
+          default: false,
         },
-        default: ['bar'],
-        [filterId]: ['foo'],
+        default: ['foo', 'bar'],
       });
     });
 
-    test('should not override default accidentially', () => {
+    test('should reset other properties with default filter', () => {
       const actions = createEntitiesActions('foo');
       const reducer = createReducer('foo');
-      const filter = Filter.fromString('default');
-      const filterId = filterIdentifier(filter);
-      const action = actions.success([{id: 'foo'}], filter);
+      const action = actions.success([{id: 'foo'}, {id: 'bar'}]);
       const state = {
-        byId: {
-          bar: {
-            id: 'bar',
-          },
+        errors: {
+          default: 'An error',
         },
         isLoading: {
           default: true,
         },
-        default: ['bar'],
+        default: [],
       };
 
       expect(reducer(state, action)).toEqual({
         byId: {
-          bar: {
-            id: 'bar',
-          },
           foo: {
             id: 'foo',
+          },
+          bar: {
+            id: 'bar',
           },
         },
         errors: {},
         isLoading: {
-          default: true,
+          default: false,
+        },
+        default: ['foo', 'bar'],
+      });
+    });
+
+    test('should not override other filters', () => {
+      const actions = createEntitiesActions('foo');
+      const reducer = createReducer('foo');
+      const filter = Filter.fromString('name=bar');
+      const filterId = filterIdentifier(filter);
+      const otherFilter = Filter.fromString('name=foo');
+      const otherFilterId = filterIdentifier(otherFilter);
+      const action = actions.success([{id: 'foo'}, {id: 'bar'}], filter);
+      const state = {
+        errors: {
+          [otherFilterId]: 'An error',
+        },
+        isLoading: {
+          [otherFilterId]: true,
+        },
+        [otherFilterId]: ['lorem', 'ipsum'],
+      };
+
+      expect(reducer(state, action)).toEqual({
+        byId: {
+          foo: {
+            id: 'foo',
+          },
+          bar: {
+            id: 'bar',
+          },
+        },
+        errors: {
+          [otherFilterId]: 'An error',
+        },
+        isLoading: {
+          [otherFilterId]: true,
           [filterId]: false,
         },
-        default: ['bar'],
-        [filterId]: ['foo'],
+        [otherFilterId]: ['lorem', 'ipsum'],
+        [filterId]: ['foo', 'bar'],
+      });
+    });
+
+  });
+
+  describe('reducing entities loading error actions', () => {
+
+    test('should set isLoading and error with default filter', () => {
+      const actions = createEntitiesActions('foo');
+      const reducer = createReducer('foo');
+      const action = actions.error('An error');
+
+      expect(reducer(undefined, action)).toEqual({
+        byId: {},
+        errors: {
+          default: 'An error',
+        },
+        isLoading: {
+          default: false,
+        },
+        default: [],
+      });
+    });
+
+    test('should reset isLoading and error with default filter', () => {
+      const actions = createEntitiesActions('foo');
+      const reducer = createReducer('foo');
+      const action = actions.error('An error');
+      const state = {
+        errors: {
+          default: 'Another error',
+        },
+        isLoading: {
+          default: true,
+        },
+        default: ['foo', 'bar'],
+      };
+
+      expect(reducer(state, action)).toEqual({
+        byId: {},
+        errors: {
+          default: 'An error',
+        },
+        isLoading: {
+          default: false,
+        },
+        default: ['foo', 'bar'],
+      });
+    });
+
+    test('should not override other filters', () => {
+      const actions = createEntitiesActions('foo');
+      const reducer = createReducer('foo');
+      const filter = Filter.fromString('name=bar');
+      const filterId = filterIdentifier(filter);
+      const otherFilter = Filter.fromString('name=foo');
+      const otherFilterId = filterIdentifier(otherFilter);
+      const action = actions.error('An error', filter);
+      const state = {
+        byId: {
+          lorem: {
+            id: 'lorem',
+          },
+          ipsum: {
+            id: 'ipsum',
+          },
+        },
+        errors: {
+          [otherFilterId]: 'Another error',
+        },
+        isLoading: {
+          [otherFilterId]: true,
+        },
+        [otherFilterId]: ['lorem', 'ipsum'],
+      };
+
+      expect(reducer(state, action)).toEqual({
+        byId: {
+          lorem: {
+            id: 'lorem',
+          },
+          ipsum: {
+            id: 'ipsum',
+          },
+        },
+        errors: {
+          [otherFilterId]: 'Another error',
+          [filterId]: 'An error',
+        },
+        isLoading: {
+          [otherFilterId]: true,
+          [filterId]: false,
+        },
+        [otherFilterId]: ['lorem', 'ipsum'],
+        [filterId]: [],
       });
     });
 
