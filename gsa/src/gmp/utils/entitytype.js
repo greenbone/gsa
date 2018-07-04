eef21acbc4085fb2642422d49f3e0e6249e015f9
Björn Ricks <bjoern.ricks@greenbone.net>
2018-07-04 11:45:24 +0200

    Add a typeName function entitytype utils module
    The function returns a translated name of the entity type.

--- gsa/src/gmp/utils/entitytype.js
+++ gsa/src/gmp/utils/entitytype.js
@@ -20,6 +20,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+import _ from '../locale';
+
 import {is_defined} from './identity';
 
 export const getEntityType = (model = {}) => {
@@ -48,6 +50,80 @@ const types = {
 export const normalizeType = type => {
   const ctype = types[type];
   return is_defined(ctype) ? ctype : type;
+};
+
+export const typeName = type => {
+  type = normalizeType(type);
+  switch (type) {
+    case 'agent':
+      return _('Agent');
+    case 'alert':
+      return _('Alert');
+    case 'allinfo':
+      return _('All SecInfo');
+    case 'asset':
+      return _('Asset');
+    case 'cpe':
+      return _('CPE');
+    case 'cve':
+      return _('CVE');
+    case 'credential':
+      return _('Credential');
+    case 'certbund':
+      return _('CERT-Bund Advisory');
+    case 'dfncert':
+      return _('DFN-CERT Advisory');
+    case 'filter':
+      return _('Filter');
+    case 'group':
+      return _('Group');
+    case 'host':
+      return _('Host');
+    case 'info':
+      return _('SecInfo');
+    case 'operatingsystem':
+      return _('Operating System');
+    case 'ovaldef':
+      return _('OVAL Definition');
+    case 'note':
+      return _('Note');
+    case 'nvt':
+      return _('NVT');
+    case 'override':
+      return _('Override');
+    case 'permission':
+      return _('Permission');
+    case 'portlist':
+      return _('Port List');
+    case 'portrange':
+      return _('Port Range');
+    case 'report':
+      return _('Report');
+    case 'reportformat':
+      return _('Report Format');
+    case 'result':
+      return _('Result');
+    case 'role':
+      return _('Role');
+    case 'scanconfig':
+      return _('Scan Config');
+    case 'scanner':
+      return _('Scanner');
+    case 'schedule':
+      return _('Schedule');
+    case 'tag':
+      return _('Tag');
+    case 'target':
+      return _('Target');
+    case 'task':
+      return _('Task');
+    case 'user':
+      return _('User');
+    case 'vuln':
+      return _('Vulnerability');
+    default:
+      return type;
+  }
 };
 
 // vim: set ts=2 sw=2 tw=80:
