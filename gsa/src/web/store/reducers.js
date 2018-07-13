88b4497ecaebdc2080711bb2a3cb00a7578bab32
Björn Ricks <bjoern.ricks@greenbone.net>
2018-07-13 13:26:18 +0200

    Update the root reducer to clear the store
    Change the current state to be empty if the clear store action is
    dispatched. This will pass the empty state to all reducers and therefore
    re-initalize the complete state to the defaults from the reducers.

--- gsa/src/web/store/reducers.js
+++ gsa/src/web/store/reducers.js
@@ -26,6 +26,7 @@ import dashboardData from './dashboard/data/reducers';
 import dashboardSettings from './dashboard/settings/reducers';
 
 import entities from './entities/reducers';
+import {CLEAR_STORE} from 'web/store/actions';
 
 const rootReducer = combineReducers({
   dashboardData,
@@ -33,6 +34,13 @@ const rootReducer = combineReducers({
   entities,
 });
 
-export default rootReducer;
+const clearStoreReducer = (state = {}, action) => {
+  if (action.type === CLEAR_STORE) {
+    state = {};
+  }
+  return rootReducer(state, action);
+};
+
+export default clearStoreReducer;
 
 // vim: set ts=2 sw=2 tw=80:
