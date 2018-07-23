c0325fec152ae2d1e38f1061a57fd9f1f19e9462
Björn Ricks <bjoern.ricks@greenbone.net>
2018-07-23 15:26:48 +0200

    Add own combineReducers function
    Allow to return undefined from to be combined reducers. It's not really
    obvious why returning undefined is not allowed for the combinedRedcuers
    function from redux.

---
+++ gsa/src/web/store/utils.js
@@ -0,0 +1,47 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2018 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import 'core-js/fn/object/entries';
+
+/**
+ * A combineReducers version to allow to return undefined for a state.
+ *
+ * @param {Object} reducers An object containing reducer functions.
+ *
+ * @returns {Function} A new combined reducer function
+ */
+export const combineReducers = reducers => (state = {}, action) => {
+  let hasChanged = false;
+  const nextState = {};
+
+  for (const [name, reducer] of Object.entries(reducers)) {
+    const prevStateForReducer = state[name];
+    const nextStateForReducers = reducer(prevStateForReducer, action);
+
+    nextState[name] = nextStateForReducers;
+    hasChanged = hasChanged || prevStateForReducer !== nextStateForReducers;
+  }
+
+  return hasChanged ? nextState : state;
+};
+
+// vim: set ts=2 sw=2 tw=80:
