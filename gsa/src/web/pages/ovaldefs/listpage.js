25abc60913103f33419434e8311c749f73a7eb04
Björn Ricks <bjoern.ricks@greenbone.net>
2018-07-26 16:09:14 +0200

    Adjust imports for ovaldefs list page

--- gsa/src/web/pages/ovaldefs/listpage.js
+++ gsa/src/web/pages/ovaldefs/listpage.js
@@ -27,17 +27,17 @@ import _ from 'gmp/locale';
 
 import {OVALDEFS_FILTER_FILTER} from 'gmp/models/filter';
 
-import EntitiesPage from '../../entities/page.js';
-import withEntitiesContainer from '../../entities/withEntitiesContainer.js';
+import EntitiesPage from 'web/entities/page';
+import withEntitiesContainer from 'web/entities/withEntitiesContainer';
 
-import DashboardControls from '../../components/dashboard/controls';
+import DashboardControls from 'web/components/dashboard/controls';
 
-import ManualIcon from '../../components/icon/manualicon.js';
+import ManualIcon from 'web/components/icon/manualicon';
 
-import OvaldefFilterDialog from './filterdialog.js';
-import OvaldefsTable from './table.js';
+import OvaldefFilterDialog from './filterdialog';
+import OvaldefsTable from './table';
 
-import OvaldefDashboard, {OVALDEF_DASHBOARD_ID} from './dashboard/index.js';
+import OvaldefDashboard, {OVALDEF_DASHBOARD_ID} from './dashboard';
 
 
 const ToolBarIcons = props => {
