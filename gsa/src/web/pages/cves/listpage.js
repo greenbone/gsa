08d0c6969e7d9db2ffbc4fea920d0244131e33ac
Björn Ricks <bjoern.ricks@greenbone.net>
2018-07-26 14:49:30 +0200

    Load cves from the redux store at cves list page

--- gsa/src/web/pages/cves/listpage.js
+++ gsa/src/web/pages/cves/listpage.js
@@ -28,11 +28,16 @@ import _ from 'gmp/locale';
 import {CVES_FILTER_FILTER} from 'gmp/models/filter';
 
 import EntitiesPage from 'web/entities/page';
-import withEntitiesContainer from 'web/entities/withEntitiesContainer';
+import withEntitiesContainer from 'web/entities/withEntitiesContainer2';
 
 import DashboardControls from 'web/components/dashboard/controls';
 
 import ManualIcon from 'web/components/icon/manualicon';
+
+import {
+  loadEntities,
+  selector as entitiesSelector,
+} from 'web/store/entities/cves';
 
 import CveFilterDialog from './filterdialog';
 import CvesTable from './table';
@@ -64,7 +69,9 @@ const Page = props => (
 );
 
 export default withEntitiesContainer('cve', {
+  entitiesSelector,
   filtersFilter: CVES_FILTER_FILTER,
+  loadEntities,
 })(Page);
 
 // vim: set ts=2 sw=2 tw=80:
