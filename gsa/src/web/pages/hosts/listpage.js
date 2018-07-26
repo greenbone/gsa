36fc1210593f249e658ecaf317a3a403ca752c07
Björn Ricks <bjoern.ricks@greenbone.net>
2018-07-26 15:01:50 +0200

    Adjust imports at host list page

--- gsa/src/web/pages/hosts/listpage.js
+++ gsa/src/web/pages/hosts/listpage.js
@@ -27,27 +27,26 @@ import _ from 'gmp/locale';
 
 import {HOSTS_FILTER_FILTER} from 'gmp/models/filter';
 
-import IconDivider from '../../components/layout/icondivider.js';
+import IconDivider from 'web/components/layout/icondivider';
 
-import PropTypes from '../../utils/proptypes.js';
-import withCapabilities from '../../utils/withCapabilities';
+import PropTypes from 'web/utils/proptypes';
+import withCapabilities from 'web/utils/withCapabilities';
 
-import EntitiesPage from '../../entities/page.js';
-import withEntitiesContainer from '../../entities/withEntitiesContainer.js';
+import EntitiesPage from 'web/entities/page';
+import withEntitiesContainer from 'web/entities/withEntitiesContainer';
 
-import {goto_details} from '../../entity/component.js';
+import {goto_details} from 'web/entity/component';
 
-import DashboardControls from '../../components/dashboard/controls';
+import DashboardControls from 'web/components/dashboard/controls';
 
-import ManualIcon from '../../components/icon/manualicon.js';
-import NewIcon from '../../components/icon/newicon.js';
+import ManualIcon from 'web/components/icon/manualicon';
+import NewIcon from 'web/components/icon/newicon';
 
-import HostsFilterDialog from './filterdialog.js';
-import HostsTable from './table.js';
-import HostComponent from './component.js';
+import HostsFilterDialog from './filterdialog';
+import HostsTable from './table';
+import HostComponent from './component';
 
 import HostsDashboard, {HOSTS_DASHBOARD_ID} from './dashboard';
-
 
 const ToolBarIcons = withCapabilities(({
   capabilities,
