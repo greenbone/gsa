77810b7a354e32ce5737b73fc85c703fdf9278e7
Björn Ricks <bjoern.ricks@greenbone.net>
2018-07-26 08:33:10 +0200

    Use an interval of five seconds for the autorefresh interval
    By default use an autorefresh interval of five seconds now. Two seconds
    is a way to fast when some pages need 200 ms and more for rendering.

--- gsa/public/config.js
+++ gsa/public/config.js
@@ -1,6 +1,8 @@
-window.config = {
+const FIVE_SECONDS = 5;
+
+config = {
   loglevel: 'warn',
-  autorefresh: 2,
+  autorefresh: FIVE_SECONDS,
   manualurl: 'http://docs.greenbone.net/GSM-Manual/gos-4/',
   protocoldocurl: 'http://docs.greenbone.net/API/OMP/omp-7.0.html',
 };
