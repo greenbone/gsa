51215afdba3ec4a0924f03f046bebf4ea0a393e8
Steffen Waterkamp <steffen.waterkamp@greenbone.net>
2018-07-31 13:00:31 +0200

    Update imports

--- gsa/src/web/pages/alerts/row.js
+++ gsa/src/web/pages/alerts/row.js
@@ -26,32 +26,32 @@ import _ from 'gmp/locale';
 
 import {isDefined} from 'gmp/utils/identity';
 
-import compose from '../../utils/compose.js';
-import withCapabilities from '../../utils/withCapabilities.js';
-import PropTypes from '../../utils/proptypes.js';
-import {renderComponent, renderYesNo} from '../../utils/render.js';
+import compose from 'web/utils/compose';
+import PropTypes from 'web/utils/proptypes';
+import {renderComponent, renderYesNo} from 'web/utils/render';
+import withCapabilities from 'web/utils/withCapabilities';
 
-import EntityNameTableData from '../../entities/entitynametabledata.js';
-import {withEntityActions} from '../../entities/actions.js';
-import {withEntityRow} from '../../entities/row.js';
+import EntityNameTableData from 'web/entities/entitynametabledata';
+import {withEntityActions} from 'web/entities/actions';
+import {withEntityRow} from 'web/entities/row';
 
-import CloneIcon from '../../entity/icon/cloneicon.js';
-import EditIcon from '../../entity/icon/editicon.js';
-import TrashIcon from '../../entity/icon/trashicon.js';
+import CloneIcon from 'web/entity/icon/cloneicon';
+import EditIcon from 'web/entity/icon/editicon';
+import TrashIcon from 'web/entity/icon/trashicon';
 
-import ExportIcon from '../../components/icon/exporticon.js';
-import Icon from '../../components/icon/icon.js';
+import ExportIcon from 'web/components/icon/exporticon';
+import Icon from 'web/components/icon/icon';
 
-import IconDivider from '../../components/layout/icondivider.js';
+import IconDivider from 'web/components/layout/icondivider';
 
-import DetailsLink from '../../components/link/detailslink.js';
+import DetailsLink from 'web/components/link/detailslink';
 
-import TableData from '../../components/table/data.js';
-import TableRow from '../../components/table/row.js';
+import TableData from 'web/components/table/data';
+import TableRow from 'web/components/table/row';
 
-import Condition from './condition.js';
-import Event from './event.js';
-import Method from './method.js';
+import Condition from './condition';
+import Event from './event';
+import Method from './method';
 
 const Actions = ({
   entity,
