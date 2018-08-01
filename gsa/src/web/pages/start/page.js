acae9f8b2b10a78fc58b5dd343fb0ef4f83c5aaf
Björn Ricks <bjoern.ricks@greenbone.net>
2018-08-01 16:32:06 +0200

    Don't raise a proptype warning about missing isLoading
    For the StartPage dashboards isLoading is always false.

--- gsa/src/web/pages/start/page.js
+++ gsa/src/web/pages/start/page.js
@@ -391,6 +391,7 @@ class StartPage extends React.Component {
                         <Dashboard
                           {...settings}
                           id={id}
+                          isLoading={false}
                           items={rows}
                           loadSettings={this.handleLoadDashboardSettings}
                           saveSettings={this.handleSaveDashboardSettings}
