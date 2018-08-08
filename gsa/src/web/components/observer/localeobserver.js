b1fba47e7a9f7fecac1f3dbb3f722f4ad6419f54
Björn Ricks <bjoern.ricks@greenbone.net>
2018-08-08 14:35:03 +0200

    Add a LocaleObserver component
    The component is responsible for setting the locale in the redux store.
    Also it ensures to re-render its children if the locale has changed (via
    changed key prop see https://reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html#recommendation-fully-uncontrolled-component-with-a-key)

---
+++ gsa/src/web/components/observer/localeobserver.js
@@ -0,0 +1,85 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2016 - 2018 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import React from 'react';
+
+import {connect} from 'react-redux';
+
+import {onLanguageChange} from 'gmp/locale/lang';
+
+import {isDefined} from 'gmp/utils/identity';
+
+import PropTypes from 'web/utils/proptypes';
+
+import {setLocale} from 'web/store/usersettings/actions';
+
+class LocaleObserver extends React.Component {
+
+  constructor(...args) {
+    super(...args);
+
+    this.state = {};
+
+    this.handleLanguageChange = this.handleLanguageChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.unsubscribeFromLanguageChange = onLanguageChange(
+      this.handleLanguageChange);
+  }
+
+  componentWillUnmount() {
+    if (isDefined(this.unsubscribeFromLanguageChange)) {
+      this.unsubscribeFromLanguageChange();
+    }
+  }
+
+  handleLanguageChange(locale) {
+    this.props.setLocale(locale);
+
+    this.setState({locale});
+  }
+
+  render() {
+    const {children} = this.props;
+    const {locale} = this.state;
+
+    if (!isDefined(locale)) {
+      // don't render if no locale has been detected yet
+      return null;
+    }
+
+    return (
+      <React.Fragment key={locale}>
+        {children}
+      </React.Fragment>
+    );
+  }
+}
+
+LocaleObserver.propTypes = {
+  setLocale: PropTypes.func.isRequired,
+};
+
+export default connect(undefined, {
+  setLocale,
+})(LocaleObserver);
