1c338addbac5c5583096c70d09829ee183898a7a
Björn Ricks <bjoern.ricks@greenbone.net>
2018-09-03 14:56:25 +0200

    Add new gmp utils module for formatting numbers

---
+++ gsa/src/gmp/utils/__tests__/number.js
@@ -0,0 +1,56 @@
+/* Greenbone Security Assistant
+ *
+ * Authors:
+ * Björn Ricks <bjoern.ricks@greenbone.net>
+ *
+ * Copyright:
+ * Copyright (C) 2018 Greenbone Networks GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import {severityValue, fixedValue} from 'gmp/utils/number';
+
+describe('severityValue function tests', () => {
+
+  test('should convert numbers to severity', () => {
+    expect(severityValue(0)).toEqual('0.0');
+    expect(severityValue(1)).toEqual('1.0');
+    expect(severityValue(1.0)).toEqual('1.0');
+    expect(severityValue(1.1)).toEqual('1.1');
+    expect(severityValue(1.10)).toEqual('1.1');
+    expect(severityValue(1.15)).toEqual('1.1');
+    expect(severityValue(1.16)).toEqual('1.2');
+    expect(severityValue(1.19)).toEqual('1.2');
+  });
+
+});
+
+describe('fixedValue function tests', () => {
+
+  test('should convert numbers to fixed values', () => {
+    const num = 12345.6789;
+
+    expect(fixedValue(num)).toEqual('12345.6789');
+    expect(fixedValue(num, 1)).toEqual('12345.7');
+    expect(fixedValue(num, 6)).toEqual('12345.678900');
+
+    expect(fixedValue(2.34, 1)).toEqual('2.3');
+    expect(fixedValue(2.35, 1)).toEqual('2.4');
+    expect(fixedValue(2.55, 1)).toEqual('2.5');
+  });
+
+});
+
+// vim: set ts=2 sw=2 tw=80:
