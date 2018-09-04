e36aa7f6ef18ba4897f7cd00bb155b10e8a21d5f
Björn Ricks <bjoern.ricks@greenbone.net>
2018-09-04 08:10:53 +0200

    Add new ping gmp command
    The ping command just responses an action result. It's intended to test
    if the user session is still valid. It doesn't extend the session. If
    the session is due a 401 response is returned by gsad.

--- gsad/src/gsad_gmp.c
+++ gsad/src/gsad_gmp.c
@@ -23854,6 +23854,23 @@ renew_session_gmp (gvm_connection_t *connection, credentials_t * credentials,
   return html;
 }
 
+/**
+ * @brief Get assets, envelope the result.
+ *
+ * @param[in]  connection     Connection to manager.
+ * @param[in]  credentials  Username and password for authentication.
+ * @param[in]  params       Request parameters.
+ * @param[out] response_data  Extra data return for the HTTP response.
+ *
+ * @return Enveloped XML object.
+ */
+char *
+ping_gmp (gvm_connection_t *connection, credentials_t * credentials,
+          params_t *params, cmd_response_data_t* response_data)
+{
+  return action_result (connection, credentials, params, response_data,
+                        "ping", "pong", NULL, NULL);
+}
 
 /* Manager communication. */
 
