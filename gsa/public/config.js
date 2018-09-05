38d00e31e7c501d6ca18ce02a10ec8fc9567e020
Björn Ricks <bjoern.ricks@greenbone.net>
2018-09-05 14:30:20 +0200

    Reduce default reload interval to 15 seconds
    By default only reload a page every 15 seconds without user input. This
    reduces the work load at gsad and gvmd.
    
    If the reload interval should be reduced the reloadInterval function
    should be set at the corresponding page instead of lowering the default
    now.

--- gsa/public/config.js
+++ gsa/public/config.js
@@ -1,8 +1,8 @@
-const FIVE_SECONDS = 5;
+const FIFTEEN_SECONDS = 15;
 
 config = {
   loglevel: 'warn',
-  autorefresh: FIVE_SECONDS,
+  autorefresh: FIFTEEN_SECONDS,
   manualurl: 'http://docs.greenbone.net/GSM-Manual/gos-4/',
   protocoldocurl: 'http://docs.greenbone.net/API/OMP/omp-7.0.html',
 };
