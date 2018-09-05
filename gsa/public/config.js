d65d2b61e3e0ec2a7c4cc79b36d57f842d3f954c
Björn Ricks <bjoern.ricks@greenbone.net>
2018-09-05 15:20:41 +0200

    Remove obsolete autorefresh from config file

--- gsa/public/config.js
+++ gsa/public/config.js
@@ -1,8 +1,5 @@
-const FIFTEEN_SECONDS = 15;
-
 config = {
   loglevel: 'warn',
-  autorefresh: FIFTEEN_SECONDS,
   manualurl: 'http://docs.greenbone.net/GSM-Manual/gos-4/',
   protocoldocurl: 'http://docs.greenbone.net/API/OMP/omp-7.0.html',
 };
