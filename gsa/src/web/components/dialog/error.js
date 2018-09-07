4e242a901a54a8820bdd75943dcfd683017c7142
Steffen Waterkamp <steffen.waterkamp@greenbone.net>
2018-09-07 17:05:17 +0200

    Use styled-components for DialogError

--- gsa/src/web/components/dialog/error.js
+++ gsa/src/web/components/dialog/error.js
@@ -23,7 +23,7 @@
  */
 import React from 'react';
 
-import glamorous from 'glamorous';
+import styled from 'styled-components';
 
 import _ from 'gmp/locale';
 
@@ -37,27 +37,27 @@ import Layout from '../layout/layout.js';
 
 import CloseButton from './closebutton.js';
 
-const StyledLayout = glamorous(Layout)({
-  padding: '15px',
-  margin: '20px 1em',
-  border: '1px solid ' + Theme.mediumLightRed,
-  borderRadius: '4px',
-  color: Theme.darkRed,
-  backgroundColor: Theme.lightRed,
-});
+const StyledLayout = styled(Layout)`
+  padding: 15px;
+  margin: 20px 1em;
+  border: 1px solid ${Theme.mediumLightRed};
+  border-radius: 4px;
+  color: ${Theme.darkRed};
+  background-color: ${Theme.lightRed};
+`;
 
-const DialogCloseButton = glamorous(CloseButton)({
-  border: '1px solid ' + Theme.lightRed,
-  background: '0',
-  color: Theme.darkRed,
+const DialogCloseButton = styled(CloseButton)`
+  border: 1px solid ${Theme.lightRed};
+  background: 0;
+  color: ${Theme.darkRed};
 
-  ':hover': {
-    border: '1px solid ' + Theme.darkRed,
-    background: '0',
-    color: Theme.black,
-    opacity: '.5',
-  },
-});
+  :hover {
+    border: 1px solid ${Theme.darkRed};
+    background: 0;
+    color: ${Theme.black};
+    opacity: .5;
+  };
+`;
 
 const DialogError = ({error, onCloseClick}) => {
   if (!isDefined(error)) {
