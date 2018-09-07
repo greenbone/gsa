0e11323e2555a76a56f4387d15e5b3b982de85b6
Björn Ricks <bjoern.ricks@greenbone.net>
2018-09-07 12:25:33 +0200

    Add actions, selector, reducer and load function for delta reports
    Allow to handle delta reports in the redux store.

--- gsa/src/web/store/entities/__tests__/reports.js
+++ gsa/src/web/store/entities/__tests__/reports.js
@@ -225,4 +225,304 @@ describe('report loadEntity function tests', () => {
   });
 });
 
+testEntityActions('deltaReport', deltaEntityActions);
+testReducerForEntity('deltaReport', deltaReducer, deltaEntityActions);
+
+describe('delta report loadDeltaReport function tests', () => {
+
+  test('should load delta report successfully', () => {
+    const id = 'a1';
+    const deltaId = 'a2';
+    const identifier = `${id}+${deltaId}`;
+
+    const rootState = createState('deltaReport', {
+      isLoading: {
+        [identifier]: false,
+      },
+    });
+    const getState = jest
+      .fn()
+      .mockReturnValue(rootState);
+
+    const dispatch = jest.fn();
+
+    const getDelta = jest
+      .fn()
+      .mockReturnValue(Promise.resolve({
+        data: {foo: 'bar'},
+      }));
+
+    const gmp = {
+      [entityType]: {
+        getDelta,
+      },
+    };
+
+    expect(loadDeltaReport).toBeDefined();
+    expect(isFunction(loadDeltaReport)).toBe(true);
+
+    return loadDeltaReport(gmp)(id, deltaId)(dispatch, getState).then(() => {
+      expect(getState).toBeCalled();
+      expect(getDelta).toBeCalledWith({id}, {id: deltaId}, {filter: undefined});
+      expect(dispatch).toHaveBeenCalledTimes(2);
+      expect(dispatch.mock.calls[0]).toEqual([{
+        type: types.ENTITY_LOADING_REQUEST,
+        entityType: 'deltaReport',
+        id: identifier,
+      }]);
+      expect(dispatch.mock.calls[1]).toEqual([{
+        type: types.ENTITY_LOADING_SUCCESS,
+        entityType: 'deltaReport',
+        data: {foo: 'bar'},
+        id: identifier,
+      }]);
+    });
+  });
+
+  test('should load delta report with results filter successfully', () => {
+    const id = 'a1';
+    const deltaId = 'a2';
+    const identifier = `${id}+${deltaId}`;
+
+    const rootState = createState('deltaReport', {
+      isLoading: {
+        [identifier]: false,
+      },
+    });
+    const getState = jest
+      .fn()
+      .mockReturnValue(rootState);
+
+    const dispatch = jest.fn();
+
+    const getDelta = jest
+      .fn()
+      .mockReturnValue(Promise.resolve({
+        data: {foo: 'bar'},
+      }));
+
+    const gmp = {
+      [entityType]: {
+        getDelta,
+      },
+    };
+
+    const filter = Filter.fromString('foo=bar');
+
+    expect(loadDeltaReport).toBeDefined();
+    expect(isFunction(loadDeltaReport)).toBe(true);
+
+    return loadDeltaReport(gmp)(id, deltaId, filter)(dispatch, getState)
+      .then(() => {
+      expect(getState).toBeCalled();
+      expect(getDelta).toBeCalledWith({id}, {id: deltaId}, {filter});
+      expect(dispatch).toHaveBeenCalledTimes(2);
+      expect(dispatch.mock.calls[0]).toEqual([{
+        type: types.ENTITY_LOADING_REQUEST,
+        entityType: 'deltaReport',
+        id: identifier,
+      }]);
+      expect(dispatch.mock.calls[1]).toEqual([{
+        type: types.ENTITY_LOADING_SUCCESS,
+        entityType: 'deltaReport',
+        data: {foo: 'bar'},
+        id: identifier,
+      }]);
+    });
+  });
+
+  test('should not load delta report if isLoading is true', () => {
+    const id = 'a1';
+    const deltaId = 'a2';
+    const identifier = `${id}+${deltaId}`;
+    const rootState = createState('deltaReport', {
+      isLoading: {
+        [identifier]: true,
+      },
+    });
+
+    const getState = jest
+      .fn()
+      .mockReturnValue(rootState);
+
+    const dispatch = jest.fn();
+
+    const getDelta = jest
+      .fn()
+      .mockReturnValue(Promise.resolve([{id: 'foo'}]));
+
+    const gmp = {
+      [entityType]: {
+        getDelta,
+      },
+    };
+
+    return loadDeltaReport(gmp)(id, deltaId)(dispatch, getState).then(() => {
+      expect(getState).toBeCalled();
+      expect(dispatch).not.toBeCalled();
+      expect(getDelta).not.toBeCalled();
+    });
+  });
+
+  test('should fail loading delta report with an error', () => {
+    const id = 'a1';
+    const deltaId = 'a2';
+    const identifier = `${id}+${deltaId}`;
+    const rootState = createState('deltaReport', {
+      [identifier]: {
+        isLoading: false,
+      },
+    });
+
+    const getState = jest
+      .fn()
+      .mockReturnValue(rootState);
+
+    const dispatch = jest.fn();
+
+    const getDelta = jest
+      .fn()
+      .mockReturnValue(Promise.reject('An Error'));
+
+    const gmp = {
+      [entityType]: {
+        getDelta,
+      },
+    };
+
+    return loadDeltaReport(gmp)(id, deltaId)(dispatch, getState).then(() => {
+      expect(getState).toBeCalled();
+      expect(getDelta).toBeCalledWith({id}, {id: deltaId}, {filter: undefined});
+      expect(dispatch).toHaveBeenCalledTimes(2);
+      expect(dispatch.mock.calls[0]).toEqual([{
+        type: types.ENTITY_LOADING_REQUEST,
+        entityType: 'deltaReport',
+        id: identifier,
+      }]);
+      expect(dispatch.mock.calls[1]).toEqual([{
+        type: types.ENTITY_LOADING_ERROR,
+        entityType: 'deltaReport',
+        error: 'An Error',
+        id: identifier,
+      }]);
+    });
+  });
+});
+
+describe('deltaSelector isLoading tests', () => {
+
+  test('should be false for undefined state', () => {
+    const id = 'a1';
+    const deltaId = 'a2';
+    const rootState = createRootState({});
+    const selector = deltaSelector(rootState);
+
+    expect(selector.isLoading(id, deltaId)).toBe(false);
+  });
+
+  test('should be false for empty state', () => {
+    const id = 'a1';
+    const deltaId = 'a2';
+    const rootState = createState('deltaReport', {});
+    const selector = deltaSelector(rootState);
+
+    expect(selector.isLoading(id, deltaId)).toEqual(false);
+  });
+
+  test('should be false for unkown id', () => {
+    const id = 'a1';
+    const deltaId = 'a2';
+    const rootState = createState('deltaReport', {
+      isLoading: {
+        foo: true,
+      },
+    });
+    const selector = deltaSelector(rootState);
+
+    expect(selector.isLoading(id, deltaId)).toBe(false);
+  });
+
+  test('should be false if false in state', () => {
+    const id = 'a1';
+    const deltaId = 'a2';
+    const identifier = `${id}+${deltaId}`;
+    const rootState = createState('deltaId', {
+      isLoading: {
+        [identifier]: false,
+      },
+    });
+    const selector = deltaSelector(rootState);
+
+    expect(selector.isLoading(id, deltaId)).toBe(false);
+  });
+
+  test('should be true if true in state', () => {
+    const id = 'a1';
+    const deltaId = 'a2';
+    const identifier = `${id}+${deltaId}`;
+    const rootState = createState('deltaReport', {
+      isLoading: {
+        [identifier]: true,
+      },
+    });
+    const selector = deltaSelector(rootState);
+
+    expect(selector.isLoading(id, deltaId)).toBe(true);
+  });
+
+});
+
+describe('deltaSelector getEntity tests', () => {
+
+  test('should return undefined for empty state', () => {
+    const rootState = createRootState({});
+    const selector = deltaSelector(rootState);
+
+    expect(selector.getEntity('bar')).toBeUndefined();
+  });
+
+  test('should return undefined for empty byId', () => {
+    const rootState = createState('deltaReport', {
+      byId: {},
+    });
+    const selector = deltaSelector(rootState);
+
+    expect(selector.getEntity('bar')).toBeUndefined();
+  });
+
+  test('should return undefined for unkown id', () => {
+    const rootState = createState('deltaReport', {
+      byId: {
+        foo: {
+          id: 'foo',
+        },
+      },
+    });
+    const selector = deltaSelector(rootState);
+
+    expect(selector.getEntity('bar')).toBeUndefined();
+  });
+
+  test('should return entity data', () => {
+    const id = 'a1';
+    const deltaId = 'a2';
+    const identifier = `${id}+${deltaId}`;
+
+    const rootState = createState('deltaReport', {
+      byId: {
+        [identifier]: {
+          id: 'bar',
+          lorem: 'ipsum',
+        },
+      },
+    });
+    const selector = deltaSelector(rootState);
+
+    expect(selector.getEntity(id, deltaId)).toEqual({
+      id: 'bar',
+      lorem: 'ipsum',
+    });
+  });
+});
+
 // vim: set ts=2 sw=2 tw=80:
