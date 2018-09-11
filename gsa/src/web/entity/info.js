13f5ac8a86c1e926ceeddd2d1b5bc8a8ede10b24
Steffen Waterkamp <steffen.waterkamp@greenbone.net>
2018-09-11 14:15:24 +0200

    Use styled-components for EntityInfo

--- gsa/src/web/entity/info.js
+++ gsa/src/web/entity/info.js
@@ -23,7 +23,7 @@
 
 import React from 'react';
 
-import glamorous from 'glamorous';
+import styled from 'styled-components';
 
 import _ from 'gmp/locale';
 import {dateTimeWithTimeZone} from 'gmp/locale/date';
@@ -47,21 +47,18 @@ OwnerInfo.propTypes = {
   owner: PropTypes.object,
 };
 
-export const InfoLayout = glamorous(Layout)(
-  'entity-info',
-  {
-    borderSpacing: '0px',
-    color: Theme.mediumGray,
-    fontSize: '10px',
+export const InfoLayout = styled(Layout)`
+  border-spacing: 0px;
+  color: ${Theme.mediumGray};
+  font-size: 10px;
 
-    '& :nth-child(even)': {
-      marginLeft: '3px',
-    },
-    '& :nth-child(odd)': {
-      marginLeft: '30px',
-    },
-  },
-);
+  & :nth-child(even) {
+    margin-left: 3px;
+  };
+  & :nth-child(odd) {
+    margin-left: 30px;
+  };
+`;
 
 const EntityInfo = ({
   entity,
