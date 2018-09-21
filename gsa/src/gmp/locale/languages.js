4eb5747b7962d2f9dde42ed64957a94174f54cfe
Björn Ricks <bjoern.ricks@greenbone.net>
2018-09-21 13:41:00 +0200

    Add function to return all language codes

--- gsa/src/gmp/locale/languages.js
+++ gsa/src/gmp/locale/languages.js
@@ -20,7 +20,11 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+import 'core-js/fn/object/keys';
+
 export const BROWSER_LANGUAGE = 'Browser Language';
+
+export const getLanguageCodes = () => Object.keys(Languages);
 
 const Languages = {
   ar: {
