3132e82d9ef12cfed40cbc72a069968a6cc7bb2e
Björn Ricks <bjoern.ricks@greenbone.net>
2018-09-24 15:35:27 +0200

    Fix loading the filters for dashboard displays
    The api for loadEntities has changed.

--- gsa/src/web/components/dashboard/display/filterselection.js
+++ gsa/src/web/components/dashboard/display/filterselection.js
@@ -169,7 +169,7 @@ const mapStateToProps = (state, {filtersFilter}) => {
 };
 
 const mapDispatchToProps = (dispatch, {gmp, filtersFilter}) => ({
-  loadFilters: () => dispatch(loadEntities({gmp, filter: filtersFilter})),
+  loadFilters: () => dispatch(loadEntities(gmp)(filtersFilter)),
 });
 
 export default compose(
