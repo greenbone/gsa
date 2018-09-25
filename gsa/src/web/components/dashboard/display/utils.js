23944672b466ccb1f296c6ba1e761c3d9f4cd580
Steffen Waterkamp <steffen.waterkamp@greenbone.net>
2018-09-25 17:06:34 +0200

    Lazy translate QOD_TYPES for NVTs by Qod-Type display

--- gsa/src/web/components/dashboard/display/utils.js
+++ gsa/src/web/components/dashboard/display/utils.js
@@ -23,7 +23,7 @@
  */
 import 'core-js/fn/object/keys';
 
-import _ from 'gmp/locale';
+import {_, _l} from 'gmp/locale/lang';
 
 import {scaleOrdinal, scaleLinear} from 'd3-scale';
 
@@ -105,20 +105,20 @@ export const vulnsByHostsColorScale = scaleLinear()
   ]);
 
 export const QOD_TYPES = {
-  '': _('None'),
-  general_note: _('General note'),
-  executable_version: _('Executable version'),
-  package: _('Package check'),
-  registry: _('Registry check'),
-  remote_active: _('Remote active'),
-  remote_analysis: _('Remote analysis'),
-  remote_app: _('Remote app'),
-  remote_banner: _('Remote banner'),
-  remote_probe: _('Remote probe'),
-  remote_banner_unreliable: _('Unreliable rem. banner'),
-  executable_version_unreliable: _('Unreliable exec. version'),
-  remote_vul: _('Remote vulnerability'),
-  exploit: _('Exploit'),
+  '': _l('None'),
+  general_note: _l('General note'),
+  executable_version: _l('Executable version'),
+  package: _l('Package check'),
+  registry: _l('Registry check'),
+  remote_active: _l('Remote active'),
+  remote_analysis: _l('Remote analysis'),
+  remote_app: _l('Remote app'),
+  remote_banner: _l('Remote banner'),
+  remote_probe: _l('Remote probe'),
+  remote_banner_unreliable: _l('Unreliable rem. banner'),
+  executable_version_unreliable: _l('Unreliable exec. version'),
+  remote_vul: _l('Remote vulnerability'),
+  exploit: _l('Exploit'),
 };
 
 export const qodColorScale = scaleOrdinal()
