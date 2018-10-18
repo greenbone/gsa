bf3b8c46bb93c2cd863e79d5c73e9f5676ad143f
Björn Ricks <bjoern.ricks@greenbone.net>
2018-10-18 13:31:38 +0200

    Add tests for UserLink component
    Add a first test using react-testing-library and our wrappers.

---
+++ gsa/src/web/components/link/__tests__/userlink.js
@@ -0,0 +1,65 @@
+/* Copyright (C) 2018 Greenbone Networks GmbH
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import React from 'react';
+
+import date from 'gmp/models/date';
+
+import {longDate, setLocale} from 'gmp/locale/date';
+
+import {setSessionTimeout, setUsername} from 'web/store/usersettings/actions';
+
+import {rendererWithStoreAndRouter, cleanup} from 'web/utils/testing';
+
+import UserLink from '../userlink';
+
+setLocale('en');
+
+afterEach(cleanup);
+
+describe('UserLink tests', () => {
+
+  test('should render username and timeout', () => {
+    const timeout = date().add(5, 'minutes');
+
+    const {store, render} = rendererWithStoreAndRouter();
+
+    store.dispatch(setSessionTimeout(timeout));
+    store.dispatch(setUsername('foo'));
+
+    const {element} = render(<UserLink />);
+
+    expect(element.textContent).toEqual('foo');
+    expect(element.title).toMatch(longDate(timeout));
+  });
+
+  test('should apply styling', () => {
+    const timeout = date('2018-10-10');
+
+    const {store, render} = rendererWithStoreAndRouter();
+
+    store.dispatch(setSessionTimeout(timeout));
+    store.dispatch(setUsername('foo'));
+
+    const {element} = render(<UserLink />);
+    expect(element).toMatchSnapshot();
+  });
+
+});
+
+// vim: set ts=2 sw=2 tw=80:
