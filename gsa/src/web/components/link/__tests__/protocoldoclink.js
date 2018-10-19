b66fb44c7ec12a8df0672c4d61b1c947d3596df7
Björn Ricks <bjoern.ricks@greenbone.net>
2018-10-19 09:45:22 +0200

    Add test for ProtocolDocLink

---
+++ gsa/src/web/components/link/__tests__/protocoldoclink.js
@@ -0,0 +1,50 @@
+/* Copyright (C) 2018 Greenbone Networks GmbH
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import React from 'react';
+
+import {
+  cleanup,
+  rendererWith,
+} from 'web/utils/testing';
+
+import ProtocolDocLink from '../protocoldoclink';
+
+afterEach(cleanup);
+
+describe('ProtocolDocLink tests', () => {
+
+  test('should render ProtocolDocLink', () => {
+    const gmp = {
+      settings: {
+        protocoldocurl: 'http://foo.bar',
+      },
+    };
+
+    const {render} = rendererWith({gmp});
+    const {element} = render(<ProtocolDocLink title="Foo" />);
+
+    expect(element).toHaveAttribute('title', 'Foo');
+    expect(element).toHaveAttribute('href', 'http://foo.bar');
+    expect(element).toHaveAttribute('target', '_blank');
+    expect(element).toHaveAttribute('rel', 'noopener noreferrer');
+  });
+
+});
+
+// vim: set ts=2 sw=2 tw=80:
