4ad81d724623f16698254b3b2eb9be6bd9812e9b
Steffen Waterkamp <steffen.waterkamp@greenbone.net>
2018-10-25 14:10:42 +0200

    Use StripedTable for EditNvtDetailsDialog

--- gsa/src/web/pages/scanconfigs/editnvtdetailsdialog.js
+++ gsa/src/web/pages/scanconfigs/editnvtdetailsdialog.js
@@ -43,7 +43,7 @@ import DetailsLink from 'web/components/link/detailslink';
 import Link from 'web/components/link/link';
 
 import SimpleTable from 'web/components/table/simpletable';
-import Table from 'web/components/table/table';
+import Table from 'web/components/table/stripedtable';
 import TableBody from 'web/components/table/body';
 import TableData from 'web/components/table/data';
 import TableHead from 'web/components/table/head';
