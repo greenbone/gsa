01a91ef11aef74b459febd1535ef823ca0e113dd
Steffen Waterkamp <steffen.waterkamp@greenbone.net>
2018-11-01 14:01:07 +0100

    Add polyfill for Object.entries() to Settings model

--- gsa/src/gmp/models/settings.js
+++ gsa/src/gmp/models/settings.js
@@ -21,6 +21,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+import 'core-js/fn/object/entries';
+
 import {isDefined} from '../utils/identity';
 
 class Settings {
