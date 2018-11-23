29060858e0ccabc3775845f0fb2155983b49bba4
Steffen Waterkamp <steffen.waterkamp@greenbone.net>
2018-11-23 12:45:47 +0100

    Move edit.svg to /svg subfolder
    We will have many graphics stored here, so it is seems a better idea to 
    split the icon components and their respective .svg's into two folders.

--- gsa/src/web/components/icon/editicon.js
+++ gsa/src/web/components/icon/editicon.js
@@ -25,7 +25,7 @@ import React from 'react';
 
 import SvgIcon from './svgicon';
 
-import {ReactComponent as Icon} from './edit.svg';
+import {ReactComponent as Icon} from './svg/edit.svg';
 
 const EditIcon = props => (
   <SvgIcon
