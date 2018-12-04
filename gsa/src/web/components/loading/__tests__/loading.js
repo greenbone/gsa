7de4194dc24e5e0c68eca819c4cd2812696efe10
Björn Ricks <bjoern.ricks@greenbone.net>
2018-12-04 16:10:01 +0100

    Update licence headers

--- gsa/src/web/components/loading/__tests__/loading.js
+++ gsa/src/web/components/loading/__tests__/loading.js
@@ -1,10 +1,6 @@
-/* Greenbone Security Assistant
+/* Copyright (C) 2018 Greenbone Networks GmbH
  *
- * Authors:
- * Steffen Waterkamp <steffen.waterkamp@greenbone.net>
- *
- * Copyright:
- * Copyright (C) 2018 Greenbone Networks GmbH
+ * SPDX-License-Identifier: GPL-2.0-or-later
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -20,7 +16,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
 import React from 'react';
 
 import {render} from 'web/utils/testing';
