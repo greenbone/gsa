07d5e6838f512c890af07bb1aaf3dc9c41ad3d6e
Steffen Waterkamp <steffen.waterkamp@greenbone.net>
2018-12-07 12:08:31 +0100

    Add tests for reportComposerDefaults usersettings actions

--- gsa/src/web/store/usersettings/__tests__/actions.js
+++ gsa/src/web/store/usersettings/__tests__/actions.js
@@ -1,10 +1,6 @@
-/* Greenbone Security Assistant
+/* Copyright (C) 2018 Greenbone Networks GmbH
  *
- * Authors:
- * Björn Ricks <bjoern.ricks@greenbone.net>
- *
- * Copyright:
- * Copyright (C) 2018 Greenbone Networks GmbH
+ * SPDX-License-Identifier: GPL-2.0-or-later
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -20,15 +16,24 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+
 import moment from 'gmp/models/date';
 
+import {isFunction} from 'gmp/utils/identity';
+
 import {
+  loadReportComposerDefaults,
+  saveReportComposerDefaults,
   setLocale,
   setSessionTimeout,
   setTimezone,
   setUsername,
   renewSessionTimeout,
+  reportComposerDefaultsLoadingActions,
   updateTimezone,
+  USER_SETTINGS_LOAD_REPORT_COMPOSER_DEFAULTS_REQUEST,
+  USER_SETTINGS_LOAD_REPORT_COMPOSER_DEFAULTS_SUCCESS,
+  USER_SETTINGS_LOAD_REPORT_COMPOSER_DEFAULTS_ERROR,
   USER_SETTINGS_SET_LOCALE,
   USER_SETTINGS_SET_SESSION_TIMEOUT,
   USER_SETTINGS_SET_TIMEZONE,
@@ -65,6 +70,29 @@ describe('settings actions tests', () => {
     });
   });
 
+  test('should create report composer defaults loading request action', () => {
+    const action = reportComposerDefaultsLoadingActions.request();
+    expect(action).toEqual({
+      type: USER_SETTINGS_LOAD_REPORT_COMPOSER_DEFAULTS_REQUEST,
+    });
+  });
+
+  test('should create report composer defaults loading success action', () => {
+    const action = reportComposerDefaultsLoadingActions.success({foo: 'bar'});
+    expect(action).toEqual({
+      type: USER_SETTINGS_LOAD_REPORT_COMPOSER_DEFAULTS_SUCCESS,
+      data: {foo: 'bar'},
+    });
+  });
+
+  test('should create report composer defaults loading error action', () => {
+    const action = reportComposerDefaultsLoadingActions.error('error');
+    expect(action).toEqual({
+      type: USER_SETTINGS_LOAD_REPORT_COMPOSER_DEFAULTS_ERROR,
+      error: 'error',
+    });
+  });
+
   test('should update timezone', () => {
     const dispatch = jest.fn();
     const gmp = {
@@ -98,6 +126,118 @@ describe('settings actions tests', () => {
         timeout: sessionTimeout,
       });
       expect(renewSession).toBeCalled();
+    });
+  });
+
+  describe('loadReportComposerDefaults tests', () => {
+
+    test('should dispatch request and success actions', () => {
+      const dispatch = jest.fn();
+
+      const data = {foo: 'bar'};
+      const getReportComposerDefaults = jest.fn().mockReturnValue(
+        Promise.resolve({data}));
+
+      const gmp = {
+        user: {
+          getReportComposerDefaults,
+        },
+      };
+
+      expect(isFunction(loadReportComposerDefaults)).toBe(true);
+
+      return loadReportComposerDefaults(gmp)()(dispatch).then(() => {
+        expect(getReportComposerDefaults).toBeCalled();
+        expect(dispatch).toHaveBeenCalledTimes(2);
+        expect(dispatch.mock.calls[0]).toEqual([{
+          type: USER_SETTINGS_LOAD_REPORT_COMPOSER_DEFAULTS_REQUEST,
+        }]);
+        expect(dispatch.mock.calls[1]).toEqual([{
+          type: USER_SETTINGS_LOAD_REPORT_COMPOSER_DEFAULTS_SUCCESS,
+          data,
+        }]);
+      });
+    });
+  });
+
+  test('should dispatch request and error actions', () => {
+    const dispatch = jest.fn();
+
+    const getReportComposerDefaults = jest.fn().mockReturnValue(
+      Promise.reject('error'));
+
+    const gmp = {
+      user: {
+        getReportComposerDefaults,
+      },
+    };
+
+    expect(isFunction(getReportComposerDefaults)).toBe(true);
+
+    return loadReportComposerDefaults(gmp)()(dispatch).then(() => {
+      expect(getReportComposerDefaults).toBeCalled();
+      expect(dispatch).toHaveBeenCalledTimes(2);
+      expect(dispatch.mock.calls[0]).toEqual([{
+        type: USER_SETTINGS_LOAD_REPORT_COMPOSER_DEFAULTS_REQUEST,
+      }]);
+      expect(dispatch.mock.calls[1]).toEqual([{
+        type: USER_SETTINGS_LOAD_REPORT_COMPOSER_DEFAULTS_ERROR,
+        error: 'error',
+      }]);
+    });
+  });
+
+  describe('saveReportComposerDefaults tests', () => {
+
+    test('should dispatch success actions', () => {
+      const dispatch = jest.fn();
+
+      const data = {foo: 'bar'};
+      const saveReportComposerDefaultsMock = jest.fn().mockReturnValue(
+        Promise.resolve({data}));
+
+      const gmp = {
+        user: {
+          saveReportComposerDefaults: saveReportComposerDefaultsMock,
+        },
+      };
+
+      const defaults = {fake: 'someDefaults'};
+
+      expect(isFunction(saveReportComposerDefaults)).toBe(true);
+
+      return saveReportComposerDefaults(gmp)(defaults)(dispatch).then(() => {
+        expect(saveReportComposerDefaultsMock).toHaveBeenCalledWith(defaults);
+        expect(dispatch).toHaveBeenCalledWith({
+          type: USER_SETTINGS_LOAD_REPORT_COMPOSER_DEFAULTS_SUCCESS,
+          data: defaults,
+        });
+      });
+    });
+  });
+
+  test('should dispatch error actions', () => {
+    const dispatch = jest.fn();
+
+    const saveReportComposerDefaultsMock = jest.fn().mockReturnValue(
+      Promise.reject('error'));
+
+    const gmp = {
+      user: {
+        saveReportComposerDefaults: saveReportComposerDefaultsMock,
+      },
+    };
+
+    const defaults = {fake: 'someDefaults'};
+
+    expect(isFunction(saveReportComposerDefaults)).toBe(true);
+
+    return saveReportComposerDefaults(gmp)(defaults)(dispatch).then(() => {
+      expect(saveReportComposerDefaultsMock).toHaveBeenCalledWith(defaults);
+      expect(dispatch).toHaveBeenCalledWith({
+        type: USER_SETTINGS_LOAD_REPORT_COMPOSER_DEFAULTS_ERROR,
+        error: 'error',
+      });
     });
   });
 
