36b0c60e43aebc20c0a43289fdc89f23760a5386
Björn Ricks <bjoern.ricks@greenbone.net>
2018-12-10 15:23:21 +0100

    Add tests for TimezoneSelect component

---
+++ gsa/src/web/components/form/__tests__/timezoneselect.js
@@ -0,0 +1,103 @@
+/* Copyright (C) 2018 Greenbone Networks GmbH
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import React from 'react';
+
+import {setLocale} from 'gmp/locale/lang';
+import timezones from 'gmp/timezones';
+
+import {render, fireEvent} from 'web/utils/testing';
+
+import TimezoneSelect from '../timezoneselect';
+
+setLocale('en');
+
+describe('TimezoneSelect tests', () => {
+
+  test('should render', () => {
+    const {element, getByTestId} = render(
+      <TimezoneSelect/>
+    );
+
+    const selected = getByTestId('select-selected-value');
+    expect(selected).toHaveTextContent('Coordinated Universal Time/UTC');
+
+    expect(element).toMatchSnapshot();
+  });
+
+
+  test('should render all timezones in selection', () => {
+    const {getByTestId, getAllByTestId} = render(
+      <TimezoneSelect/>
+    );
+
+    const button = getByTestId('select-open-button');
+    fireEvent.click(button);
+
+    const items = getAllByTestId('select-item');
+    expect(items.length).toEqual(timezones.length + 1);
+  });
+
+  test('should call onChange handler', () => {
+    const handler = jest.fn();
+    const {getByTestId, getAllByTestId} = render(
+      <TimezoneSelect
+        onChange={handler}
+      />
+    );
+
+    const button = getByTestId('select-open-button');
+    fireEvent.click(button);
+
+    const items = getAllByTestId('select-item');
+    fireEvent.click(items[1]);
+
+    expect(handler).toHaveBeenCalledWith(timezones[0].name, undefined);
+  });
+
+  test('should call onChange handler with name', () => {
+    const handler = jest.fn();
+    const {getByTestId, getAllByTestId} = render(
+      <TimezoneSelect
+        name="foo"
+        onChange={handler}
+      />
+    );
+
+    const button = getByTestId('select-open-button');
+    fireEvent.click(button);
+
+    const items = getAllByTestId('select-item');
+    fireEvent.click(items[1]);
+
+    expect(handler).toHaveBeenCalledWith(timezones[0].name, 'foo');
+  });
+
+  test('should render selected value', () => {
+    const timezone = timezones[1]; // eslint-disable-line prefer-destructuring
+    const {getByTestId} = render(
+      <TimezoneSelect
+        value={timezone.name}
+      />
+    );
+
+    const selected = getByTestId('select-selected-value');
+    expect(selected).toHaveTextContent(timezone.name);
+  });
+
+});
