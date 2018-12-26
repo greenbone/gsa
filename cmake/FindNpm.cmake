ca02febe49f50f6d652e9ae4f2f8f025422b4182
Jan-Oliver Wagner <jan-oliver.wagner@greenbone.net>
2018-12-26 11:03:26 +0100

    Fix name in comment.

--- cmake/FindNpm.cmake
+++ cmake/FindNpm.cmake
@@ -16,7 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
 
-# CMake find module for yarn package manager
+# CMake find module for node package manager
 
 find_package (Node REQUIRED)
 
