f8c9bf26ec28528062edf8cd57e37eea128cfe25
Björn Ricks <bjoern.ricks@greenbone.net>
2019-01-23 12:30:02 +0100

    Add a explicit dialog for editing a ticket
    The dialog is very simple. It only allows to modify the ticket comment.

---
+++ gsa/src/web/pages/tickets/editdialog.js
@@ -0,0 +1,78 @@
+/* Copyright (C) 2019 Greenbone Networks GmbH
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import React from 'react';
+
+import _ from 'gmp/locale';
+
+import SaveDialog from 'web/components/dialog/savedialog';
+
+import FormGroup from 'web/components/form/formgroup';
+import TextArea from 'web/components/form/textarea';
+import Layout from 'web/components/layout/layout';
+
+import PropTypes from 'web/utils/proptypes';
+
+const EditTicketDialog = ({
+  comment = '',
+  ticketId,
+  title = _('Edit Ticket'),
+  onClose,
+  onSave,
+}) => (
+  <SaveDialog
+    title={title}
+    onClose={onClose}
+    onSave={onSave}
+    values={{
+      ticketId,
+    }}
+    defaultValues={{
+      comment,
+    }}
+  >
+    {({
+      values,
+      onValueChange,
+    }) => (
+      <Layout flex="column">
+        <FormGroup title={_('Comment')}>
+          <TextArea
+            name="comment"
+            grow="1"
+            rows="5"
+            value={values.comment}
+            onChange={onValueChange}
+          />
+        </FormGroup>
+      </Layout>
+    )}
+  </SaveDialog>
+);
+
+EditTicketDialog.propTypes = {
+  comment: PropTypes.string,
+  ticketId: PropTypes.id.isRequired,
+  title: PropTypes.toString,
+  onClose: PropTypes.func.isRequired,
+  onSave: PropTypes.func.isRequired,
+};
+
+export default EditTicketDialog;
+
+// vim: set ts=2 sw=2 tw=80:
