7402a7b20891223164d9ccb600fa707b4da035a1
Björn Ricks <bjoern.ricks@greenbone.net>
2019-02-06 12:13:19 +0100

    Add prettier config file

---
+++ gsa/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: false,
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
