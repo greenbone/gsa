90c738efd1b223c424c35d4899df27f91f1cd49d
Steffen Waterkamp <steffen.waterkamp@greenbone.net>
2019-04-04 17:02:15 +0200

    Add tests for entity icons

---
+++ gsa/src/web/entity/icon/__tests__/deleteicon.js
@@ -0,0 +1,107 @@
+/* Copyright (C) 2019 Greenbone Networks GmbH
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import React from 'react';
+
+import Capabilities from 'gmp/capabilities/capabilities';
+
+import Task from 'gmp/models/task';
+
+import {rendererWith, fireEvent} from 'web/utils/testing';
+
+import Theme from 'web/utils/theme';
+
+import DeleteIcon from '../deleteicon';
+
+describe('Entity DeleteIcon component tests', () => {
+  test('should render in active state with correct permissions', () => {
+    const caps = new Capabilities(['everything']);
+    const entity = new Task({
+      permissions: {permission: [{name: 'delete_task'}]},
+    });
+    const clickHandler = jest.fn();
+
+    const {render} = rendererWith({capabilities: caps});
+
+    const {element} = render(
+      <DeleteIcon entity={entity} onClick={clickHandler} />,
+    );
+
+    expect(element).toMatchSnapshot();
+    expect(caps.mayDelete('task')).toEqual(true);
+    expect(entity.userCapabilities.mayDelete('task')).toEqual(true);
+
+    fireEvent.click(element);
+
+    expect(clickHandler).toHaveBeenCalled();
+    expect(element).not.toHaveStyleRule('fill', Theme.inputBorderGray, {
+      modifier: `svg path`,
+    });
+  });
+
+  test('should deactivate if wrong command level permissions are given', () => {
+    const caps = new Capabilities(['authenticate']);
+    const entity = new Task({
+      permissions: {permission: [{name: 'delete_task'}]},
+    });
+    const clickHandler = jest.fn();
+
+    const {render} = rendererWith({capabilities: caps});
+
+    const {element} = render(
+      <DeleteIcon entity={entity} onClick={clickHandler} />,
+    );
+
+    expect(element).toMatchSnapshot();
+    expect(caps.mayDelete('task')).toEqual(false);
+    expect(entity.userCapabilities.mayDelete('task')).toEqual(true);
+
+    fireEvent.click(element);
+    expect(clickHandler).not.toHaveBeenCalled();
+    expect(element).toHaveStyleRule('fill', Theme.inputBorderGray, {
+      modifier: `svg path`,
+    });
+  });
+
+  test('should deactivate if wrong resource level permissions are given', () => {
+    const caps = new Capabilities(['everything']);
+    const entity = new Task({
+      permissions: {permission: [{name: 'delete_schedule'}]},
+    });
+    const clickHandler = jest.fn();
+
+    const {render} = rendererWith({capabilities: caps});
+
+    const {element} = render(
+      <DeleteIcon entity={entity} onClick={clickHandler} />,
+    );
+
+    expect(element).toMatchSnapshot();
+    expect(caps.mayDelete('task')).toEqual(true);
+    expect(entity.userCapabilities.mayDelete('task')).toEqual(false);
+
+    fireEvent.click(element);
+
+    expect(clickHandler).not.toHaveBeenCalled();
+    expect(element).toHaveStyleRule('fill', Theme.inputBorderGray, {
+      modifier: `svg path`,
+    });
+  });
+});
+
+// vim: set ts=2 sw=2 tw=80:
