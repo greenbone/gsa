9f288534913cda3c5bb0fdfca596b75b7ddb8c28
Sarah Diedrich <sarah.diedrich@greenbone.net>
2019-04-26 16:36:51 +0200

    Add test for toolbar

---
+++ gsa/src/web/components/bar/__tests__/toolbar.js
@@ -0,0 +1,31 @@
+/* Copyright (C) 2019 Greenbone Networks GmbH
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import React from 'react';
+
+import {render} from 'web/utils/testing';
+
+import Toolbar from '../toolbar';
+
+describe('Toolbar tests', () => {
+  test('should render', () => {
+    const {element} = render(<Toolbar />);
+
+    expect(element).toMatchSnapshot();
+  });
+});
