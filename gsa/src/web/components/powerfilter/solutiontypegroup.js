f54db98ed94b79a4e0228cbbc01819fa6753481b
Sarah Diedrich <sarah.diedrich@greenbone.net>
2019-05-16 14:31:36 +0200

    Adjust solution type group to allow translation to fit into dialog

--- gsa/src/web/components/powerfilter/solutiontypegroup.js
+++ gsa/src/web/components/powerfilter/solutiontypegroup.js
@@ -30,6 +30,19 @@ import SolutionTypeIcon from 'web/components/icon/solutiontypeicon';
 
 import {isDefined} from 'gmp/utils/identity';
 
+import Table from 'web/components/table/table';
+import TableBody from 'web/components/table/body';
+import TableRow from 'web/components/table/row';
+import TableData from 'web/components/table/data';
+
+import styled from 'styled-components';
+
+const StyledSpan = styled.span`
+  display: flex;
+  flex-direction: row;
+  flex-basis: auto;
+`;
+
 class SolutionTypesFilterGroup extends React.Component {
   constructor(...args) {
     super(...args);
@@ -55,53 +68,75 @@ class SolutionTypesFilterGroup extends React.Component {
 
     return (
       <FormGroup title={_('Solution Type')}>
-        <Radio
-          checked={!isDefined(solutionType) || solutionType === 'All'}
-          name="All"
-          onChange={this.handleSolutionTypeChange}
-        >
-          <span>{_('All')}</span>
-        </Radio>
-        <Radio
-          checked={solutionType === 'Workaround'}
-          name="Workaround"
-          onChange={this.handleSolutionTypeChange}
-        >
-          <SolutionTypeIcon type="Workaround" />
-          <span>{_('Workaround')}</span>
-        </Radio>
-        <Radio
-          checked={solutionType === 'Mitigation'}
-          name="Mitigation"
-          onChange={this.handleSolutionTypeChange}
-        >
-          <SolutionTypeIcon type="Mitigation" />
-          <span>{_('Mitigation')}</span>
-        </Radio>
-        <Radio
-          checked={solutionType === 'VendorFix'}
-          name="VendorFix"
-          onChange={this.handleSolutionTypeChange}
-        >
-          <SolutionTypeIcon type="VendorFix" />
-          <span>{_('Vendor fix')}</span>
-        </Radio>
-        <Radio
-          checked={solutionType === 'NoneAvailable'}
-          name="NoneAvailable"
-          onChange={this.handleSolutionTypeChange}
-        >
-          <SolutionTypeIcon type="NoneAvailable" />
-          <span>{_('None available')}</span>
-        </Radio>
-        <Radio
-          checked={solutionType === 'WillNotFix'}
-          name="WillNotFix"
-          onChange={this.handleSolutionTypeChange}
-        >
-          <SolutionTypeIcon type="WillNotFix" />
-          <span>{_('Will not fix')}</span>
-        </Radio>
+        <StyledSpan>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableData>
+                  <Radio
+                    checked={!isDefined(solutionType) || solutionType === 'All'}
+                    name="All"
+                    onChange={this.handleSolutionTypeChange}
+                  >
+                    <span>{_('All')}</span>
+                  </Radio>
+                </TableData>
+                <TableData>
+                  <Radio
+                    checked={solutionType === 'Workaround'}
+                    name="Workaround"
+                    onChange={this.handleSolutionTypeChange}
+                  >
+                    <SolutionTypeIcon type="Workaround" />
+                    <span>{_('Workaround')}</span>
+                  </Radio>
+                </TableData>
+                <TableData>
+                  <Radio
+                    checked={solutionType === 'Mitigation'}
+                    name="Mitigation"
+                    onChange={this.handleSolutionTypeChange}
+                  >
+                    <SolutionTypeIcon type="Mitigation" />
+                    <span>{_('Mitigation')}</span>
+                  </Radio>
+                </TableData>
+              </TableRow>
+              <TableRow>
+                <TableData>
+                  <Radio
+                    checked={solutionType === 'VendorFix'}
+                    name="VendorFix"
+                    onChange={this.handleSolutionTypeChange}
+                  >
+                    <SolutionTypeIcon type="VendorFix" />
+                    <span>{_('Vendor fix')}</span>
+                  </Radio>
+                </TableData>
+                <TableData>
+                  <Radio
+                    checked={solutionType === 'NoneAvailable'}
+                    name="NoneAvailable"
+                    onChange={this.handleSolutionTypeChange}
+                  >
+                    <SolutionTypeIcon type="NoneAvailable" />
+                    <span>{_('None available')}</span>
+                  </Radio>
+                </TableData>
+                <TableData>
+                  <Radio
+                    checked={solutionType === 'WillNotFix'}
+                    name="WillNotFix"
+                    onChange={this.handleSolutionTypeChange}
+                  >
+                    <SolutionTypeIcon type="WillNotFix" />
+                    <span>{_('Will not fix')}</span>
+                  </Radio>{' '}
+                </TableData>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </StyledSpan>
       </FormGroup>
     );
   }
