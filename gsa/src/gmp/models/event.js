f0c6b5599238aa2d691e87d9c3af6659d63339b6
Björn Ricks <bjoern.ricks@greenbone.net>
2019-08-02 11:19:19 +0200

    Fix calculating the next due date in event
    ical times are very dangerous to use. Use moment.js to create a current
    datetime object and compare the unix timestamps (which is always in UTC)
    for getting the due date.

--- gsa/src/gmp/models/event.js
+++ gsa/src/gmp/models/event.js
@@ -352,14 +352,14 @@ class Event {
 
   get nextDate() {
     if (this.isRecurring()) {
-      const now = ical.Time.now();
+      const now = date();
       const it = this.event.iterator();
 
       let retries = 0;
       while (true && retries <= 5) {
         try {
           const next = it.next();
-          if (next.compare(now) >= 0) {
+          if (next.toUnixTime() >= now.unix()) {
             return convertIcalDate(next, this.timezone);
           }
           retries = 0;
