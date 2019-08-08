6fe061d0e336357ecf373a5dcaa0f6c99c1a8729
Björn Ricks <bjoern.ricks@greenbone.net>
2019-08-08 13:53:35 +0200

    Add a new component for rendering global styles

---
+++ gsa/src/web/components/layout/globalstyles.js
@@ -0,0 +1,84 @@
+/* Copyright (C) 2019 Greenbone Networks GmbH
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import {createGlobalStyle} from 'styled-components';
+
+import Theme from 'web/utils/theme';
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+    font-family: ${Theme.Font.default};
+    font-size: ${Theme.Font.defaultSize};
+    color: ${Theme.black};
+  }
+
+  a:link {
+    color: ${Theme.blue};
+    text-decoration: none;
+  }
+
+  a:hover, a:focus {
+    color: blue;
+    text-decoration: underline;
+  }
+
+  pre {
+    white-space: pre-wrap;
+    word-wrap: break-word;
+  }
+
+  img {
+    border: 0;
+  }
+
+  svg {
+    overflow: hidden;
+  }
+
+  h1 {
+    font-size: 20px;
+  }
+
+  h2 {
+    font-size: 18px;
+  }
+
+  h3 {
+    font-size: 16px;
+  }
+
+  h4 {
+    font-size: 14px;
+    margin-bottom: 0;
+  }
+
+  p {
+    margin: 0 0 10px;
+  }
+`;
+
+export default GlobalStyles;
