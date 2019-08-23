de24e9dfa3e847d9ec326779434ce95f50a16e5d
Sarah Diedrich <sarah.diedrich@greenbone.net>
2019-08-23 11:22:59 +0200

    fix sensor icon not visible

--- gsa/src/web/pages/audits/row.js
+++ gsa/src/web/pages/audits/row.js
@@ -121,7 +121,7 @@ const Row = ({
             {entity.alterable === 1 && (
               <AlterableIcon size="small" title={_('Audit is alterable')} />
             )}
-            {isDefined(scanner) && scanner.type === GMP_SCANNER_TYPE && (
+            {isDefined(scanner) && scanner.scannerType === GMP_SCANNER_TYPE && (
               <SensorIcon
                 size="small"
                 title={_('Audit is configured to run on sensor {{name}}', {
