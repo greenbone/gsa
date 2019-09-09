d27788229a7c2b703f6a43321dae9b04b12e0d51
Crystal Lai <crystal.lai@greenbone.net>
2019-09-09 10:45:50 +0200

    Added document title to dashboards, task: listpage and detailspage

--- gsa/src/web/pages/tasks/listpage.js
+++ gsa/src/web/pages/tasks/listpage.js
@@ -39,6 +39,7 @@ import ManualIcon from 'web/components/icon/manualicon';
 import WizardIcon from 'web/components/icon/wizardicon';
 
 import IconDivider from 'web/components/layout/icondivider';
+import PageTitle from 'web/components/layout/pagetitle';
 
 import IconMenu from 'web/components/menu/iconmenu';
 import MenuEntry from 'web/components/menu/menuentry';
@@ -151,45 +152,48 @@ const Page = ({
       modifytaskwizard,
       taskwizard,
     }) => (
-      <EntitiesPage
-        {...props}
-        dashboard={() => (
-          <TaskDashboard
-            filter={filter}
-            onFilterChanged={onFilterChanged}
-            onInteraction={onInteraction}
-          />
-        )}
-        dashboardControls={() => (
-          <DashboardControls
-            dashboardId={TASK_DASHBOARD_ID}
-            onInteraction={onInteraction}
-          />
-        )}
-        filter={filter}
-        filterEditDialog={TaskFilterDialog}
-        filtersFilter={TASKS_FILTER_FILTER}
-        sectionIcon={<TaskIcon size="large" />}
-        table={Table}
-        title={_('Tasks')}
-        toolBarIcons={ToolBarIcons}
-        onAdvancedTaskWizardClick={advancedtaskwizard}
-        onContainerTaskCreateClick={createcontainer}
-        onError={onError}
-        onFilterChanged={onFilterChanged}
-        onInteraction={onInteraction}
-        onModifyTaskWizardClick={modifytaskwizard}
-        onReportImportClick={reportimport}
-        onTaskCloneClick={clone}
-        onTaskCreateClick={create}
-        onTaskDeleteClick={delete_func}
-        onTaskDownloadClick={download}
-        onTaskEditClick={edit}
-        onTaskResumeClick={resume}
-        onTaskStartClick={start}
-        onTaskStopClick={stop}
-        onTaskWizardClick={taskwizard}
-      />
+      <React.Fragment>
+        <PageTitle title={_('Tasks')} />
+        <EntitiesPage
+          {...props}
+          dashboard={() => (
+            <TaskDashboard
+              filter={filter}
+              onFilterChanged={onFilterChanged}
+              onInteraction={onInteraction}
+            />
+          )}
+          dashboardControls={() => (
+            <DashboardControls
+              dashboardId={TASK_DASHBOARD_ID}
+              onInteraction={onInteraction}
+            />
+          )}
+          filter={filter}
+          filterEditDialog={TaskFilterDialog}
+          filtersFilter={TASKS_FILTER_FILTER}
+          sectionIcon={<TaskIcon size="large" />}
+          table={Table}
+          title={_('Tasks')}
+          toolBarIcons={ToolBarIcons}
+          onAdvancedTaskWizardClick={advancedtaskwizard}
+          onContainerTaskCreateClick={createcontainer}
+          onError={onError}
+          onFilterChanged={onFilterChanged}
+          onInteraction={onInteraction}
+          onModifyTaskWizardClick={modifytaskwizard}
+          onReportImportClick={reportimport}
+          onTaskCloneClick={clone}
+          onTaskCreateClick={create}
+          onTaskDeleteClick={delete_func}
+          onTaskDownloadClick={download}
+          onTaskEditClick={edit}
+          onTaskResumeClick={resume}
+          onTaskStartClick={start}
+          onTaskStopClick={stop}
+          onTaskWizardClick={taskwizard}
+        />
+      </React.Fragment>
     )}
   </TaskComponent>
 );
