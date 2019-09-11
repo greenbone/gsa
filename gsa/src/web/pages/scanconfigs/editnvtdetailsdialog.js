f8bdada33e6d5e2c185e65ccc7e2d139d07c6838
Björn Ricks <bjoern.ricks@greenbone.net>
2019-09-11 09:32:20 +0200

    Allow to show loading indicator for edit nvt details dialog
    Support showing a loading indicator when loading the nvt details. This
    will allow to open the dialog before the nvt details data is loaded.

--- gsa/src/web/pages/scanconfigs/editnvtdetailsdialog.js
+++ gsa/src/web/pages/scanconfigs/editnvtdetailsdialog.js
@@ -39,6 +39,8 @@ import Layout from 'web/components/layout/layout';
 import DetailsLink from 'web/components/link/detailslink';
 import Link from 'web/components/link/link';
 
+import Loading from 'web/components/loading/loading';
+
 import SimpleTable from 'web/components/table/simpletable';
 import Table from 'web/components/table/stripedtable';
 import TableBody from 'web/components/table/body';
@@ -50,7 +52,7 @@ import TableRow from 'web/components/table/row';
 import NvtPreference from '../nvts/nvtpreference';
 import Preformatted from '../nvts/preformatted';
 
-const createPrefValues = preferences => {
+const createPrefValues = (preferences = []) => {
   const preferenceValues = {};
 
   preferences.forEach(pref => {
@@ -75,6 +77,7 @@ const EditDialog = ({
   configName,
   configNameLabel,
   defaultTimeout,
+  isLoadingNvt = true,
   nvtAffectedSoftware,
   nvtCvssVector,
   nvtFamily,
@@ -93,14 +96,28 @@ const EditDialog = ({
     createPrefValues(preferences),
   );
 
+  const [controlledTimeout, setControlledTimeout] = useState(
+    isDefined(timeout) ? timeout : '',
+  );
+  const [useDefaultTimeout, setDefaultTimeout] = useState(
+    isDefined(timeout) ? '0' : '1',
+  );
+
   useEffect(() => {
     setPreferenceValues(createPrefValues(preferences));
   }, [preferences]);
+
+  useEffect(() => {
+    setControlledTimeout(timeout);
+    setDefaultTimeout(isDefined(timeout) ? '0' : '1');
+  }, [timeout]);
 
   const controlledData = {
     configId,
     nvtOid,
     preferenceValues,
+    timeout: controlledTimeout,
+    useDefaultTimeout,
   };
 
   return (
@@ -108,154 +125,154 @@ const EditDialog = ({
       title={title}
       onClose={onClose}
       onSave={onSave}
-      defaultValues={{
-        timeout: isDefined(timeout) ? timeout : '',
-        useDefaultTimeout: isDefined(timeout) ? '0' : '1',
-      }}
       values={controlledData}
     >
-      {({values: state, onValueChange}) => (
-        <Layout flex="column">
-          <SimpleTable>
-            <TableBody>
-              <TableRow>
-                <TableData>{_('Name')}</TableData>
-                <TableData>
-                  <span>
-                    <DetailsLink id={nvtOid} type="nvt">
-                      {nvtName}
-                    </DetailsLink>
-                  </span>
-                </TableData>
-              </TableRow>
-              <TableRow>
-                <TableData>{configNameLabel}</TableData>
-                <TableData>{configName}</TableData>
-              </TableRow>
-              <TableRow>
-                <TableData>{_('Family')}</TableData>
-                <TableData>{nvtFamily}</TableData>
-              </TableRow>
-              <TableRow>
-                <TableData>{_('OID')}</TableData>
-                <TableData>{nvtOid}</TableData>
-              </TableRow>
-              <TableRow>
-                <TableData>{_('Last Modified')}</TableData>
-                <TableData>
-                  <DateTime date={nvtLastModified} />
-                </TableData>
-              </TableRow>
-            </TableBody>
-          </SimpleTable>
-
-          {isDefined(nvtSummary) && (
-            <div>
-              <h1>{_('Summary')}</h1>
-              <Preformatted>{nvtSummary}</Preformatted>
-            </div>
-          )}
-
-          {isDefined(nvtAffectedSoftware) && (
-            <div>
-              <h1>{_('Affected Software/OS')}</h1>
-              <Preformatted>{nvtAffectedSoftware}</Preformatted>
-            </div>
-          )}
-
-          <div>
-            <h1>{_('Vulnerability Scoring')}</h1>
+      {({values: state, onValueChange}) =>
+        isLoadingNvt ? (
+          <Loading />
+        ) : (
+          <Layout flex="column">
             <SimpleTable>
               <TableBody>
                 <TableRow>
-                  <TableData>{_('CVSS base')}</TableData>
+                  <TableData>{_('Name')}</TableData>
                   <TableData>
-                    <SeverityBar severity={nvtSeverity} />
+                    <span>
+                      <DetailsLink id={nvtOid} type="nvt">
+                        {nvtName}
+                      </DetailsLink>
+                    </span>
                   </TableData>
                 </TableRow>
-                {isDefined(nvtCvssVector) && (
-                  <TableRow>
-                    <TableData>{_('CVSS base vector')}</TableData>
-                    <TableData>
-                      <Link
-                        to="cvsscalculator"
-                        query={{cvssVector: nvtCvssVector}}
-                      >
-                        {nvtCvssVector}
-                      </Link>
-                    </TableData>
-                  </TableRow>
-                )}
+                <TableRow>
+                  <TableData>{configNameLabel}</TableData>
+                  <TableData>{configName}</TableData>
+                </TableRow>
+                <TableRow>
+                  <TableData>{_('Family')}</TableData>
+                  <TableData>{nvtFamily}</TableData>
+                </TableRow>
+                <TableRow>
+                  <TableData>{_('OID')}</TableData>
+                  <TableData>{nvtOid}</TableData>
+                </TableRow>
+                <TableRow>
+                  <TableData>{_('Last Modified')}</TableData>
+                  <TableData>
+                    <DateTime date={nvtLastModified} />
+                  </TableData>
+                </TableRow>
               </TableBody>
             </SimpleTable>
-          </div>
 
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>{_('Name')}</TableHead>
-                <TableHead>{_('New Value')}</TableHead>
-                <TableHead>{_('Default Value')}</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableData>{_('Timeout')}</TableData>
-                <TableData>
-                  <Divider flex="column">
-                    <Divider>
-                      <Radio
-                        value="1"
-                        name="useDefaultTimeout"
-                        checked={state.useDefaultTimeout === '1'}
-                        onChange={onValueChange}
-                      />
-                      <span>
-                        {_('Apply default timeout')}
-                        {isDefined(defaultTimeout)
-                          ? ' (' + defaultTimeout + ')'
-                          : ''}
-                      </span>
+            {isDefined(nvtSummary) && (
+              <div>
+                <h1>{_('Summary')}</h1>
+                <Preformatted>{nvtSummary}</Preformatted>
+              </div>
+            )}
+
+            {isDefined(nvtAffectedSoftware) && (
+              <div>
+                <h1>{_('Affected Software/OS')}</h1>
+                <Preformatted>{nvtAffectedSoftware}</Preformatted>
+              </div>
+            )}
+
+            <div>
+              <h1>{_('Vulnerability Scoring')}</h1>
+              <SimpleTable>
+                <TableBody>
+                  <TableRow>
+                    <TableData>{_('CVSS base')}</TableData>
+                    <TableData>
+                      <SeverityBar severity={nvtSeverity} />
+                    </TableData>
+                  </TableRow>
+                  {isDefined(nvtCvssVector) && (
+                    <TableRow>
+                      <TableData>{_('CVSS base vector')}</TableData>
+                      <TableData>
+                        <Link
+                          to="cvsscalculator"
+                          query={{cvssVector: nvtCvssVector}}
+                        >
+                          {nvtCvssVector}
+                        </Link>
+                      </TableData>
+                    </TableRow>
+                  )}
+                </TableBody>
+              </SimpleTable>
+            </div>
+
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>{_('Name')}</TableHead>
+                  <TableHead>{_('New Value')}</TableHead>
+                  <TableHead>{_('Default Value')}</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableData>{_('Timeout')}</TableData>
+                  <TableData>
+                    <Divider flex="column">
+                      <Divider>
+                        <Radio
+                          value="1"
+                          name="useDefaultTimeout"
+                          checked={state.useDefaultTimeout === '1'}
+                          onChange={value => setDefaultTimeout(value)}
+                        />
+                        <span>
+                          {_('Apply default timeout')}
+                          {isDefined(defaultTimeout)
+                            ? ' (' + defaultTimeout + ')'
+                            : ''}
+                        </span>
+                      </Divider>
+                      <Divider>
+                        <Radio
+                          value="0"
+                          name="useDefaultTimeout"
+                          checked={state.useDefaultTimeout === '0'}
+                          onChange={value => setDefaultTimeout(value)}
+                        />
+                        <TextField
+                          disabled={state.useDefaultTimeout === '1'}
+                          name="timeout"
+                          value={state.timeout}
+                          onChange={value => setControlledTimeout(value)}
+                        />
+                      </Divider>
                     </Divider>
-                    <Divider>
-                      <Radio
-                        value="0"
-                        name="useDefaultTimeout"
-                        checked={state.useDefaultTimeout === '0'}
-                        onChange={onValueChange}
-                      />
-                      <TextField
-                        disabled={state.useDefaultTimeout === '1'}
-                        name="timeout"
-                        value={state.timeout}
-                        onChange={onValueChange}
-                      />
-                    </Divider>
-                  </Divider>
-                </TableData>
-                <TableData>
-                  {isDefined(defaultTimeout) ? defaultTimeout : ''}
-                </TableData>
-              </TableRow>
-              {preferences.map(pref => {
-                const prefValue = isDefined(preferenceValues[pref.name])
-                  ? preferenceValues[pref.name].value
-                  : undefined;
-                return (
-                  <NvtPreference
-                    key={pref.name}
-                    preference={pref}
-                    value={prefValue}
-                    onChange={value => {
-                      preferenceValues[pref.name].value = value.value;
-                    }}
-                  />
-                );
-              })}
-            </TableBody>
-          </Table>
-        </Layout>
-      )}
+                  </TableData>
+                  <TableData>
+                    {isDefined(defaultTimeout) ? defaultTimeout : ''}
+                  </TableData>
+                </TableRow>
+                {preferences.map(pref => {
+                  const prefValue = isDefined(preferenceValues[pref.name])
+                    ? preferenceValues[pref.name].value
+                    : undefined;
+                  return (
+                    <NvtPreference
+                      key={pref.name}
+                      preference={pref}
+                      value={prefValue}
+                      onChange={value => {
+                        preferenceValues[pref.name].value = value.value;
+                      }}
+                    />
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </Layout>
+        )
+      }
     </SaveDialog>
   );
 };
@@ -265,6 +282,7 @@ EditDialog.propTypes = {
   configName: PropTypes.string.isRequired,
   configNameLabel: PropTypes.string.isRequired,
   defaultTimeout: PropTypes.number,
+  isLoadingNvt: PropTypes.bool,
   nvtAffectedSoftware: PropTypes.string,
   nvtCvssVector: PropTypes.string,
   nvtFamily: PropTypes.string,
