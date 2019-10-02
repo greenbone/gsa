8e69516d04b5128f88bf1e07b74dd0485b46fcef
Sarah Diedrich <sarah.diedrich@greenbone.net>
2019-10-02 14:18:33 +0200

    add tests for scanconfig table

---
+++ gsa/src/web/pages/scanconfigs/__tests__/table.js
@@ -0,0 +1,229 @@
+/* Copyright (C) 2019 Greenbone Networks GmbH
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import React from 'react';
+
+import CollectionCounts from 'gmp/collection/collectioncounts';
+
+import Filter from 'gmp/models/filter';
+import ScanConfig, {
+  OPENVAS_SCAN_CONFIG_TYPE,
+  SCANCONFIG_TREND_STATIC,
+  SCANCONFIG_TREND_DYNAMIC,
+} from 'gmp/models/scanconfig';
+
+import {setUsername} from 'web/store/usersettings/actions';
+
+import {rendererWith, fireEvent} from 'web/utils/testing';
+
+import Table from '../table';
+
+const config = ScanConfig.fromElement({
+  _id: '12345',
+  name: 'foo',
+  comment: 'bar',
+  owner: {name: 'admin'},
+  type: OPENVAS_SCAN_CONFIG_TYPE,
+  permissions: {permission: [{name: 'everything'}]},
+  family_count: {
+    __text: 2,
+    growing: SCANCONFIG_TREND_STATIC,
+  },
+  nvt_count: {
+    __text: 4,
+    growing: SCANCONFIG_TREND_DYNAMIC,
+  },
+});
+
+const config2 = ScanConfig.fromElement({
+  _id: '123456',
+  name: 'lorem',
+  comment: 'ipsum',
+  owner: {name: 'admin'},
+  type: OPENVAS_SCAN_CONFIG_TYPE,
+  permissions: {permission: [{name: 'everything'}]},
+  family_count: {
+    __text: 3,
+    growing: SCANCONFIG_TREND_STATIC,
+  },
+  nvt_count: {
+    __text: 5,
+    growing: SCANCONFIG_TREND_DYNAMIC,
+  },
+});
+
+const config3 = ScanConfig.fromElement({
+  _id: '1234567',
+  name: 'hello',
+  comment: 'world',
+  owner: {name: 'admin'},
+  type: OPENVAS_SCAN_CONFIG_TYPE,
+  permissions: {permission: [{name: 'everything'}]},
+  family_count: {
+    __text: 1,
+    growing: SCANCONFIG_TREND_STATIC,
+  },
+  nvt_count: {
+    __text: 1,
+    growing: SCANCONFIG_TREND_DYNAMIC,
+  },
+});
+
+const counts = new CollectionCounts({
+  first: 1,
+  all: 1,
+  filtered: 1,
+  length: 1,
+  rows: 2,
+});
+
+const filter = Filter.fromString('rows=2');
+
+describe('Scan Config table tests', () => {
+  test('should render', () => {
+    const handleScanConfigClone = jest.fn();
+    const handleScanConfigDelete = jest.fn();
+    const handleScanConfigDownload = jest.fn();
+    const handleScanConfigEdit = jest.fn();
+
+    const gmp = {
+      settings: {},
+    };
+
+    const {render, store} = rendererWith({
+      gmp,
+      capabilities: true,
+      store: true,
+    });
+
+    store.dispatch(setUsername('admin'));
+
+    const {baseElement} = render(
+      <Table
+        filter={filter}
+        entities={[config, config2, config3]}
+        entitiesCounts={counts}
+        onScanConfigCloneClick={handleScanConfigClone}
+        onScanConfigDeleteClick={handleScanConfigDelete}
+        onScanConfigDownloadClick={handleScanConfigDownload}
+        onScanConfigEditClick={handleScanConfigEdit}
+      />,
+    );
+
+    expect(baseElement).toMatchSnapshot();
+    const header = baseElement.querySelectorAll('th');
+    expect(header[0]).toHaveTextContent('Name');
+    expect(header[1]).toHaveTextContent('Type');
+    expect(header[2]).toHaveTextContent('Family');
+    expect(header[3]).toHaveTextContent('NVTs');
+    expect(header[4]).toHaveTextContent('Actions');
+    expect(header[5]).toHaveTextContent('Total');
+    expect(header[6]).toHaveTextContent('Trend');
+    expect(header[7]).toHaveTextContent('Total');
+    expect(header[8]).toHaveTextContent('Trend');
+  });
+
+  test('should unfold all details', () => {
+    const handleScanConfigClone = jest.fn();
+    const handleScanConfigDelete = jest.fn();
+    const handleScanConfigDownload = jest.fn();
+    const handleScanConfigEdit = jest.fn();
+
+    const gmp = {
+      settings: {},
+    };
+
+    const {render, store} = rendererWith({
+      gmp,
+      capabilities: true,
+      router: true,
+      store: true,
+    });
+
+    store.dispatch(setUsername('admin'));
+
+    const {element, getAllByTestId} = render(
+      <Table
+        filter={filter}
+        entities={[config, config2, config3]}
+        entitiesCounts={counts}
+        onScanConfigCloneClick={handleScanConfigClone}
+        onScanConfigDeleteClick={handleScanConfigDelete}
+        onScanConfigDownloadClick={handleScanConfigDownload}
+        onScanConfigEditClick={handleScanConfigEdit}
+      />,
+    );
+
+    expect(element).not.toHaveTextContent('Comment');
+
+    const icons = getAllByTestId('svg-icon');
+    fireEvent.click(icons[0]);
+    expect(icons[0]).toHaveAttribute('title', 'Unfold all details');
+    expect(element).toHaveTextContent('Comment');
+  });
+
+  test('should call click handlers', () => {
+    const handleScanConfigClone = jest.fn();
+    const handleScanConfigDelete = jest.fn();
+    const handleScanConfigDownload = jest.fn();
+    const handleScanConfigEdit = jest.fn();
+
+    const gmp = {
+      settings: {},
+    };
+
+    const {render, store} = rendererWith({
+      gmp,
+      capabilities: true,
+      router: true,
+      store: true,
+    });
+
+    store.dispatch(setUsername('admin'));
+
+    const {getAllByTestId} = render(
+      <Table
+        filter={filter}
+        entities={[config, config2, config3]}
+        entitiesCounts={counts}
+        onScanConfigCloneClick={handleScanConfigClone}
+        onScanConfigDeleteClick={handleScanConfigDelete}
+        onScanConfigDownloadClick={handleScanConfigDownload}
+        onScanConfigEditClick={handleScanConfigEdit}
+      />,
+    );
+
+    const icons = getAllByTestId('svg-icon');
+
+    fireEvent.click(icons[7]);
+    expect(handleScanConfigDelete).toHaveBeenCalledWith(config);
+    expect(icons[7]).toHaveAttribute('title', 'Move Scan Config to trashcan');
+
+    fireEvent.click(icons[8]);
+    expect(handleScanConfigEdit).toHaveBeenCalledWith(config);
+    expect(icons[8]).toHaveAttribute('title', 'Edit Scan Config');
+
+    fireEvent.click(icons[9]);
+    expect(handleScanConfigClone).toHaveBeenCalledWith(config);
+    expect(icons[9]).toHaveAttribute('title', 'Clone Scan Config');
+
+    fireEvent.click(icons[10]);
+    expect(handleScanConfigDownload).toHaveBeenCalledWith(config);
+    expect(icons[10]).toHaveAttribute('title', 'Export Scan Config');
+  });
+});
