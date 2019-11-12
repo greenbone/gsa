23d2c4ff6724d135c554302457508503cee51993
Crystal Lai <crystal.lai@greenbone.net>
2019-11-12 15:04:53 +0100

    Added validation test for ticket createdialog

--- gsa/src/web/pages/tickets/__tests__/createdialog.js
+++ gsa/src/web/pages/tickets/__tests__/createdialog.js
@@ -132,4 +132,33 @@ describe('CreateTicketDialog component tests', () => {
       note: 'foobar',
     });
   });
+
+  test('should not save invalid form states', () => {
+    const handleClose = jest.fn();
+    const handleSave = jest.fn();
+    const handleUserIdChange = jest.fn();
+
+    const {getByTestId, baseElement} = render(
+      <CreateTicketDialog
+        resultId="r1"
+        userId="u1"
+        users={users}
+        onClose={handleClose}
+        onSave={handleSave}
+        onUserIdChange={handleUserIdChange}
+      />,
+    );
+
+    const saveButton = getByTestId('dialog-save-button');
+    const noteInput = baseElement.querySelector('textarea');
+    fireEvent.change(noteInput, {target: {value: ''}});
+
+    fireEvent.click(saveButton);
+
+    expect(handleSave).not.toHaveBeenCalled();
+
+    const errorBubbles = queryAllByTestId(baseElement, 'error-bubble');
+
+    expect(errorBubbles[0]).toHaveTextContent('Ticket note is required.');
+  });
 });
