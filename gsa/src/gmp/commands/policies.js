cb99daeaf59deae83abb4b4e1dea6fdf83182443
Sarah Diedrich <sarah.diedrich@greenbone.net>
2019-11-15 13:48:23 +0100

    use base config as base for policies

--- gsa/src/gmp/commands/policies.js
+++ gsa/src/gmp/commands/policies.js
@@ -31,7 +31,7 @@ import Policy from '../models/policy';
 import EntitiesCommand from './entities';
 import EntityCommand from './entity';
 import {convert, convertSelect, convertPreferences} from './scanconfigs';
-import {EMPTY_SCAN_CONFIG_ID} from 'gmp/models/scanconfig';
+import {BASE_SCAN_CONFIG_ID} from 'gmp/models/scanconfig';
 
 const log = logger.getLogger('gmp.commands.policies');
 
@@ -52,7 +52,7 @@ export class PolicyCommand extends EntityCommand {
   create({name, comment}) {
     const data = {
       cmd: 'create_config',
-      base: EMPTY_SCAN_CONFIG_ID,
+      base: BASE_SCAN_CONFIG_ID,
       comment,
       name,
       usage_type: 'policy',
