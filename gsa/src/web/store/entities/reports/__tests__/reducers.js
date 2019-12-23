6c09c583f0af1f3cc4e085ccfff9cbc52537328d
Björn Ricks <bjoern.ricks@greenbone.net>
2019-12-23 09:11:26 +0100

    Extend reports reducer tests
    * Add tests for reducing actions with filter
    * Add previous state to test (not) overriding state

--- gsa/src/web/store/entities/reports/__tests__/reducers.js
+++ gsa/src/web/store/entities/reports/__tests__/reducers.js
@@ -17,10 +17,13 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+import Filter from 'gmp/models/filter';
+
 import {isFunction} from 'gmp/utils/identity';
 
 import {entitiesActions as actions} from '../../reports';
 import {reportsReducer} from '../reducers';
+import {filterIdentifier} from 'web/store/utils';
 
 describe('report entities reducer tests', () => {
   test('should be a reportsReducer function', () => {
@@ -34,21 +37,71 @@ describe('report entities reducer tests', () => {
   test('should reduce request action', () => {
     const action = actions.request();
 
-    expect(reportsReducer(undefined, action)).toEqual({
+    const previousState = {
+      isLoading: {
+        default: false,
+      },
+      default: {
+        ids: ['bar'],
+      },
+    };
+
+    expect(reportsReducer(previousState, action)).toEqual({
       byId: {},
       errors: {},
       isLoading: {
         default: true,
       },
-      default: {},
+      default: {
+        ids: ['bar'],
+      },
+    });
+  });
+
+  test('should reduce request action with filter', () => {
+    const filter = Filter.fromString('foo=bar rows=10');
+    const action = actions.request(filter);
+
+    const previousState = {
+      isLoading: {
+        [filterIdentifier(filter)]: false,
+      },
+      [filterIdentifier(filter)]: {
+        ids: ['bar'],
+      },
+    };
+
+    expect(reportsReducer(previousState, action)).toEqual({
+      byId: {},
+      errors: {},
+      [filterIdentifier(filter)]: {
+        ids: ['bar'],
+      },
+      isLoading: {
+        [filterIdentifier(filter)]: true,
+      },
     });
   });
 
   test('should reduce success action', () => {
     const action = actions.success([{id: 'foo'}]);
 
-    expect(reportsReducer(undefined, action)).toEqual({
+    const previousState = {
       byId: {
+        bar: {
+          id: 'bar',
+        },
+      },
+      isLoading: {
+        default: true,
+      },
+    };
+
+    expect(reportsReducer(previousState, action)).toEqual({
+      byId: {
+        bar: {
+          id: 'bar',
+        },
         foo: {
           id: 'foo',
         },
@@ -63,10 +116,56 @@ describe('report entities reducer tests', () => {
     });
   });
 
+  test('should reduce success action with filter', () => {
+    const filter = Filter.fromString('foo=bar rows=10');
+    const action = actions.success([{id: 'foo'}], filter);
+
+    const previousState = {
+      byId: {
+        bar: {
+          id: 'bar',
+        },
+      },
+      isLoading: {
+        [filterIdentifier(filter)]: true,
+      },
+      [filterIdentifier(filter)]: {
+        ids: ['bar'],
+      },
+    };
+
+    expect(reportsReducer(previousState, action)).toEqual({
+      byId: {
+        foo: {
+          id: 'foo',
+        },
+        bar: {
+          id: 'bar',
+        },
+      },
+      errors: {},
+      isLoading: {
+        [filterIdentifier(filter)]: false,
+      },
+      [filterIdentifier(filter)]: {
+        ids: ['foo'],
+      },
+    });
+  });
+
   test('should reduce error action', () => {
     const action = actions.error('An error');
 
-    expect(reportsReducer(undefined, action)).toEqual({
+    const previousState = {
+      isLoading: {
+        default: true,
+      },
+      default: {
+        ids: ['bar'],
+      },
+    };
+
+    expect(reportsReducer(previousState, action)).toEqual({
       byId: {},
       errors: {
         default: 'An error',
@@ -74,7 +173,36 @@ describe('report entities reducer tests', () => {
       isLoading: {
         default: false,
       },
-      default: {},
+      default: {
+        ids: ['bar'],
+      },
+    });
+  });
+
+  test('should reduce error action with filter', () => {
+    const filter = Filter.fromString('foo=bar rows=10');
+    const action = actions.error('An error', filter);
+
+    const previousState = {
+      isLoading: {
+        [filterIdentifier(filter)]: true,
+      },
+      [filterIdentifier(filter)]: {
+        ids: ['bar'],
+      },
+    };
+
+    expect(reportsReducer(previousState, action)).toEqual({
+      byId: {},
+      errors: {
+        [filterIdentifier(filter)]: 'An error',
+      },
+      isLoading: {
+        [filterIdentifier(filter)]: false,
+      },
+      [filterIdentifier(filter)]: {
+        ids: ['bar'],
+      },
     });
   });
 });
