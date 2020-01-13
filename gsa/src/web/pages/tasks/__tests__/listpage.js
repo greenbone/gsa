9f132ba7c1f957989b8fd2d448dbe0fcb32bfcb7
Sarah Diedrich <sarah.diedrich@greenbone.net>
2020-01-13 12:07:55 +0100

    add render test for task listpage

--- gsa/src/web/pages/tasks/__tests__/listpage.js
+++ gsa/src/web/pages/tasks/__tests__/listpage.js
@@ -122,6 +122,115 @@ const renewSession = jest.fn().mockResolvedValue({
 });
 
 describe('TaskPage tests', () => {
+  test('should render full TaskPage', async () => {
+    const gmp = {
+      tasks: {
+        get: getTasks,
+        getSeverityAggregates: getAggregates,
+        getHighResultsAggregates: getAggregates,
+        getStatusAggregates: getAggregates,
+      },
+      filters: {
+        get: getFilters,
+      },
+      reportformats: {
+        get: getReportFormats,
+      },
+      dashboard: {
+        getSetting: getDashboardSetting,
+      },
+      reloadInterval,
+      settings: {manualUrl},
+      user: {currentSettings, getSetting: getUserSetting},
+    };
+
+    const {render, store} = rendererWith({
+      gmp,
+      capabilities: true,
+      store: true,
+      router: true,
+    });
+
+    store.dispatch(setTimezone('CET'));
+    store.dispatch(setUsername('admin'));
+
+    const defaultSettingfilter = Filter.fromString('foo=bar');
+    store.dispatch(loadingActions.success({rowsperpage: {value: '2'}}));
+    store.dispatch(
+      defaultFilterLoadingActions.success('task', defaultSettingfilter),
+    );
+
+    const counts = new CollectionCounts({
+      first: 1,
+      all: 1,
+      filtered: 1,
+      length: 1,
+      rows: 10,
+    });
+    const filter = Filter.fromString('first=1 rows=10');
+    const loadedFilter = Filter.fromString('first=1 rows=10');
+    store.dispatch(
+      entitiesLoadingActions.success([task], filter, loadedFilter, counts),
+    );
+
+    const {baseElement, getAllByTestId} = render(<TaskPage />);
+
+    await waitForElement(() => baseElement.querySelectorAll('table'));
+
+    const display = getAllByTestId('grid-item');
+    const icons = getAllByTestId('svg-icon');
+    const inputs = baseElement.querySelectorAll('input');
+    const header = baseElement.querySelectorAll('th');
+    const row = baseElement.querySelectorAll('tr');
+    const selects = getAllByTestId('select-selected-value');
+
+    // Toolbar Icons
+    expect(icons[0]).toHaveAttribute('title', 'Help: Tasks');
+    expect(icons[1]).toHaveTextContent('wizard.svg');
+    expect(icons[2]).toHaveTextContent('new.svg');
+
+    // Powerfilter
+    expect(inputs[0]).toHaveAttribute('name', 'userFilterString');
+    expect(icons[3]).toHaveAttribute('title', 'Update Filter');
+    expect(icons[4]).toHaveAttribute('title', 'Remove Filter');
+    expect(icons[5]).toHaveAttribute('title', 'Reset to Default Filter');
+    expect(icons[6]).toHaveAttribute('title', 'Help: Powerfilter');
+    expect(icons[7]).toHaveAttribute('title', 'Edit Filter');
+    expect(selects[0]).toHaveAttribute('title', 'Loaded filter');
+    expect(selects[0]).toHaveTextContent('--');
+
+    // Dashboard
+    expect(icons[9]).toHaveAttribute('title', 'Add new Dashboard Display');
+    expect(icons[10]).toHaveAttribute('title', 'Reset to Defaults');
+    expect(display[0]).toHaveTextContent('Tasks by Severity Class (Total: 0)');
+    expect(display[1]).toHaveTextContent(
+      'Tasks with most High Results per Host',
+    );
+    expect(display[2]).toHaveTextContent('Tasks by Status (Total: 0)');
+
+    // Table
+    expect(header[0]).toHaveTextContent('Name');
+    expect(header[1]).toHaveTextContent('Status');
+    expect(header[2]).toHaveTextContent('Reports');
+    expect(header[3]).toHaveTextContent('Last Report');
+    expect(header[4]).toHaveTextContent('Severity');
+    expect(header[5]).toHaveTextContent('Trend');
+    expect(header[6]).toHaveTextContent('Actions');
+
+    expect(row[1]).toHaveTextContent('foo');
+    expect(row[1]).toHaveTextContent('(bar)');
+    expect(row[1]).toHaveTextContent('Done');
+    expect(row[1]).toHaveTextContent('Sat, Aug 10, 2019 2:51 PM CEST');
+    expect(row[1]).toHaveTextContent('5.0 (Medium)');
+
+    expect(icons[24]).toHaveAttribute('title', 'Start');
+    expect(icons[25]).toHaveAttribute('title', 'Task is not stopped');
+    expect(icons[26]).toHaveAttribute('title', 'Move Task to trashcan');
+    expect(icons[27]).toHaveAttribute('title', 'Edit Task');
+    expect(icons[28]).toHaveAttribute('title', 'Clone Task');
+    expect(icons[29]).toHaveAttribute('title', 'Export Task');
+  });
+
   test('should call commands for bulk actions', async () => {
     const deleteByFilter = jest.fn().mockResolvedValue({
       foo: 'bar',
