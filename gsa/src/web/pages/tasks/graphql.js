6ccaf51aebadcc0eb7510d6e193c90e997b0023e
Sarah Diedrich <sarah.diedrich@greenbone.net>
2020-05-05 10:21:40 +0200

    add useGetScanConfigs

--- gsa/src/web/pages/tasks/graphql.js
+++ gsa/src/web/pages/tasks/graphql.js
@@ -321,3 +321,19 @@ export const GET_SCANNERS = gql`
 export const useGetScanners = () => {
   return toFruitfulQuery(useLazyQuery)(GET_SCANNERS);
 };
+
+export const GET_SCAN_CONFIGS = gql`
+  query ScanConfigs($filterString: String) {
+    scanConfigs(filterString: $filterString) {
+      nodes {
+        name
+        type
+        id
+      }
+    }
+  }
+`;
+
+export const useGetScanConfigs = () => {
+  return toFruitfulQuery(useLazyQuery)(GET_SCAN_CONFIGS);
+};
