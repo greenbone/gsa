534fb80705a3d83679158040eeb471425b136d8f
Björn Ricks <bjoern.ricks@greenbone.net>
2020-06-11 17:05:52 +0200

    Test logout callback in Authorized component
    If the callback function is called the user should be redirected to the
    login page.

--- gsa/src/web/__tests__/authorized.js
+++ gsa/src/web/__tests__/authorized.js
@@ -173,4 +173,56 @@ describe('Authorized tests', () => {
 
     expect(history.location.pathname).toMatch('/login');
   });
+
+  test('should redirect to login page on logout', async () => {
+    const [mock, resultFunc] = createIsAuthenticatedQueryMock();
+    let logout;
+    const subscribeToLogout = jest.fn(func => (logout = func));
+    const renewDate = date('2020-03-20');
+    const [
+      renewSessionMock,
+      renewSessionResultFunc,
+    ] = createRenewSessionQueryMock(renewDate);
+    const renewSession = jest
+      .fn()
+      .mockReturnValue(Promise.resolve({data: renewDate}));
+    const gmp = {
+      settings: {
+        enableHyperionOnly: false,
+      },
+      subscribeToLogout,
+      user: {
+        renewSession,
+      },
+    };
+    const {render, history} = rendererWith({
+      queryMocks: [mock, renewSessionMock],
+      store: true,
+      gmp,
+      router: true,
+    });
+
+    render(
+      <Authorized>
+        <div data-testid="child" />
+      </Authorized>,
+    );
+
+    expect(subscribeToLogout).toHaveBeenCalled();
+
+    expect(screen.getByTestId('loading')).toBeInTheDocument();
+
+    await wait();
+
+    expect(resultFunc).toHaveBeenCalled();
+    expect(renewSessionResultFunc).toHaveBeenCalled();
+
+    expect(screen.getByTestId('child')).toBeInTheDocument();
+
+    logout();
+
+    await wait();
+
+    expect(history.location.pathname).toMatch('/login');
+  });
 });
