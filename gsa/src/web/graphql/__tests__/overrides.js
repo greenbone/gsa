509c445f9dd690900eb38cf1352cbd2abb0b0fc2
Steffen Waterkamp <steffen.waterkamp@greenbone.net>
2020-08-20 12:00:03 +0200

    Satisfy linter

--- gsa/src/web/graphql/__tests__/overrides.js
+++ gsa/src/web/graphql/__tests__/overrides.js
@@ -17,7 +17,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-import React, {useState} from 'react';
+import React from 'react';
 
 import {isDefined} from 'gmp/utils/identity';
 
