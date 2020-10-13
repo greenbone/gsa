2b449b263b7a4f9c345471af2478f51b7e02c5ae
Sarah Diedrich <sarah.diedrich@greenbone.net>
2020-10-13 14:19:36 +0200

    fix linter warning

--- gsa/src/web/graphql/__tests__/credentials.js
+++ gsa/src/web/graphql/__tests__/credentials.js
@@ -16,6 +16,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+/* eslint-disable react/prop-types */
+
 import React from 'react';
 
 import {isDefined} from 'gmp/utils/identity';
