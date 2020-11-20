f4fc7125814c308ce082ee70c78a6e8b4aa83523
Crystal Lai <crystal.lai@greenbone.net>
2020-11-20 13:54:28 +0100

    Get rid of unnecessary disabling of eslint

--- gsa/src/web/entity/useEntityTimeout.js
+++ gsa/src/web/entity/useEntityTimeout.js
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-/* eslint-disable react/prop-types */
 
 import {useCallback} from 'react';
 import {isDefined} from 'gmp/utils/identity';
