622be16e41755b43504f261dffc237a0d847b660
Crystal Lai <crystal.lai@greenbone.net>
2021-01-22 11:18:10 +0100

    Refactor policy listpage

--- gsa/src/web/pages/policies/listpage.js
+++ gsa/src/web/pages/policies/listpage.js
@@ -15,11 +15,16 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import _ from 'gmp/locale';
 
 import {RESET_FILTER, SCANCONFIGS_FILTER_FILTER} from 'gmp/models/filter';
+
+import {hasValue} from 'gmp/utils/identity';
+
+import Download from 'web/components/form/download';
+import useDownload from 'web/components/form/useDownload';
 
 import PolicyIcon from 'web/components/icon/policyicon';
 import UploadIcon from 'web/components/icon/uploadicon';
@@ -29,7 +34,11 @@ import ManualIcon from 'web/components/icon/manualicon';
 import IconDivider from 'web/components/layout/icondivider';
 import PageTitle from 'web/components/layout/pagetitle';
 
+import DialogNotification from 'web/components/notification/dialognotification';
+import useDialogNotification from 'web/components/notification/useDialogNotification';
+
 import EntitiesPage from 'web/entities/page';
+import useEntitiesReloadInterval from 'web/entities/useEntitiesReloadInterval';
 import withEntitiesContainer from 'web/entities/withEntitiesContainer';
 
 import {
@@ -37,8 +46,17 @@ import {
   selector as entitiesSelector,
 } from 'web/store/entities/policies';
 
+import {useLazyGetPolicies} from 'web/graphql/policies';
+
 import PropTypes from 'web/utils/proptypes';
 import withCapabilities from 'web/utils/withCapabilities';
+import useFilterSortBy from 'web/utils/useFilterSortby';
+import usePageFilter from 'web/utils/usePageFilter';
+import useChangeFilter from 'web/utils/useChangeFilter';
+import usePrevious from 'web/utils/usePrevious';
+import useReload from 'web/components/loading/useReload';
+import useSelection from 'web/utils/useSelection';
+import useUserSessionTimeout from 'web/utils/useUserSessionTimeout';
 
 import PoliciesComponent from './component';
 import Table from './table';
@@ -66,63 +84,144 @@ ToolBarIcons.propTypes = {
   onPolicyImportClick: PropTypes.func.isRequired,
 };
 
-const PoliciesPage = ({
-  onChanged,
-  onDownloaded,
-  onError,
-  onInteraction,
-  ...props
-}) => (
-  <PoliciesComponent
-    onCloned={onChanged}
-    onCloneError={onError}
-    onCreated={onChanged}
-    onDeleted={onChanged}
-    onDeleteError={onError}
-    onDownloaded={onDownloaded}
-    onDownloadError={onError}
-    onImported={onChanged}
-    onInteraction={onInteraction}
-    onSaved={onChanged}
-  >
-    {({
-      clone,
-      create,
-      createAudit,
-      delete: deleteFunc,
-      download,
-      edit,
-      import: importFunc,
-    }) => (
-      <React.Fragment>
-        <PageTitle title={_('Policies')} />
-        <EntitiesPage
-          {...props}
-          filtersFilter={SCANCONFIGS_FILTER_FILTER}
-          sectionIcon={<PolicyIcon size="large" />}
-          table={Table}
-          title={_('Policies')}
-          toolBarIcons={ToolBarIcons}
-          onError={onError}
-          onInteraction={onInteraction}
-          onPolicyImportClick={importFunc}
-          onPolicyCloneClick={clone}
-          onPolicyCreateClick={create}
-          onCreateAuditClick={createAudit}
-          onPolicyDeleteClick={deleteFunc}
-          onPolicyDownloadClick={download}
-          onPolicyEditClick={edit}
-        />
-      </React.Fragment>
-    )}
-  </PoliciesComponent>
-);
+const PoliciesPage = props => {
+  const [downloadRef, handleDownload] = useDownload();
+  const [, renewSessionTimeout] = useUserSessionTimeout();
+  const [filter, isLoadingFilter] = usePageFilter('policy');
+  const prevFilter = usePrevious(filter);
+  const simpleFilter = filter.withoutView();
+  const {
+    change: changeFilter,
+    remove: removeFilter,
+    reset: resetFilter,
+  } = useChangeFilter('policy');
+  const {
+    dialogState: notificationDialogState,
+    closeDialog: closeNotificationDialog,
+    showError,
+  } = useDialogNotification();
+  const {
+    selectionType,
+    selected = [],
+    changeSelectionType,
+    select,
+    deselect,
+  } = useSelection();
+  const [sortBy, sortDir, handleSortChange] = useFilterSortBy(
+    filter,
+    changeFilter,
+  );
 
-PoliciesPage.propTypes = {
-  onChanged: PropTypes.func.isRequired,
-  onDownloaded: PropTypes.func.isRequired,
-  onError: PropTypes.func.isRequired,
-  onInteraction: PropTypes.func.isRequired,
+  // Policy list state variables and methods
+  const [
+    getPolicies,
+    {counts, policies, error, loading: isLoading, refetch, called}, // like scan configs, pagination doesn't work with usePagination
+  ] = useLazyGetPolicies();
+
+  const timeoutFunc = useEntitiesReloadInterval(policies);
+
+  const [startReload, stopReload, hasRunningTimer] = useReload(
+    refetch,
+    timeoutFunc,
+  );
+
+  // Side effects
+  useEffect(() => {
+    // load policies initially after the filter is resolved
+    if (!isLoadingFilter && hasValue(filter) && !called) {
+      getPolicies({
+        filterString: filter.toFilterString(),
+        first: filter.get('rows'),
+      });
+    }
+  }, [isLoadingFilter, filter, getPolicies, called]);
+  useEffect(() => {
+    // reload if filter has changed
+    if (hasValue(refetch) && !filter.equals(prevFilter)) {
+      refetch({
+        filterString: filter.toFilterString(),
+        first: undefined,
+        last: undefined,
+      });
+    }
+  }, [filter, prevFilter, simpleFilter, refetch]);
+
+  useEffect(() => {
+    // start reloading if policies are available and no timer is running yet
+    if (hasValue(policies) && !hasRunningTimer) {
+      startReload();
+    }
+  }, [policies, startReload]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => stopReload, [stopReload]);
+  return (
+    <PoliciesComponent
+      onCloned={refetch}
+      onCloneError={showError}
+      onCreated={refetch}
+      onDeleted={refetch}
+      onDeleteError={showError}
+      onDownloaded={handleDownload}
+      onDownloadError={showError}
+      onImported={refetch}
+      onInteraction={renewSessionTimeout}
+      onSaved={refetch}
+    >
+      {({
+        clone,
+        create,
+        createAudit,
+        delete: deleteFunc,
+        download,
+        edit,
+        import: importFunc,
+      }) => (
+        <React.Fragment>
+          <PageTitle title={_('Policies')} />
+          <EntitiesPage
+            {...props}
+            entities={policies}
+            entitiesCounts={counts}
+            entitiesError={error}
+            entitiesSelected={selected}
+            filter={filter}
+            filtersFilter={SCANCONFIGS_FILTER_FILTER}
+            isLoading={isLoading}
+            isUpdating={isLoading}
+            sectionIcon={<PolicyIcon size="large" />}
+            selectionType={selectionType}
+            sortBy={sortBy}
+            sortDir={sortDir}
+            table={Table}
+            title={_('Policies')}
+            toolBarIcons={ToolBarIcons}
+            onCreateAuditClick={createAudit}
+            onEntitySelected={select}
+            onEntityDeselected={deselect}
+            onError={showError}
+            onFilterChanged={changeFilter}
+            onFilterCreated={changeFilter}
+            onFilterReset={resetFilter}
+            onFilterRemoved={removeFilter}
+            onInteraction={renewSessionTimeout}
+            onPolicyImportClick={importFunc}
+            onPolicyCloneClick={clone}
+            onPolicyCreateClick={create}
+            onPolicyDeleteClick={deleteFunc}
+            onPolicyDownloadClick={download}
+            onPolicyEditClick={edit}
+            onSelectionTypeChange={changeSelectionType}
+            onSortChange={handleSortChange}
+          />
+          <DialogNotification
+            {...notificationDialogState}
+            onCloseClick={closeNotificationDialog}
+          />
+          <Download ref={downloadRef} />
+        </React.Fragment>
+      )}
+    </PoliciesComponent>
+  );
 };
 
 export default withEntitiesContainer('policy', {
