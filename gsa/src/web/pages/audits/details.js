265ec196936150dccd72c45759942c155472f09d
Crystal Lai <crystal.lai@greenbone.net>
2021-02-12 15:37:08 +0100

    Refactor details

--- gsa/src/web/pages/audits/details.js
+++ gsa/src/web/pages/audits/details.js
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import {connect} from 'react-redux';
 
@@ -43,12 +43,12 @@ import DetailsBlock from 'web/entity/block';
 import {compareAlerts} from 'web/pages/tasks/details';
 
 import {
-  loadEntity as loadSchedule,
+  loadEntity as loadScheduleAction,
   selector as scheduleSelector,
 } from 'web/store/entities/schedules';
 
 import {
-  loadEntity as loadPolicy,
+  loadEntity as loadPolicyAction,
   selector as policySelector,
 } from 'web/store/entities/policies';
 
@@ -57,242 +57,238 @@ import compose from 'web/utils/compose';
 import withGmp from 'web/utils/withGmp';
 import {renderYesNo} from 'web/utils/render';
 
-class AuditDetails extends React.Component {
-  componentDidMount() {
-    const {entity} = this.props;
-
+const AuditDetails = ({entity, loadPolicy, loadSchedule, ...props}) => {
+  useEffect(() => {
     if (hasValue(entity.config)) {
-      this.props.loadPolicy(entity.config.id);
+      loadPolicy(entity.config.id);
     }
     if (hasValue(entity.schedule)) {
-      this.props.loadSchedule(entity.schedule.id);
+      loadSchedule(entity.schedule.id);
     }
+  }, [entity, loadPolicy, loadSchedule]);
+
+  const {links = true, schedule} = props;
+  const {
+    alerts,
+    autoDelete,
+    autoDeleteData,
+    averageDuration,
+    policy,
+    hostsOrdering,
+    inAssets,
+    preferences,
+    scanner,
+    schedulePeriods,
+    target,
+    reports,
+    maxChecks,
+    maxHosts,
+  } = entity;
+
+  const {lastReport} = reports;
+
+  const {iface = {}} = preferences;
+
+  let dur;
+  const hasDuration = hasValue(lastReport) && hasValue(lastReport.scanStart);
+  if (hasDuration) {
+    if (hasValue(lastReport.scanEnd)) {
+      const diff = lastReport.scanEnd.diff(lastReport.scanStart);
+      dur = duration(diff).humanize();
+    } else {
+      dur = _('Not finished yet');
+    }
+  } else {
+    dur = _('No scans yet');
   }
 
-  render() {
-    const {links = true, entity, schedule} = this.props;
-    const {
-      alerts,
-      autoDelete,
-      autoDeleteData,
-      averageDuration,
-      policy,
-      hostsOrdering,
-      inAssets,
-      preferences,
-      scanner,
-      schedulePeriods,
-      target,
-      reports,
-      maxChecks,
-      maxHosts,
-    } = entity;
+  const hasAvDuration = hasValue(averageDuration) && averageDuration > 0;
+  const avDuration = hasAvDuration ? averageDuration.humanize() : '';
 
-    const {lastReport} = reports;
-
-    const {iface = {}} = preferences;
-
-    let dur;
-    const hasDuration = hasValue(lastReport) && hasValue(lastReport.scanStart);
-    if (hasDuration) {
-      if (hasValue(lastReport.scanEnd)) {
-        const diff = lastReport.scanEnd.diff(lastReport.scanStart);
-        dur = duration(diff).humanize();
-      } else {
-        dur = _('Not finished yet');
-      }
-    } else {
-      dur = _('No scans yet');
-    }
-
-    const hasAvDuration = hasValue(averageDuration) && averageDuration > 0;
-    const avDuration = hasAvDuration ? averageDuration.humanize() : '';
-
-    return (
-      <Layout grow="1" flex="column">
-        {hasValue(target) && (
-          <DetailsBlock title={_('Target')}>
-            <DetailsLink textOnly={!links} type="target" id={target.id}>
-              {target.name}
-            </DetailsLink>
-          </DetailsBlock>
-        )}
-
-        {hasValue(alerts) && (
-          <DetailsBlock title={_('Alerts')}>
-            <HorizontalSep>
-              {alerts.sort(compareAlerts).map(alert => (
-                <span key={alert.id}>
-                  <DetailsLink textOnly={!links} type="alert" id={alert.id}>
-                    {alert.name}
-                  </DetailsLink>
-                </span>
-              ))}
-            </HorizontalSep>
-          </DetailsBlock>
-        )}
-
-        {hasValue(scanner) && (
-          <DetailsBlock title={_('Scanner')}>
-            <DetailsTable>
-              <TableBody>
-                <TableRow>
-                  <TableData>{_('Name')}</TableData>
-                  <TableData>
-                    <span>
-                      <DetailsLink
-                        textOnly={!links}
-                        type="scanner"
-                        id={scanner.id}
-                      >
-                        {scanner.name}
-                      </DetailsLink>
-                    </span>
-                  </TableData>
-                </TableRow>
-                <TableRow>
-                  <TableData>{_('Type')}</TableData>
-                  <TableData>{scannerTypeName(scanner.scannerType)}</TableData>
-                </TableRow>
-                {hasValue(policy) && (
-                  <TableRow>
-                    <TableData>{_('Policy')}</TableData>
-                    <TableData>
-                      <span>
-                        <DetailsLink
-                          textOnly={!links}
-                          type="policy"
-                          id={policy.id}
-                        >
-                          {policy.name}
-                        </DetailsLink>
-                      </span>
-                    </TableData>
-                  </TableRow>
-                )}
-                {hasValue(policy) &&
-                  policy.policyType === OPENVAS_SCAN_CONFIG_TYPE && (
-                    <TableRow>
-                      <TableData>{_('Order for target hosts')}</TableData>
-                      <TableData>{hostsOrdering}</TableData>
-                    </TableRow>
-                  )}
-                {hasValue(policy) &&
-                  policy.policyType === OPENVAS_SCAN_CONFIG_TYPE && (
-                    <TableRow>
-                      <TableData>{_('Network Source Interface')}</TableData>
-                      <TableData>{iface.value}</TableData>
-                    </TableRow>
-                  )}
-                {hasValue(policy) &&
-                  policy.policyType === OPENVAS_SCAN_CONFIG_TYPE &&
-                  hasValue(maxChecks) && (
-                    <TableRow>
-                      <TableData>
-                        {_('Maximum concurrently executed NVTs per host')}
-                      </TableData>
-                      <TableData>{maxChecks}</TableData>
-                    </TableRow>
-                  )}
-                {hasValue(policy) &&
-                  policy.policyType === OPENVAS_SCAN_CONFIG_TYPE &&
-                  hasValue(maxHosts) && (
-                    <TableRow>
-                      <TableData>
-                        {_('Maximum concurrently scanned hosts')}
-                      </TableData>
-                      <TableData>{maxHosts}</TableData>
-                    </TableRow>
-                  )}
-              </TableBody>
-            </DetailsTable>
-          </DetailsBlock>
-        )}
-
-        <DetailsBlock title={_('Assets')}>
-          <DetailsTable>
-            <TableBody>
-              <TableRow>
-                <TableData>{_('Add to Assets')}</TableData>
-                <TableData>{renderYesNo(inAssets)}</TableData>
-              </TableRow>
-            </TableBody>
-          </DetailsTable>
+  return (
+    <Layout grow="1" flex="column">
+      {hasValue(target) && (
+        <DetailsBlock title={_('Target')}>
+          <DetailsLink textOnly={!links} type="target" id={target.id}>
+            {target.name}
+          </DetailsLink>
         </DetailsBlock>
+      )}
 
-        {hasValue(schedule) && (
-          <DetailsBlock title={_('Schedule')}>
-            <DetailsTable>
-              <TableBody>
-                <TableRow>
-                  <TableData>{_('Name')}</TableData>
-                  <TableData>
-                    <span>
-                      <DetailsLink
-                        textOnly={!links}
-                        type="schedule"
-                        id={schedule.id}
-                      >
-                        {schedule.name}
-                      </DetailsLink>
-                    </span>
-                  </TableData>
-                </TableRow>
-                {hasValue(schedule.event) && (
-                  <TableRow>
-                    <TableData>{_('Next')}</TableData>
-                    <TableData>
-                      <DateTime date={schedule.event.nextDate} />
-                    </TableData>
-                  </TableRow>
-                )}
-              </TableBody>
-            </DetailsTable>
-          </DetailsBlock>
-        )}
+      {hasValue(alerts) && (
+        <DetailsBlock title={_('Alerts')}>
+          <HorizontalSep>
+            {alerts.sort(compareAlerts).map(alert => (
+              <span key={alert.id}>
+                <DetailsLink textOnly={!links} type="alert" id={alert.id}>
+                  {alert.name}
+                </DetailsLink>
+              </span>
+            ))}
+          </HorizontalSep>
+        </DetailsBlock>
+      )}
 
-        <DetailsBlock title={_('Scan')}>
+      {hasValue(scanner) && (
+        <DetailsBlock title={_('Scanner')}>
           <DetailsTable>
             <TableBody>
               <TableRow>
-                <TableData>{_('Duration of last Scan')}</TableData>
-                <TableData>{dur}</TableData>
-              </TableRow>
-              {hasAvDuration && (
-                <TableRow>
-                  <TableData>{_('Average Scan duration')}</TableData>
-                  <TableData>{avDuration}</TableData>
-                </TableRow>
-              )}
-              {schedulePeriods > 0 && (
-                <TableRow>
-                  <TableData>{_('Period')}</TableData>
-                  <TableData>
-                    {schedulePeriods > 1
-                      ? _('{{nr}} more times', {nr: schedulePeriods})
-                      : _('Once')}
-                  </TableData>
-                </TableRow>
-              )}
-              <TableRow>
-                <TableData>{_('Auto delete Reports')}</TableData>
+                <TableData>{_('Name')}</TableData>
                 <TableData>
-                  {autoDelete === 'keep'
-                    ? _(
-                        'Automatically delete oldest reports but always keep ' +
-                          'newest {{nr}} reports',
-                        {nr: autoDeleteData},
-                      )
-                    : _('Do not automatically delete reports')}
+                  <span>
+                    <DetailsLink
+                      textOnly={!links}
+                      type="scanner"
+                      id={scanner.id}
+                    >
+                      {scanner.name}
+                    </DetailsLink>
+                  </span>
                 </TableData>
               </TableRow>
+              <TableRow>
+                <TableData>{_('Type')}</TableData>
+                <TableData>{scannerTypeName(scanner.scannerType)}</TableData>
+              </TableRow>
+              {hasValue(policy) && (
+                <TableRow>
+                  <TableData>{_('Policy')}</TableData>
+                  <TableData>
+                    <span>
+                      <DetailsLink
+                        textOnly={!links}
+                        type="policy"
+                        id={policy.id}
+                      >
+                        {policy.name}
+                      </DetailsLink>
+                    </span>
+                  </TableData>
+                </TableRow>
+              )}
+              {hasValue(policy) &&
+                policy.policyType === OPENVAS_SCAN_CONFIG_TYPE && (
+                  <TableRow>
+                    <TableData>{_('Order for target hosts')}</TableData>
+                    <TableData>{hostsOrdering}</TableData>
+                  </TableRow>
+                )}
+              {hasValue(policy) &&
+                policy.policyType === OPENVAS_SCAN_CONFIG_TYPE && (
+                  <TableRow>
+                    <TableData>{_('Network Source Interface')}</TableData>
+                    <TableData>{iface.value}</TableData>
+                  </TableRow>
+                )}
+              {hasValue(policy) &&
+                policy.policyType === OPENVAS_SCAN_CONFIG_TYPE &&
+                hasValue(maxChecks) && (
+                  <TableRow>
+                    <TableData>
+                      {_('Maximum concurrently executed NVTs per host')}
+                    </TableData>
+                    <TableData>{maxChecks}</TableData>
+                  </TableRow>
+                )}
+              {hasValue(policy) &&
+                policy.policyType === OPENVAS_SCAN_CONFIG_TYPE &&
+                hasValue(maxHosts) && (
+                  <TableRow>
+                    <TableData>
+                      {_('Maximum concurrently scanned hosts')}
+                    </TableData>
+                    <TableData>{maxHosts}</TableData>
+                  </TableRow>
+                )}
             </TableBody>
           </DetailsTable>
         </DetailsBlock>
-      </Layout>
-    );
-  }
-}
+      )}
+
+      <DetailsBlock title={_('Assets')}>
+        <DetailsTable>
+          <TableBody>
+            <TableRow>
+              <TableData>{_('Add to Assets')}</TableData>
+              <TableData>{renderYesNo(inAssets)}</TableData>
+            </TableRow>
+          </TableBody>
+        </DetailsTable>
+      </DetailsBlock>
+
+      {hasValue(schedule) && (
+        <DetailsBlock title={_('Schedule')}>
+          <DetailsTable>
+            <TableBody>
+              <TableRow>
+                <TableData>{_('Name')}</TableData>
+                <TableData>
+                  <span>
+                    <DetailsLink
+                      textOnly={!links}
+                      type="schedule"
+                      id={schedule.id}
+                    >
+                      {schedule.name}
+                    </DetailsLink>
+                  </span>
+                </TableData>
+              </TableRow>
+              {hasValue(schedule.event) && (
+                <TableRow>
+                  <TableData>{_('Next')}</TableData>
+                  <TableData>
+                    <DateTime date={schedule.event.nextDate} />
+                  </TableData>
+                </TableRow>
+              )}
+            </TableBody>
+          </DetailsTable>
+        </DetailsBlock>
+      )}
+
+      <DetailsBlock title={_('Scan')}>
+        <DetailsTable>
+          <TableBody>
+            <TableRow>
+              <TableData>{_('Duration of last Scan')}</TableData>
+              <TableData>{dur}</TableData>
+            </TableRow>
+            {hasAvDuration && (
+              <TableRow>
+                <TableData>{_('Average Scan duration')}</TableData>
+                <TableData>{avDuration}</TableData>
+              </TableRow>
+            )}
+            {schedulePeriods > 0 && (
+              <TableRow>
+                <TableData>{_('Period')}</TableData>
+                <TableData>
+                  {schedulePeriods > 1
+                    ? _('{{nr}} more times', {nr: schedulePeriods})
+                    : _('Once')}
+                </TableData>
+              </TableRow>
+            )}
+            <TableRow>
+              <TableData>{_('Auto delete Reports')}</TableData>
+              <TableData>
+                {autoDelete === 'keep'
+                  ? _(
+                      'Automatically delete oldest reports but always keep ' +
+                        'newest {{nr}} reports',
+                      {nr: autoDeleteData},
+                    )
+                  : _('Do not automatically delete reports')}
+              </TableData>
+            </TableRow>
+          </TableBody>
+        </DetailsTable>
+      </DetailsBlock>
+    </Layout>
+  );
+};
 
 AuditDetails.propTypes = {
   entity: PropTypes.model.isRequired,
@@ -318,8 +314,8 @@ const mapStateToProps = (rootState, {entity = {}}) => {
 };
 
 const mapDispatchToProps = (dispatch, {gmp}) => ({
-  loadPolicy: id => dispatch(loadPolicy(gmp)(id)),
-  loadSchedule: id => dispatch(loadSchedule(gmp)(id)),
+  loadPolicy: id => dispatch(loadPolicyAction(gmp)(id)),
+  loadSchedule: id => dispatch(loadScheduleAction(gmp)(id)),
 });
 
 export default compose(
