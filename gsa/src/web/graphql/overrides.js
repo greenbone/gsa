ddecc435b79e723299a6cf846dabc90e6db48d90
Steffen Waterkamp <swaterkamp@gmx.de>
2021-02-23 07:34:03 +0100

    Implement override mutations

--- gsa/src/web/graphql/overrides.js
+++ gsa/src/web/graphql/overrides.js
@@ -18,7 +18,7 @@
 
 import {useCallback} from 'react';
 
-import {useLazyQuery} from '@apollo/client';
+import {useLazyQuery, useMutation, useQuery} from '@apollo/client';
 
 import gql from 'graphql-tag';
 
@@ -95,4 +95,241 @@ export const useLazyGetOverrides = (variables, options) => {
   return [getOverrides, {...other, counts, overrides, pageInfo}];
 };
 
+export const GET_OVERRIDE = gql`
+  query Override($id: UUID!) {
+    override(id: $id) {
+      id
+      active
+      creationTime
+      endTime
+      hosts
+      inUse
+      modificationTime
+      nvt {
+        id
+        name
+      }
+      owner
+      result {
+        id
+        name
+      }
+      permissions {
+        name
+      }
+      port
+      severity
+      task {
+        id
+        name
+      }
+      text
+      userTags {
+        count
+        tags {
+          name
+          id
+          value
+          comment
+        }
+      }
+      writable
+    }
+  }
+`;
+
+export const useGetOverride = (id, options) => {
+  const {data, ...other} = useQuery(GET_OVERRIDE, {
+    ...options,
+    variables: {id},
+  });
+  const override = isDefined(data?.override)
+    ? Override.fromObject(data.override)
+    : undefined;
+  return {override, ...other};
+};
+
+export const CREATE_OVERRIDE = gql`
+  mutation createOverride($input: CreateOverrideInput!) {
+    createOverride(input: $input) {
+      id
+    }
+  }
+`;
+
+export const useCreateOverride = options => {
+  const [queryCreateOverride, {data, ...other}] = useMutation(
+    CREATE_OVERRIDE,
+    options,
+  );
+
+  const createOverride = useCallback(
+    // eslint-disable-next-line no-shadow
+    (inputObject, options) =>
+      queryCreateOverride({...options, variables: {input: inputObject}}),
+    [queryCreateOverride],
+  );
+  const overrideId = data?.createOverride?.id;
+  return [createOverride, {...other, id: overrideId}];
+};
+
+export const MODIFY_OVERRIDE = gql`
+  mutation modifyOverride($input: ModifyOverrideInput!) {
+    modifyOverride(input: $input) {
+      ok
+    }
+  }
+`;
+
+export const useModifyOverride = options => {
+  const [queryModifyOverride, data] = useMutation(MODIFY_OVERRIDE, options);
+  const modifyOverride = useCallback(
+    // eslint-disable-next-line no-shadow
+    (inputObject, options) =>
+      queryModifyOverride({...options, variables: {input: inputObject}}),
+    [queryModifyOverride],
+  );
+  return [modifyOverride, data];
+};
+
+export const DELETE_OVERRIDES_BY_IDS = gql`
+  mutation deleteOverridesByIds($ids: [UUID]!) {
+    deleteOverridesByIds(ids: $ids) {
+      ok
+    }
+  }
+`;
+
+export const useDeleteOverridesByIds = options => {
+  const [queryDeleteOverridesByIds, data] = useMutation(
+    DELETE_OVERRIDES_BY_IDS,
+    options,
+  );
+  const deleteOverridesByIds = useCallback(
+    // eslint-disable-next-line no-shadow
+    (ids, options) => queryDeleteOverridesByIds({...options, variables: {ids}}),
+    [queryDeleteOverridesByIds],
+  );
+  return [deleteOverridesByIds, data];
+};
+
+export const useDeleteOverride = options => {
+  const [queryDeleteOverride, data] = useMutation(
+    DELETE_OVERRIDES_BY_IDS,
+    options,
+  );
+  const deleteOverride = useCallback(
+    // eslint-disable-next-line no-shadow
+    (id, options) => queryDeleteOverride({...options, variables: {ids: [id]}}),
+    [queryDeleteOverride],
+  );
+  return [deleteOverride, data];
+};
+
+export const DELETE_OVERRIDES_BY_FILTER = gql`
+  mutation deleteOverridesByFilter($filterString: String!) {
+    deleteOverridesByFilter(filterString: $filterString) {
+      ok
+    }
+  }
+`;
+
+export const useDeleteOverridesByFilter = options => {
+  const [queryDeleteOverridesByFilter, data] = useMutation(
+    DELETE_OVERRIDES_BY_FILTER,
+    options,
+  );
+  const deleteOverridesByFilter = useCallback(
+    // eslint-disable-next-line no-shadow
+    (filterString, options) =>
+      queryDeleteOverridesByFilter({
+        ...options,
+        variables: {filterString},
+      }),
+    [queryDeleteOverridesByFilter],
+  );
+  return [deleteOverridesByFilter, data];
+};
+
+export const EXPORT_OVERRIDES_BY_FILTER = gql`
+  mutation exportOverridesByFilter($filterString: String) {
+    exportOverridesByFilter(filterString: $filterString) {
+      exportedEntities
+    }
+  }
+`;
+
+export const useExportOverridesByFilter = options => {
+  const [queryExportOverridesByFilter] = useMutation(
+    EXPORT_OVERRIDES_BY_FILTER,
+    options,
+  );
+  const exportOverridesByFilter = useCallback(
+    // eslint-disable-next-line no-shadow
+    filterString =>
+      queryExportOverridesByFilter({
+        ...options,
+        variables: {
+          filterString,
+        },
+      }),
+    [queryExportOverridesByFilter, options],
+  );
+
+  return exportOverridesByFilter;
+};
+
+export const EXPORT_OVERRIDES_BY_IDS = gql`
+  mutation exportOverridesByIds($ids: [UUID]!) {
+    exportOverridesByIds(ids: $ids) {
+      exportedEntities
+    }
+  }
+`;
+
+export const useExportOverridesByIds = options => {
+  const [queryExportOverridesByIds] = useMutation(
+    EXPORT_OVERRIDES_BY_IDS,
+    options,
+  );
+
+  const exportOverridesByIds = useCallback(
+    // eslint-disable-next-line no-shadow
+    overrideIds =>
+      queryExportOverridesByIds({
+        ...options,
+        variables: {
+          ids: overrideIds,
+        },
+      }),
+    [queryExportOverridesByIds, options],
+  );
+
+  return exportOverridesByIds;
+};
+
+export const CLONE_OVERRIDE = gql`
+  mutation cloneOverride($id: UUID!) {
+    cloneOverride(id: $id) {
+      id
+    }
+  }
+`;
+
+export const useCloneOverride = options => {
+  const [queryCloneOverride, {data, ...other}] = useMutation(
+    CLONE_OVERRIDE,
+    options,
+  );
+  const cloneOverride = useCallback(
+    // eslint-disable-next-line no-shadow
+    (id, options) =>
+      queryCloneOverride({...options, variables: {id}}).then(
+        result => result.data.cloneOverride.id,
+      ),
+    [queryCloneOverride],
+  );
+  const overrideId = data?.cloneOverride?.id;
+  return [cloneOverride, {...other, id: overrideId}];
+};
 // vim: set ts=2 sw=2 tw=80:
