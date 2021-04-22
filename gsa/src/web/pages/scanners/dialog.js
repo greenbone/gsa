42310dc55033c00c6170163eeed3ae53dc7d85ed
Björn Ricks <bjoern.ricks@greenbone.net>
2021-04-22 17:05:32 +0200

    Set default scanner type only if it is not set yet

--- gsa/src/web/pages/scanners/dialog.js
+++ gsa/src/web/pages/scanners/dialog.js
@@ -24,7 +24,7 @@ import _ from 'gmp/locale';
 import {longDate} from 'gmp/locale/date';
 
 import {filter, map} from 'gmp/utils/array';
-import {isDefined} from 'gmp/utils/identity';
+import {hasValue, isDefined} from 'gmp/utils/identity';
 import {selectSaveId} from 'gmp/utils/id';
 
 import {parseInt} from 'gmp/parser';
@@ -159,10 +159,10 @@ const ScannerDialog = ({
   const {gmp} = props;
 
   if (gmp.settings.enableGreenboneSensor) {
-    type = GREENBONE_SENSOR_SCANNER_TYPE;
+    type = hasValue(type) ? type : GREENBONE_SENSOR_SCANNER_TYPE;
     SCANNER_TYPES = [GREENBONE_SENSOR_SCANNER_TYPE, OSP_SCANNER_TYPE];
   } else {
-    type = OSP_SCANNER_TYPE;
+    type = hasValue(type) ? type : OSP_SCANNER_TYPE;
     SCANNER_TYPES = [OSP_SCANNER_TYPE];
   }
 
