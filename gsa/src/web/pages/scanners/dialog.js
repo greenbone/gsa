569560a9a875a2456a1d52ddd3254bb6a0fc373d
Björn Ricks <bjoern.ricks@greenbone.net>
2021-04-22 16:02:34 +0200

    Pre-select greenbone sensor als default scanner type
    For creating a scanner use the greenbone sensor as default scanner type
    when opening the dialog.

--- gsa/src/web/pages/scanners/dialog.js
+++ gsa/src/web/pages/scanners/dialog.js
@@ -118,7 +118,7 @@ const ScannerDialog = ({
   name = _('Unnamed'),
   port = '22',
   title = _('New Scanner'),
-  type = OSP_SCANNER_TYPE,
+  type,
   which_cert,
   onClose,
   onCredentialChange,
@@ -159,8 +159,10 @@ const ScannerDialog = ({
   const {gmp} = props;
 
   if (gmp.settings.enableGreenboneSensor) {
-    SCANNER_TYPES = [OSP_SCANNER_TYPE, GREENBONE_SENSOR_SCANNER_TYPE];
+    type = GREENBONE_SENSOR_SCANNER_TYPE;
+    SCANNER_TYPES = [GREENBONE_SENSOR_SCANNER_TYPE, OSP_SCANNER_TYPE];
   } else {
+    type = OSP_SCANNER_TYPE;
     SCANNER_TYPES = [OSP_SCANNER_TYPE];
   }
 
