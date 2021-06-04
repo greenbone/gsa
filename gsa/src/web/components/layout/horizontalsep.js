6bf8ea8da9c11f3061eed94f63a0aa9785efabed
Steffen Waterkamp <swaterkamp@gmx.de>
2021-06-04 11:09:43 +0200

    Allow to wrap HorizontalSep

--- gsa/src/web/components/layout/horizontalsep.js
+++ gsa/src/web/components/layout/horizontalsep.js
@@ -17,11 +17,14 @@
  */
 import styled from 'styled-components';
 
+import {isDefined} from 'gmp/utils/identity';
+
 import PropTypes from 'web/utils/proptypes';
 
 import Divider from './divider';
 
 const HorizontalSep = styled(Divider)`
+  flex-wrap: ${props => (isDefined(props.wrap) ? props.wrap : null)};
   & > *:not(:last-child)::after {
     content: ${({separator = '•'}) => `'${separator}'`};
     margin-left: ${({spacing = '5px'}) => spacing};
@@ -31,6 +34,7 @@ const HorizontalSep = styled(Divider)`
 HorizontalSep.propTypes = {
   separator: PropTypes.string,
   spacing: PropTypes.string,
+  wrap: PropTypes.oneOf([true, 'wrap', 'nowrap']),
 };
 
 export default HorizontalSep;
