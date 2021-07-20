b08565f758b8aaa736c593158ba02fd46c932f4a
Björn Ricks <bjoern.ricks@greenbone.net>
2021-07-20 15:35:11 +0200

    Update copyright for --help output
    (cherry picked from commit a6a9ceaf01aff75126cbd76bbf1a505725ee7d5e)

--- gsad/src/gsad.c
+++ gsad/src/gsad.c
@@ -174,7 +174,7 @@
 #define DEFAULT_GSAD_PER_IP_CONNECTION_LIMIT 30
 
 #define COPYRIGHT                                                        \
-  "Copyright (C) 2010 - 2020 Greenbone Networks GmbH\n"                  \
+  "Copyright (C) 2010 - 2021 Greenbone Networks GmbH\n"                  \
   "License: AGPL-3.0-or-later\n"                                         \
   "This is free software: you are free to change and redistribute it.\n" \
   "There is NO WARRANTY, to the extent permitted by law.\n\n"
