6c49a9b67255a3e42b9a404287ab03432bec350b
Steffen Waterkamp <swaterkamp@gmx.de>
2022-01-12 13:08:22 +0100

    Add new Greenbone Logo with text for login page

---
+++ src/web/components/img/greenboneloginlogo.js
@@ -0,0 +1,40 @@
+/* Copyright (C) 2019-2021 Greenbone Networks GmbH
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from 'react';
+
+import styled from 'styled-components';
+
+import _ from 'gmp/locale';
+
+import Img from './img';
+
+const Image = styled(Img)`
+  display: flex;
+  width: 300px;
+`;
+
+const LoginLogo = props => {
+  return (
+    <Image alt={_('Greenbone Networks GmbH')} src={'greenbonehorizontal.png'} />
+  );
+};
+
+export default LoginLogo;
+
+// vim: set ts=2 sw=2 tw=80:
