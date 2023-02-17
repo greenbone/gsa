b797b7307f4b8f32e2d7d04043e0e2b8335a7e81
swaterkamp <steffen.waterkamp@greenbone.net>
2023-02-17 16:51:56 +0100

    Add tests for radiuskey adjustments
    (cherry picked from commit ca7265a05e15c0796a27d6aa117ef0d8d5983b1c)

---
+++ src/web/pages/radius/__tests__/radiuspage.js
@@ -0,0 +1,81 @@
+/* Copyright (C) 2023 Greenbone Networks GmbH
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import Settings from 'gmp/models/settings';
+import {rendererWith, fireEvent, wait} from 'web/utils/testing';
+
+import RadiusAuthentication from '../radiuspage';
+
+describe('RADIUS page renders', () => {
+  test('should render page with no radius key', async () => {
+    const settings = new Settings();
+    settings.set('method:radius_connect', {
+      enabled: 'true',
+      radiushost: 'foo',
+    });
+    const gmp = {
+      user: {
+        currentAuthSettings: jest.fn().mockReturnValue(
+          Promise.resolve({
+            data: settings,
+          }),
+        ),
+      },
+      settings: {
+        manualUrl: 'http://docs.greenbone.net/GSM-Manual/gos-5/',
+      },
+    };
+
+    const {render} = rendererWith({gmp, store: true});
+
+    const {element, queryByText} = render(<RadiusAuthentication />);
+
+    await wait();
+
+    expect(queryByText('********')).not.toBeInTheDocument();
+  });
+  test('should show ******** instead of a key', async () => {
+    const settings = new Settings();
+    settings.set('method:radius_connect', {
+      enabled: 'true',
+      radiushost: 'foo',
+      radiuskey: '********',
+    });
+    const gmp = {
+      user: {
+        currentAuthSettings: jest.fn().mockReturnValue(
+          Promise.resolve({
+            data: settings,
+          }),
+        ),
+      },
+      settings: {
+        manualUrl: 'http://docs.greenbone.net/GSM-Manual/gos-5/',
+      },
+    };
+
+    const {render} = rendererWith({gmp, store: true});
+
+    const {element, getByText} = render(<RadiusAuthentication />);
+
+    await wait();
+
+    expect(getByText('********')).toBeInTheDocument();
+  });
+});
