7b9ae843fe92b2eab7d991114455fede9270f6b7
Timo Pollmeier <timo.pollmeier@greenbone.net>
2023-04-18 12:16:22 +0200

    Revert "Fix: "Default" label of SMB Max Version dropdown"
    This reverts commit 5b81450915a837eff95973e3fc0ab6b17a3a020e.

--- src/web/pages/alerts/smbmethodpart.js
+++ src/web/pages/alerts/smbmethodpart.js
@@ -18,7 +18,6 @@
 import React from 'react';
 
 import _ from 'gmp/locale';
-import {_l} from 'gmp/locale/lang';
 
 import {
   SMB_CREDENTIAL_TYPES,
@@ -40,7 +39,7 @@ import TextField from 'web/components/form/textfield';
 import NewIcon from 'web/components/icon/newicon';
 
 const smbMaxProtocolItems = [
-  {label: _l('Default'), value: ''},
+  {label: _('Default'), value: ''},
   {label: 'NT1', value: 'NT1'},
   {label: 'SMB2', value: 'SMB2'},
   {label: 'SMB3', value: 'SMB3'},
