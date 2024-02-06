f01e1453a40d197ddb2c420afbe87d54b081c6a9
Ahmed Abdelsalam <ahmed.abdelsalam@greenbone.net>
2024-02-06 17:48:31 +0100

    Adding tests for audit reports actions and gmp commands

---
+++ src/gmp/commands/__tests__/auditreport.js
@@ -0,0 +1,44 @@
+/* Copyright (C) 2024 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import {createHttp, createEntityResponse} from '../testing';
+import {AuditReportCommand} from '../auditreports';
+
+describe('ReportCommand tests', () => {
+  test('should request single report', () => {
+    const response = createEntityResponse('report', {_id: 'foo'});
+    const fakeHttp = createHttp(response);
+
+    expect.hasAssertions();
+
+    const cmd = new AuditReportCommand(fakeHttp);
+    return cmd.get({id: 'foo'}).then(resp => {
+      expect(fakeHttp.request).toHaveBeenCalledWith('get', {
+        args: {
+          cmd: 'get_report',
+          report_id: 'foo',
+          ignore_pagination: 1,
+          details: 1,
+          lean: 1,
+        },
+      });
+
+      const {data} = resp;
+      expect(data.id).toEqual('foo');
+    });
+  });
+});
