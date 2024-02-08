05218240a754f351eaaa0b1833d7f0a1bcba92d2
Ahmed Abdelsalam <ahmed.abdelsalam@greenbone.net>
2024-02-08 17:02:48 +0100

    Added tests for audit delta reports and compliance power filter

---
+++ src/web/components/powerfilter/__tests__/compliancelevelsgroup.js
@@ -0,0 +1,252 @@
+/* Copyright (C) 2024 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+import React from 'react';
+
+import {render, fireEvent} from 'web/utils/testing';
+
+import ComplianceLevelsFilterGroup from '../compliancelevelsgroup';
+
+import Filter from 'gmp/models/filter';
+
+describe('ComplianceLevelsFilterGroup audit reports tests', () => {
+  test('should call change handler', () => {
+    const filter = Filter.fromString('report_compliance_levels=');
+    const handleChange = jest.fn();
+    const handleRemove = jest.fn();
+    const {element} = render(
+      <ComplianceLevelsFilterGroup
+        filter={filter}
+        onChange={handleChange}
+        onRemove={handleRemove}
+      />,
+    );
+
+    const checkbox = element.querySelectorAll('input');
+    fireEvent.click(checkbox[0]);
+
+    expect(handleChange).toHaveBeenCalled();
+    expect(handleChange).toHaveBeenCalledWith('y', 'report_compliance_levels');
+  });
+
+  test('should check checkbox', () => {
+    const filter = Filter.fromString('report_compliance_levels=yn');
+    const handleChange = jest.fn();
+    const handleRemove = jest.fn();
+    const {element} = render(
+      <ComplianceLevelsFilterGroup
+        filter={filter}
+        onChange={handleChange}
+        onRemove={handleRemove}
+      />,
+    );
+
+    const checkbox = element.querySelectorAll('input');
+
+    expect(checkbox[0].checked).toEqual(true);
+    expect(checkbox[1].checked).toEqual(true);
+  });
+
+  test('should uncheck checkbox', () => {
+    const filter1 = Filter.fromString('report_compliance_levels=yni');
+    const filter2 = Filter.fromString('report_compliance_levels=yn');
+    const handleChange = jest.fn();
+    const handleRemove = jest.fn();
+    const {element, rerender} = render(
+      <ComplianceLevelsFilterGroup
+        filter={filter1}
+        onChange={handleChange}
+        onRemove={handleRemove}
+      />,
+    );
+
+    const checkbox = element.querySelectorAll('input');
+
+    expect(checkbox[0].checked).toEqual(true);
+    expect(checkbox[1].checked).toEqual(true);
+    expect(checkbox[1].checked).toEqual(true);
+
+    rerender(
+      <ComplianceLevelsFilterGroup
+        filter={filter2}
+        onChange={handleChange}
+        onRemove={handleRemove}
+      />,
+    );
+
+    expect(checkbox[0].checked).toEqual(true);
+    expect(checkbox[1].checked).toEqual(true);
+    expect(checkbox[2].checked).toEqual(false);
+  });
+
+  test('should be unchecked by default', () => {
+    const filter = Filter.fromString();
+    const handleChange = jest.fn();
+    const handleRemove = jest.fn();
+    const {element} = render(
+      <ComplianceLevelsFilterGroup
+        filter={filter}
+        onChange={handleChange}
+        onRemove={handleRemove}
+      />,
+    );
+
+    const checkbox = element.querySelectorAll('input');
+
+    expect(checkbox[0].checked).toEqual(false);
+    expect(checkbox[1].checked).toEqual(false);
+    expect(checkbox[2].checked).toEqual(false);
+    expect(checkbox[3].checked).toEqual(false);
+  });
+
+  test('should call remove handler', () => {
+    const filter = Filter.fromString('report_compliance_levels=y');
+    const handleChange = jest.fn();
+    const handleRemove = jest.fn();
+    const {element} = render(
+      <ComplianceLevelsFilterGroup
+        filter={filter}
+        onChange={handleChange}
+        onRemove={handleRemove}
+      />,
+    );
+
+    const checkbox = element.querySelectorAll('input');
+    expect(checkbox[0].checked).toEqual(true);
+
+    fireEvent.click(checkbox[0]);
+
+    expect(handleRemove).toHaveBeenCalled();
+  });
+});
+
+describe('ComplianceLevelsFilterGroup audit results tests', () => {
+  test('should call change handler', () => {
+    const filter = Filter.fromString('compliance_levels=');
+    const handleChange = jest.fn();
+    const handleRemove = jest.fn();
+    const {element} = render(
+      <ComplianceLevelsFilterGroup
+        filter={filter}
+        isResult={true}
+        onChange={handleChange}
+        onRemove={handleRemove}
+      />,
+    );
+
+    const checkbox = element.querySelectorAll('input');
+    fireEvent.click(checkbox[0]);
+
+    expect(handleChange).toHaveBeenCalled();
+    expect(handleChange).toHaveBeenCalledWith('y', 'compliance_levels');
+  });
+
+  test('should check checkbox', () => {
+    const filter = Filter.fromString('compliance_levels=yn');
+    const handleChange = jest.fn();
+    const handleRemove = jest.fn();
+    const {element} = render(
+      <ComplianceLevelsFilterGroup
+        filter={filter}
+        isResult={true}
+        onChange={handleChange}
+        onRemove={handleRemove}
+      />,
+    );
+
+    const checkbox = element.querySelectorAll('input');
+
+    expect(checkbox[0].checked).toEqual(true);
+    expect(checkbox[1].checked).toEqual(true);
+  });
+
+  test('should uncheck checkbox', () => {
+    const filter1 = Filter.fromString('compliance_levels=yni');
+    const filter2 = Filter.fromString('compliance_levels=yn');
+    const handleChange = jest.fn();
+    const handleRemove = jest.fn();
+    const {element, rerender} = render(
+      <ComplianceLevelsFilterGroup
+        filter={filter1}
+        isResult={true}
+        onChange={handleChange}
+        onRemove={handleRemove}
+      />,
+    );
+
+    const checkbox = element.querySelectorAll('input');
+
+    expect(checkbox[0].checked).toEqual(true);
+    expect(checkbox[1].checked).toEqual(true);
+    expect(checkbox[1].checked).toEqual(true);
+
+    rerender(
+      <ComplianceLevelsFilterGroup
+        filter={filter2}
+        isResult={true}
+        onChange={handleChange}
+        onRemove={handleRemove}
+      />,
+    );
+
+    expect(checkbox[0].checked).toEqual(true);
+    expect(checkbox[1].checked).toEqual(true);
+    expect(checkbox[2].checked).toEqual(false);
+  });
+
+  test('should be unchecked by default', () => {
+    const filter = Filter.fromString();
+    const handleChange = jest.fn();
+    const handleRemove = jest.fn();
+    const {element} = render(
+      <ComplianceLevelsFilterGroup
+        filter={filter}
+        isResult={true}
+        onChange={handleChange}
+        onRemove={handleRemove}
+      />,
+    );
+
+    const checkbox = element.querySelectorAll('input');
+
+    expect(checkbox[0].checked).toEqual(false);
+    expect(checkbox[1].checked).toEqual(false);
+    expect(checkbox[2].checked).toEqual(false);
+    expect(checkbox[3].checked).toEqual(false);
+  });
+
+  test('should call remove handler', () => {
+    const filter = Filter.fromString('compliance_levels=y');
+    const handleChange = jest.fn();
+    const handleRemove = jest.fn();
+    const {element} = render(
+      <ComplianceLevelsFilterGroup
+        filter={filter}
+        isResult={true}
+        onChange={handleChange}
+        onRemove={handleRemove}
+      />,
+    );
+
+    const checkbox = element.querySelectorAll('input');
+    expect(checkbox[0].checked).toEqual(true);
+
+    fireEvent.click(checkbox[0]);
+
+    expect(handleRemove).toHaveBeenCalled();
+  });
+});
