02f88cbfaf66eb096157b83ed672f48f691d92fa
Björn Ricks <bjoern.ricks@greenbone.net>
2024-04-16 12:08:21 +0200

    Add: Add a hook to load the user's capabilities from the backend
    The capabilities of the user are provided via a react context and
    therefore it should be able to load them independently from the redux
    store.

---
+++ src/web/hooks/useLoadCapabilities.jsx
@@ -0,0 +1,45 @@
+/* SPDX-FileCopyrightText: 2024 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import {useEffect, useState} from 'react';
+
+import logger from 'gmp/log';
+import Capabilities from 'gmp/capabilities/capabilities';
+
+import useGmp from '../utils/useGmp';
+
+const log = logger.getLogger('web.useLoadCapabilities');
+
+/**
+ * Hook to load the user's capabilities from the backend
+ */
+const useLoadCapabilities = () => {
+  const gmp = useGmp();
+  const [capabilities, setCapabilities] = useState();
+
+  useEffect(() => {
+    gmp.user
+      .currentCapabilities()
+      .then(response => {
+        const loadedCapabilities = response.data;
+        log.debug('User capabilities', loadedCapabilities);
+        setCapabilities(loadedCapabilities);
+      })
+      .catch(rejection => {
+        if (rejection.isError()) {
+          log.error(
+            'An error occurred during fetching capabilities',
+            rejection,
+          );
+        }
+        // use empty capabilities
+        setCapabilities(new Capabilities());
+      });
+  }, [gmp.user]);
+
+  return capabilities;
+};
+
+export default useLoadCapabilities;
