37f1394e790c3b51112512984e2b48ad681b818e
Björn Ricks <bjoern.ricks@greenbone.net>
2024-04-16 13:42:41 +0200

    Change: Update DialogNotification for new Dialog UI
    Adjust the DialogNotification component for new Dialog.

--- src/web/components/notification/dialognotification.jsx
+++ src/web/components/notification/dialognotification.jsx
@@ -24,35 +24,27 @@ import PropTypes from 'web/utils/proptypes';
 import useTranslation from 'web/hooks/useTranslation';
 
 import Dialog from 'web/components/dialog/dialog';
-import DialogContent from 'web/components/dialog/content';
 import DialogFooter from 'web/components/dialog/footer';
-import DialogTitle from 'web/components/dialog/title';
-import ScrollableContent from 'web/components/dialog/scrollablecontent';
 
-const DialogNotification = ({title, message, onCloseClick}) => {
+const DialogNotification = ({
+  title,
+  message,
+  onCloseClick,
+  onClose = onCloseClick,
+}) => {
   const [_] = useTranslation();
 
   if (!hasValue(message)) {
     return null;
   }
   return (
-    <Dialog width="400px" onClose={onCloseClick}>
-      {({close, moveProps, heightProps}) => (
-        <DialogContent>
-          <DialogTitle title={title} onCloseClick={close} {...moveProps} />
-          <ScrollableContent
-            {...heightProps}
-            data-testid="dialog-notification-message"
-          >
-            {message}
-          </ScrollableContent>
-          <DialogFooter
-            title={_('Close')}
-            onClick={close}
-            data-testid="dialog-notification-footer"
-          />
-        </DialogContent>
-      )}
+    <Dialog opened={true} size="400px" title={title} onClose={onClose}>
+      {message}
+      <DialogFooter
+        title={_('Close')}
+        onClick={onCloseClick}
+        data-testid="dialog-notification-footer"
+      />
     </Dialog>
   );
 };
@@ -60,6 +52,7 @@ const DialogNotification = ({title, message, onCloseClick}) => {
 DialogNotification.propTypes = {
   message: PropTypes.string,
   title: PropTypes.string,
+  onClose: PropTypes.func,
   onCloseClick: PropTypes.func,
 };
 
