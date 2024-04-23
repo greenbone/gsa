c709678974bf89b7b2192412ba2450c6ab54467f
Björn Ricks <bjoern.ricks@greenbone.net>
2024-04-23 17:11:54 +0200

    Use different testing setup for the redux store
    The redux store doesn't need a specific setup or react rendering setup.
    Therefore it can extracted into an own testing setup and at the end run
    faster.

--- vitest.projects.ts
+++ vitest.projects.ts
@@ -12,6 +12,7 @@ export default defineWorkspace([
         'src/web/**/*.{test,spec}.?(c|m)[jt]s?(x)',
         'src/web/**/__tests__/*.?(c|m)[jt]s?(x)',
       ],
+      exclude: ['src/web/store/**'],
     },
   },
   {
@@ -24,6 +25,17 @@ export default defineWorkspace([
         'src/gmp/**/*.{test,spec}.?(c|m)[jt]s?(x)',
         'src/gmp/**/__tests__/*.?(c|m)[jt]s?(x)',
         'src/__tests__/*.?(c|m)[jt]s?(x)',
+      ],
+    },
+  },
+  {
+    extends: './vite.config.ts',
+    test: {
+      name: 'store',
+      environment: 'node',
+      include: [
+        'src/web/store/**/*.{test,spec}.?(c|m)[jt]s?(x)',
+        'src/web/store/**/__tests__/*.?(c|m)[jt]s?(x)',
       ],
     },
   },
