a28986e6539703cc8703626ac96d4ea689491611
Björn Ricks <bjoern.ricks@greenbone.net>
2024-04-24 11:53:44 +0200

    Deactivate vitest globals (expect, describe, test, ...)
    With importing all test functions the globals are unnecessary and
    should not be activated anymore.

--- vitest.projects.ts
+++ vitest.projects.ts
@@ -6,7 +6,6 @@ export default defineWorkspace([
     test: {
       name: 'web',
       environment: 'jsdom',
-      globals: true,
       setupFiles: './src/web/setupTests.js',
       include: [
         'src/web/**/*.{test,spec}.?(c|m)[jt]s?(x)',
