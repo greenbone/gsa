110fb0f3b6e5365451d6e69f77c10c2b19c37fb2
Björn Ricks <bjoern.ricks@greenbone.net>
2024-05-06 14:16:01 +0200

    Update and fix overrides list page tests

--- src/web/pages/overrides/__tests__/listpage.jsx
+++ src/web/pages/overrides/__tests__/listpage.jsx
@@ -19,6 +19,17 @@ import {loadingActions} from 'web/store/usersettings/defaults/actions';
 
 import {rendererWith, fireEvent, screen, wait} from 'web/utils/testing';
 
+import {
+  clickElement,
+  getCheckBoxes,
+  getPowerFilter,
+  getSelectElement,
+  getSelectItemElementsForSelect,
+  getTableBody,
+  getTableFooter,
+  getTextInputs,
+} from 'web/components/testing';
+
 import OverridesPage, {ToolBarIcons} from '../listpage';
 
 const override = Override.fromElement({
@@ -123,10 +134,10 @@ describe('OverridesPage tests', () => {
     store.dispatch(setTimezone('CET'));
     store.dispatch(setUsername('admin'));
 
-    const defaultSettingfilter = Filter.fromString('foo=bar');
+    const defaultSettingFilter = Filter.fromString('foo=bar');
     store.dispatch(loadingActions.success({rowsperpage: {value: '2'}}));
     store.dispatch(
-      defaultFilterLoadingActions.success('override', defaultSettingfilter),
+      defaultFilterLoadingActions.success('override', defaultSettingFilter),
     );
 
     const counts = new CollectionCounts({
@@ -147,8 +158,9 @@ describe('OverridesPage tests', () => {
     await wait();
 
     const display = screen.getAllByTestId('grid-item');
-    const inputs = baseElement.querySelectorAll('input');
-    const selects = screen.getAllByTestId('select-selected-value');
+    const powerFilter = getPowerFilter();
+    const select = getSelectElement(powerFilter);
+    const inputs = getTextInputs(powerFilter);
 
     // Toolbar Icons
     expect(screen.getAllByTitle('Help: Overrides')[0]).toBeInTheDocument();
@@ -163,8 +175,8 @@ describe('OverridesPage tests', () => {
     ).toBeInTheDocument();
     expect(screen.getAllByTitle('Help: Powerfilter')[0]).toBeInTheDocument();
     expect(screen.getAllByTitle('Edit Filter')[0]).toBeInTheDocument();
-    expect(selects[0]).toHaveAttribute('title', 'Loaded filter');
-    expect(selects[0]).toHaveTextContent('--');
+    expect(select).toHaveAttribute('title', 'Loaded filter');
+    expect(select).toHaveValue('--');
 
     // Dashboard
     expect(
@@ -243,10 +255,10 @@ describe('OverridesPage tests', () => {
     store.dispatch(setTimezone('CET'));
     store.dispatch(setUsername('admin'));
 
-    const defaultSettingfilter = Filter.fromString('foo=bar');
+    const defaultSettingFilter = Filter.fromString('foo=bar');
     store.dispatch(loadingActions.success({rowsperpage: {value: '2'}}));
     store.dispatch(
-      defaultFilterLoadingActions.success('override', defaultSettingfilter),
+      defaultFilterLoadingActions.success('override', defaultSettingFilter),
     );
 
     const counts = new CollectionCounts({
@@ -267,23 +279,15 @@ describe('OverridesPage tests', () => {
     await wait();
 
     // export page contents
-    const exportIcon = screen.getAllByTitle('Export page contents');
-
-    expect(exportIcon[0]).toBeInTheDocument();
-    fireEvent.click(exportIcon[0]);
-
-    await wait();
-
+    const exportIcon = screen.getAllByTitle('Export page contents')[0];
+    await clickElement(exportIcon);
     expect(exportByFilter).toHaveBeenCalled();
 
     // move page contents to trashcan
-    const deleteIcon = screen.getAllByTitle('Move page contents to trashcan');
-
-    expect(deleteIcon[0]).toBeInTheDocument();
-    fireEvent.click(deleteIcon[0]);
-
-    await wait();
-
+    const deleteIcon = screen.getAllByTitle(
+      'Move page contents to trashcan',
+    )[0];
+    await clickElement(deleteIcon);
     expect(deleteByFilter).toHaveBeenCalled();
   });
 
@@ -325,10 +329,10 @@ describe('OverridesPage tests', () => {
     store.dispatch(setTimezone('CET'));
     store.dispatch(setUsername('admin'));
 
-    const defaultSettingfilter = Filter.fromString('foo=bar');
+    const defaultSettingFilter = Filter.fromString('foo=bar');
     store.dispatch(loadingActions.success({rowsperpage: {value: '2'}}));
     store.dispatch(
-      defaultFilterLoadingActions.success('override', defaultSettingfilter),
+      defaultFilterLoadingActions.success('override', defaultSettingFilter),
     );
 
     const counts = new CollectionCounts({
@@ -344,43 +348,30 @@ describe('OverridesPage tests', () => {
       entitiesLoadingActions.success([override], filter, loadedFilter, counts),
     );
 
-    const {element} = render(<OverridesPage />);
+    render(<OverridesPage />);
 
     await wait();
 
-    const selectFields = screen.getAllByTestId('select-open-button');
-    fireEvent.click(selectFields[1]);
-
-    const selectItems = screen.getAllByTestId('select-item');
-    fireEvent.click(selectItems[1]);
-
-    const selected = screen.getAllByTestId('select-selected-value');
-    expect(selected[1]).toHaveTextContent('Apply to selection');
-
-    const inputs = element.querySelectorAll('input');
+    // change to apply to selection
+    const tableFooter = getTableFooter();
+    const select = getSelectElement(tableFooter);
+    const selectItems = await getSelectItemElementsForSelect(select);
+    await clickElement(selectItems[1]);
+    expect(select).toHaveValue('Apply to selection');
 
     // select a override
-    fireEvent.click(inputs[1]);
-    await wait();
+    const tableBody = getTableBody();
+    const inputs = getCheckBoxes(tableBody);
+    await clickElement(inputs[1]);
 
     // export selected override
-    const exportIcon = screen.getAllByTitle('Export selection');
-
-    expect(exportIcon[0]).toBeInTheDocument();
-    fireEvent.click(exportIcon[0]);
-
-    await wait();
-
+    const exportIcon = screen.getAllByTitle('Export selection')[0];
+    await clickElement(exportIcon);
     expect(exportByIds).toHaveBeenCalled();
 
     // move selected override to trashcan
-    const deleteIcon = screen.getAllByTitle('Move selection to trashcan');
-
-    expect(deleteIcon[0]).toBeInTheDocument();
-    fireEvent.click(deleteIcon[0]);
-
-    await wait();
-
+    const deleteIcon = screen.getAllByTitle('Move selection to trashcan')[0];
+    await clickElement(deleteIcon);
     expect(deleteByIds).toHaveBeenCalled();
   });
 
@@ -422,10 +413,10 @@ describe('OverridesPage tests', () => {
     store.dispatch(setTimezone('CET'));
     store.dispatch(setUsername('admin'));
 
-    const defaultSettingfilter = Filter.fromString('foo=bar');
+    const defaultSettingFilter = Filter.fromString('foo=bar');
     store.dispatch(loadingActions.success({rowsperpage: {value: '2'}}));
     store.dispatch(
-      defaultFilterLoadingActions.success('override', defaultSettingfilter),
+      defaultFilterLoadingActions.success('override', defaultSettingFilter),
     );
 
     const counts = new CollectionCounts({
@@ -445,35 +436,21 @@ describe('OverridesPage tests', () => {
 
     await wait();
 
-    const selectFields = screen.getAllByTestId('select-open-button');
-    fireEvent.click(selectFields[1]);
-
-    const selectItems = screen.getAllByTestId('select-item');
-    fireEvent.click(selectItems[2]);
-
-    await wait();
-
-    const selected = screen.getAllByTestId('select-selected-value');
-    expect(selected[1]).toHaveTextContent('Apply to all filtered');
+    // change to all filtered
+    const tableFooter = getTableFooter();
+    const select = getSelectElement(tableFooter);
+    const selectItems = await getSelectItemElementsForSelect(select);
+    await clickElement(selectItems[2]);
+    expect(select).toHaveValue('Apply to all filtered');
 
     // export all filtered overrides
-    const exportIcon = screen.getAllByTitle('Export all filtered');
-
-    expect(exportIcon[0]).toBeInTheDocument();
-    fireEvent.click(exportIcon[0]);
-
-    await wait();
-
+    const exportIcon = screen.getAllByTitle('Export all filtered')[0];
+    await clickElement(exportIcon);
     expect(exportByFilter).toHaveBeenCalled();
 
     // move all filtered overrides to trashcan
-    const deleteIcon = screen.getAllByTitle('Move all filtered to trashcan');
-
-    expect(deleteIcon[0]).toBeInTheDocument();
-    fireEvent.click(deleteIcon[0]);
-
-    await wait();
-
+    const deleteIcon = screen.getAllByTitle('Move all filtered to trashcan')[0];
+    await clickElement(deleteIcon);
     expect(deleteByFilter).toHaveBeenCalled();
   });
 });
@@ -545,7 +522,7 @@ describe('OverridesPage ToolBarIcons test', () => {
       <ToolBarIcons onOverrideCreateClick={handleOverrideCreateClick} />,
     );
 
-    const icons = queryAllByTestId('svg-icon'); // this test is probably approppriate to keep in the old format
+    const icons = queryAllByTestId('svg-icon'); // this test is probably appropriate to keep in the old format
     expect(icons.length).toBe(1);
     expect(icons[0]).toHaveAttribute('title', 'Help: Overrides');
   });
