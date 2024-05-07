4b23c75e3177405aea84f8e4a67734f112746a46
Björn Ricks <bjoern.ricks@greenbone.net>
2024-05-07 10:38:31 +0200

    Update and fix report config pages tests

--- src/web/pages/reportconfigs/__tests__/listpage.jsx
+++ src/web/pages/reportconfigs/__tests__/listpage.jsx
@@ -17,7 +17,9 @@ import {entitiesLoadingActions} from 'web/store/entities/scanconfigs';
 import {loadingActions} from 'web/store/usersettings/defaults/actions';
 import {defaultFilterLoadingActions} from 'web/store/usersettings/defaultfilters/actions';
 
-import {rendererWith, waitFor, fireEvent, act} from 'web/utils/testing';
+import {rendererWith, fireEvent, wait, screen} from 'web/utils/testing';
+
+import {clickElement} from 'web/components/testing';
 
 import ReportConfigsPage, {ToolBarIcons} from '../listpage';
 
@@ -87,10 +89,10 @@ describe('ReportConfigsPage tests', () => {
 
     store.dispatch(setUsername('admin'));
 
-    const defaultSettingfilter = Filter.fromString('foo=bar');
+    const defaultSettingFilter = Filter.fromString('foo=bar');
     store.dispatch(loadingActions.success({rowsperpage: {value: '2'}}));
     store.dispatch(
-      defaultFilterLoadingActions.success('reportconfig', defaultSettingfilter),
+      defaultFilterLoadingActions.success('reportconfig', defaultSettingFilter),
     );
 
     const counts = new CollectionCounts({
@@ -108,7 +110,7 @@ describe('ReportConfigsPage tests', () => {
 
     const {baseElement} = render(<ReportConfigsPage />);
 
-    await waitFor(() => baseElement.querySelectorAll('table'));
+    await wait();
 
     expect(baseElement).toBeVisible();
   });
@@ -147,10 +149,10 @@ describe('ReportConfigsPage tests', () => {
 
     store.dispatch(setUsername('admin'));
 
-    const defaultSettingfilter = Filter.fromString('foo=bar');
+    const defaultSettingFilter = Filter.fromString('foo=bar');
     store.dispatch(loadingActions.success({rowsperpage: {value: '2'}}));
     store.dispatch(
-      defaultFilterLoadingActions.success('reportconfig', defaultSettingfilter),
+      defaultFilterLoadingActions.success('reportconfig', defaultSettingFilter),
     );
 
     const counts = new CollectionCounts({
@@ -166,22 +168,15 @@ describe('ReportConfigsPage tests', () => {
       entitiesLoadingActions.success([config], filter, loadedFilter, counts),
     );
 
-    const {baseElement, getAllByTestId} = render(<ReportConfigsPage />);
+    render(<ReportConfigsPage />);
 
-    await waitFor(() => baseElement.querySelectorAll('table'));
+    await wait();
 
-    const icons = getAllByTestId('svg-icon');
-
-    await act(async () => {
-      expect(icons[17]).toHaveAttribute('title', 'Add tag to page contents');
-
-      expect(icons[18]).toHaveAttribute(
-        'title',
-        'Move page contents to trashcan',
-      );
-      fireEvent.click(icons[18]);
-      expect(deleteByFilter).toHaveBeenCalled();
-    });
+    const deleteIcon = screen.getAllByTitle(
+      'Move page contents to trashcan',
+    )[0];
+    await clickElement(deleteIcon);
+    expect(deleteByFilter).toHaveBeenCalled();
   });
 
   describe('ReportConfigsPage ToolBarIcons test', () => {
