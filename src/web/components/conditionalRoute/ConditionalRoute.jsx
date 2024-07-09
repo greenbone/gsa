065b11209563024c2216ca80893e19a96ed11659
daniele-mng <daniele.mangiagli@greenbone.net>
2024-07-09 11:38:03 +0200

    Add: conditional route component for feature flag

---
+++ src/web/components/conditionalRoute/ConditionalRoute.jsx
@@ -0,0 +1,29 @@
+/* SPDX-FileCopyrightText: 2024 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import PropTypes from 'prop-types';
+
+import {Route, Redirect} from 'react-router-dom';
+import useCapabilities from 'web/hooks/useCapabilities';
+
+const ConditionalRoute = ({component: Component, feature, ...rest}) => {
+  const capabilities = useCapabilities();
+  const isEnabled = capabilities._featuresEnabled[feature];
+  return (
+    <Route
+      render={props =>
+        isEnabled ? <Component {...props} /> : <Redirect to="/notfound" />
+      }
+      {...rest}
+    />
+  );
+};
+
+ConditionalRoute.propTypes = {
+  component: PropTypes.elementType.isRequired,
+  feature: PropTypes.string.isRequired,
+};
+
+export default ConditionalRoute;
