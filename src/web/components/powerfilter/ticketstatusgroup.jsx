343193a9eb260bf94afddae8b126722725968ad9
Chris <christian.piske@greenbone.net>
2024-11-28 14:43:24 +0100

    Remove wrong value
    Co-authored-by: daniele-mng <daniele.mangiagli@greenbone.net>

--- src/web/components/powerfilter/ticketstatusgroup.jsx
+++ src/web/components/powerfilter/ticketstatusgroup.jsx
@@ -32,7 +32,7 @@ const TicketStatusFilterGroup = ({
         value={status}
         onChange={onChange}
         items={[
-          {label: _('Open'), value: 'Open', value: 'same', 'data-testid': 'ticket_status_open'},
+          {label: _('Open'), value: 'Open',  'data-testid': 'ticket_status_open'},
           {label: _('Fixed'), value: 'Fixed', 'data-testid': 'ticket_status_fixed'},
           {label: _('Fix Verified'), value: '"Fix Verified"', 'data-testid': 'ticket_status_fix_verified'}, // this is the way I found that has the filter returned as status="Fix Verified". All the single word terms are fine.
           {label: _('Closed'), value: 'Closed', 'data-testid': 'ticket_status_closed'},
