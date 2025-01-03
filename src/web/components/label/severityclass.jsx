1268179736f41569299588307f6b6c2435456422
daniele-mng <daniele.mangiagli@greenbone.net>
2025-01-03 10:55:00 +0100

    change: ui severity class

--- src/web/components/label/severityclass.jsx
+++ src/web/components/label/severityclass.jsx
@@ -3,89 +3,82 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import _ from 'gmp/locale';
-import React from 'react';
+import {useTranslation} from 'react-i18next';
 import styled from 'styled-components';
-
+import Theme from 'web/utils/theme';
 
 const Label = styled.div`
-  text-align: center;
+  box-sizing: border-box;
+  position: relative;
   font-weight: normal;
   font-style: normal;
   color: white;
   padding: 1px;
-  display: inline-block;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
   width: 70px;
   height: 1.5em;
   font-size: 0.8em;
+  text-align: center;
+  color: ${props => props.$textColor};
   background-color: ${props => props.$backgroundColor};
+  border-radius: 5px;
+  overflow: hidden;
   border-color: ${props => props.$borderColor};
 `;
 
-const HighLabel = props => {
-  return (
-    <Label
-      {...props}
-      $backgroundColor="#C83814"
-      $borderColor="#C83814"
-      data-testid="severity-class-High"
-    >
-      {_('High')}
-    </Label>
-  );
-};
+const createLabel =
+  (backgroundColor, borderColor, textColor, testId, text) => props => {
+    const [_] = useTranslation();
+    return (
+      <Label
+        {...props}
+        $backgroundColor={backgroundColor}
+        $borderColor={borderColor}
+        $textColor={textColor}
+        data-testid={testId}
+      >
+        {_(text)}
+      </Label>
+    );
+  };
 
-const MediumLabel = props => {
-  return (
-    <Label
-      {...props}
-      $backgroundColor="#F0A519"
-      $borderColor="#F0A519"
-      data-testid="severity-class-Medium"
-    >
-      {_('Medium')}
-    </Label>
-  );
-};
-
-const LowLabel = props => {
-  return (
-    <Label
-      {...props}
-      $backgroundColor="#4F91C7"
-      $borderColor="#4F91C7"
-      data-testid="severity-class-Low"
-    >
-      {_('Low')}
-    </Label>
-  );
-};
-
-const LogLabel = props => {
-  return (
-    <Label
-      {...props}
-      $backgroundColor="#191919"
-      $borderColor="#191919"
-      data-testid="severity-class-Log"
-    >
-      {_('Log')}
-    </Label>
-  );
-};
-
-const FalsePositiveLabel = props => {
-  return (
-    <Label
-      {...props}
-      $backgroundColor="#191919"
-      $borderColor="#191919"
-      data-testid="severity-class-False-Positive"
-    >
-      {_('False Pos.')}
-    </Label>
-  );
-};
+const HighLabel = createLabel(
+  Theme.errorRed,
+  Theme.errorRed,
+  Theme.white,
+  'severity-class-High',
+  'High',
+);
+const MediumLabel = createLabel(
+  Theme.severityWarnYellow,
+  Theme.severityWarnYellow,
+  Theme.black,
+  'severity-class-Medium',
+  'Medium',
+);
+const LowLabel = createLabel(
+  Theme.severityLowBlue,
+  Theme.severityLowBlue,
+  Theme.white,
+  'severity-class-Low',
+  'Low',
+);
+const LogLabel = createLabel(
+  Theme.mediumGray,
+  Theme.mediumGray,
+  Theme.white,
+  'severity-class-Log',
+  'Log',
+);
+const FalsePositiveLabel = createLabel(
+  Theme.mediumGray,
+  Theme.mediumGray,
+  Theme.white,
+  'severity-class-False-Positive',
+  'False Pos.',
+);
 
 export const SeverityClassLabels = {
   High: HighLabel,
