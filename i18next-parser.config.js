fce7b583175670e2fb82262a6f32a7b20a5e31d9
Björn Ricks <bjoern.ricks@greenbone.net>
2025-01-09 12:18:40 +0100

    Change: Use a new translations extraction tooling
    The old tooling was a bit *hackish* and used a babel plugin internally.
    Because this tool needed also babel plugins from the deprecated
    create-react-app project it got replaced with i18next-parser.

---
+++ i18next-parser.config.js
@@ -0,0 +1,30 @@
+/* SPDX-FileCopyrightText: 2025 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+const jsLexer = [{lexer: 'JavascriptLexer', functions: ['_', '_l']}];
+const jsxLexer = [{lexer: 'JsxLexer', functions: ['_', '_l']}];
+
+export default {
+  contextSeparator: null,
+  defaultNamespace: 'gsa',
+  input: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'],
+  keepRemoved: false,
+  keySeparator: false,
+  lexers: {
+    mjs: jsLexer,
+    js: jsLexer,
+    ts: jsLexer,
+    jsx: jsxLexer,
+    tsx: jsxLexer,
+
+    default: jsLexer,
+  },
+  locales: ['de', 'zh_TW', 'zh_CN'],
+  namespaceSeparator: false,
+  output: 'public/locales/gsa-$LOCALE.json',
+  pluralSeparator: false,
+  resetDefaultValueLocale: 'en',
+  sort: true,
+};
