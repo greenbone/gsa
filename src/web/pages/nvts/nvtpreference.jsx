0d6f7ac537c35f6a2d8d4933556734dc212b0f91
daniele-mng <daniele.mangiagli@greenbone.net>
2025-01-13 20:43:13 +0100

    refactor nvtpreferance.jsx

--- src/web/pages/nvts/nvtpreference.jsx
+++ src/web/pages/nvts/nvtpreference.jsx
@@ -6,7 +6,8 @@
 import _ from 'gmp/locale';
 import {map} from 'gmp/utils/array';
 import {isEmpty} from 'gmp/utils/string';
-import React from 'react';
+import {useState} from 'react';
+import styled from 'styled-components';
 import Checkbox from 'web/components/form/checkbox';
 import FileField from 'web/components/form/filefield';
 import PasswordField from 'web/components/form/passwordfield';
@@ -15,132 +16,121 @@ import TextField from 'web/components/form/textfield';
 import YesNoRadio from 'web/components/form/yesnoradio';
 import Column from 'web/components/layout/column';
 import Divider from 'web/components/layout/divider';
-import Layout from 'web/components/layout/layout';
 import TableData from 'web/components/table/data';
 import TableRow from 'web/components/table/row';
 import PropTypes from 'web/utils/proptypes';
 
+const StyledTableData = styled(TableData)`
+  overflow-wrap: break-word;
+  white-space: normal;
+  word-break: break-word;
+`;
+
 const noop_convert = value => value;
 
-class NvtPreference extends React.Component {
-  constructor(...args) {
-    super(...args);
+const NvtPreference = ({preference, value = '', onChange}) => {
+  const [checked, setChecked] = useState(false);
 
-    this.state = {
-      checked: false,
-    };
-
-    this.onCheckedChange = this.onCheckedChange.bind(this);
-    this.onPreferenceChange = this.onPreferenceChange.bind(this);
-  }
-
-  onPreferenceChange(value) {
-    const {onChange, preference} = this.props;
+  const onPreferenceChange = value => {
     onChange({type: 'setValue', newState: {name: preference.name, value}});
-  }
+  };
 
-  onCheckedChange(value) {
+  const onCheckedChange = value => {
     if (value) {
-      this.onPreferenceChange('');
+      onPreferenceChange('');
     } else {
-      this.onPreferenceChange(undefined);
+      onPreferenceChange(undefined);
     }
-    this.setState({checked: value});
-  }
+    setChecked(value);
+  };
+  const {type} = preference;
 
-  render() {
-    const {preference, value = ''} = this.props;
+  let input;
 
-    const {checked} = this.state;
-    const {type} = preference;
-
-    let input;
-
-    if (type === 'checkbox') {
-      input = (
-        <YesNoRadio
-          convert={noop_convert}
-          noValue="no"
-          value={value}
-          yesValue="yes"
-          onChange={this.onPreferenceChange}
+  if (type === 'checkbox') {
+    input = (
+      <YesNoRadio
+        convert={noop_convert}
+        noValue="no"
+        value={value}
+        yesValue="yes"
+        onChange={onPreferenceChange}
+      />
+    );
+  } else if (type === 'password') {
+    input = (
+      <Column>
+        <Checkbox
+          checked={checked}
+          title={_('Replace existing password with')}
+          onChange={onCheckedChange}
         />
-      );
-    } else if (type === 'password') {
-      input = (
-        <Column>
-          <Checkbox
-            checked={checked}
-            title={_('Replace existing password with')}
-            onChange={this.onCheckedChange}
-          />
-          <PasswordField
-            aria-label="Password"
-            disabled={!checked}
-            value={value}
-            onChange={this.onPreferenceChange}
-          />
-        </Column>
-      );
-    } else if (type === 'file') {
-      input = (
-        <Divider>
-          <Checkbox
-            checked={checked}
-            title={
-              isEmpty(preference.value)
-                ? _('Upload file')
-                : _('Replace existing file')
-            }
-            onChange={this.onCheckedChange}
-          />
-          <FileField
-            aria-label="File"
-            disabled={!checked}
-            onChange={this.onPreferenceChange}
-          />
-        </Divider>
-      );
-    } else if (type === 'radio') {
-      input = (
-        <Layout flex="column">
-          <Radio
-            checked={value === preference.value}
-            title={preference.value}
-            value={preference.value}
-            onChange={this.onPreferenceChange}
-          />
-          {map(preference.alt, alt => {
-            return (
-              <Radio
-                key={alt}
-                checked={value === alt}
-                title={alt}
-                value={alt}
-                onChange={this.onPreferenceChange}
-              />
-            );
-          })}
-        </Layout>
-      );
-    } else {
-      input = (
-        <TextField
-          name={preference.name}
+        <PasswordField
+          aria-label="Password"
+          disabled={!checked}
           value={value}
-          onChange={this.onPreferenceChange}
+          onChange={onPreferenceChange}
         />
-      );
-    }
-    return (
-      <TableRow>
-        <TableData>{preference.hr_name}</TableData>
-        <TableData>{input}</TableData>
-        <TableData>{preference.default}</TableData>
-      </TableRow>
+      </Column>
+    );
+  } else if (type === 'file') {
+    input = (
+      <Divider>
+        <Checkbox
+          checked={checked}
+          title={
+            isEmpty(preference.value)
+              ? _('Upload file')
+              : _('Replace existing file')
+          }
+          onChange={onCheckedChange}
+        />
+        <FileField
+          aria-label="File"
+          disabled={!checked}
+          onChange={onPreferenceChange}
+        />
+      </Divider>
+    );
+  } else if (type === 'radio') {
+    input = (
+      <Column>
+        <Radio
+          checked={value === preference.value}
+          title={preference.value}
+          value={preference.value}
+          onChange={onPreferenceChange}
+        />
+        {map(preference.alt, alt => {
+          return (
+            <Radio
+              key={alt}
+              checked={value === alt}
+              title={alt}
+              value={alt}
+              onChange={onPreferenceChange}
+            />
+          );
+        })}
+      </Column>
+    );
+  } else {
+    input = (
+      <TextField
+        name={preference.name}
+        value={value}
+        onChange={onPreferenceChange}
+      />
     );
   }
-}
+  return (
+    <TableRow>
+      <TableData>{preference.hr_name}</TableData>
+      <TableData>{input}</TableData>
+      <StyledTableData>{preference.default}</StyledTableData>
+    </TableRow>
+  );
+};
 
 NvtPreference.propTypes = {
   preference: PropTypes.shape({
