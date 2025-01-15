ef6445b7160f3d34b2cd1e6298db55cd554d234c
Björn Ricks <bjoern.ricks@greenbone.net>
2025-01-15 14:22:57 +0100

    Add: Add a new react hook for getting a URL to the manual
    Introduce a new hook that returns a link to the manual depending on the
    GSA settings and users locale.

---
+++ src/web/hooks/__tests__/useManualURL.jsx
@@ -0,0 +1,73 @@
+/* SPDX-FileCopyrightText: 2025 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import {describe, test, expect} from '@gsa/testing';
+import {setLocale} from 'web/store/usersettings/actions';
+import {rendererWith} from 'web/utils/testing';
+
+import useManualURL from '../useManualURL';
+
+const gmp = {settings: {manualUrl: 'http://localhost/manual'}};
+
+describe('useManualURL', () => {
+  test('should return the manual URL for the default language', () => {
+    const {renderHook} = rendererWith({
+      store: true,
+      gmp,
+    });
+    const {result} = renderHook(() => useManualURL());
+
+    expect(result.current).toBe('http://localhost/manual/en');
+  });
+
+  test('should return the manual URL for the German language', () => {
+    const {renderHook} = rendererWith({
+      store: true,
+      gmp,
+    });
+    const {result} = renderHook(() => useManualURL('de'));
+
+    expect(result.current).toBe('http://localhost/manual/de');
+  });
+
+  test('should return the manual URL for the users language', () => {
+    const {renderHook, store} = rendererWith({
+      store: true,
+      gmp,
+    });
+    store.dispatch(setLocale('de'));
+    const {result} = renderHook(() => useManualURL());
+
+    expect(result.current).toBe('http://localhost/manual/de');
+  });
+
+  test('should return the en manual URL for unknown language', () => {
+    const {renderHook} = rendererWith({
+      store: true,
+      gmp,
+    });
+    const {result} = renderHook(() => useManualURL('foo'));
+
+    expect(result.current).toBe('http://localhost/manual/en');
+  });
+
+  test('should return the en manual URL considering the lanuage mapping', () => {
+    const {renderHook, store} = rendererWith({
+      store: true,
+      gmp: {
+        settings: {
+          ...gmp.settings,
+          manualLanguageMapping: {
+            fr: 'foo',
+          },
+        },
+      },
+    });
+    store.dispatch(setLocale('fr'));
+    const {result} = renderHook(() => useManualURL());
+
+    expect(result.current).toBe('http://localhost/manual/foo');
+  });
+});
