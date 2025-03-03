0e56ba84fee8e4076c11f34c5618066addecde30
Björn Ricks <bjoern.ricks@greenbone.net>
2025-03-03 14:30:42 +0100

    Extract actionFunction into an own JS module
    Several hooks will use this function. Therefore move it to an own JS
    module.

--- src/web/entity/EntityComponent.jsx
+++ src/web/entity/EntityComponent.jsx
@@ -6,34 +6,10 @@
 import {isDefined} from 'gmp/utils/identity';
 import {useDispatch} from 'react-redux';
 import useEntityDownload from 'web/entity/hooks/useEntityDownload';
+import {actionFunction} from 'web/entity/hooks/utils';
 import useGmp from 'web/hooks/useGmp';
 import {createDeleteEntity} from 'web/store/entities/utils/actions';
 import PropTypes from 'web/utils/PropTypes';
-
-/**
- * Executes a promise and handles success and error callbacks.
- *
- * @param {Promise} promise - The promise to be executed.
- * @param {Function} [onSuccess] - Optional callback function to be called on successful resolution of the promise.
- * @param {Function} [onError] - Optional callback function to be called if the promise is rejected.
- * @returns {Promise<*>} - The result of the onSuccess callback if provided, otherwise the resolved value of the promise.
- *                         If the promise is rejected the result of the onError callback if provided.
- *                         Otherwise the error from the rejected promise is thrown.
- * @throws {*} - The error from the rejected promise if onError callback is not provided.
- */
-const actionFunction = async (promise, onSuccess, onError) => {
-  try {
-    const response = await promise;
-    if (isDefined(onSuccess)) {
-      return onSuccess(response);
-    }
-  } catch (error) {
-    if (isDefined(onError)) {
-      return onError(error);
-    }
-    throw error;
-  }
-};
 
 const EntityComponent = ({
   children,
