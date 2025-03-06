95382e992c51719e60280eed049055bf98617055
Björn Ricks <bjoern.ricks@greenbone.net>
2025-03-06 08:30:56 +0100

    Add: Add docstring from useUserName
    Explain using useUserName.

--- src/web/hooks/useUserName.js
+++ src/web/hooks/useUserName.js
@@ -7,6 +7,11 @@ import {useSelector, useDispatch} from 'react-redux';
 import {setUsername} from 'web/store/usersettings/actions';
 import {getUsername} from 'web/store/usersettings/selectors';
 
+/**
+ * Custom hook to get and set the username.
+ *
+ * @returns {Array} An array containing the current username and a function to set the username.
+ */
 const useUserName = () => {
   const dispatch = useDispatch();
   return [
