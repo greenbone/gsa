37822eb2f5545eca511fc36a3e80b5a5bac2d247
Björn Ricks <bjoern.ricks@greenbone.net>
2025-03-07 14:09:15 +0100

    Change: Refactor RowDetailsToggle
    Replace withClickHandler with useClickHandler and add data-testid for
    easier testing.

--- src/web/entities/Row.jsx
+++ src/web/entities/Row.jsx
@@ -3,11 +3,12 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import withClickHandler from 'web/components/form/withClickHandler';
+import useClickHandler from 'web/components/form/useClickHandler';
 import Theme from 'web/utils/Theme';
 
-export const RowDetailsToggle = withClickHandler()(styled.span`
+const StyledSpan = styled.span`
   cursor: pointer;
   text-decoration: none;
   color: ${Theme.blue};
@@ -18,4 +19,24 @@ export const RowDetailsToggle = withClickHandler()(styled.span`
   @media print {
     color: ${Theme.black};
   }
-`);
+`;
+
+export const RowDetailsToggle = ({name, onClick, ...props}) => {
+  const handleClick = useClickHandler({
+    onClick,
+    name,
+    nameFunc: (event, props) => props.name,
+  });
+  return (
+    <StyledSpan
+      data-testid="row-details-toggle"
+      {...props}
+      name={name}
+      onClick={handleClick}
+    />
+  );
+};
+
+RowDetailsToggle.propTypes = {name: PropTypes.string, onClick: PropTypes.func};
+
+export default RowDetailsToggle;
