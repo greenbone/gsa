ede25b1d17e113aa9fa35d89f7a1224a05b9ddb3
Björn Ricks <bjoern.ricks@greenbone.net>
2025-03-25 09:33:21 +0100

    change: Add return type for GmpProvider and useGmp implicitly

--- src/web/components/provider/GmpProvider.tsx
+++ src/web/components/provider/GmpProvider.tsx
@@ -3,8 +3,9 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+import Gmp from 'gmp/gmp';
 import React from 'react';
 
-const GmpContext = React.createContext(undefined);
+const GmpContext = React.createContext<Gmp | undefined>(undefined);
 
 export default GmpContext;
