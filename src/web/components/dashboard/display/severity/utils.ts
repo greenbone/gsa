c709b9d8fd4a46b15d206a2839181b4a67266cdd
Björn Ricks <bjoern.ricks@greenbone.net>
2025-04-01 09:27:00 +0200

    change: Use Severity Display untils module to TypeScript
    Add types for the filterValueToFilterTerms function.

--- src/web/components/dashboard/display/severity/utils.ts
+++ src/web/components/dashboard/display/severity/utils.ts
@@ -7,15 +7,28 @@ import FilterTerm from 'gmp/models/filter/filterterm';
 import {isDefined} from 'gmp/utils/identity';
 import {LOG_VALUE} from 'web/utils/severity';
 
+interface FilterValueToFilterTermsParams {
+  start?: number;
+  end?: number;
+}
+
+type FilterValueToFilterTermsReturnType = [FilterTerm?, FilterTerm?];
+
 /**
  * Converts filter values to filter terms based on severity range.
  *
- * @param {Object} params - The filter values.
- * @param {(string|number)} params.start - The start value of the filter range.
- * @param {(string|number)} params.end - The end value of the filter range.
- * @returns {FilterTerm[]} An array of FilterTerm objects representing the filter terms.
+ * @param params - The filter values.
+ * @param params.start - The start value of the filter range.
+ * @param params.end - The end value of the filter range.
+ * @returns An array of FilterTerm objects representing the filter terms.
  */
-export const filterValueToFilterTerms = ({start, end}) => {
+export const filterValueToFilterTerms = ({
+  start,
+  end,
+}: FilterValueToFilterTermsParams): FilterValueToFilterTermsReturnType => {
+  if (!isDefined(start)) {
+    return [];
+  }
   if (isDefined(end)) {
     const startTerm = FilterTerm.fromString(`severity>${start}`);
     const endTerm = FilterTerm.fromString(`severity<${end}`);
