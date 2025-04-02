4060be4f76fdffa893c87655663a7b23ce637254
Björn Ricks <bjoern.ricks@greenbone.net>
2025-04-02 09:46:34 +0200

    Update tests for SeverityBar
    Fix tests for using the different severity ratings.

--- src/web/components/bar/__tests__/SeverityBar.test.jsx
+++ src/web/components/bar/__tests__/SeverityBar.test.jsx
@@ -4,43 +4,121 @@
  */
 
 import {describe, test, expect} from '@gsa/testing';
+import {
+  DEFAULT_SEVERITY_RATING,
+  SEVERITY_RATING_CVSS_2,
+  SEVERITY_RATING_CVSS_3,
+} from 'gmp/utils/severity';
 import SeverityBar from 'web/components/bar/SeverityBar';
-import {render} from 'web/utils/Testing';
+import {rendererWith} from 'web/utils/Testing';
 import Theme from 'web/utils/Theme';
-
 
 describe('SeverityBar tests', () => {
   test('should render', () => {
-    const {element} = render(<SeverityBar severity="9.5" />);
+    const gmp = {
+      settings: {
+        severityRating: DEFAULT_SEVERITY_RATING,
+      },
+    };
+
+    const {render} = rendererWith({gmp});
+    const {element} = render(<SeverityBar severity="5.5" />);
 
     expect(element).toBeVisible();
   });
 
-  test('should render text content', () => {
+  test('should render text content with high severity', () => {
+    const gmp = {
+      settings: {
+        severityRating: SEVERITY_RATING_CVSS_2,
+      },
+    };
+
+    const {render} = rendererWith({gmp});
     const {element} = render(<SeverityBar severity="9.5" />);
-    expect(element).toHaveTextContent('9.5 (Critical)');
+    expect(element).toHaveTextContent('9.5 (High)');
   });
 
   test('should render text content with medium severity', () => {
+    const gmp = {
+      settings: {
+        severityRating: DEFAULT_SEVERITY_RATING,
+      },
+    };
+    const {render} = rendererWith({gmp});
     const {element} = render(<SeverityBar severity="5" />);
     expect(element).toHaveTextContent('5.0 (Medium)');
   });
 
-  test('should render title', () => {
+  test('should render text content with high severity', () => {
+    const gmp = {
+      settings: {
+        severityRating: SEVERITY_RATING_CVSS_2,
+      },
+    };
+
+    const {render} = rendererWith({gmp});
+    const {element} = render(<SeverityBar severity="9" />);
+    expect(element).toHaveTextContent('9.0 (High)');
+  });
+
+  test('should render text content with critical severity', () => {
+    const gmp = {
+      settings: {
+        severityRating: SEVERITY_RATING_CVSS_3,
+      },
+    };
+    const {render} = rendererWith({gmp});
+    const {element} = render(<SeverityBar severity="9" />);
+    expect(element).toHaveTextContent('9.0 (Critical)');
+  });
+
+  test('should render title with high severity', () => {
+    const gmp = {
+      settings: {
+        severityRating: SEVERITY_RATING_CVSS_2,
+      },
+    };
+    const {render} = rendererWith({gmp});
     const {getByTestId} = render(<SeverityBar severity="9.5" />);
     const progressbarBox = getByTestId('progressbar-box');
 
-    expect(progressbarBox).toHaveAttribute('title', 'Critical');
+    expect(progressbarBox).toHaveAttribute('title', 'High');
   });
 
   test('should render title with medium severity', () => {
+    const gmp = {
+      settings: {
+        severityRating: DEFAULT_SEVERITY_RATING,
+      },
+    };
+    const {render} = rendererWith({gmp});
     const {getByTestId} = render(<SeverityBar severity="5" />);
     const progressbarBox = getByTestId('progressbar-box');
 
     expect(progressbarBox).toHaveAttribute('title', 'Medium');
   });
 
+  test('should render title with critical severity', () => {
+    const gmp = {
+      settings: {
+        severityRating: SEVERITY_RATING_CVSS_3,
+      },
+    };
+    const {render} = rendererWith({gmp});
+    const {getByTestId} = render(<SeverityBar severity="9" />);
+    const progressbarBox = getByTestId('progressbar-box');
+
+    expect(progressbarBox).toHaveAttribute('title', 'Critical');
+  });
+
   test('should allow to overwrite title with toolTip', () => {
+    const gmp = {
+      settings: {
+        severityRating: DEFAULT_SEVERITY_RATING,
+      },
+    };
+    const {render} = rendererWith({gmp});
     const {element} = render(
       <SeverityBar severity="9.5" toolTip="tooltip text" />,
     );
@@ -49,6 +127,12 @@ describe('SeverityBar tests', () => {
   });
 
   test('should render progress', () => {
+    const gmp = {
+      settings: {
+        severityRating: DEFAULT_SEVERITY_RATING,
+      },
+    };
+    const {render} = rendererWith({gmp});
     const {getByTestId} = render(<SeverityBar severity="9.5" />);
     const progress = getByTestId('progress');
 
@@ -56,6 +140,12 @@ describe('SeverityBar tests', () => {
   });
 
   test('should not render progress > 100', () => {
+    const gmp = {
+      settings: {
+        severityRating: DEFAULT_SEVERITY_RATING,
+      },
+    };
+    const {render} = rendererWith({gmp});
     const {getByTestId} = render(<SeverityBar severity="10.1" />);
     const progress = getByTestId('progress');
 
@@ -63,23 +153,41 @@ describe('SeverityBar tests', () => {
   });
 
   test('should not render progress < 0', () => {
+    const gmp = {
+      settings: {
+        severityRating: DEFAULT_SEVERITY_RATING,
+      },
+    };
+    const {render} = rendererWith({gmp});
     const {getByTestId} = render(<SeverityBar severity="-0.1" />);
     const progress = getByTestId('progress');
 
     expect(progress).toHaveStyleRule('width', '0%');
   });
 
-  test('should render background', () => {
+  test('should render background with high severity', () => {
+    const gmp = {
+      settings: {
+        severityRating: SEVERITY_RATING_CVSS_2,
+      },
+    };
+    const {render} = rendererWith({gmp});
     const {getByTestId} = render(<SeverityBar severity="9.5" />);
     const progress = getByTestId('progress');
 
     expect(progress).toHaveStyleRule(
       'background',
-      `linear-gradient(90deg, ${Theme.severityClassCritical} 0%, ${Theme.severityClassCritical} 100%)`,
+      `linear-gradient(90deg, ${Theme.severityClassHigh} 0%, ${Theme.severityClassHigh} 100%)`,
     );
   });
 
   test('should render background for medium severity', () => {
+    const gmp = {
+      settings: {
+        severityRating: DEFAULT_SEVERITY_RATING,
+      },
+    };
+    const {render} = rendererWith({gmp});
     const {getByTestId} = render(<SeverityBar severity="5" />);
     const progress = getByTestId('progress');
 
@@ -89,7 +197,29 @@ describe('SeverityBar tests', () => {
     );
   });
 
+  test('should render background for critical severity', () => {
+    const gmp = {
+      settings: {
+        severityRating: SEVERITY_RATING_CVSS_3,
+      },
+    };
+    const {render} = rendererWith({gmp});
+    const {getByTestId} = render(<SeverityBar severity="9" />);
+    const progress = getByTestId('progress');
+
+    expect(progress).toHaveStyleRule(
+      'background',
+      `linear-gradient(90deg, ${Theme.severityClassCritical} 0%, ${Theme.severityClassCritical} 100%)`,
+    );
+  });
+
   test('should render without severity prop', () => {
+    const gmp = {
+      settings: {
+        severityRating: DEFAULT_SEVERITY_RATING,
+      },
+    };
+    const {render} = rendererWith({gmp});
     const {getByTestId} = render(<SeverityBar />);
     const progress = getByTestId('progress');
 
