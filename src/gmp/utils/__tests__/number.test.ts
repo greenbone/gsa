7a47692c6614a599ef3b1a821af6c5f569631f29
Björn Ricks <bjoern.ricks@greenbone.net>
2025-04-14 09:53:20 +0200

    change: Improve and cleanup number module
    * Use typscript for the module
    * Don't export fixedValue function because it's not used outside of the
      module
    * Clarify inputs and outputs of severityValue function

--- src/gmp/utils/__tests__/number.test.ts
+++ src/gmp/utils/__tests__/number.test.ts
@@ -4,7 +4,7 @@
  */
 
 import {describe, test, expect} from '@gsa/testing';
-import {severityValue, fixedValue} from 'gmp/utils/number';
+import {severityValue} from 'gmp/utils/number';
 
 describe('severityValue function tests', () => {
   test('should convert numbers to severity', () => {
@@ -16,19 +16,7 @@ describe('severityValue function tests', () => {
     expect(severityValue(1.15)).toEqual('1.1');
     expect(severityValue(1.16)).toEqual('1.2');
     expect(severityValue(1.19)).toEqual('1.2');
-  });
-});
-
-describe('fixedValue function tests', () => {
-  test('should convert numbers to fixed values', () => {
-    const num = 12345.6789;
-
-    expect(fixedValue(num)).toEqual('12345.6789');
-    expect(fixedValue(num, 1)).toEqual('12345.7');
-    expect(fixedValue(num, 6)).toEqual('12345.678900');
-
-    expect(fixedValue(2.34, 1)).toEqual('2.3');
-    expect(fixedValue(2.35, 1)).toEqual('2.4');
-    expect(fixedValue(2.55, 1)).toEqual('2.5');
+    expect(severityValue(undefined)).toBeUndefined();
+    expect(severityValue('A')).toBeUndefined();
   });
 });
