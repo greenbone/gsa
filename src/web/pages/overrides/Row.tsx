a185801e9a9d55a2b76d2e5587b0f9738e74405b
Björn Ricks <bjoern.ricks@greenbone.net>
2025-04-15 11:21:13 +0200

    change: Use TypeScript for override Row component

--- src/web/pages/overrides/Row.tsx
+++ src/web/pages/overrides/Row.tsx
@@ -4,6 +4,7 @@
  */
 
 import _ from 'gmp/locale';
+import Model from 'gmp/model';
 import {isDefined} from 'gmp/utils/identity';
 import {severityValue} from 'gmp/utils/number';
 import {shorten} from 'gmp/utils/string';
@@ -18,19 +19,43 @@ import withEntitiesActions from 'web/entities/withEntitiesActions';
 import CloneIcon from 'web/entity/icon/CloneIcon';
 import EditIcon from 'web/entity/icon/EditIcon';
 import TrashIcon from 'web/entity/icon/TrashIcon';
-import PropTypes from 'web/utils/PropTypes';
 import {
   extraRiskFactor,
   translateRiskFactor,
   LOG_VALUE,
 } from 'web/utils/severity';
 
-const render_severity = severity => {
+// should be changed to an Override model in future
+interface Override extends Model {
+  text: string;
+  nvt?: {
+    name: string;
+  };
+  hosts: string[];
+  port: string;
+  severity: number;
+  newSeverity: number;
+}
+
+interface OverrideRowProps extends ActionsProps {
+  actionsComponent?: React.ComponentType<ActionsProps>;
+  onToggleDetailsClick: () => void;
+}
+
+interface ActionsProps {
+  entity: Override;
+  onOverrideDeleteClick: (entity: Override) => void | Promise<void>;
+  onOverrideDownloadClick: (entity: Override) => void | Promise<void>;
+  onOverrideCloneClick: (entity: Override) => void | Promise<void>;
+  onOverrideEditClick: (entity: Override) => void | Promise<void>;
+}
+
+const renderSeverity = (severity: number): string => {
   if (isDefined(severity)) {
     if (severity <= LOG_VALUE) {
       return translateRiskFactor(extraRiskFactor(severity));
     }
-    return '> ' + severityValue(severity - 0.1);
+    return '> ' + (severityValue(severity - 0.1) as string);
   }
   return _('Any');
 };
@@ -42,42 +67,35 @@ const Actions = withEntitiesActions(
     onOverrideDownloadClick,
     onOverrideCloneClick,
     onOverrideEditClick,
-  }) => (
+  }: ActionsProps) => (
     <IconDivider grow align={['center', 'center']}>
-      <TrashIcon
+      <TrashIcon<Override>
         entity={entity}
         name="override"
         onClick={onOverrideDeleteClick}
       />
       <EditIcon entity={entity} name="override" onClick={onOverrideEditClick} />
-      <CloneIcon
+      <CloneIcon<Override>
         entity={entity}
         name="override"
         onClick={onOverrideCloneClick}
       />
-      <ExportIcon
+      <ExportIcon<Override>
         title={_('Export Override')}
         value={entity}
+        /* @ts-expect-error */
         onClick={onOverrideDownloadClick}
       />
     </IconDivider>
   ),
 );
 
-Actions.propTypes = {
-  entity: PropTypes.model.isRequired,
-  onOverrideCloneClick: PropTypes.func.isRequired,
-  onOverrideDeleteClick: PropTypes.func.isRequired,
-  onOverrideDownloadClick: PropTypes.func.isRequired,
-  onOverrideEditClick: PropTypes.func.isRequired,
-};
-
 const Row = ({
   actionsComponent: ActionsComponent = Actions,
   entity,
   onToggleDetailsClick,
   ...props
-}) => (
+}: OverrideRowProps) => (
   <TableRow>
     <TableData>
       <span>
@@ -91,7 +109,7 @@ const Row = ({
       {shorten(entity.hosts.join(', '))}
     </TableData>
     <TableData title={entity.port}>{shorten(entity.port)}</TableData>
-    <TableData>{render_severity(entity.severity)}</TableData>
+    <TableData>{renderSeverity(entity.severity)}</TableData>
     <TableData>
       <SeverityBar severity={entity.newSeverity} />
     </TableData>
@@ -99,11 +117,5 @@ const Row = ({
     <ActionsComponent {...props} entity={entity} />
   </TableRow>
 );
-
-Row.propTypes = {
-  actionsComponent: PropTypes.component,
-  entity: PropTypes.model.isRequired,
-  onToggleDetailsClick: PropTypes.func.isRequired,
-};
 
 export default Row;
