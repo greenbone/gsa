5cf4a6c787bdaa5e85f018e7d11ecfae86cb3015
Björn Ricks <bjoern.ricks@greenbone.net>
2025-04-16 14:38:00 +0200

    change: Use TypeScript for the GmpHttp request class

--- src/gmp/http/gmp.ts
+++ src/gmp/http/gmp.ts
@@ -3,12 +3,22 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import Http from 'gmp/http/http';
-import transform from 'gmp/http/transform/fastxml';
+import Http, {HttpOptions} from 'gmp/http/http';
+import {Meta} from 'gmp/http/response';
+import transform, {XmlMeta, XmlResponseData} from 'gmp/http/transform/fastxml';
 import {buildServerUrl} from 'gmp/http/utils';
 
-class GmpHttp extends Http {
-  constructor(settings) {
+interface GmpHttpSettings
+  extends HttpOptions<string, Meta, XmlResponseData, XmlMeta> {
+  apiServer: string;
+  apiProtocol: string;
+  token: string;
+}
+
+class GmpHttp extends Http<string, Meta, XmlResponseData, XmlMeta> {
+  settings: GmpHttpSettings;
+
+  constructor(settings: GmpHttpSettings) {
     const {apiServer, apiProtocol, timeout} = settings;
     const url = buildServerUrl(apiServer, 'gmp', apiProtocol);
     super(url, {timeout, transform});
