5d1fd50a99bab78da9f8977122edb7d2c28ec01e
Björn Ricks <bjoern.ricks@greenbone.net>
2025-04-17 14:18:01 +0200

    test: Convert TargetCommand tests to use async/await
    Improved readability of the tests

--- src/gmp/commands/__tests__/target.test.js
+++ src/gmp/commands/__tests__/target.test.js
@@ -8,262 +8,246 @@ import {TargetCommand} from 'gmp/commands/targets';
 import {createActionResultResponse, createHttp} from 'gmp/commands/testing';
 
 describe('TargetCommand tests', () => {
-  test('should create target', () => {
+  test('should create target', async () => {
     const response = createActionResultResponse();
     const fakeHttp = createHttp(response);
 
     expect.hasAssertions();
 
     const cmd = new TargetCommand(fakeHttp);
-    return cmd
-      .create({
-        allowSimultaneousIPs: '1',
-        name: 'name',
+    const resp = await cmd.create({
+      allowSimultaneousIPs: '1',
+      name: 'name',
+      comment: 'comment',
+      targetSource: 'manual',
+      targetExcludeSource: 'manual',
+      hostsFilter: undefined,
+      inUse: false,
+      hosts: '123.456, 678.9',
+      excludeHosts: '',
+      reverseLookupOnly: '0',
+      reverseLookupUnify: '1',
+      portListId: 'pl_id1',
+      aliveTests: 'Scan Config Default',
+      port: 22,
+      sshCredentialId: 'ssh_id',
+      sshElevateCredentialId: '0',
+      smbCredentialId: '0',
+      esxiCredentialId: '0',
+      snmpCredentialId: '0',
+      krb5CredentialId: '0',
+    });
+    expect(fakeHttp.request).toHaveBeenCalledWith('post', {
+      data: {
+        cmd: 'create_target',
+        allow_simultaneous_ips: '1',
+        alive_tests: 'Scan Config Default',
         comment: 'comment',
-        targetSource: 'manual',
-        targetExcludeSource: 'manual',
-        hostsFilter: undefined,
-        inUse: false,
+        esxi_credential_id: '0',
+        exclude_file: undefined,
+        exclude_hosts: '',
+        file: undefined,
         hosts: '123.456, 678.9',
-        excludeHosts: '',
-        reverseLookupOnly: '0',
-        reverseLookupUnify: '1',
-        portListId: 'pl_id1',
-        aliveTests: 'Scan Config Default',
+        hosts_filter: undefined,
+        name: 'name',
         port: 22,
-        sshCredentialId: 'ssh_id',
-        sshElevateCredentialId: '0',
-        smbCredentialId: '0',
-        esxiCredentialId: '0',
-        snmpCredentialId: '0',
-        krb5CredentialId: '0',
-      })
-      .then(resp => {
-        expect(fakeHttp.request).toHaveBeenCalledWith('post', {
-          data: {
-            cmd: 'create_target',
-            allow_simultaneous_ips: '1',
-            alive_tests: 'Scan Config Default',
-            comment: 'comment',
-            esxi_credential_id: '0',
-            exclude_file: undefined,
-            exclude_hosts: '',
-            file: undefined,
-            hosts: '123.456, 678.9',
-            hosts_filter: undefined,
-            name: 'name',
-            port: 22,
-            port_list_id: 'pl_id1',
-            reverse_lookup_unify: '1',
-            reverse_lookup_only: '0',
-            smb_credential_id: '0',
-            snmp_credential_id: '0',
-            ssh_credential_id: 'ssh_id',
-            ssh_elevate_credential_id: '0',
-            target_exclude_source: 'manual',
-            target_source: 'manual',
-            krb5_credential_id: '0',
-          },
-        });
-
-        const {data} = resp;
-        expect(data.id).toEqual('foo');
-      });
+        port_list_id: 'pl_id1',
+        reverse_lookup_unify: '1',
+        reverse_lookup_only: '0',
+        smb_credential_id: '0',
+        snmp_credential_id: '0',
+        ssh_credential_id: 'ssh_id',
+        ssh_elevate_credential_id: '0',
+        target_exclude_source: 'manual',
+        target_source: 'manual',
+        krb5_credential_id: '0',
+      },
+    });
+    const {data} = resp;
+    expect(data.id).toEqual('foo');
   });
 
-  test('should nullify ssh_elevate_credential in create command', () => {
+  test('should nullify ssh_elevate_credential in create command', async () => {
     const response = createActionResultResponse();
     const fakeHttp = createHttp(response);
 
     expect.hasAssertions();
 
     const cmd = new TargetCommand(fakeHttp);
-    return cmd
-      .create({
-        allowSimultaneousIPs: '1',
-        name: 'name',
+    const resp = await cmd.create({
+      allowSimultaneousIPs: '1',
+      name: 'name',
+      comment: 'comment',
+      targetSource: 'manual',
+      targetExcludeSource: 'manual',
+      hostsFilter: undefined,
+      inUse: false,
+      hosts: '123.456, 678.9',
+      excludeHosts: '',
+      reverseLookupOnly: '0',
+      reverseLookupUnify: '1',
+      portListId: 'pl_id1',
+      aliveTests: 'Scan Config Default',
+      port: 22,
+      sshCredentialId: '0',
+      sshElevateCredentialId: 'ssh_elevate_id',
+      smbCredentialId: '0',
+      esxiCredentialId: '0',
+      snmpCredentialId: '0',
+      krb5CredentialId: '0',
+    });
+    expect(fakeHttp.request).toHaveBeenCalledWith('post', {
+      data: {
+        cmd: 'create_target',
+        allow_simultaneous_ips: '1',
+        alive_tests: 'Scan Config Default',
         comment: 'comment',
-        targetSource: 'manual',
-        targetExcludeSource: 'manual',
-        hostsFilter: undefined,
-        inUse: false,
+        esxi_credential_id: '0',
+        exclude_file: undefined,
+        exclude_hosts: '',
+        file: undefined,
         hosts: '123.456, 678.9',
-        excludeHosts: '',
-        reverseLookupOnly: '0',
-        reverseLookupUnify: '1',
-        portListId: 'pl_id1',
-        aliveTests: 'Scan Config Default',
+        hosts_filter: undefined,
+        name: 'name',
         port: 22,
-        sshCredentialId: '0',
-        sshElevateCredentialId: 'ssh_elevate_id',
-        smbCredentialId: '0',
-        esxiCredentialId: '0',
-        snmpCredentialId: '0',
-        krb5CredentialId: '0',
-      })
-      .then(resp => {
-        expect(fakeHttp.request).toHaveBeenCalledWith('post', {
-          data: {
-            cmd: 'create_target',
-            allow_simultaneous_ips: '1',
-            alive_tests: 'Scan Config Default',
-            comment: 'comment',
-            esxi_credential_id: '0',
-            exclude_file: undefined,
-            exclude_hosts: '',
-            file: undefined,
-            hosts: '123.456, 678.9',
-            hosts_filter: undefined,
-            name: 'name',
-            port: 22,
-            port_list_id: 'pl_id1',
-            reverse_lookup_unify: '1',
-            reverse_lookup_only: '0',
-            smb_credential_id: '0',
-            snmp_credential_id: '0',
-            ssh_credential_id: '0',
-            ssh_elevate_credential_id: '0',
-            target_exclude_source: 'manual',
-            target_source: 'manual',
-            krb5_credential_id: '0',
-          },
-        });
-
-        const {data} = resp;
-        expect(data.id).toEqual('foo');
-      });
+        port_list_id: 'pl_id1',
+        reverse_lookup_unify: '1',
+        reverse_lookup_only: '0',
+        smb_credential_id: '0',
+        snmp_credential_id: '0',
+        ssh_credential_id: '0',
+        ssh_elevate_credential_id: '0',
+        target_exclude_source: 'manual',
+        target_source: 'manual',
+        krb5_credential_id: '0',
+      },
+    });
+    const {data} = resp;
+    expect(data.id).toEqual('foo');
   });
 
-  test('should save target', () => {
+  test('should save target', async () => {
     const response = createActionResultResponse();
     const fakeHttp = createHttp(response);
 
     expect.hasAssertions();
 
     const cmd = new TargetCommand(fakeHttp);
-    return cmd
-      .save({
-        id: 'target_id1',
-        allowSimultaneousIPs: '1',
-        name: 'name',
+    const resp = await cmd.save({
+      id: 'target_id1',
+      allowSimultaneousIPs: '1',
+      name: 'name',
+      comment: 'comment',
+      targetSource: 'manual',
+      targetExcludeSource: 'manual',
+      hostsFilter: undefined,
+      excludeFile: undefined,
+      inUse: false,
+      hosts: '123.456, 678.9',
+      excludeHosts: '',
+      reverseLookupOnly: '0',
+      reverseLookupUnify: '1',
+      portListId: 'pl_id1',
+      aliveTests: 'Scan Config Default',
+      port: 22,
+      sshCredentialId: 'ssh_id',
+      sshElevateCredentialId: '0',
+      smbCredentialId: '0',
+      esxiCredentialId: '0',
+      snmpCredentialId: '0',
+      krb5CredentialId: '0',
+    });
+    expect(fakeHttp.request).toHaveBeenCalledWith('post', {
+      data: {
+        cmd: 'save_target',
+        allow_simultaneous_ips: '1',
+        alive_tests: 'Scan Config Default',
         comment: 'comment',
-        targetSource: 'manual',
-        targetExcludeSource: 'manual',
-        hostsFilter: undefined,
-        excludeFile: undefined,
-        inUse: false,
+        esxi_credential_id: '0',
+        exclude_file: undefined,
+        exclude_hosts: '',
+        file: undefined,
         hosts: '123.456, 678.9',
-        excludeHosts: '',
-        reverseLookupOnly: '0',
-        reverseLookupUnify: '1',
-        portListId: 'pl_id1',
-        aliveTests: 'Scan Config Default',
+        hosts_filter: undefined,
+        in_use: '0',
+        name: 'name',
         port: 22,
-        sshCredentialId: 'ssh_id',
-        sshElevateCredentialId: '0',
-        smbCredentialId: '0',
-        esxiCredentialId: '0',
-        snmpCredentialId: '0',
-        krb5CredentialId: '0',
-      })
-      .then(resp => {
-        expect(fakeHttp.request).toHaveBeenCalledWith('post', {
-          data: {
-            cmd: 'save_target',
-            allow_simultaneous_ips: '1',
-            alive_tests: 'Scan Config Default',
-            comment: 'comment',
-            esxi_credential_id: '0',
-            exclude_file: undefined,
-            exclude_hosts: '',
-            file: undefined,
-            hosts: '123.456, 678.9',
-            hosts_filter: undefined,
-            in_use: '0',
-            name: 'name',
-            port: 22,
-            port_list_id: 'pl_id1',
-            reverse_lookup_unify: '1',
-            reverse_lookup_only: '0',
-            smb_credential_id: '0',
-            snmp_credential_id: '0',
-            ssh_credential_id: 'ssh_id',
-            ssh_elevate_credential_id: '0',
-            krb5_credential_id: '0',
-            target_exclude_source: 'manual',
-            target_id: 'target_id1',
-            target_source: 'manual',
-          },
-        });
-
-        const {data} = resp;
-        expect(data.id).toEqual('foo');
-      });
+        port_list_id: 'pl_id1',
+        reverse_lookup_unify: '1',
+        reverse_lookup_only: '0',
+        smb_credential_id: '0',
+        snmp_credential_id: '0',
+        ssh_credential_id: 'ssh_id',
+        ssh_elevate_credential_id: '0',
+        krb5_credential_id: '0',
+        target_exclude_source: 'manual',
+        target_id: 'target_id1',
+        target_source: 'manual',
+      },
+    });
+    const {data} = resp;
+    expect(data.id).toEqual('foo');
   });
-  test('should nullify ssh_elevate_credential in save command', () => {
+  test('should nullify ssh_elevate_credential in save command', async () => {
     const response = createActionResultResponse();
     const fakeHttp = createHttp(response);
 
     expect.hasAssertions();
 
     const cmd = new TargetCommand(fakeHttp);
-    return cmd
-      .save({
-        id: 'target_id1',
-        allowSimultaneousIPs: '1',
-        name: 'name',
+    const resp = await cmd.save({
+      id: 'target_id1',
+      allowSimultaneousIPs: '1',
+      name: 'name',
+      comment: 'comment',
+      targetSource: 'manual',
+      targetExcludeSource: 'manual',
+      hostsFilter: undefined,
+      excludeFile: undefined,
+      inUse: false,
+      hosts: '123.456, 678.9',
+      excludeHosts: '',
+      reverseLookupOnly: '0',
+      reverseLookupUnify: '1',
+      portListId: 'pl_id1',
+      aliveTests: 'Scan Config Default',
+      port: 22,
+      sshCredentialId: '0',
+      sshElevateCredentialId: 'ssh_elevate_id',
+      smbCredentialId: '0',
+      esxiCredentialId: '0',
+      snmpCredentialId: '0',
+      krb5CredentialId: '0',
+    });
+    expect(fakeHttp.request).toHaveBeenCalledWith('post', {
+      data: {
+        cmd: 'save_target',
+        allow_simultaneous_ips: '1',
+        alive_tests: 'Scan Config Default',
         comment: 'comment',
-        targetSource: 'manual',
-        targetExcludeSource: 'manual',
-        hostsFilter: undefined,
-        excludeFile: undefined,
-        inUse: false,
+        esxi_credential_id: '0',
+        exclude_file: undefined,
+        exclude_hosts: '',
+        file: undefined,
         hosts: '123.456, 678.9',
-        excludeHosts: '',
-        reverseLookupOnly: '0',
-        reverseLookupUnify: '1',
-        portListId: 'pl_id1',
-        aliveTests: 'Scan Config Default',
+        hosts_filter: undefined,
+        in_use: '0',
+        name: 'name',
         port: 22,
-        sshCredentialId: '0',
-        sshElevateCredentialId: 'ssh_elevate_id',
-        smbCredentialId: '0',
-        esxiCredentialId: '0',
-        snmpCredentialId: '0',
-        krb5CredentialId: '0',
-      })
-      .then(resp => {
-        expect(fakeHttp.request).toHaveBeenCalledWith('post', {
-          data: {
-            cmd: 'save_target',
-            allow_simultaneous_ips: '1',
-            alive_tests: 'Scan Config Default',
-            comment: 'comment',
-            esxi_credential_id: '0',
-            exclude_file: undefined,
-            exclude_hosts: '',
-            file: undefined,
-            hosts: '123.456, 678.9',
-            hosts_filter: undefined,
-            in_use: '0',
-            name: 'name',
-            port: 22,
-            port_list_id: 'pl_id1',
-            reverse_lookup_unify: '1',
-            reverse_lookup_only: '0',
-            smb_credential_id: '0',
-            snmp_credential_id: '0',
-            ssh_credential_id: '0',
-            ssh_elevate_credential_id: '0',
-            target_exclude_source: 'manual',
-            target_id: 'target_id1',
-            target_source: 'manual',
-            krb5_credential_id: '0',
-          },
-        });
-
-        const {data} = resp;
-        expect(data.id).toEqual('foo');
-      });
+        port_list_id: 'pl_id1',
+        reverse_lookup_unify: '1',
+        reverse_lookup_only: '0',
+        smb_credential_id: '0',
+        snmp_credential_id: '0',
+        ssh_credential_id: '0',
+        ssh_elevate_credential_id: '0',
+        target_exclude_source: 'manual',
+        target_id: 'target_id1',
+        target_source: 'manual',
+        krb5_credential_id: '0',
+      },
+    });
+    const {data} = resp;
+    expect(data.id).toEqual('foo');
   });
 });
