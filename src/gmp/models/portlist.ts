584d6ef6fa9e575a6336c6cda9eb40a868fd9644
Björn Ricks <bjoern.ricks@greenbone.net>
2025-04-24 09:57:37 +0200

    change: Use TypeScript for PortList model

---
+++ src/gmp/models/portlist.ts
@@ -0,0 +1,111 @@
+/* SPDX-FileCopyrightText: 2024 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import Model, {
+  Element,
+  ModelElement,
+  ModelProperties,
+  parseModelFromElement,
+} from 'gmp/model';
+import {parseInt, parseBoolean} from 'gmp/parser';
+import {map} from 'gmp/utils/array';
+import {isDefined} from 'gmp/utils/identity';
+
+interface PortRangeElement extends ModelElement {
+  type?: string;
+  port_list_id?: string;
+}
+
+interface PortRangeProperties extends ModelProperties {
+  protocol_type?: string;
+}
+
+interface PortListElement extends ModelElement {
+  id?: string;
+  port_ranges?: {
+    port_range: PortRangeElement[];
+  };
+  port_count?: {
+    all?: string;
+    tcp?: string;
+    udp?: string;
+  };
+  targets?: {
+    target: ModelElement[];
+  };
+  predefined?: string;
+}
+
+interface PortCount {
+  all: number;
+  tcp: number;
+  udp: number;
+}
+
+interface PortListProperties extends ModelProperties {
+  port_ranges?: PortRangeProperties[];
+  port_count: {
+    all: number;
+    tcp: number;
+    udp: number;
+  };
+  targets?: Model[];
+  predefined?: boolean;
+}
+
+class PortRange extends Model {
+  static entityType = 'portrange';
+  protocol_type!: string;
+  port_list_id!: string;
+
+  parseProperties(element: PortRangeElement): PortRangeProperties {
+    return PortRange.parseElement(element);
+  }
+
+  static parseElement(element: PortRangeElement): PortRangeProperties {
+    const ret = super.parseElement(element) as PortRangeProperties;
+    ret.protocol_type = element.type;
+    return ret;
+  }
+}
+
+class PortList extends Model {
+  static entityType = 'portlist';
+  port_ranges!: PortRange[];
+  port_count!: PortCount;
+  targets!: Model[];
+  predefined!: boolean;
+
+  static parseElement(element: PortListElement): PortListProperties {
+    const ret = super.parseElement(element) as PortListProperties;
+
+    const ranges =
+      isDefined(ret.port_ranges) && isDefined(element.port_ranges)
+        ? element.port_ranges.port_range
+        : [];
+
+    ret.port_ranges = map(ranges, (range: PortRangeElement) => {
+      range.port_list_id = ret.id;
+      return PortRange.fromElement(range as Element);
+    });
+
+    const {port_count} = element;
+    ret.port_count = {
+      all: parseInt(port_count?.all) || 0,
+      tcp: parseInt(port_count?.tcp) || 0,
+      udp: parseInt(port_count?.udp) || 0,
+    };
+
+    ret.targets = map(element?.targets?.target, target =>
+      parseModelFromElement(target, 'target'),
+    );
+
+    ret.predefined = parseBoolean(element.predefined);
+
+    return ret;
+  }
+}
+
+export default PortList;
