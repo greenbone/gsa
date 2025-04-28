c073312c3df9c4fea60f7fa9ecd7405a0d84f070
Björn Ricks <bjoern.ricks@greenbone.net>
2025-04-28 12:18:41 +0200

    Updated EntitiesCommand tests for getEntitiesResponse changes
    With the current changes the getEntititesResponse method now raises an
    error because it is not implemented.

--- src/gmp/commands/__tests__/entities.test.js
+++ src/gmp/commands/__tests__/entities.test.js
@@ -3,13 +3,13 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import {describe, test, expect} from '@gsa/testing';
+import {describe, test, expect, testing} from '@gsa/testing';
 import EntitiesCommand from 'gmp/commands/entities';
 import {createEntitiesResponse, createHttp} from 'gmp/commands/testing';
 import Filter from 'gmp/models/filter';
 
 describe('EntitiesCommand tests', () => {
-  test('should add filter parameter', () => {
+  test('should add filter parameter', async () => {
     const filter = Filter.fromString('foo=bar');
     const response = createEntitiesResponse('foo', []);
     const fakeHttp = createHttp(response);
@@ -17,17 +17,17 @@ describe('EntitiesCommand tests', () => {
     expect.hasAssertions();
 
     const cmd = new EntitiesCommand(fakeHttp, 'foo');
-    return cmd.get({filter}).then(() => {
-      expect(fakeHttp.request).toHaveBeenCalledWith('get', {
-        args: {
-          cmd: 'get_foos',
-          filter: 'foo=bar',
-        },
-      });
+    cmd.getEntitiesResponse = testing.fn().mockReturnValue(data => data);
+    await cmd.get({filter});
+    expect(fakeHttp.request).toHaveBeenCalledWith('get', {
+      args: {
+        cmd: 'get_foos',
+        filter: 'foo=bar',
+      },
     });
   });
 
-  test('should add filter_id parameter', () => {
+  test('should add filter_id parameter', async () => {
     const filter = Filter.fromElement({_id: 'bar'});
     const response = createEntitiesResponse('foo', []);
     const fakeHttp = createHttp(response);
@@ -35,17 +35,17 @@ describe('EntitiesCommand tests', () => {
     expect.hasAssertions();
 
     const cmd = new EntitiesCommand(fakeHttp, 'foo');
-    return cmd.get({filter}).then(() => {
-      expect(fakeHttp.request).toHaveBeenCalledWith('get', {
-        args: {
-          cmd: 'get_foos',
-          filter_id: 'bar',
-        },
-      });
+    cmd.getEntitiesResponse = testing.fn().mockReturnValue(data => data);
+    await cmd.get({filter});
+    expect(fakeHttp.request).toHaveBeenCalledWith('get', {
+      args: {
+        cmd: 'get_foos',
+        filter_id: 'bar',
+      },
     });
   });
 
-  test('should prefer filter_id over filter parameter', () => {
+  test('should prefer filter_id over filter parameter', async () => {
     const filter = Filter.fromElement({
       _id: 'bar',
       keywords: {
@@ -60,17 +60,17 @@ describe('EntitiesCommand tests', () => {
     expect.hasAssertions();
 
     const cmd = new EntitiesCommand(fakeHttp, 'foo');
-    return cmd.get({filter}).then(() => {
-      expect(fakeHttp.request).toHaveBeenCalledWith('get', {
-        args: {
-          cmd: 'get_foos',
-          filter_id: 'bar',
-        },
-      });
+    cmd.getEntitiesResponse = testing.fn().mockReturnValue(data => data);
+    await cmd.get({filter});
+    expect(fakeHttp.request).toHaveBeenCalledWith('get', {
+      args: {
+        cmd: 'get_foos',
+        filter_id: 'bar',
+      },
     });
   });
 
-  test('deleteByIds() should should call bulk_delete with correct ids', () => {
+  test('deleteByIds() should should call bulk_delete with correct ids', async () => {
     const response = createEntitiesResponse('foo', []);
     const fakeHttp = createHttp(response);
 
@@ -81,15 +81,14 @@ describe('EntitiesCommand tests', () => {
     expect.hasAssertions();
 
     const cmd = new EntitiesCommand(fakeHttp, 'foo');
-    return cmd.deleteByIds(ids).then(() => {
-      expect(fakeHttp.request).toHaveBeenCalledWith('post', {
-        data: {
-          'bulk_selected:123': 1,
-          'bulk_selected:456': 1,
-          cmd: 'bulk_delete',
-          resource_type: 'foo',
-        },
-      });
+    await cmd.deleteByIds(ids);
+    expect(fakeHttp.request).toHaveBeenCalledWith('post', {
+      data: {
+        'bulk_selected:123': 1,
+        'bulk_selected:456': 1,
+        cmd: 'bulk_delete',
+        resource_type: 'foo',
+      },
     });
   });
 });
