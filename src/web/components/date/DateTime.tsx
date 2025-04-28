581be26d498794a46eadf0130fdb4e187f8f0174
daniele-mng <daniele.mangiagli@greenbone.net>
2025-04-28 16:38:41 +0200

    Datetime support 2 lines display

--- src/web/components/date/DateTime.tsx
+++ src/web/components/date/DateTime.tsx
@@ -7,24 +7,48 @@ import {ensureDate} from 'gmp/locale/date';
 import {Date} from 'gmp/models/date';
 import {isDefined, hasValue} from 'gmp/utils/identity';
 import useUserTimezone from 'web/hooks/useUserTimezone';
-import {formattedUserSettingDateTimeWithTimeZone} from 'web/utils/userSettingTimeDateFormatters';
+import {
+  formattedUserSettingDateTimeWithTimeZone,
+  formattedUserSettingDateTimeObject,
+} from 'web/utils/userSettingTimeDateFormatters';
 
 interface DateTimeProps {
   date?: Date | string;
   formatter?: (date: Date, timezone: string) => string | undefined;
   timezone?: string;
+  showTimezoneAsSeparateLine?: boolean;
 }
 
-const DateTime: React.FC<DateTimeProps> = ({
+const DateTime = ({
   formatter = formattedUserSettingDateTimeWithTimeZone,
   timezone,
   date,
+  showTimezoneAsSeparateLine = false,
 }: DateTimeProps) => {
   const [userTimezone] = useUserTimezone();
 
   date = ensureDate(date);
   if (!hasValue(timezone)) {
     timezone = userTimezone;
+  }
+
+  if (!isDefined(date) || !date.isValid()) {
+    return null;
+  }
+
+  if (showTimezoneAsSeparateLine) {
+    const formattedDate = formattedUserSettingDateTimeObject(date, timezone);
+
+    if (!formattedDate) {
+      return null;
+    }
+
+    return (
+      <div>
+        <p>{formattedDate.datetime}</p>
+        <p>{formattedDate.timezone}</p>
+      </div>
+    );
   }
 
   return !isDefined(date) || !date.isValid() ? null : formatter(date, timezone);
