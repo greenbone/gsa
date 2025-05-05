e81f003e6c75b2f4a0cf45632679e9d68cc2a931
daniele-mng <daniele.mangiagli@greenbone.net>
2025-05-05 15:01:40 +0200

    rename files

--- src/web/pages/results/DetailsPage.jsx
+++ src/web/pages/results/DetailsPage.jsx
@@ -52,7 +52,7 @@ import withEntityContainer from 'web/entity/withEntityContainer';
 import useCapabilities from 'web/hooks/useCapabilities';
 import useGmp from 'web/hooks/useGmp';
 import useTranslation from 'web/hooks/useTranslation';
-import NoteComponent from 'web/pages/notes/Component';
+import NoteComponent from 'web/pages/notes/NoteComponent';
 import OverrideComponent from 'web/pages/overrides/Component';
 import ResultDetails from 'web/pages/results/Details';
 import TicketComponent from 'web/pages/tickets/Component';
