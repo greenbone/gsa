3efb56a3aca0e521018f0bab5a704d2a14ce54b0
Björn Ricks <bjoern.ricks@greenbone.net>
2025-05-22 13:24:26 +0200

    change: Use TypeScript for YesNoRadio form component

--- src/web/components/form/__tests__/YesNoRadio.test.tsx
+++ src/web/components/form/__tests__/YesNoRadio.test.tsx
@@ -6,16 +6,15 @@
 import {describe, test, expect, testing} from '@gsa/testing';
 import {NO_VALUE, YES_VALUE} from 'gmp/parser';
 import YesNoRadio from 'web/components/form/YesNoRadio';
-import {getRadioInputs} from 'web/components/testing';
-import {render, fireEvent} from 'web/utils/Testing';
+import {render, fireEvent, screen} from 'web/utils/Testing';
 
-const getLabels = element => element.querySelectorAll('label');
+const getLabels = (element: HTMLElement) => element.querySelectorAll('label');
 
 describe('YesNoRadio tests', () => {
   test('should render', () => {
-    const {element} = render(<YesNoRadio />);
+    const {container} = render(<YesNoRadio />);
 
-    const labels = getLabels(element);
+    const labels = getLabels(container);
 
     expect(labels[0]).toHaveTextContent('Yes');
     expect(labels[1]).toHaveTextContent('No');
@@ -23,37 +22,28 @@ describe('YesNoRadio tests', () => {
 
   test('should call change handler', () => {
     const onChange = testing.fn();
-    const {element} = render(<YesNoRadio onChange={onChange} />);
+    render(<YesNoRadio onChange={onChange} />);
 
-    const inputs = getRadioInputs(element);
-    expect(inputs.length).toEqual(2);
-
-    fireEvent.click(inputs[0]);
-
+    fireEvent.click(screen.getByTestId('radio-yes'));
     expect(onChange).toHaveBeenCalledWith(YES_VALUE, undefined);
 
-    fireEvent.click(inputs[1]);
-
+    fireEvent.click(screen.getByTestId('radio-no'));
     expect(onChange).toHaveBeenCalledWith(NO_VALUE, undefined);
   });
 
   test('should call change handler with name', () => {
     const onChange = testing.fn();
-    const {element} = render(<YesNoRadio name="foo" onChange={onChange} />);
+    render(<YesNoRadio name="foo" onChange={onChange} />);
 
-    const inputs = getRadioInputs(element);
-    expect(inputs.length).toEqual(2);
-
-    fireEvent.click(inputs[0]);
-
+    fireEvent.click(screen.getByTestId('radio-yes'));
     expect(onChange).toHaveBeenCalledWith(YES_VALUE, 'foo');
   });
 
   test('should allow to set values for yes and no state', () => {
     const onChange = testing.fn();
-    const {element} = render(
-      <YesNoRadio
-        convert={v => v}
+    render(
+      <YesNoRadio<string>
+        convert={v => v as string}
         name="ipsum"
         noValue="bar"
         yesValue="foo"
@@ -61,51 +51,34 @@ describe('YesNoRadio tests', () => {
       />,
     );
 
-    const inputs = getRadioInputs(element);
-    expect(inputs.length).toEqual(2);
-
-    fireEvent.click(inputs[0]);
-
+    fireEvent.click(screen.getByTestId('radio-yes'));
     expect(onChange).toHaveBeenCalledWith('foo', 'ipsum');
 
-    fireEvent.click(inputs[1]);
-
+    fireEvent.click(screen.getByTestId('radio-no'));
     expect(onChange).toHaveBeenCalledWith('bar', 'ipsum');
   });
 
   test('should call change handler only if checked state changes', () => {
     const onChange = testing.fn();
-    const {element} = render(
-      <YesNoRadio value={YES_VALUE} onChange={onChange} />,
-    );
+    render(<YesNoRadio value={YES_VALUE} onChange={onChange} />);
 
-    const inputs = getRadioInputs(element);
-    expect(inputs.length).toEqual(2);
-
-    fireEvent.click(inputs[0]);
-
+    fireEvent.click(screen.getByTestId('radio-yes'));
     expect(onChange).not.toHaveBeenCalled();
 
-    fireEvent.click(inputs[1]);
-
+    fireEvent.click(screen.getByTestId('radio-no'));
     expect(onChange).toHaveBeenCalledWith(NO_VALUE, undefined);
   });
 
   test('should not call change handler if disabled', () => {
     const onChange = testing.fn();
-    const {element} = render(
+    render(
       <YesNoRadio disabled={true} value={YES_VALUE} onChange={onChange} />,
     );
 
-    const inputs = getRadioInputs(element);
-    expect(inputs.length).toEqual(2);
-
-    fireEvent.click(inputs[0]);
-
+    fireEvent.click(screen.getByTestId('radio-yes'));
     expect(onChange).not.toHaveBeenCalled();
 
-    fireEvent.click(inputs[1]);
-
+    fireEvent.click(screen.getByTestId('radio-no'));
     expect(onChange).not.toHaveBeenCalled();
   });
 });
