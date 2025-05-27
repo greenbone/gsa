825444d873f45e050452e750025b1431c78f3a36
Björn Ricks <bjoern.ricks@greenbone.net>
2025-05-27 11:22:35 +0200

    change: Use TypeScript for useFormValidation hook

--- src/web/components/form/useFormValidation.ts
+++ src/web/components/form/useFormValidation.ts
@@ -7,25 +7,46 @@ import {_, _l} from 'gmp/locale/lang';
 import {hasValue, isDefined, isFunction} from 'gmp/utils/identity';
 import {useState, useEffect, useCallback} from 'react';
 
-const useFormValidation = (
-  validationRules,
-  values,
-  {onValidationSuccess, onValidationError, fieldsToValidate},
+type ValidationFunc<TValue, TValues> = (
+  value: TValue,
+  values?: TValues,
+) => string | undefined | void;
+
+type ValidationRules<TValues> = {
+  [K in keyof TValues]: ValidationFunc<TValues[K], TValues>;
+};
+
+type ErrorType<TValues> = {
+  [K in keyof TValues]?: string | undefined;
+};
+
+const useFormValidation = <TValues extends {}>(
+  validationRules: ValidationRules<TValues>,
+  values: TValues,
+  {
+    onValidationSuccess,
+    onValidationError,
+    fieldsToValidate,
+  }: {
+    onValidationSuccess?: (values: TValues) => void;
+    onValidationError?: (message: string) => void;
+    fieldsToValidate?: string[];
+  },
 ) => {
   const [hasError, setHasError] = useState(false);
-  const [errors, setErrors] = useState({});
+  const [errors, setErrors] = useState<ErrorType<TValues>>({});
 
   const validateValue = useCallback(
-    (value, name) => {
+    (value: unknown, name: string) => {
       const validationRule = validationRules[name];
-      let validity;
+      let validity: string | undefined | void;
 
       try {
         validity = isFunction(validationRule)
           ? validationRule(value, values)
           : undefined;
       } catch (e) {
-        validity = e.message;
+        validity = (e as Error).message;
       }
 
       setErrors(prevErrors => ({
@@ -42,11 +63,11 @@ const useFormValidation = (
     // update validity status e.g. to remove errors if a user has set a
     // correct value now
     const fieldNames = fieldsToValidate ?? Object.keys(values);
-    fieldNames.forEach(name => validateValue(values[name], name, values));
+    fieldNames.forEach(name => validateValue(values[name], name));
   }, [fieldsToValidate, validateValue, values]);
 
   const validate = useCallback(
-    formValues => {
+    (formValues: TValues) => {
       const fieldNames = fieldsToValidate ?? Object.keys(formValues);
 
       const hasErrorInState = fieldNames.some(name => hasValue(errors[name])); // checks if any field is invalid
@@ -77,10 +98,11 @@ const useFormValidation = (
 };
 
 // Some common validation rules
-export const shouldBeNonEmpty = (string = '') =>
+export const shouldBeNonEmpty = (string: string = '') =>
   String(string).trim().length > 0;
-export const shouldBeValidPassword = (password = '') => password.length > 5;
-export const shouldBeValidName = (string = '') =>
+export const shouldBeValidPassword = (password: string = '') =>
+  password.length > 5;
+export const shouldBeValidName = (string: string = '') =>
   string.match(/^[.#\-_ ,/a-z0-9äüöÄÜÖß]+$/i) !== null; // this is analogue to the
 // regex in gsad.c for 'name' - it needs to be checked, whether :alnum: contains
 // more than the characters above
