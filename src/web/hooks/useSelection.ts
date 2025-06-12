8a273952aecba4561cfac5cf43ebe9af96825b06
Björn Ricks <bjoern.ricks@greenbone.net>
2025-06-12 14:44:29 +0200

    change: Use TypeScript for useSelection hook and SelectionType

--- src/web/hooks/useSelection.ts
+++ src/web/hooks/useSelection.ts
@@ -7,49 +7,47 @@ import {useState, useCallback} from 'react';
 import {isDefined} from 'gmp/utils/identity';
 import SelectionType from 'web/utils/SelectionType';
 
+type SelectionTypeType = (typeof SelectionType)[keyof typeof SelectionType];
+
 /**
  * Hook to manage selection of entities
  *
- * @param {int} initialSelectionType The initial selection type to be used.
+ * @param initialSelectionType The initial selection type to be used.
  *    Default is SelectionType.SELECTION_PAGE_CONTENTS
- * @returns {Object} selected, selectionType, select, deselect, changeSelectionType
+ * @returns Object selected, selectionType, select, deselect, changeSelectionType
  */
-const useSelection = (
-  initialSelectionType = SelectionType.SELECTION_PAGE_CONTENTS,
+const useSelection = <TSelect = {}>(
+  initialSelectionType: SelectionTypeType = SelectionType.SELECTION_PAGE_CONTENTS,
 ) => {
-  const [selected, setSelected] = useState(
-    initialSelectionType === SelectionType.SELECTION_USER ? [] : undefined,
-  );
+  const [selected, setSelected] = useState<Array<TSelect>>([]);
   const [selectionType, setSelectionType] = useState(initialSelectionType);
 
-  const select = useCallback(obj => {
+  const select = useCallback((obj: TSelect) => {
     // ensure the using component gets re-rendered by creating new array
     setSelected(prevSelected =>
       isDefined(prevSelected) && !prevSelected.includes(obj)
         ? [...prevSelected, obj]
-        : undefined,
+        : [],
     );
   }, []);
 
-  const deselect = useCallback(obj => {
+  const deselect = useCallback((obj: TSelect) => {
     // ensure the using component gets re-rendered by creating new array
     setSelected(prevSelected => {
       if (isDefined(prevSelected) && prevSelected.includes(obj)) {
         return prevSelected.filter(o => o !== obj);
       }
-      return undefined;
+      return [];
     });
   }, []);
 
-  const changeSelectionType = useCallback(newSelectionType => {
-    if (newSelectionType === SelectionType.SELECTION_USER) {
+  const changeSelectionType = useCallback(
+    (newSelectionType: SelectionTypeType) => {
       setSelected([]);
-    } else {
-      setSelected();
-    }
-
-    setSelectionType(newSelectionType);
-  }, []);
+      setSelectionType(newSelectionType);
+    },
+    [],
+  );
   return {selected, selectionType, select, deselect, changeSelectionType};
 };
 
