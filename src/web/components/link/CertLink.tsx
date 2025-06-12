bf7e27f0a2f521d1f52c6bb32ab7bdbc64a629df
Björn Ricks <bjoern.ricks@greenbone.net>
2025-06-12 11:59:26 +0200

    change: Use TypeScript for CertLink component

--- src/web/components/link/CertLink.tsx
+++ src/web/components/link/CertLink.tsx
@@ -3,13 +3,17 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import React from 'react';
 import {isString} from 'gmp/utils/identity';
 import DetailsLink from 'web/components/link/DetailsLink';
 import useTranslation from 'web/hooks/useTranslation';
-import PropTypes from 'web/utils/PropTypes';
 
-const CertLink = ({id, textOnly = false, type}) => {
+interface CertLinkProps {
+  id: string;
+  textOnly?: boolean;
+  type: string;
+}
+
+const CertLink = ({id, textOnly = false, type}: CertLinkProps) => {
   const [_] = useTranslation();
   const lcType = isString(type) ? type.toLowerCase() : undefined;
   if (lcType !== 'cert-bund' && lcType !== 'dfn-cert') {
@@ -21,13 +25,13 @@ const CertLink = ({id, textOnly = false, type}) => {
     );
   }
 
-  let info_type;
-  let title;
+  let info_type: string;
+  let title: string;
 
   if (lcType === 'cert-bund') {
     info_type = 'certbund';
     title = _('View details of CERT-Bund Advisory {{name}}', {name: id});
-  } else if (lcType === 'dfn-cert') {
+  } else {
     title = _('View details of DFN-CERT Advisory {{name}}', {name: id});
     info_type = 'dfncert';
   }
@@ -42,12 +46,6 @@ const CertLink = ({id, textOnly = false, type}) => {
       {id}
     </DetailsLink>
   );
-};
-
-CertLink.propTypes = {
-  id: PropTypes.string.isRequired,
-  textOnly: PropTypes.bool,
-  type: PropTypes.string.isRequired,
 };
 
 export default CertLink;
