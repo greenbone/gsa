54b50bba7852774058ec6a63a1624fae8ae0b7e3
Björn Ricks <bjoern.ricks@greenbone.net>
2025-06-13 10:03:23 +0200

    change: Use TypeScript fro useLoadCapabilities hook and tests

--- src/web/hooks/__tests__/useLoadCapabilities.test.tsx
+++ src/web/hooks/__tests__/useLoadCapabilities.test.tsx
@@ -5,6 +5,7 @@
 
 import {describe, test, expect, testing} from '@gsa/testing';
 import {rendererWith, screen, wait} from 'web/testing';
+import Capabilities from 'gmp/capabilities/capabilities';
 import useLoadCapabilities from 'web/hooks/useLoadCapabilities';
 
 const TestComponent = () => {
@@ -14,8 +15,8 @@ const TestComponent = () => {
       {capabilities &&
         capabilities.map(capability => {
           return (
-            <div key={capability.name} data-testid="capability">
-              {capability.name}
+            <div key={capability} data-testid="capability">
+              {capability}
             </div>
           );
         })}
@@ -25,11 +26,11 @@ const TestComponent = () => {
 
 describe('useLoadCapabilities tests', () => {
   test('should load capabilities', async () => {
-    const capabilities = [{name: 'cap_1'}, {name: 'cap_2'}];
+    const capabilities = new Capabilities(['cap_1', 'cap_2']);
     const response = {data: capabilities};
     const gmp = {
       user: {
-        currentCapabilities: testing.fn(() => Promise.resolve(response)),
+        currentCapabilities: testing.fn().mockResolvedValue(response),
       },
     };
     const {render} = rendererWith({gmp});
