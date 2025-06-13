9ed094820ee111539484fad9e73814fcc0428ebf
Björn Ricks <bjoern.ricks@greenbone.net>
2025-06-13 10:27:14 +0200

    change: Use TypeScript for useFilterSortBy hook and tests
    Also extend the tests.

--- src/web/hooks/useFilterSortBy.ts
+++ src/web/hooks/useFilterSortBy.ts
@@ -4,29 +4,39 @@
  */
 
 import {useCallback} from 'react';
+import Filter from 'gmp/models/filter';
 import {isDefined} from 'gmp/utils/identity';
 
 const ASC = 'asc';
 const DESC = 'desc';
 
+type ChangeFilterFunc = (filter: Filter) => void;
+type SortChangeFunc = (field: string) => void;
+type SortDirection = typeof ASC | typeof DESC;
+
 /**
  * Hook to get the sort by field and direction from a filter
  *
- * @param {Filter} filter The current filter
- * @param {Function} changeFilter Function to call when the filter changes
+ * @param filter The current filter
+ * @param changeFilter Function to call when the filter changes
  * @returns Array of the sort by field, sort direction and a function to change the sort by field
  */
-const useFilterSortBy = (filter, changeFilter) => {
+const useFilterSortBy = (
+  filter: Filter,
+  changeFilter: ChangeFilterFunc,
+): [string | undefined, SortDirection, SortChangeFunc] => {
   const reverseField = isDefined(filter)
-    ? filter.get('sort-reverse')
+    ? (filter.get('sort-reverse') as string)
     : undefined;
   const reverse = isDefined(reverseField);
   const sortBy =
-    reverse || !isDefined(filter) ? reverseField : filter.get('sort');
+    reverse || !isDefined(filter)
+      ? reverseField
+      : (filter.get('sort') as string);
   const sortDir = reverse ? DESC : ASC;
 
   const sortChange = useCallback(
-    field => {
+    (field: string) => {
       let sort = 'sort';
       const newFilter = filter.copy().first();
       const sortField = filter.getSortBy();
