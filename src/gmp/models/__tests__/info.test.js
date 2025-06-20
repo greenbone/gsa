755baa7679179c9185278f805f6fc372375f066d
Björn Ricks <bjoern.ricks@greenbone.net>
2025-06-20 12:33:14 +0200

    Remove now obsolete Info model
    The Info model was a base class for all secinfo models like CVEs, CPEs,
    etc. These models got refactored to being directly based on the Model
    class.

--- src/gmp/models/__tests__/info.test.js
+++
@@ -1,9 +0,0 @@
-/* SPDX-FileCopyrightText: 2024 Greenbone AG
- *
- * SPDX-License-Identifier: AGPL-3.0-or-later
- */
-
-import Info from 'gmp/models/info';
-import {testModel} from 'gmp/models/testing';
-
-testModel(Info, 'info');
