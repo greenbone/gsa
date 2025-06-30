15521fe90e9b5ead48569e672c3094b072fd3292
Björn Ricks <bjoern.ricks@greenbone.net>
2025-06-30 10:42:44 +0200

    change: Use TypeScript for Tag model

--- src/gmp/models/__tests__/tag.test.ts
+++ src/gmp/models/__tests__/tag.test.ts
@@ -10,37 +10,39 @@ import {testModel} from 'gmp/models/testing';
 testModel(Tag, 'tag');
 
 describe('Tag model tests', () => {
+  test('should use defaults', () => {
+    const tag = new Tag();
+    expect(tag.resourceType).toBeUndefined();
+    expect(tag.resourceCount).toEqual(0);
+    expect(tag.value).toBeUndefined();
+  });
+
+  test('should parse empty element', () => {
+    const tag = Tag.fromElement();
+    expect(tag.resourceType).toBeUndefined();
+    expect(tag.resourceCount).toEqual(0);
+    expect(tag.value).toBeUndefined();
+  });
+
   test('should parse resources', () => {
-    const elem = {
+    const tag = Tag.fromElement({
       resources: {
         type: 'foo',
         count: {
-          total: '42',
+          total: 42,
         },
       },
-    };
-    const tag = Tag.fromElement(elem);
+    });
 
     expect(tag.resourceType).toEqual('foo');
     expect(tag.resourceCount).toEqual(42);
   });
 
-  test('should return count of 0 and no type if no resources are given', () => {
-    const tag = Tag.fromElement({});
-
-    expect(tag.resourceType).toBeUndefined();
-    expect(tag.resourceCount).toEqual(0);
-  });
-
-  test('should parse value to undefined, if no value is given', () => {
-    const tag = Tag.fromElement({});
-
-    expect(tag.value).toBeUndefined();
-  });
-
   test('should parse value', () => {
     const tag = Tag.fromElement({value: 'foo'});
-
     expect(tag.value).toEqual('foo');
+
+    const tag2 = Tag.fromElement({value: 42});
+    expect(tag2.value).toEqual('42');
   });
 });
