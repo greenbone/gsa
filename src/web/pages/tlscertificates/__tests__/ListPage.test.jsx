cb327f042d8cc6ed5bc1103977831fa5a33eeb01
Björn Ricks <bjoern.ricks@greenbone.net>
2025-07-02 11:36:05 +0200

    test: Use correct Model class for the Tls Certificate List Page
    Accidentally a Task model was used instead of TlsCertificate model. This
    works until the Task parsing is changed.

--- src/web/pages/tlscertificates/__tests__/ListPage.test.jsx
+++ src/web/pages/tlscertificates/__tests__/ListPage.test.jsx
@@ -7,7 +7,7 @@ import {describe, test, expect, testing} from '@gsa/testing';
 import {screen, within, rendererWith, wait} from 'web/testing';
 import CollectionCounts from 'gmp/collection/CollectionCounts';
 import Filter from 'gmp/models/filter';
-import TlsCertificate from 'gmp/models/task';
+import TlsCertificate from 'gmp/models/tlscertificate';
 import {currentSettingsDefaultResponse} from 'web/pages/__mocks__/CurrentSettings';
 import TlsCertificatePage from 'web/pages/tlscertificates/ListPage';
 import {entitiesLoadingActions} from 'web/store/entities/tasks';
