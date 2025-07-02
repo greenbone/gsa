6e1510d83745deae0ecc59c3fba74c0eb54e3204
Björn Ricks <bjoern.ricks@greenbone.net>
2025-07-02 08:41:28 +0200

    change: Update Result Row and Details Page component tests
    The Results model got adapted to return undefined instead of empty
    strings of some value is not set.

--- src/web/pages/results/Row.jsx
+++ src/web/pages/results/Row.jsx
@@ -135,7 +135,7 @@ const Row = ({
       </TableData>
       <TableData>
         <IconDivider>
-          {host.hostname.length > 0 && (
+          {isDefined(host.hostname) && (
             <span title={host.hostname}>{shorten(host.hostname, 40)}</span>
           )}
           {isDefined(deltaHostname) &&
