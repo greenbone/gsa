6776f3dcde5791402cb493b4d4a4f91911115e77
Björn Ricks <bjoern.ricks@greenbone.net>
2025-07-08 16:02:13 +0200

    Add: Allow to set a data-testid on a DetailsBlock component
    Allow for easier testing of DetailsBlock components in future.

--- src/web/entity/Block.jsx
+++ src/web/entity/Block.jsx
@@ -7,8 +7,14 @@ import React from 'react';
 import Layout from 'web/components/layout/Layout';
 import PropTypes from 'web/utils/PropTypes';
 
-const DetailsBlock = ({id, children, className, title}) => (
-  <Layout className={className} flex="column" id={id}>
+const DetailsBlock = ({
+  id,
+  children,
+  className,
+  title,
+  'data-testid': dataTestId,
+}) => (
+  <Layout className={className} data-testid={dataTestId} flex="column" id={id}>
     <h2>{title}</h2>
     <div>{children}</div>
   </Layout>
@@ -16,6 +22,7 @@ const DetailsBlock = ({id, children, className, title}) => (
 
 DetailsBlock.propTypes = {
   className: PropTypes.string,
+  'data-testid': PropTypes.string,
   id: PropTypes.string,
   title: PropTypes.string.isRequired,
 };
