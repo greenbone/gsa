fa52d676c083dadb416c6d886797eebf07a19aca
Björn Ricks <bjoern.ricks@greenbone.net>
2025-07-09 10:45:08 +0200

    change: Use TypeScript for Audit report models

--- src/gmp/models/compliance.ts
+++ src/gmp/models/compliance.ts
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+import {_l} from 'gmp/locale/lang';
+
 export const COMPLIANCE = {
   YES: 'yes',
   NO: 'no',
@@ -11,3 +13,13 @@ export const COMPLIANCE = {
 } as const;
 
 export type ComplianceType = (typeof COMPLIANCE)[keyof typeof COMPLIANCE];
+
+const COMPLIANCE_TRANSLATIONS = {
+  yes: _l('Yes'),
+  no: _l('No'),
+  incomplete: _l('Incomplete'),
+  undefined: _l('Undefined'),
+} as const;
+
+export const getTranslatableReportCompliance = (compliance: ComplianceType) =>
+  `${COMPLIANCE_TRANSLATIONS[compliance]}`;
