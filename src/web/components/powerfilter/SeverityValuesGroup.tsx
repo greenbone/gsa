8b2251d8b5be68558f8bb2ac6d5a1f30125afde2
Björn Ricks <bjoern.ricks@greenbone.net>
2025-07-14 13:59:37 +0200

    change: Use TypeScript for SeverityValuesGroup Powerfilter component

--- src/web/components/powerfilter/SeverityValuesGroup.tsx
+++ src/web/components/powerfilter/SeverityValuesGroup.tsx
@@ -3,18 +3,30 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import React, {useState} from 'react';
+import {useState} from 'react';
+import Filter from 'gmp/models/filter';
 import {parseSeverity} from 'gmp/parser';
 import {isDefined} from 'gmp/utils/identity';
 import FormGroup from 'web/components/form/FormGroup';
 import NumberField from 'web/components/form/NumberField';
 import RelationSelector from 'web/components/powerfilter/RelationSelector';
-import PropTypes from 'web/utils/PropTypes';
 import {UNSET_VALUE} from 'web/utils/Render';
 
-const SeverityValuesGroup = ({filter, name, title, onChange}) => {
+interface SeverityValuesGroupProps {
+  filter: Filter;
+  name?: string;
+  title?: string;
+  onChange?: (value: number, name?: string, relation?: string) => void;
+}
+
+const SeverityValuesGroup = ({
+  filter,
+  name,
+  title,
+  onChange,
+}: SeverityValuesGroupProps) => {
   const term = filter.getTerm(name);
-  const severity = isDefined(term) ? parseSeverity(term.value) : 0;
+  const severity = parseSeverity(term?.value) ?? 0;
 
   const [rel, setRel] = useState(isDefined(term) ? term.relation : UNSET_VALUE);
   const keyword = name;
@@ -25,7 +37,7 @@ const SeverityValuesGroup = ({filter, name, title, onChange}) => {
         relation={rel}
         onChange={newRel => {
           setRel(newRel);
-          onChange(severity, keyword, newRel);
+          isDefined(onChange) && onChange(severity, keyword, newRel);
         }}
       />
       <NumberField
@@ -36,18 +48,11 @@ const SeverityValuesGroup = ({filter, name, title, onChange}) => {
         type="int"
         value={severity}
         onChange={(value = severity, name = keyword) =>
-          onChange(value, name, rel)
+          isDefined(onChange) && onChange(value, name, rel)
         }
       />
     </FormGroup>
   );
-};
-
-SeverityValuesGroup.propTypes = {
-  filter: PropTypes.filter,
-  name: PropTypes.string,
-  title: PropTypes.string,
-  onChange: PropTypes.func,
 };
 
 export default SeverityValuesGroup;
--- src/web/components/powerfilter/__tests__/SeverityValuesGroup.test.tsx
+++ src/web/components/powerfilter/__tests__/SeverityValuesGroup.test.tsx
@@ -4,40 +4,21 @@
  */
 
 import {describe, test, expect, testing} from '@gsa/testing';
-import {openSelectElement, screen, render, fireEvent} from 'web/testing';
+import {
+  openSelectElement,
+  screen,
+  render,
+  fireEvent,
+  changeInputValue,
+} from 'web/testing';
 import Filter from 'gmp/models/filter';
 import SeverityValuesGroup from 'web/components/powerfilter/SeverityValuesGroup';
-
-const getTitle = () => {
-  return document.body.querySelector('.mantine-Text-root');
-};
-
-const getSeverityInput = () => {
-  return document.body.querySelector('.mantine-NumberInput-input');
-};
 
 describe('Severity Values Group Tests', () => {
   test('should render', () => {
     const filter = Filter.fromString('severity>3');
     const name = 'severity';
     const onChange = testing.fn();
-
-    const {element} = render(
-      <SeverityValuesGroup
-        filter={filter}
-        name={name}
-        title="foo"
-        onChange={onChange}
-      />,
-    );
-
-    expect(element).toBeVisible();
-  });
-
-  test('arguments are processed correctly', () => {
-    const onChange = testing.fn();
-    const filter = Filter.fromString('severity=3');
-    const name = 'severity';
 
     render(
       <SeverityValuesGroup
@@ -48,11 +29,8 @@ describe('Severity Values Group Tests', () => {
       />,
     );
 
-    const numField = getSeverityInput();
-
-    expect(getTitle()).toHaveTextContent('foo');
-    expect(numField).toHaveAttribute('name', 'severity');
-    expect(numField).toHaveAttribute('value', '3');
+    expect(screen.getByTestId('relation-selector')).toBeVisible();
+    expect(screen.getByTestId('severity-value-filter')).toBeVisible();
   });
 
   test('should initialize value with 0 in case no filter value is given', () => {
@@ -69,30 +47,24 @@ describe('Severity Values Group Tests', () => {
       />,
     );
 
-    const numField = getSeverityInput();
-
-    expect(numField).toHaveAttribute('name', 'severity');
-    expect(numField).toHaveAttribute('value', '0');
+    const numField = screen.getByName('severity');
+    expect(numField).toHaveValue('0');
   });
 
   test('should change value', () => {
     const onChange = testing.fn();
     const filter = Filter.fromString('severity=3');
-    const name = 'severity';
 
     render(
       <SeverityValuesGroup
         filter={filter}
-        name={name}
+        name="severity"
         title="foo"
         onChange={onChange}
       />,
     );
 
-    const numField = getSeverityInput();
-
-    fireEvent.change(numField, {target: {value: '9'}});
-
+    changeInputValue(screen.getByName('severity'), '9');
     expect(onChange).toHaveBeenCalledWith(9, 'severity', '=');
   });
 
@@ -110,11 +82,21 @@ describe('Severity Values Group Tests', () => {
       />,
     );
 
-    await openSelectElement();
+    const select = screen.getByName(name) as HTMLSelectElement;
+    await openSelectElement(select);
 
     const domItems = screen.getSelectItemElements();
     fireEvent.click(domItems[3]);
     expect(onChange).toBeCalled();
     expect(onChange).toBeCalledWith(3, 'severity', '<');
+  });
+
+  test('should render title', () => {
+    const title = 'Severity';
+    const filter = Filter.fromString('severity=3');
+    render(
+      <SeverityValuesGroup filter={filter} name="severity" title={title} />,
+    );
+    expect(screen.getByText(title)).toBeVisible();
   });
 });
