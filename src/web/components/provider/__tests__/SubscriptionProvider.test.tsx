de32a3cc545d778f99f160a698e0c66f2cf78f58
Björn Ricks <bjoern.ricks@greenbone.net>
2025-07-23 12:03:02 +0200

    change: Use TypeScript for the SubsciptionProvider

---
+++ src/web/components/provider/__tests__/SubscriptionProvider.test.tsx
@@ -0,0 +1,47 @@
+/* SPDX-FileCopyrightText: 2025 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import React from 'react';
+import {describe, test, expect, testing} from '@gsa/testing';
+import {fireEvent, render, screen} from 'web/testing';
+import SubscriptionProvider, {
+  Subscriber,
+  SubscriptionContext,
+} from 'web/components/provider/SubscriptionProvider';
+
+const TestComponent = ({subscriber}: {subscriber: Subscriber}) => {
+  const subscribe = React.useContext(SubscriptionContext);
+
+  React.useEffect(() => {
+    if (subscribe) {
+      const unsubscribe = subscribe('testEvent', subscriber);
+      return () => unsubscribe();
+    }
+  }, [subscribe, subscriber]);
+
+  return null;
+};
+
+describe('SubscriptionProvider', () => {
+  test('should subscribe and notify subscribers correctly', () => {
+    const mockSubscriber = testing.fn();
+    render(
+      <SubscriptionProvider>
+        {({notify}) => (
+          <>
+            <TestComponent subscriber={mockSubscriber} />
+            <button onClick={() => notify('testEvent')('testArg')}>
+              Notify
+            </button>
+          </>
+        )}
+      </SubscriptionProvider>,
+    );
+
+    const notifyButton = screen.getByText('Notify');
+    fireEvent.click(notifyButton);
+    expect(mockSubscriber).toHaveBeenCalledWith('testArg');
+  });
+});
