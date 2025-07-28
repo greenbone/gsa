050f31b61e6fe99d427faae81b9085c977b49fc6
Björn Ricks <bjoern.ricks@greenbone.net>
2025-07-28 11:41:21 +0200

    change: Split useEntitySave into save and create
    It's better to distinguish both create and save upfront instead of
    relying of the id argument. Data for creating and saving an entity might
    be completely different too.

---
+++ src/web/entity/hooks/__tests__/useEntityCreate.ts
@@ -0,0 +1,62 @@
+/* SPDX-FileCopyrightText: 2025 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import {describe, test, expect, testing} from '@gsa/testing';
+import {rendererWith} from 'web/testing';
+import useEntityCreate from 'web/entity/hooks/useEntityCreate';
+
+describe('useEntityCreate', () => {
+  test('should allow to create an entity', async () => {
+    const entity = {name: 'foo'};
+    const createEntity = testing.fn().mockResolvedValue(entity);
+    const gmp = {
+      foo: {create: createEntity},
+    };
+    const onCreated = testing.fn();
+    const onCreateError = testing.fn();
+    const onInteraction = testing.fn();
+    const {renderHook} = rendererWith({gmp, store: true});
+
+    const {result} = renderHook(() =>
+      useEntityCreate('foo', {
+        onCreated,
+        onCreateError,
+        onInteraction,
+      }),
+    );
+    expect(result.current).toBeDefined();
+    await result.current(entity);
+    expect(createEntity).toHaveBeenCalledWith(entity);
+    expect(onCreated).toHaveBeenCalledWith(entity);
+    expect(onCreateError).not.toHaveBeenCalled();
+    expect(onInteraction).toHaveBeenCalledOnce();
+  });
+
+  test('should call onCreateError when creating an entity fails', async () => {
+    const createEntity = testing.fn().mockRejectedValue(new Error('error'));
+    const entity = {name: 'foo'};
+    const gmp = {
+      foo: {create: createEntity},
+    };
+    const onCreated = testing.fn();
+    const onCreateError = testing.fn();
+    const onInteraction = testing.fn();
+    const {renderHook} = rendererWith({gmp, store: true});
+
+    const {result} = renderHook(() =>
+      useEntityCreate('foo', {
+        onCreated,
+        onCreateError,
+        onInteraction,
+      }),
+    );
+    expect(result.current).toBeDefined();
+    await result.current(entity);
+    expect(createEntity).toHaveBeenCalledWith(entity);
+    expect(onCreated).not.toHaveBeenCalled();
+    expect(onCreateError).toHaveBeenCalledOnce();
+    expect(onInteraction).toHaveBeenCalledOnce();
+  });
+});
--- src/web/entity/hooks/useEntitySave.ts
+++ src/web/entity/hooks/useEntitySave.ts
@@ -7,62 +7,44 @@ import {isDefined} from 'gmp/utils/identity';
 import actionFunction from 'web/entity/hooks/actionFunction';
 import useGmp from 'web/hooks/useGmp';
 
-interface EntitySave {
-  id?: string;
-}
-
-interface EntitySaved {
-  id: string;
-}
-
-interface SaveCallbacks {
-  onSaveError?: (error: unknown) => void;
-  onSaved?: (entity: EntitySaved) => void;
-  onCreated?: (entity: EntitySaved) => void;
-  onCreateError?: (error: unknown) => void;
+interface EntitySaveCallbacks<TSaveResponse = unknown, TSaveError = unknown> {
+  onSaveError?: (error: TSaveError) => void;
+  onSaved?: (response: TSaveResponse) => void;
   onInteraction?: () => void;
 }
 
 /**
  * Custom hook to handle saving or creating an entity.
  *
- * @param {string} name - The name of the entity.
- * @param {SaveCallbacks} [callbacks={}] - Optional callbacks for various save events.
- * @param {Function} [callbacks.onSaveError] - Callback function to be called on save error.
- * @param {Function} [callbacks.onSaved] - Callback function to be called when the entity is saved.
- * @param {Function} [callbacks.onCreated] - Callback function to be called when the entity is created.
- * @param {Function} [callbacks.onCreateError] - Callback function to be called on create error.
- * @param {Function} [callbacks.onInteraction] - Callback function to be called on interaction.
- * @returns {Function} - A function to handle saving the entity. The function takes an entity as an argument.
- *                       If the entity has an id, it will be saved, otherwise it will be created.
  */
-const useEntitySave = (
+const useEntitySave = <
+  TSaveData = {},
+  TSaveResponse = unknown,
+  TSaveError = unknown,
+>(
   name: string,
   {
     onSaveError,
     onSaved,
-    onCreated,
-    onCreateError,
     onInteraction,
-  }: SaveCallbacks = {},
+  }: EntitySaveCallbacks<TSaveResponse, TSaveError> = {},
 ) => {
   const gmp = useGmp();
-  const cmd = gmp[name];
-
+  const cmd = gmp[name] as {
+    save: (data: TSaveData) => Promise<TSaveResponse>;
+  };
   const handleInteraction = () => {
     if (isDefined(onInteraction)) {
       onInteraction();
     }
   };
 
-  const handleEntitySave = async (data: EntitySave) => {
+  const handleEntitySave = async (data: TSaveData) => {
     handleInteraction();
-
-    if (isDefined(data.id)) {
-      return actionFunction(cmd.save(data), onSaved, onSaveError);
-    }
-
-    return actionFunction(cmd.create(data), onCreated, onCreateError);
+    return actionFunction(cmd.save(data as TSaveData), {
+      onSuccess: onSaved,
+      onError: onSaveError,
+    });
   };
   return handleEntitySave;
 };
