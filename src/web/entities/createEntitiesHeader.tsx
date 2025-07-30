e70668345602f2f2bc0fa786b9bb67a89e4406f9
Björn Ricks <bjoern.ricks@greenbone.net>
2025-07-30 13:47:10 +0200

    change: Use TypeScript for createEntitiesHeader and withEntitiesHeader
    Split the header module into two distinct modules for each function/HOC
    and convert them to typescript.

---
+++ src/web/entities/createEntitiesHeader.tsx
@@ -0,0 +1,94 @@
+/* SPDX-FileCopyrightText: 2025 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import React from 'react';
+import TableHead from 'web/components/table/TableHead';
+import TableHeader from 'web/components/table/TableHeader';
+import TableRow from 'web/components/table/TableRow';
+import withEntitiesHeader, {
+  ActionsColumn,
+  WithEntitiesHeaderComponentProps,
+} from 'web/entities/withEntitiesHeader';
+import {SortDirectionType} from 'web/utils/SortDirection';
+
+interface ToString {
+  toString: () => string;
+}
+
+export interface CreateEntitiesHeaderProps
+  extends WithEntitiesHeaderComponentProps {
+  currentSortBy?: string;
+  currentSortDir?: SortDirectionType;
+  sort?: boolean;
+  onSortChange?: (sortBy: string) => void;
+}
+
+interface CreateEntitiesHeaderColumn {
+  name: string;
+  displayName: ToString;
+  width: string;
+  align?: 'left' | 'center' | 'right';
+}
+
+/**
+ * A higher order component to create table headers from a column description
+ * array.
+ *
+ * @param columns - An array of column description objects in the form of:
+ *                                  [{
+ *                                      name: 'foo',
+ *                                      displayName: _l('Foo'),
+ *                                      width: '20%',
+ *                                      align: ['center', 'center'],
+ *                                   }, {
+ *                                     ...
+ *                                   }, ... ]
+ * @param actionsColumn - React element, undefined, or boolean value.
+ * @param options - Default properties for the Component.
+ *
+ * @return A new EntitiesHeader component.
+ */
+export function createEntitiesHeader<TOptions = {}>(
+  columns: CreateEntitiesHeaderColumn[],
+  actionsColumn: ActionsColumn,
+  options: TOptions = {} as TOptions,
+) {
+  const Header = ({
+    actionsColumn,
+    currentSortBy,
+    currentSortDir,
+    sort = true,
+    onSortChange,
+  }: CreateEntitiesHeaderProps) => (
+    <TableHeader>
+      <TableRow>
+        {columns.map(column => {
+          const {name, displayName, width, align} = column;
+          return (
+            <TableHead
+              key={name}
+              align={align}
+              currentSortBy={currentSortBy}
+              currentSortDir={currentSortDir}
+              sort={sort}
+              sortBy={name}
+              title={String(displayName)}
+              width={width}
+              onSortChange={onSortChange}
+            />
+          );
+        })}
+        {actionsColumn}
+      </TableRow>
+    </TableHeader>
+  );
+
+  return withEntitiesHeader<CreateEntitiesHeaderProps, TOptions>(
+    actionsColumn,
+    options,
+  )(Header);
+}
+
+export default createEntitiesHeader;
---
+++ src/web/entities/withEntitiesHeader.tsx
@@ -0,0 +1,83 @@
+/* SPDX-FileCopyrightText: 2024 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import React from 'react';
+import {_, _l} from 'gmp/locale/lang';
+import TableHead from 'web/components/table/TableHead';
+import {updateDisplayName} from 'web/utils/displayName';
+import SelectionType, {SelectionTypeType} from 'web/utils/SelectionType';
+
+const defaultActions = (
+  <TableHead align="center" title={_l('Actions')} width="8%" />
+);
+
+export type ActionsColumn = React.ReactElement | null;
+
+/**
+ * Props for the component passed to withEntitiesHeader gets provided.
+ */
+export interface WithEntitiesHeaderComponentProps {
+  selectionType?: SelectionTypeType;
+  actionsColumn?: ActionsColumn;
+}
+
+/**
+ * A higher order component to create table headers which support entity
+ * selection.
+ *
+ * If a React element instance is passed via actionsColumn, the element will be
+ * forwarded as actionsColumn to the Component.
+ *
+ * If actionsColumn is undefined, a default table head column will be passed as
+ * actionsColumn to the Component.
+ *
+ * If actionsColumn is true, a default table head column will be passed as actionsColumn
+ * to the Component if the current selectionType (passed via props) is SELECTION_USER.
+ *
+ * If actionsColumn is false, no actions (a null value in React) will be passed to
+ * the Component.
+ *
+ * @param actionsColumn - React element, undefined, or boolean value.
+ * @param options - Default properties for the Component.
+ * @param Component - React component rendered as header.
+ *
+ * @return A new EntitiesHeader component.
+ */
+function withEntitiesHeader<
+  TProps extends WithEntitiesHeaderComponentProps,
+  TOptions = {},
+>(
+  actionsColumn: React.ReactElement | null | true = defaultActions,
+  options: TOptions = {} as TOptions,
+) {
+  return (Component: React.ComponentType<TProps>) => {
+    if (!actionsColumn) {
+      actionsColumn = null;
+    }
+
+    const HeaderWrapper = ({selectionType, ...props}: TProps) => {
+      let column: ActionsColumn;
+      if (actionsColumn && selectionType === SelectionType.SELECTION_USER) {
+        column = <TableHead width="6em">{_('Actions')}</TableHead>;
+      } else if (actionsColumn === true) {
+        column = null;
+      } else {
+        column = actionsColumn;
+      }
+      return (
+        <Component
+          {...(options as TOptions)}
+          {...(props as TProps)}
+          actionsColumn={column}
+          selectionType={selectionType}
+        />
+      );
+    };
+
+    return updateDisplayName(HeaderWrapper, Component, 'withEntitiesHeader');
+  };
+}
+
+export default withEntitiesHeader;
