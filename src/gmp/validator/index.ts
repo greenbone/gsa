9b39ddd82b22a75613d8fe4f00bc491656879764
daniele-mng <daniele.mangiagli@greenbone.net>
2025-08-04 15:40:54 +0200

    deps: add zod

---
+++ src/gmp/validator/index.ts
@@ -0,0 +1,7 @@
+/* SPDX-FileCopyrightText: 2025 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+// eslint-disable-next-line no-restricted-imports
+export {z} from './validator';
---
+++ src/gmp/validator/validator.ts
@@ -0,0 +1,8 @@
+/* SPDX-FileCopyrightText: 2025 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import * as zod from 'zod';
+
+export const z = zod;
