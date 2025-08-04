f51c08b3f36fbe6947a9b8a8baea43e0b36e452b
Björn Ricks <bjoern.ricks@greenbone.net>
2025-08-04 08:56:21 +0200

    change: Use TypeScript for RowDetailsToggle component

---
+++ src/web/entities/RowDetailsToggle.tsx
@@ -0,0 +1,54 @@
+/* SPDX-FileCopyrightText: 2024 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import {HTMLAttributes, MouseEvent} from 'react';
+import styled from 'styled-components';
+import useClickHandler from 'web/components/form/useClickHandler';
+import Theme from 'web/utils/Theme';
+
+interface RowDetailsToggleProps<TValue>
+  extends Omit<HTMLAttributes<HTMLSpanElement>, 'onClick'> {
+  'data-testid'?: string;
+  name?: string;
+  value?: TValue;
+  onClick?: (value: TValue, name?: string) => void;
+}
+
+const StyledSpan = styled.span`
+  cursor: pointer;
+  text-decoration: none;
+  color: ${Theme.blue};
+  :hover {
+    text-decoration: underline;
+    color: ${Theme.blue};
+  }
+  @media print {
+    color: ${Theme.black};
+  }
+`;
+
+const RowDetailsToggle = <TValue,>({
+  'data-testid': dataTestId = 'row-details-toggle',
+  name,
+  value,
+  onClick,
+  ...props
+}: RowDetailsToggleProps<TValue>) => {
+  const handleClick = useClickHandler<
+    Pick<RowDetailsToggleProps<TValue>, 'name' | 'value'>,
+    TValue,
+    MouseEvent<HTMLSpanElement>
+  >({
+    onClick,
+    nameFunc: (event, props) => props.name,
+    valueFunc: (event, props) => props.value as TValue,
+    props: {name, value},
+  });
+  return (
+    <StyledSpan {...props} data-testid={dataTestId} onClick={handleClick} />
+  );
+};
+
+export default RowDetailsToggle;
