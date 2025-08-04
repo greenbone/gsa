a9c1b30a407ebff10267716d4c668d7171e5597f
Björn Ricks <bjoern.ricks@greenbone.net>
2025-08-04 10:30:15 +0200

    Update TaskRow onToggleDetailsClick handler type

--- src/web/pages/tasks/TaskRow.tsx
+++ src/web/pages/tasks/TaskRow.tsx
@@ -36,9 +36,9 @@ interface TaskReportTotalProps {
   links?: boolean;
 }
 
-interface TaskRowProps extends TaskActionsProps {
+export interface TaskRowProps extends TaskActionsProps {
   actionsComponent?: React.ComponentType<TaskActionsProps>;
-  onToggleDetailsClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  onToggleDetailsClick: (value: unknown, name: string) => void;
 }
 
 const TaskReport = ({report, links}: TaskReportProps) => {
