c36dddc0f9fa4317825a814a0f478d4691fed02e
daniele-mng <daniele.mangiagli@greenbone.net>
2025-08-18 17:37:20 +0200

    change: zod for baseModel and entityModel

--- src/gmp/models/entitymodel.ts
+++ src/gmp/models/entitymodel.ts
@@ -3,14 +3,16 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+import {Dayjs} from 'dayjs';
+import {z} from 'zod';
 import Capabilities from 'gmp/capabilities/capabilities';
 import BaseModel, {
-  BaseModelElement,
-  BaseModelProperties,
+  BaseModelPropertiesSchema,
   parseBaseModelProperties,
+  BaseModelElementSchema,
 } from 'gmp/models/basemodel';
-import {Date as GmpDate} from 'gmp/models/date';
-import UserTag, {UserTagElement} from 'gmp/models/usertag';
+import dayjs, {Date as GmpDate} from 'gmp/models/date';
+import UserTag, {UserTagElementSchema} from 'gmp/models/usertag';
 import {
   NO_VALUE,
   parseBoolean,
@@ -25,49 +27,67 @@ import {map} from 'gmp/utils/array';
 import {isDefined} from 'gmp/utils/identity';
 import {isEmpty} from 'gmp/utils/string';
 
-export interface Owner {
-  name: string;
-}
+export const ownerSchema = z.object({
+  name: z.string(),
+});
 
-export interface EntityModelPermissionElement {
-  name: string;
-}
+export type Owner = z.infer<typeof ownerSchema>;
 
-export interface EntityModelElement extends BaseModelElement {
-  name?: string;
-  end_time?: string;
-  timestamp?: string;
-  permissions?: {
-    permission: EntityModelPermissionElement | EntityModelPermissionElement[];
-  };
-  user_tags?: {
-    tag: UserTagElement | UserTagElement[];
-  };
-  writable?: YesNo;
-  orphan?: YesNo;
-  active?: YesNo;
-  trash?: YesNo;
-  in_use?: YesNo;
-  owner?: Owner;
-  summary?: string;
-  comment?: string;
-}
+export const EntityModelPermissionElementSchema = z.object({
+  name: z.string(),
+});
 
-export interface EntityModelProperties extends BaseModelProperties {
-  active?: YesNo;
-  comment?: string;
-  endTime?: GmpDate;
-  inUse?: boolean;
-  name?: string;
-  orphan?: YesNo;
-  owner?: Owner;
-  summary?: string;
-  timestamp?: GmpDate;
-  trash?: YesNo;
-  userCapabilities?: Capabilities;
-  userTags?: UserTag[];
-  writable?: YesNo;
-}
+export type EntityModelPermissionElement = z.infer<
+  typeof EntityModelPermissionElementSchema
+>;
+
+export const EntityModelElementSchema = BaseModelElementSchema.extend({
+  name: z.string().optional(),
+  end_time: z.string().optional(),
+  timestamp: z.string().optional(),
+  permissions: z
+    .object({
+      permission: z.union([
+        EntityModelPermissionElementSchema,
+        z.array(EntityModelPermissionElementSchema),
+      ]),
+    })
+    .optional(),
+  user_tags: z
+    .object({
+      tag: z.union([UserTagElementSchema, z.array(UserTagElementSchema)]),
+    })
+    .optional(),
+  writable: z.custom<YesNo>().optional(),
+  orphan: z.custom<YesNo>().optional(),
+  active: z.custom<YesNo>().optional(),
+  trash: z.custom<YesNo>().optional(),
+  in_use: z.custom<YesNo>().optional(),
+  owner: ownerSchema.optional(),
+  summary: z.string().optional(),
+  comment: z.string().optional(),
+});
+
+export type EntityModelElement = z.infer<typeof EntityModelElementSchema>;
+
+export const EntityModelPropertiesSchema = BaseModelPropertiesSchema.extend({
+  active: z.union([z.literal(YES_VALUE), z.literal(NO_VALUE)]).optional(),
+  creationTime: z.instanceof(dayjs as unknown as typeof Dayjs).optional(),
+  endTime: z.instanceof(dayjs as unknown as typeof Dayjs).optional(),
+  comment: z.string().optional(),
+  inUse: z.boolean().optional(),
+  name: z.string().optional(),
+  orphan: z.union([z.literal(YES_VALUE), z.literal(NO_VALUE)]).optional(),
+  owner: ownerSchema.optional(),
+  summary: z.string().optional(),
+  timestamp: z.instanceof(dayjs as unknown as typeof Dayjs).optional(),
+  trash: z.union([z.literal(YES_VALUE), z.literal(NO_VALUE)]).optional(),
+  userCapabilities: z.instanceof(Capabilities).optional(),
+  userTags: z.array(z.instanceof(UserTag)).optional(),
+  writable: z.union([z.literal(YES_VALUE), z.literal(NO_VALUE)]).optional(),
+});
+
+export type EntityModelProperties = z.infer<typeof EntityModelPropertiesSchema>;
 
 /**
  * A model representing an entity
@@ -154,10 +174,20 @@ class EntityModel extends BaseModel {
   }
 
   static fromElement(
-    element: EntityModelElement = {},
+    element: Partial<
+      EntityModelElement & {
+        _id?: string;
+        creation_time?: string;
+        modification_time?: string;
+        type?: string | number;
+      }
+    > = {},
     entityType?: string,
   ): EntityModel {
-    return new this(parseEntityModelProperties(element), entityType);
+    return new this(
+      parseEntityModelProperties(element as EntityModelElement),
+      entityType,
+    );
   }
 }
 
