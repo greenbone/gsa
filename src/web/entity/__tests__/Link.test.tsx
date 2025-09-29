00db9615921abe6ec0e77398254f4b9d6848dfe5
Björn Ricks <bjoern.ricks@greenbone.net>
2025-09-29 11:56:15 +0200

    Change: Convert EntityLink to TypeScript
    Use TypeScript for EntityLink component and its tests.

--- src/web/entity/__tests__/Link.test.tsx
+++ src/web/entity/__tests__/Link.test.tsx
@@ -5,37 +5,37 @@
 
 import {describe, test, expect} from '@gsa/testing';
 import {screen, rendererWith} from 'web/testing';
-import Capabilities from 'gmp/capabilities/capabilities';
 import Model from 'gmp/models/model';
 import {YES_VALUE} from 'gmp/parser';
 import EntityLink from 'web/entity/Link';
 
-const caps = new Capabilities(['everything']);
-const wrongCaps = new Capabilities(['get_config']);
-
 describe('EntityLink component tests', () => {
   test('should render', () => {
-    const entity = Model.fromElement({
-      _id: '123',
-      entityType: 'foo',
-      name: 'bar',
-    });
+    const entity = Model.fromElement(
+      {
+        _id: '123',
+        name: 'bar',
+      },
+      'task',
+    );
 
-    const {render} = rendererWith({capabilities: caps, router: true});
+    const {render} = rendererWith({capabilities: true, router: true});
     render(<EntityLink entity={entity} />);
 
     const a = screen.getByTestId('details-link');
-    expect(a.getAttribute('href')).toEqual('/foo/123');
+    expect(a.getAttribute('href')).toEqual('/task/123');
   });
 
   test('should render with text only', () => {
-    const entity = Model.fromElement({
-      _id: '123',
-      entityType: 'foo',
-      name: 'bar',
-    });
+    const entity = Model.fromElement(
+      {
+        _id: '123',
+        name: 'bar',
+      },
+      'task',
+    );
 
-    const {render} = rendererWith({capabilities: caps, router: true});
+    const {render} = rendererWith({capabilities: true, router: true});
     const {element} = render(<EntityLink entity={entity} textOnly={true} />);
 
     const a = element.querySelector('a');
@@ -45,31 +45,35 @@ describe('EntityLink component tests', () => {
   });
 
   test('should link to trashcan', () => {
-    const entity = Model.fromElement({
-      _id: '123',
-      entityType: 'foo',
-      name: 'bar',
-      trash: YES_VALUE,
-    });
+    const entity = Model.fromElement(
+      {
+        _id: '123',
+        name: 'bar',
+        trash: YES_VALUE,
+      },
+      'task',
+    );
 
-    const {render} = rendererWith({capabilities: caps, router: true});
+    const {render} = rendererWith({capabilities: true, router: true});
     const {element} = render(<EntityLink entity={entity} />);
 
-    const a = element.querySelector('a');
+    const a = element.querySelector('a') as HTMLAnchorElement;
 
-    expect(a.getAttribute('href')).toEqual('/trashcan#foo');
+    expect(a.getAttribute('href')).toEqual('/trashcan#task');
     expect(element).toHaveTextContent('bar (in Trashcan)');
   });
 
   test('should only show text when wrong capabilities are given', () => {
-    const entity = Model.fromElement({
-      _id: '123',
-      entityType: 'foo',
-      name: 'bar',
-    });
+    const entity = Model.fromElement(
+      {
+        _id: '123',
+        name: 'bar',
+      },
+      'task',
+    );
 
     const {render} = rendererWith({
-      capabilities: wrongCaps,
+      capabilities: false,
       router: true,
     });
     const {element} = render(<EntityLink entity={entity} />);
@@ -81,15 +85,18 @@ describe('EntityLink component tests', () => {
   });
 
   test('should indicate orphaned status', () => {
-    const entity = Model.fromElement({
-      _id: '123',
-      entityType: 'foo',
-      name: 'bar',
-      deleted: YES_VALUE,
-    });
+    const entity = Model.fromElement(
+      {
+        _id: '123',
+        name: 'bar',
+        // @ts-expect-error
+        deleted: YES_VALUE,
+      },
+      'task',
+    );
 
     const {render} = rendererWith({
-      capabilities: caps,
+      capabilities: true,
       router: true,
     });
     const {element} = render(<EntityLink entity={entity} />);
