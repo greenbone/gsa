1e154392955851dfc0861ac17ac61caf43814bf6
Björn Ricks <bjoern.ricks@greenbone.net>
2025-10-01 14:59:43 +0200

    Change: Rename and simplify query hook for getting entities
    I've dropped the select part for now which can be re-added later again.

---
+++ src/web/queries/useGetEntities.ts
@@ -0,0 +1,63 @@
+/* SPDX-FileCopyrightText: 2025 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import {useQuery} from '@tanstack/react-query';
+import CollectionCounts from 'gmp/collection/CollectionCounts';
+import EntitiesCommand from 'gmp/commands/entities';
+import Filter from 'gmp/models/filter';
+import Model from 'gmp/models/model';
+import {EntityType, pluralizeType} from 'gmp/utils/entitytype';
+import {isDefined} from 'gmp/utils/identity';
+import useGmp from 'web/hooks/useGmp';
+
+export interface UseGetEntitiesReturn<T> {
+  entities: T[];
+  entitiesCounts: CollectionCounts;
+  filter?: Filter;
+}
+
+interface UseGetEntitiesParams {
+  queryId: string;
+  filter?: Filter;
+  entityType: EntityType;
+  enabled?: boolean;
+  refetchInterval?: number;
+}
+
+export function useGetEntities<
+  TModel extends Model,
+  TEntityCommand extends EntitiesCommand<TModel> = EntitiesCommand<TModel>,
+>({
+  queryId,
+  filter = undefined,
+  entityType,
+  enabled = true,
+  refetchInterval = undefined,
+}: UseGetEntitiesParams) {
+  const gmp = useGmp();
+  const {token} = gmp.settings;
+  return useQuery<UseGetEntitiesReturn<TModel>>({
+    enabled: enabled && !!token,
+    queryKey: [queryId, token, filter?.toFilterString()],
+    queryFn: async () => {
+      const entityKey = pluralizeType(entityType);
+      const gmpCommand = gmp[entityKey] as TEntityCommand | undefined;
+
+      if (!isDefined(gmpCommand)) {
+        throw new Error(`GMP command ${entityKey} not found`);
+      }
+      const response = await gmpCommand.get({filter});
+      const entities = response.data;
+      const entitiesCounts = response.meta.counts;
+      const responseFilter = response.meta.filter;
+      return {
+        entities,
+        entitiesCounts,
+        filter: responseFilter,
+      };
+    },
+    refetchInterval,
+  });
+}
