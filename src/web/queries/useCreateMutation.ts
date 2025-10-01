24f9de9b930dce78a30ef4f0d2c2b6432e387a91
Björn Ricks <bjoern.ricks@greenbone.net>
2025-10-01 15:01:35 +0200

    Change: Improve generic mutation query hooks
    Translations are required for the success messages and it should be
    ensured that only allowed entity types are used.

--- src/web/queries/useCreateMutation.ts
+++ src/web/queries/useCreateMutation.ts
@@ -3,27 +3,37 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+import {EntityType, typeName} from 'gmp/utils/entitytype';
+import useTranslation from 'web/hooks/useTranslation';
 import {useGmpMutation} from 'web/queries/useGmpMutation';
 
-interface UseCreateMutationParams<TOutput> {
-  entityKey: string;
+interface UseCreateMutationParams<TOutput, TError> {
+  entityType: EntityType;
+  invalidateQueryIds?: string[];
   method?: string;
   onSuccess?: (data: TOutput) => void;
-  onError?: (error: unknown) => void;
+  onError?: (error: TError) => void;
 }
 
-export function useCreateMutation<TInput = unknown, TOutput = unknown>({
-  entityKey,
+export function useCreateMutation<
+  TInput = unknown,
+  TOutput = unknown,
+  TError = Error,
+>({
+  entityType,
+  invalidateQueryIds,
   method = 'create',
   onSuccess,
   onError,
-}: UseCreateMutationParams<TOutput>) {
-  const entityLabel = entityKey.charAt(0).toUpperCase() + entityKey.slice(1);
-
-  return useGmpMutation<TInput, TOutput>({
-    entityKey,
+}: UseCreateMutationParams<TOutput, TError>) {
+  const [_] = useTranslation();
+  return useGmpMutation<TInput, TOutput, TError>({
+    entityType,
+    invalidateQueryIds,
     method,
-    successMessage: `${entityLabel} successfully created`,
+    successMessage: _('{{entity}} successfully created', {
+      entity: typeName(entityType),
+    }),
     onSuccess,
     onError,
   });
