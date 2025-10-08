074f0bcf5bcf37f4d5ace26b4ff041430c926f36
Björn Ricks <bjoern.ricks@greenbone.net>
2025-10-08 10:21:37 +0200

    Change: Allow to specify the error type of useDeleteMutation

--- src/web/queries/useDeleteMutation.ts
+++ src/web/queries/useDeleteMutation.ts
@@ -8,26 +8,27 @@ import {EntityType, typeName} from 'gmp/utils/entitytype';
 import useTranslation from 'web/hooks/useTranslation';
 import {useGmpMutation} from 'web/queries/useGmpMutation';
 
-interface UseDeleteMutationParams<TOutput> {
+interface UseDeleteMutationParams<TOutput, TError> {
   entityType: EntityType;
   invalidateQueryIds?: string[];
   method?: string;
   onSuccess?: (data: TOutput) => void;
-  onError?: (error: unknown) => void;
+  onError?: (error: TError) => void;
 }
 
 export function useDeleteMutation<
   TInput = EntityCommandParams,
   TOutput = void,
+  TError = Error,
 >({
   entityType,
   invalidateQueryIds,
   method = 'delete',
   onSuccess,
   onError,
-}: UseDeleteMutationParams<TOutput>) {
+}: UseDeleteMutationParams<TOutput, TError>) {
   const [_] = useTranslation();
-  return useGmpMutation<TInput, TOutput>({
+  return useGmpMutation<TInput, TOutput, TError>({
     entityType,
     invalidateQueryIds,
     method,
