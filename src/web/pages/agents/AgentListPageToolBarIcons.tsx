9664b8b787c755278918965c776b300d1048c0de
Björn Ricks <bjoern.ricks@greenbone.net>
2025-10-08 10:25:26 +0200

    Remove not available download from agent list page tool bar
    Reword the help icon title text too.

--- src/web/pages/agents/AgentListPageToolBarIcons.tsx
+++ src/web/pages/agents/AgentListPageToolBarIcons.tsx
@@ -3,37 +3,21 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import {DownloadIcon} from 'web/components/icon';
 import ManualIcon from 'web/components/icon/ManualIcon';
 import IconDivider from 'web/components/layout/IconDivider';
 
 import useTranslation from 'web/hooks/useTranslation';
 
-interface AgentsListPageToolBarIconsProps {
-  onAgentCreateClick?: () => void;
-}
-
-const AgentsListPageToolBarIcons = ({
-  onAgentCreateClick,
-}: AgentsListPageToolBarIconsProps) => {
-  /* TODO FIX Permission for super admin */
-  //  const capabilities = useCapabilities();
+const AgentsListPageToolBarIcons = () => {
   const [_] = useTranslation();
   return (
     <IconDivider>
       <ManualIcon
         /* TODO add correct link */
-        anchor="creating-and-managing-agents-lists"
+        anchor="creating-and-managing-agents"
         page="scanning"
-        title={_('Help: Agents Lists')}
+        title={_('Help: Agents')}
       />
-      {
-        /* TODO check permissions */
-        /* capabilities.mayCreate('agent') &&  */ <DownloadIcon
-          title={_('Download Agents List')}
-          onClick={onAgentCreateClick}
-        />
-      }
     </IconDivider>
   );
 };
