f6b8af37ff7bd8f6d203242d06cd07e2caa032cf
Björn Ricks <bjoern.ricks@greenbone.net>
2025-10-13 09:01:16 +0200

    Change: Improve FileField
    * Use correct props for the FileField
    * Update the onChange handler to expect undefined too
    * Add a label for the clear button
    * Ensure the clear button is shown
    * Use undefined for not having a value (internally it's null)

--- src/web/components/form/__tests__/FileField.test.tsx
+++ src/web/components/form/__tests__/FileField.test.tsx
@@ -4,8 +4,22 @@
  */
 
 import {describe, test, expect, testing} from '@gsa/testing';
-import {render, fireEvent} from 'web/testing';
+import {render, fireEvent, screen} from 'web/testing';
 import FileField from 'web/components/form/FileField';
+
+const getFileInput = () => {
+  const input = document.querySelector<HTMLElement>('input[type=file]');
+  if (!input) {
+    throw new Error('File input not found');
+  }
+  return input;
+};
+
+const changeFileInput = (input: HTMLElement, file: string) => {
+  fireEvent.change(input, {
+    target: {files: [file]},
+  });
+};
 
 describe('FileField tests', () => {
   test('should render', () => {
@@ -17,38 +31,40 @@ describe('FileField tests', () => {
   test('should call change handler with file', () => {
     const onChange = testing.fn();
 
-    const {baseElement} = render(<FileField name="foo" onChange={onChange} />);
+    render(<FileField name="foo" onChange={onChange} />);
 
-    const input = baseElement.querySelector('input[type=file]') as Element;
-    fireEvent.change(input, {
-      target: {files: ['bar']},
-    });
+    const input = getFileInput();
+    changeFileInput(input, 'bar');
     expect(onChange).toHaveBeenCalledWith('bar', 'foo');
   });
 
   test('should call change handler with file and name', () => {
     const onChange = testing.fn();
 
-    const {baseElement} = render(<FileField name="foo" onChange={onChange} />);
+    render(<FileField name="foo" onChange={onChange} />);
 
-    const input = baseElement.querySelector('input[type=file]') as Element;
-
-    fireEvent.change(input, {target: {files: ['bar']}});
-
+    const input = getFileInput();
+    changeFileInput(input, 'bar');
     expect(onChange).toHaveBeenCalledWith('bar', 'foo');
   });
 
   test('should not call change handler if disabled', () => {
     const onChange = testing.fn();
 
-    const {baseElement} = render(
-      <FileField disabled={true} onChange={onChange} />,
-    );
+    render(<FileField disabled={true} onChange={onChange} />);
 
-    const input = baseElement.querySelector('input[type=file]') as Element;
-
-    fireEvent.change(input, {target: {files: ['bar']}});
-
+    const input = getFileInput();
+    changeFileInput(input, 'bar');
     expect(onChange).not.toHaveBeenCalled();
+  });
+
+  test('should allow to reset the field value', () => {
+    const onChange = testing.fn();
+    const file = new File(['content'], 'example.txt', {type: 'text/plain'});
+    render(<FileField name="someFile" value={file} onChange={onChange} />);
+
+    const removeButton = screen.getByRole('button', {name: 'Clear input'});
+    fireEvent.click(removeButton);
+    expect(onChange).toHaveBeenCalledWith(undefined, 'someFile');
   });
 });
