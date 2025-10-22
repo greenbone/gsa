cc0e9b5b0868bd6db54fec6977866a9e4781225a
Björn Ricks <bjoern.ricks@greenbone.net>
2025-10-22 14:25:02 +0200

    Add query hooks for Agent Installers
    Implement query hooks for Agent Installers.

---
+++ src/web/hooks/useQuery/agentinstallers.ts
@@ -0,0 +1,27 @@
+/* SPDX-FileCopyrightText: 2025 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import type AgentInstaller from 'gmp/models/agentinstaller';
+import type Filter from 'gmp/models/filter';
+import useGmp from 'web/hooks/useGmp';
+import useGetEntities from 'web/queries/useGetEntities';
+
+interface UseGetAgentInstallersParams {
+  filter?: Filter;
+  enabled?: boolean;
+}
+
+export const useGetAgentInstallers = ({
+  filter,
+  enabled,
+}: UseGetAgentInstallersParams) => {
+  const gmp = useGmp();
+  return useGetEntities<AgentInstaller>({
+    gmpMethod: gmp.agentinstallers.get.bind(gmp.agentinstallers),
+    queryId: 'get_agentinstallers',
+    filter,
+    enabled,
+  });
+};
