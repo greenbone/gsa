d46e1130d13111321340ad661126eb571bbecf74
Björn Ricks <bjoern.ricks@greenbone.net>
2025-11-06 09:01:25 +0100

    Drop: Remove unnecessary properties from AgentInstaller model
    The properties are removed from GMP now because they aren't necessary.

--- src/gmp/models/__tests__/agent-installer.test.ts
+++ src/gmp/models/__tests__/agent-installer.test.ts
@@ -5,12 +5,11 @@
 
 import {describe, test, expect} from '@gsa/testing';
 import AgentInstaller from 'gmp/models/agent-installer';
-import date from 'gmp/models/date';
 import {testModel} from 'gmp/models/testing';
 
-testModel(AgentInstaller, 'agentinstaller');
-
 describe('AgentInstaller model tests', () => {
+  testModel(AgentInstaller, 'agentinstaller');
+
   test('should use defaults', () => {
     const agentInstaller = new AgentInstaller();
 
@@ -23,10 +22,6 @@ describe('AgentInstaller model tests', () => {
     expect(agentInstaller.fileExtension).toBeUndefined();
     expect(agentInstaller.version).toBeUndefined();
     expect(agentInstaller.checksum).toBeUndefined();
-    expect(agentInstaller.fileSize).toBeUndefined();
-    expect(agentInstaller.fileValidity).toBeUndefined();
-    expect(agentInstaller.lastUpdate).toBeUndefined();
-    expect(agentInstaller.cpes).toEqual([]);
   });
 
   test('should parse contentType', () => {
@@ -67,93 +62,5 @@ describe('AgentInstaller model tests', () => {
     });
 
     expect(agentInstaller.checksum).toBe('abc123');
-  });
-
-  test('should parse fileSize', () => {
-    const agentInstaller = AgentInstaller.fromElement({
-      file_size: 2048,
-    });
-
-    expect(agentInstaller.fileSize).toBe(2048);
-  });
-
-  test('should parse fileValidity', () => {
-    const agentInstallerValid = AgentInstaller.fromElement({
-      file_validity: 'valid',
-    });
-    expect(agentInstallerValid.fileValidity).toBe('valid');
-
-    const agentInstallerInvalid = AgentInstaller.fromElement({
-      file_validity: 'invalid',
-    });
-    expect(agentInstallerInvalid.fileValidity).toBe('invalid');
-
-    const agentInstallerUnknown = AgentInstaller.fromElement({
-      file_validity: 'unknown',
-    });
-    expect(agentInstallerUnknown.fileValidity).toBe('unknown');
-
-    const agentInstallerUndefined = AgentInstaller.fromElement({});
-    expect(agentInstallerUndefined.fileValidity).toBeUndefined();
-  });
-
-  test('should parse lastUpdate', () => {
-    const agentInstaller = AgentInstaller.fromElement({
-      last_update: '2023-10-01T12:34:56Z',
-    });
-
-    expect(agentInstaller.lastUpdate).toEqual(date('2023-10-01T12:34:56Z'));
-  });
-
-  test('should parse cpes', () => {
-    const agentInstaller = AgentInstaller.fromElement({
-      cpes: [
-        {
-          criteria: 'cpe:/a:vendor:product:1.0',
-          version_start_incl: '1.0',
-          version_end_excl: '2.0',
-        },
-        {
-          criteria: 'cpe:/a:vendor:product:2.0',
-          version_start_excl: '2.0',
-          version_end_incl: '3.0',
-        },
-      ],
-    });
-
-    expect(agentInstaller.cpes).toEqual([
-      {
-        criteria: 'cpe:/a:vendor:product:1.0',
-        versionStartIncluding: '1.0',
-        versionStartExcluding: undefined,
-        versionEndIncluding: undefined,
-        versionEndExcluding: '2.0',
-      },
-      {
-        criteria: 'cpe:/a:vendor:product:2.0',
-        versionStartIncluding: undefined,
-        versionStartExcluding: '2.0',
-        versionEndIncluding: '3.0',
-        versionEndExcluding: undefined,
-      },
-    ]);
-
-    const agentInstaller2 = AgentInstaller.fromElement({
-      cpes: {
-        criteria: 'cpe:/a:vendor:product:1.0',
-        version_start_incl: '1.0',
-        version_end_excl: '2.0',
-      },
-    });
-
-    expect(agentInstaller2.cpes).toEqual([
-      {
-        criteria: 'cpe:/a:vendor:product:1.0',
-        versionStartIncluding: '1.0',
-        versionStartExcluding: undefined,
-        versionEndIncluding: undefined,
-        versionEndExcluding: '2.0',
-      },
-    ]);
   });
 });
