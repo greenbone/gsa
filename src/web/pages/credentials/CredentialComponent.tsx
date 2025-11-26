01ee4fceaaa998a5c983093085b232116478fe92
Björn Ricks <bjoern.ricks@greenbone.net>
2025-11-26 08:59:02 +0100

    Change: Use TypeScript for CredentialComponent
    Also add tests and use camelCase for all variables.

---
+++ src/web/pages/credentials/CredentialComponent.tsx
@@ -0,0 +1,243 @@
+/* SPDX-FileCopyrightText: 2024 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import {useState} from 'react';
+import {useSelector} from 'react-redux';
+import {type CredentialDownloadFormat} from 'gmp/commands/credential';
+import {type EntityCommandParams} from 'gmp/commands/entity';
+import {
+  ALL_CREDENTIAL_TYPES,
+  type SNMPAuthAlgorithmType,
+  type default as Credential,
+  type CredentialType,
+  type SNMPPrivacyAlgorithmType,
+} from 'gmp/models/credential';
+import {isDefined} from 'gmp/utils/identity';
+import {shorten} from 'gmp/utils/string';
+import useEntityClone, {
+  type EntityCloneResponse,
+} from 'web/entity/hooks/useEntityClone';
+import useEntityCreate, {
+  type EntityCreateResponse,
+} from 'web/entity/hooks/useEntityCreate';
+import useEntityDelete from 'web/entity/hooks/useEntityDelete';
+import useEntityDownload, {
+  type OnDownloadedFunc,
+} from 'web/entity/hooks/useEntityDownload';
+import useEntitySave, {
+  type EntitySaveResponse,
+} from 'web/entity/hooks/useEntitySave';
+import useGmp from 'web/hooks/useGmp';
+import useShallowEqualSelector from 'web/hooks/useShallowEqualSelector';
+import useTranslation from 'web/hooks/useTranslation';
+import CredentialDialog, {
+  type CredentialDialogState,
+} from 'web/pages/credentials/CredentialDialog';
+import {getUserSettingsDefaults} from 'web/store/usersettings/defaults/selectors';
+import {getUsername} from 'web/store/usersettings/selectors';
+import {generateFilename} from 'web/utils/Render';
+
+interface CredentialComponentRenderProps {
+  clone: (credential: Credential) => Promise<void>;
+  create: () => void;
+  delete: (credential: Credential) => Promise<void>;
+  edit: (credential: Credential) => void;
+  download: (credential: Credential) => Promise<void>;
+  downloadInstaller: (
+    credential: Credential,
+    format: CredentialDownloadFormat,
+  ) => Promise<void>;
+}
+
+interface CredentialComponentProps {
+  children: (props: CredentialComponentRenderProps) => React.ReactNode;
+  onCloneError?: (error: Error) => void;
+  onCloned?: (entity: EntityCloneResponse) => void;
+  onCreateError?: (error: Error) => void;
+  onCreated?: (entity: EntityCreateResponse) => void;
+  onDeleteError?: (error: Error) => void;
+  onDeleted?: () => void;
+  onDownloadError?: (error: Error) => void;
+  onDownloaded?: OnDownloadedFunc;
+  onInstallerDownloadError?: (error: Error) => void;
+  onInstallerDownloaded?: OnDownloadedFunc<ArrayBuffer>;
+  onSaveError?: (error: Error) => void;
+  onSaved?: (entity: EntitySaveResponse) => void;
+}
+
+const CredentialComponent = ({
+  children,
+  onCloneError,
+  onCloned,
+  onCreateError,
+  onCreated,
+  onDeleteError,
+  onDeleted,
+  onDownloadError,
+  onDownloaded,
+  onInstallerDownloadError,
+  onInstallerDownloaded,
+  onSaveError,
+  onSaved,
+}: CredentialComponentProps) => {
+  const gmp = useGmp();
+  const [_] = useTranslation();
+
+  const userDefaultsSelector = useShallowEqualSelector(getUserSettingsDefaults);
+  const username = useSelector(getUsername);
+  const detailsExportFileName = userDefaultsSelector.getValueByName(
+    'detailsexportfilename',
+  );
+
+  const [dialogVisible, setDialogVisible] = useState(false);
+
+  const [comment, setComment] = useState<string | undefined>();
+  const [credential, setCredential] = useState<Credential | undefined>();
+  const [credentialType, setCredentialType] = useState<
+    CredentialType | undefined
+  >();
+  const [authAlgorithm, setAuthAlgorithm] = useState<
+    SNMPAuthAlgorithmType | undefined
+  >();
+  const [name, setName] = useState<string | undefined>();
+  const [credentialLogin, setCredentialLogin] = useState<string | undefined>();
+  const [privacyAlgorithm, setPrivacyAlgorithm] = useState<
+    SNMPPrivacyAlgorithmType | undefined
+  >();
+  const [types, setTypes] =
+    useState<readonly CredentialType[]>(ALL_CREDENTIAL_TYPES);
+  const [title, setTitle] = useState<string>('');
+
+  const openCredentialsDialog = (credential?: Credential) => {
+    if (isDefined(credential)) {
+      const dialogTitle = _('Edit Credential {{name}}', {
+        name: shorten(credential.name),
+      });
+
+      setComment(credential.comment);
+      setCredential(credential);
+      setCredentialType(credential.credentialType);
+      setAuthAlgorithm(credential.authAlgorithm);
+      setName(credential.name);
+      setCredentialLogin(credential.login);
+      setPrivacyAlgorithm(credential.privacyAlgorithm);
+      setTypes([credential.credentialType as CredentialType]);
+      setTitle(dialogTitle);
+    } else {
+      // reset all values in state to not show values from last edit
+      setComment(undefined);
+      setCredential(undefined);
+      setCredentialType(undefined);
+      setAuthAlgorithm(undefined);
+      setName(undefined);
+      setCredentialLogin(undefined);
+      setPrivacyAlgorithm(undefined);
+      setTypes(ALL_CREDENTIAL_TYPES);
+      setTitle(_('New Credential'));
+    }
+
+    setDialogVisible(true);
+  };
+
+  const closeCredentialDialog = () => {
+    setDialogVisible(false);
+  };
+
+  const handleCloseCredentialDialog = () => {
+    closeCredentialDialog();
+  };
+
+  const handleDownloadInstaller = (
+    cred: Credential,
+    format: CredentialDownloadFormat,
+  ) => {
+    return gmp.credential
+      .download({id: cred.id as string}, format)
+      .then(response => {
+        const {creationTime, entityType, id, modificationTime, name} = cred;
+        const filename = generateFilename({
+          creationTime: creationTime,
+          extension: format,
+          fileNameFormat: detailsExportFileName,
+          id: id,
+          modificationTime,
+          resourceName: name,
+          resourceType: entityType,
+          username,
+        });
+        return {filename, data: response.data};
+      })
+      .then(onInstallerDownloaded, onInstallerDownloadError);
+  };
+
+  const handleEntitySave = useEntitySave(
+    (data: CredentialDialogState) =>
+      gmp.credential.save({
+        ...data,
+        id: data.id as string,
+      }),
+    {onSaveError, onSaved},
+  );
+
+  const handleEntityCreate = useEntityCreate(
+    (data: CredentialDialogState) => gmp.credential.create(data),
+    {onCreateError, onCreated},
+  );
+
+  const handleEntityClone = useEntityClone(
+    (data: EntityCommandParams) => gmp.credential.clone(data),
+    {
+      onCloneError,
+      onCloned,
+    },
+  );
+
+  const handleEntityDelete = useEntityDelete<Credential>(
+    data => gmp.credential.delete(data),
+    {onDeleteError, onDeleted},
+  );
+
+  const handleEntityDownload = useEntityDownload<Credential, string>(
+    data => gmp.credential.export({id: data.id as string}),
+    {onDownloadError, onDownloaded},
+  );
+
+  const handleSave = async (data: CredentialDialogState) => {
+    const promise = isDefined(data.id)
+      ? handleEntitySave(data)
+      : handleEntityCreate(data);
+    await promise;
+    return closeCredentialDialog();
+  };
+  return (
+    <>
+      {children({
+        clone: handleEntityClone,
+        delete: handleEntityDelete,
+        create: () => openCredentialsDialog(),
+        edit: openCredentialsDialog,
+        download: handleEntityDownload,
+        downloadInstaller: handleDownloadInstaller,
+      })}
+      {dialogVisible && (
+        <CredentialDialog
+          authAlgorithm={authAlgorithm}
+          comment={comment}
+          credential={credential}
+          credentialLogin={credentialLogin}
+          credentialType={credentialType}
+          name={name}
+          privacyAlgorithm={privacyAlgorithm}
+          title={title}
+          types={types}
+          onClose={handleCloseCredentialDialog}
+          onSave={handleSave}
+        />
+      )}
+    </>
+  );
+};
+
+export default CredentialComponent;
---
+++ src/web/pages/credentials/__tests__/CredentialComponent.test.tsx
@@ -0,0 +1,383 @@
+/* SPDX-FileCopyrightText: 2025 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import {describe, expect, test, testing} from '@gsa/testing';
+import {screen, rendererWith, wait, fireEvent} from 'web/testing';
+import Response from 'gmp/http/response';
+import Credential from 'gmp/models/credential';
+import Button from 'web/components/form/Button';
+import {currentSettingsDefaultResponse} from 'web/pages/__mocks__/current-settings';
+import CredentialComponent from 'web/pages/credentials/CredentialComponent';
+
+const currentSettings = testing
+  .fn()
+  .mockResolvedValue(currentSettingsDefaultResponse);
+
+const defaultGetCredentialResponse = new Response(new Credential({id: '123'}));
+const createGmp = ({
+  cloneCredentialResponse = {id: '123'},
+  createCredentialResponse = {id: '123'},
+  deleteCredentialResponse = undefined,
+  downloadCredentialResponse = {data: 'some-data'},
+  downloadInstallerResponse = {data: 'some-data'},
+  getCredentialResponse = defaultGetCredentialResponse,
+  saveCredentialResponse = {id: '123'},
+  cloneCredential = testing.fn().mockResolvedValue(cloneCredentialResponse),
+  createCredential = testing.fn().mockResolvedValue(createCredentialResponse),
+  deleteCredential = testing.fn().mockResolvedValue(deleteCredentialResponse),
+  downloadCredential = testing
+    .fn()
+    .mockResolvedValue(downloadCredentialResponse),
+  downloadInstaller = testing.fn().mockResolvedValue(downloadInstallerResponse),
+  getCredential = testing.fn().mockResolvedValue(getCredentialResponse),
+  saveCredential = testing.fn().mockResolvedValue(saveCredentialResponse),
+} = {}) => {
+  return {
+    settings: {
+      enableKrb5: true,
+    },
+    credential: {
+      clone: cloneCredential,
+      create: createCredential,
+      delete: deleteCredential,
+      download: downloadInstaller,
+      export: downloadCredential,
+      get: getCredential,
+      save: saveCredential,
+    },
+    user: {
+      currentSettings,
+    },
+  };
+};
+
+describe('CredentialComponent tests', () => {
+  test('should render without crashing', async () => {
+    const gmp = createGmp();
+    const {render} = rendererWith({gmp});
+    render(
+      <CredentialComponent>
+        {() => <div>Some Content</div>}
+      </CredentialComponent>,
+    );
+    await wait();
+    expect(screen.getByText('Some Content')).toBeInTheDocument();
+  });
+
+  test('should open and close CredentialDialog', async () => {
+    const gmp = createGmp();
+    const {render} = rendererWith({gmp});
+    render(
+      <CredentialComponent>
+        {({create}) => <Button data-testid="open" onClick={create} />}
+      </CredentialComponent>,
+    );
+
+    await wait();
+
+    fireEvent.click(screen.getByTestId('open'));
+    expect(screen.getByText('New Credential')).toBeInTheDocument();
+
+    const cancelButton = screen.getDialogCloseButton();
+    fireEvent.click(cancelButton);
+    expect(screen.queryByText('New Credential')).not.toBeInTheDocument();
+  });
+
+  test('should allow creating a new credential', async () => {
+    const gmp = createGmp();
+    const onCreated = testing.fn();
+    const onCreateError = testing.fn();
+    const {render} = rendererWith({gmp});
+    render(
+      <CredentialComponent onCreateError={onCreateError} onCreated={onCreated}>
+        {({create}) => <Button data-testid="open" onClick={create} />}
+      </CredentialComponent>,
+    );
+
+    fireEvent.click(screen.getByTestId('open'));
+    expect(screen.getByText('New Credential')).toBeInTheDocument();
+
+    const saveButton = screen.getDialogSaveButton();
+    fireEvent.click(saveButton);
+    await wait();
+
+    expect(screen.queryByText('New Credential')).not.toBeInTheDocument();
+    expect(onCreateError).not.toHaveBeenCalled();
+    expect(onCreated).toHaveBeenCalledWith({id: '123'});
+  });
+
+  test('should call onCreateError if creating a new credential fails', async () => {
+    const error = new Error('error');
+    const gmp = createGmp({
+      createCredential: testing.fn().mockRejectedValue(error),
+    });
+    const onCreated = testing.fn();
+    const onCreateError = testing.fn();
+    const {render} = rendererWith({gmp});
+    render(
+      <CredentialComponent onCreateError={onCreateError} onCreated={onCreated}>
+        {({create}) => <Button data-testid="open" onClick={create} />}
+      </CredentialComponent>,
+    );
+
+    fireEvent.click(screen.getByTestId('open'));
+    await wait();
+    expect(screen.getByText('New Credential')).toBeInTheDocument();
+
+    const saveButton = screen.getDialogSaveButton();
+    fireEvent.click(saveButton);
+    await wait();
+    expect(screen.queryByText('New Credential')).not.toBeInTheDocument();
+    expect(onCreateError).toHaveBeenCalledWith(error);
+    expect(onCreated).not.toHaveBeenCalled();
+  });
+
+  test('should call onSaveError if saving a credential fails', async () => {
+    const credential = new Credential({id: '123', name: 'foo'});
+    const credentialResponse = new Response(credential);
+    const error = new Error('error');
+    const gmp = createGmp({
+      getCredentialResponse: credentialResponse,
+      saveCredential: testing.fn().mockRejectedValue(error),
+    });
+    const onSaved = testing.fn();
+    const onSaveError = testing.fn();
+    const {render} = rendererWith({gmp});
+    render(
+      <CredentialComponent onSaveError={onSaveError} onSaved={onSaved}>
+        {({edit}) => (
+          <Button data-testid="open" onClick={() => edit(credential)} />
+        )}
+      </CredentialComponent>,
+    );
+
+    fireEvent.click(screen.getByTestId('open'));
+    await wait();
+    expect(screen.getByText('Edit Credential foo')).toBeInTheDocument();
+
+    const saveButton = screen.getDialogSaveButton();
+    fireEvent.click(saveButton);
+    await wait();
+
+    expect(screen.queryByText('Edit Credential foo')).not.toBeInTheDocument();
+    expect(onSaved).not.toHaveBeenCalled();
+    expect(onSaveError).toHaveBeenCalledExactlyOnceWith(error);
+  });
+
+  test('should allow cloning a credential', async () => {
+    const gmp = createGmp();
+    const onCloned = testing.fn();
+    const onCloneError = testing.fn();
+
+    const {render} = rendererWith({gmp});
+    render(
+      <CredentialComponent onCloneError={onCloneError} onCloned={onCloned}>
+        {({clone}) => (
+          <Button
+            data-testid="button"
+            onClick={() => clone(new Credential({id: '123'}))}
+          />
+        )}
+      </CredentialComponent>,
+    );
+
+    fireEvent.click(screen.getByTestId('button'));
+    await wait();
+    expect(onCloneError).not.toHaveBeenCalled();
+    expect(onCloned).toHaveBeenCalledExactlyOnceWith({id: '123'});
+  });
+
+  test('should call onCloneError when cloning a credential fails', async () => {
+    const error = new Error('error');
+    const onCloned = testing.fn();
+    const onCloneError = testing.fn();
+    const gmp = createGmp({
+      cloneCredential: testing.fn().mockRejectedValue(error),
+    });
+    const {render} = rendererWith({gmp});
+    render(
+      <CredentialComponent onCloneError={onCloneError} onCloned={onCloned}>
+        {({clone}) => (
+          <Button
+            data-testid="button"
+            onClick={() => clone(new Credential({id: '123'}))}
+          />
+        )}
+      </CredentialComponent>,
+    );
+
+    fireEvent.click(screen.getByTestId('button'));
+    await wait();
+    expect(onCloned).not.toHaveBeenCalled();
+    expect(onCloneError).toHaveBeenCalledExactlyOnceWith(error);
+  });
+
+  test('should allow deleting a credential', async () => {
+    const onDeleted = testing.fn();
+    const onDeleteError = testing.fn();
+    const gmp = createGmp();
+    const {render} = rendererWith({gmp});
+    render(
+      <CredentialComponent onDeleteError={onDeleteError} onDeleted={onDeleted}>
+        {({delete: del}) => (
+          <Button
+            data-testid="button"
+            onClick={() => del(new Credential({id: '123'}))}
+          />
+        )}
+      </CredentialComponent>,
+    );
+
+    fireEvent.click(screen.getByTestId('button'));
+    await wait();
+    expect(onDeleteError).not.toHaveBeenCalled();
+    expect(onDeleted).toHaveBeenCalledOnce();
+  });
+
+  test('should call onDeleteError when deleting a credential fails', async () => {
+    const error = new Error('error');
+    const onDeleted = testing.fn();
+    const onDeleteError = testing.fn();
+    const gmp = createGmp({
+      deleteCredential: testing.fn().mockRejectedValue(error),
+    });
+    const {render} = rendererWith({gmp});
+    render(
+      <CredentialComponent onDeleteError={onDeleteError} onDeleted={onDeleted}>
+        {({delete: del}) => (
+          <Button
+            data-testid="button"
+            onClick={() => del(new Credential({id: '123'}))}
+          />
+        )}
+      </CredentialComponent>,
+    );
+
+    fireEvent.click(screen.getByTestId('button'));
+    await wait();
+    expect(onDeleted).not.toHaveBeenCalled();
+    expect(onDeleteError).toHaveBeenCalledExactlyOnceWith(error);
+  });
+
+  test('should allow downloading a credential', async () => {
+    const onDownloaded = testing.fn();
+    const onDownloadError = testing.fn();
+    const gmp = createGmp();
+    const {render} = rendererWith({gmp});
+    render(
+      <CredentialComponent
+        onDownloadError={onDownloadError}
+        onDownloaded={onDownloaded}
+      >
+        {({download}) => (
+          <Button
+            data-testid="button"
+            onClick={() => download(new Credential({id: '123'}))}
+          />
+        )}
+      </CredentialComponent>,
+    );
+    await wait();
+
+    fireEvent.click(screen.getByTestId('button'));
+    await wait();
+    expect(onDownloadError).not.toHaveBeenCalled();
+    expect(onDownloaded).toHaveBeenCalledWith({
+      data: 'some-data',
+      filename: 'credential-123.xml',
+    });
+  });
+
+  test('should call onDownloadError when downloading a credential fails', async () => {
+    const error = new Error('error');
+    const onDownloaded = testing.fn();
+    const onDownloadError = testing.fn();
+    const gmp = createGmp({
+      downloadCredential: testing.fn().mockRejectedValue(error),
+    });
+    const {render} = rendererWith({gmp});
+    render(
+      <CredentialComponent
+        onDownloadError={onDownloadError}
+        onDownloaded={onDownloaded}
+      >
+        {({download}) => (
+          <Button
+            data-testid="button"
+            onClick={() => download(new Credential({id: '123'}))}
+          />
+        )}
+      </CredentialComponent>,
+    );
+    await wait();
+
+    fireEvent.click(screen.getByTestId('button'));
+    await wait();
+    expect(onDownloaded).not.toHaveBeenCalled();
+    expect(onDownloadError).toHaveBeenCalledExactlyOnceWith(error);
+  });
+
+  test('should allow downloading a credential installer', async () => {
+    const onDownloaded = testing.fn();
+    const onDownloadError = testing.fn();
+    const gmp = createGmp();
+    const {render} = rendererWith({gmp});
+    render(
+      <CredentialComponent
+        onInstallerDownloadError={onDownloadError}
+        onInstallerDownloaded={onDownloaded}
+      >
+        {({downloadInstaller}) => (
+          <Button
+            data-testid="button"
+            onClick={() =>
+              downloadInstaller(new Credential({id: '123'}), 'rpm')
+            }
+          />
+        )}
+      </CredentialComponent>,
+    );
+    await wait();
+
+    fireEvent.click(screen.getByTestId('button'));
+    await wait();
+    expect(onDownloadError).not.toHaveBeenCalled();
+    expect(onDownloaded).toHaveBeenCalledWith({
+      data: 'some-data',
+      filename: 'credential-123.rpm',
+    });
+  });
+
+  test('should call onInstallerDownloadError when downloading a credential installer fails', async () => {
+    const error = new Error('error');
+    const onDownloaded = testing.fn();
+    const onDownloadError = testing.fn();
+    const gmp = createGmp({
+      downloadInstaller: testing.fn().mockRejectedValue(error),
+    });
+    const {render} = rendererWith({gmp});
+    render(
+      <CredentialComponent
+        onInstallerDownloadError={onDownloadError}
+        onInstallerDownloaded={onDownloaded}
+      >
+        {({downloadInstaller}) => (
+          <Button
+            data-testid="button"
+            onClick={() =>
+              downloadInstaller(new Credential({id: '123'}), 'rpm')
+            }
+          />
+        )}
+      </CredentialComponent>,
+    );
+    await wait();
+
+    fireEvent.click(screen.getByTestId('button'));
+    await wait();
+    expect(onDownloaded).not.toHaveBeenCalled();
+    expect(onDownloadError).toHaveBeenCalledExactlyOnceWith(error);
+  });
+});
