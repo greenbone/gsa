acfebe4eba53c346679e41472ed7ddc20f328e70
Björn Ricks <bjoern.ricks@greenbone.net>
2025-11-26 13:11:52 +0100

    change: Use TypeScript for CredentialListPage

--- src/web/pages/credentials/CredentialListPage.tsx
+++ src/web/pages/credentials/CredentialListPage.tsx
@@ -3,55 +3,23 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import React from 'react';
+import type Credential from 'gmp/models/credential';
 import {CREDENTIALS_FILTER_FILTER} from 'gmp/models/filter';
-import {CredentialIcon, NewIcon} from 'web/components/icon';
-import ManualIcon from 'web/components/icon/ManualIcon';
-import IconDivider from 'web/components/layout/IconDivider';
+import {CredentialIcon} from 'web/components/icon';
 import PageTitle from 'web/components/layout/PageTitle';
 import EntitiesPage from 'web/entities/EntitiesPage';
 import withEntitiesContainer from 'web/entities/withEntitiesContainer';
-import useCapabilities from 'web/hooks/useCapabilities';
 import useTranslation from 'web/hooks/useTranslation';
+import CredentialComponent from 'web/pages/credentials/CredentialComponent';
 import CredentialFilterDialog from 'web/pages/credentials/CredentialFilterDialog';
-import CredentialComponent from 'web/pages/credentials/CredentialsComponent';
-import CredentialsTable from 'web/pages/credentials/Table';
+import CredentialListPageToolBarIcons from 'web/pages/credentials/CredentialListPageToolBarIcons';
+import CredentialTable from 'web/pages/credentials/CredentialTable';
 import {
   loadEntities,
   selector as entitiesSelector,
 } from 'web/store/entities/credentials';
-import PropTypes from 'web/utils/PropTypes';
-export const ToolBarIcons = ({onCredentialCreateClick}) => {
-  const capabilities = useCapabilities();
-  const [_] = useTranslation();
-  return (
-    <IconDivider>
-      <ManualIcon
-        anchor="managing-credentials"
-        page="scanning"
-        title={_('Help: Credentials')}
-      />
-      {capabilities.mayCreate('credential') && (
-        <NewIcon
-          title={_('New Credential')}
-          onClick={onCredentialCreateClick}
-        />
-      )}
-    </IconDivider>
-  );
-};
 
-ToolBarIcons.propTypes = {
-  onCredentialCreateClick: PropTypes.func.isRequired,
-};
-
-const CredentialsPage = ({
-  onChanged,
-  onDownloaded,
-  onError,
-
-  ...props
-}) => {
+const CredentialsPage = ({onChanged, onDownloaded, onError, ...props}) => {
   const [_] = useTranslation();
   return (
     <CredentialComponent
@@ -69,43 +37,35 @@ const CredentialsPage = ({
       {({
         clone,
         create,
-        delete: delete_func,
+        delete: deleteFunc,
         download,
-        downloadinstaller,
+        downloadInstaller,
         edit,
-        save,
       }) => (
-        <React.Fragment>
+        <>
           <PageTitle title={_('Credentials')} />
-          <EntitiesPage
+          <EntitiesPage<Credential>
             {...props}
             filterEditDialog={CredentialFilterDialog}
             filtersFilter={CREDENTIALS_FILTER_FILTER}
             sectionIcon={<CredentialIcon size="large" />}
-            table={CredentialsTable}
+            table={CredentialTable}
             title={_('Credentials')}
-            toolBarIcons={ToolBarIcons}
+            toolBarIcons={CredentialListPageToolBarIcons}
             onChanged={onChanged}
             onCredentialCloneClick={clone}
             onCredentialCreateClick={create}
-            onCredentialDeleteClick={delete_func}
+            onCredentialDeleteClick={deleteFunc}
             onCredentialDownloadClick={download}
             onCredentialEditClick={edit}
-            onCredentialInstallerDownloadClick={downloadinstaller}
-            onCredentialSaveClick={save}
+            onCredentialInstallerDownloadClick={downloadInstaller}
             onDownloaded={onDownloaded}
             onError={onError}
           />
-        </React.Fragment>
+        </>
       )}
     </CredentialComponent>
   );
-};
-
-CredentialsPage.propTypes = {
-  onChanged: PropTypes.func.isRequired,
-  onDownloaded: PropTypes.func.isRequired,
-  onError: PropTypes.func.isRequired,
 };
 
 export default withEntitiesContainer('credential', {
