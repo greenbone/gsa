ba678fc7db9884a82b3ae4bcd2bce27259fa6d08
Björn Ricks <bjoern.ricks@greenbone.net>
2026-01-06 15:23:22 +0100

    Extract some LegendLine and LegendLabel components from Legend
    These two components are used outside of the Legend component and
    therefore should be in their own module.

---
+++ src/web/components/chart/base/LagendLabel.tsx
@@ -0,0 +1,16 @@
+/* SPDX-FileCopyrightText: 2024 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import styled from 'styled-components';
+
+const LegendLabel = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  flex-grow: 1;
+  margin-left: 10px;
+`;
+
+export default LegendLabel;
