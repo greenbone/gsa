c88abce5ff28d87faf4a6074fc40e8e385f9a9a5
Johannes Helmold <83279292+jhelmold@users.noreply.github.com>
2026-02-05 15:20:39 +0100

    Fix: Save all filters with the correct filter type (#5094)
    * Fix: Replaced the function call of apiType(..) by resourceType(..).
    
    * Tests for the creation and saving of filters.

---
+++ src/gmp/commands/__tests__/filters.test.ts
@@ -0,0 +1,89 @@
+/* SPDX-FileCopyrightText: 2026 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import {describe, test, expect} from '@gsa/testing';
+import {FilterCommand} from 'gmp/commands/filters';
+import {createHttp, createActionResultResponse} from 'gmp/commands/testing';
+
+describe('FilterCommand tests', () => {
+  test('should create a new filter', async () => {
+    const response = createActionResultResponse({
+      action: 'create_filter',
+      id: '123',
+      message: 'Filter created successfully',
+    });
+    const fakeHttp = createHttp(response);
+
+    const cmd = new FilterCommand(fakeHttp);
+    const result = await cmd.create({
+      name: 'Test Filter 1',
+      type: 'host',
+      term: 'name=Test',
+    });
+    expect(fakeHttp.request).toHaveBeenCalledWith('post', {
+      data: {
+        cmd: 'create_filter',
+        name: 'Test Filter 1',
+        comment: '',
+        resource_type: 'host',
+        term: 'name=Test',
+      },
+    });
+    expect(result.data.id).toEqual('123');
+  });
+
+  test('should save an existing filter', async () => {
+    const response = createActionResultResponse({
+      action: 'save_filter',
+      id: '123',
+      message: 'Filter saved successfully',
+    });
+    const fakeHttp = createHttp(response);
+
+    const cmd = new FilterCommand(fakeHttp);
+    const result = await cmd.save({
+      id: '123',
+      name: 'Test Filter 1',
+      type: 'host',
+      term: 'name=Test',
+    });
+    expect(fakeHttp.request).toHaveBeenCalledWith('post', {
+      data: {
+        cmd: 'save_filter',
+        filter_id: '123',
+        name: 'Test Filter 1',
+        comment: '',
+        resource_type: 'host',
+        term: 'name=Test',
+      },
+    });
+    expect(result.data.id).toEqual('123');
+  });
+
+  test.each([
+    {entityType: 'host', resourceType: 'host'},
+    {entityType: 'os', resourceType: 'os'},
+    {entityType: 'report', resourceType: 'report'},
+    {entityType: 'result', resourceType: 'result'},
+    {entityType: 'task', resourceType: 'task'},
+  ])(
+    'should create $entityType filter with $resourceType',
+    async ({entityType, resourceType}) => {
+      const response = createActionResultResponse({
+        action: 'create_filter',
+        id: '123',
+        message: 'Filter created successfully',
+      });
+      const fakeHttp = createHttp(response);
+
+      const cmd = new FilterCommand(fakeHttp);
+      const result = await cmd.create({type: entityType});
+      expect(fakeHttp.request).toHaveBeenCalledWith('post', {
+        data: {cmd: 'create_filter', comment: '', resource_type: resourceType},
+      });
+      expect(result.data.id).toEqual('123');
+    },
+  );
+});
