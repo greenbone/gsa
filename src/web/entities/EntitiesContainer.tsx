d1024018306f04219847d6ab9ac5f019b70b4b07
Johannes Helmold <johannes.helmold@greenbone.net>
2026-03-03 14:54:58 +0100

    Fixed formatting.

--- src/web/entities/EntitiesContainer.tsx
+++ src/web/entities/EntitiesContainer.tsx
@@ -116,7 +116,8 @@ interface EntitiesContainerProps<TModel extends Model = Model> {
   onDownload: OnDownloadedFunc;
 }
 
-interface EntitiesContainerPropsWithHOCs<TModel extends Model = Model>
+interface EntitiesContainerPropsWithHOCs<TModel extends Model = Model,
+  >
   extends EntitiesContainerProps<TModel> {
   deleteEntity: (id: string) => Promise<void>;
   loadSettings: () => void;
