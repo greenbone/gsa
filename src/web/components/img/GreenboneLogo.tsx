7d747afeb23bd8191697ef681fdd2b1b85d3a731
Björn Ricks <bjoern.ricks@greenbone.net>
2026-03-26 13:01:56 +0100

    Change: Use TypeScript for GreenboneLogo
    Rename module to GreenboneLogo too.

--- src/web/components/img/GreenboneLogo.tsx
+++ src/web/components/img/GreenboneLogo.tsx
@@ -3,11 +3,10 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import React from 'react';
 import GbLogo from 'web/components/icon/svg/greenbone.svg?url';
 
-const GreenboneLogo = props => (
-  <img alt="OPENVAS" {...props} data-testid="greenbone-logo" src={GbLogo} />
+const GreenboneLogo = () => (
+  <img alt="Greenbone Logo" data-testid="greenbone-logo" src={GbLogo} />
 );
 
 export default GreenboneLogo;
