8260c297a274a76584e0fe3a6677bb64e2ef3132
Björn Ricks <bjoern.ricks@greenbone.net>
2026-03-26 13:09:56 +0100

    Change: Use TypeScript for OsIcon component

--- src/web/components/icon/OsIcon.tsx
+++ src/web/components/icon/OsIcon.tsx
@@ -3,34 +3,38 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import React from 'react';
 import {isDefined, isString} from 'gmp/utils/identity';
 import Img from 'web/components/img/Img';
 import Divider from 'web/components/layout/Divider';
 import Layout from 'web/components/layout/Layout';
 import useTranslation from 'web/hooks/useTranslation';
 import OperatingSystems from 'web/utils/Os';
-import PropTypes from 'web/utils/PropTypes';
+
+interface OsIconProps {
+  displayOsCpe?: boolean;
+  displayOsName?: boolean;
+  osTxt?: string;
+  osCpe?: string;
+}
 
 const OsIcon = ({
   displayOsCpe = true,
   displayOsName = false,
   osTxt,
   osCpe,
-  ...props
-}) => {
+}: OsIconProps) => {
   const [_] = useTranslation();
   const os = isString(osCpe) ? OperatingSystems.find(osCpe) : undefined;
 
-  let title;
-  let os_icon;
+  let title: string | undefined;
+  let osIcon: string | undefined;
 
   if (isString(osTxt) && osTxt.includes('[possible conflict]')) {
-    os_icon = 'os_conflict.svg';
+    osIcon = 'os_conflict.svg';
     if (displayOsCpe) {
       title = _('OS Conflict: {{best_os_txt}} ({{best_os_cpe}})', {
         best_os_txt: osTxt,
-        best_os_cpe: osCpe,
+        best_os_cpe: osCpe ?? '',
       });
     } else {
       title = _('OS Conflict: {{best_os_txt}}', {
@@ -38,15 +42,15 @@ const OsIcon = ({
       });
     }
   } else if (isDefined(os)) {
-    os_icon = os.icon;
+    osIcon = os.icon;
     title = os.title;
     if (displayOsCpe) {
       title += ' (' + osCpe + ')';
     }
   }
 
-  if (!isDefined(os_icon)) {
-    os_icon = 'os_unknown.svg';
+  if (!isDefined(osIcon)) {
+    osIcon = 'os_unknown.svg';
     if (osTxt) {
       title = osTxt;
     } else {
@@ -57,18 +61,11 @@ const OsIcon = ({
   return (
     <Layout>
       <Divider title={title}>
-        <Img {...props} src={os_icon} width="16px" />
+        <Img src={osIcon} width="16px" />
         {displayOsName && isDefined(os) && <span>{os.title}</span>}
       </Divider>
     </Layout>
   );
-};
-
-OsIcon.propTypes = {
-  displayOsCpe: PropTypes.bool,
-  displayOsName: PropTypes.bool,
-  osCpe: PropTypes.string,
-  osTxt: PropTypes.string,
 };
 
 export default OsIcon;
