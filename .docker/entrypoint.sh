087e396a1891baa909e932eaec84ed73daa2fee7
Björn Ricks <bjoern.ricks@greenbone.net>
2026-04-08 12:35:00 +0200

    Misc: Fix entrypoint script for running within busybox
    The busybox container hasn't a bash and gosu installed. Therefore use
    standard shell to execute the script and check if gosu is available.

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ -n "${MOUNT_PATH}" ]; then
   setup-gsa
 fi
 
-exec gosu gsad "$@"
+
+if command -v gosu > /dev/null 2>&1; then
+  exec gosu gsad "$@"
+else
+  exec "$@"
+fi
