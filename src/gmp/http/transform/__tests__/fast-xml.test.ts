e1e71765e7800745b40ac5469ac6458f3f94f539
Björn Ricks <bjoern.ricks@greenbone.net>
2026-04-28 10:35:06 +0200

    Change: Refactor standard response data
    Always return the envelope data in a response and don't but specific
    parts into the meta. At the end the meta should be obsolete and will be
    removed.

--- src/gmp/http/transform/__tests__/fast-xml.test.ts
+++ src/gmp/http/transform/__tests__/fast-xml.test.ts
@@ -6,28 +6,38 @@
 import {describe, test, expect} from '@gsa/testing';
 import {ResponseRejection} from 'gmp/http/rejection';
 import Response, {type Meta} from 'gmp/http/response';
-import transform, {type XmlResponseData} from 'gmp/http/transform/fast-xml';
+import transform, {
+  type Envelope,
+  type XmlResponseData,
+} from 'gmp/http/transform/fast-xml';
 
 const createEnvelopedXml = (xmlStr: string) =>
   `
 <envelope>
-  <version>123</version>
-  <backend_operation>1</backend_operation>
-  <vendor_version>FooBar</vendor_version>
+  <client_address>1.2.3.4</client_address>
   <i18n>en</i18n>
-  <time></time>
+  <session>1234567890</session>
   <timezone>UTC</timezone>
+  <token>abc123</token>
+  <version>1.2.3</version>
   ${xmlStr}
 </envelope>
 `;
 
-const envelopeMeta = {
-  backendOperation: 1,
+const envelope: Envelope = {
+  client_address: '1.2.3.4',
   i18n: 'en',
-  time: '',
+  session: 1234567890,
   timezone: 'UTC',
-  version: 123,
-  vendorVersion: 'FooBar',
+  token: 'abc123',
+  version: '1.2.3',
+};
+
+const createExpectedData = (data: Record<string, unknown>) => {
+  return {
+    ...envelope,
+    ...data,
+  };
 };
 
 describe('fastxml transform tests', () => {
@@ -35,13 +45,12 @@ describe('fastxml transform tests', () => {
     const xmlStr = createEnvelopedXml(
       '<foo>foo&quot;&lt;&gt;&amp;&apos;&#x2F;&#x5C;</foo>',
     );
-    const expected = {
+    const expected = createExpectedData({
       foo: 'foo"<>&\'/\\',
-    };
+    });
     const response = new Response<string, Meta>(xmlStr, {});
     const transformedResponse = transform.success(response);
     expect(transformedResponse.data).toEqual(expected);
-    expect(transformedResponse.meta).toEqual(envelopeMeta);
   });
 
   test('should transform array buffer xml response successfully', () => {
@@ -50,41 +59,35 @@ describe('fastxml transform tests', () => {
     const xmlBuffer = encoder.encode(xmlStr).buffer;
     const response = new Response<ArrayBuffer, Meta>(xmlBuffer, {});
     const transformedResponse = transform.success(response);
-    expect(transformedResponse.data).toEqual({foo: 'bar'});
-    expect(transformedResponse.meta).toEqual(envelopeMeta);
+    expect(transformedResponse.data).toEqual(createExpectedData({foo: 'bar'}));
   });
 
   test('should transform xml object response successfully', () => {
     const xmlObj = {
       envelope: {
-        version: 123,
-        backend_operation: 1,
-        vendor_version: 'FooBar',
-        i18n: 'en',
-        time: '',
-        timezone: 'UTC',
+        ...envelope,
         foo: 'bar',
       },
     };
-    const response = new Response<XmlResponseData, Meta>(xmlObj, {});
+    const response = new Response<XmlResponseData>(xmlObj);
     const transformedResponse = transform.success(response);
-    expect(transformedResponse.data).toEqual({foo: 'bar'});
-    expect(transformedResponse.meta).toEqual(envelopeMeta);
+    expect(transformedResponse.data).toEqual(createExpectedData({foo: 'bar'}));
   });
 
   test('should transform xml encoded element attribute successfully', () => {
     const xmlStr = createEnvelopedXml(
       '<foo bar="foo&quot;&lt;&gt;&amp;&apos;&#x2F;&#x5C;"></foo>',
     );
-    const response = new Response<string, Meta>(xmlStr, {});
+    const response = new Response<string>(xmlStr);
 
     const transformedResponse = transform.success(response);
-    expect(transformedResponse.data).toEqual({
-      foo: {
-        _bar: 'foo"<>&\'/\\',
-      },
-    });
-    expect(transformedResponse.meta).toEqual(envelopeMeta);
+    expect(transformedResponse.data).toEqual(
+      createExpectedData({
+        foo: {
+          _bar: 'foo"<>&\'/\\',
+        },
+      }),
+    );
   });
 
   test('should create a rejection on parser errors', () => {
@@ -122,37 +125,13 @@ describe('fastxml transform tests', () => {
     expect(transformedRejection.message).toBe('bar');
   });
 
-  test('should drop unknown envelope information', () => {
-    const xmlStr = `
-<envelope>
-  <version>1.0.1</version>
-  <backend_operation>0.01</backend_operation>
-  <vendor_version>1.1.0</vendor_version>
-  <i18n>en</i18n>
-  <time>Fri Sep 14 11:26:40 2018 CEST</time>
-  <timezone>Europe/Berlin</timezone>
-  <foo>foo</foo>
-  <bar>bar</bar>
-</envelope>
-`;
-    const response = new Response<string>(xmlStr);
-    const transformedResponse = transform.success(response);
-    expect(transformedResponse.meta).toEqual({
-      version: '1.0.1',
-      backendOperation: 0.01,
-      vendorVersion: '1.1.0',
-      i18n: 'en',
-      time: 'Fri Sep 14 11:26:40 2018 CEST',
-      timezone: 'Europe/Berlin',
-    });
-  });
-
   test('should transform elements with long values', () => {
     const longValue = 'a'.repeat(100000);
     const xmlStr = createEnvelopedXml(`<foo>${longValue}</foo>`);
     const response = new Response<string, Meta>(xmlStr, {});
     const transformedResponse = transform.success(response);
-    expect(transformedResponse.data).toEqual({foo: longValue});
-    expect(transformedResponse.meta).toEqual(envelopeMeta);
+    expect(transformedResponse.data).toEqual(
+      createExpectedData({foo: longValue}),
+    );
   });
 });
