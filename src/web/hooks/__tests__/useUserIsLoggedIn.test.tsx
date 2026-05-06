8e2d3fb09b84d1805b6883f39681b695b3cf94ce
Björn Ricks <bjoern.ricks@greenbone.net>
2026-05-06 13:22:47 +0200

    Change: Cleanup isLoggedIn status on Gmp object
    Remove isLoggedIn method from Gmp class because it is unused.

--- src/web/hooks/__tests__/useUserIsLoggedIn.test.tsx
+++ src/web/hooks/__tests__/useUserIsLoggedIn.test.tsx
@@ -5,16 +5,15 @@
 
 import {describe, test, expect} from '@gsa/testing';
 import {rendererWith} from 'web/testing';
+import {createSession} from 'gmp/testing';
 import useUserIsLoggedIn from 'web/hooks/useUserIsLoggedIn';
 
 describe('useUserIsLoggedIn tests', () => {
   test('should return the user logged-in state', () => {
+    const session = createSession();
     const gmp = {
       settings: {
-        session: {
-          isLoggedIn: () => true,
-          subscribeToChanges: () => () => {},
-        },
+        session,
       },
     };
     const {renderHook} = rendererWith({gmp});
@@ -22,5 +21,19 @@ describe('useUserIsLoggedIn tests', () => {
     const {result} = renderHook(() => useUserIsLoggedIn());
 
     expect(result.current).toBe(true);
+  });
+
+  test('should return the user logged-out state', () => {
+    const session = createSession({isLoggedIn: () => false});
+    const gmp = {
+      settings: {
+        session,
+      },
+    };
+    const {renderHook} = rendererWith({gmp});
+
+    const {result} = renderHook(() => useUserIsLoggedIn());
+
+    expect(result.current).toBe(false);
   });
 });
