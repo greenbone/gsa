4484060fbf59d47febd54dc9a2baeaabcd229669
daniele-mng <daniele.mangiagli@greenbone.net>
2026-05-18 16:57:05 +0200

    add: tanstack query hook for get_report_errors

---
+++ src/web/hooks/use-query/report-errors.ts
@@ -0,0 +1,34 @@
+/* SPDX-FileCopyrightText: 2026 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import type Filter from 'gmp/models/filter';
+import useGmp from 'web/hooks/useGmp';
+import useGetEntities from 'web/queries/useGetEntities';
+
+interface UseGetReportErrorsParams {
+  reportId: string;
+  filter?: Filter;
+}
+
+export const useGetReportErrors = ({
+  reportId,
+  filter = undefined,
+}: UseGetReportErrorsParams) => {
+  const gmp = useGmp();
+
+  return useGetEntities({
+    gmpMethod: ({filter: reportFilter}) =>
+      gmp.reporterrors.get({
+        report_id: reportId,
+        filter: reportFilter,
+      }),
+    queryId: `get_report_errors_${reportId}`,
+    filter,
+    enabled: Boolean(reportId),
+    keepPreviousData: true,
+  });
+};
+
+export default useGetReportErrors;
