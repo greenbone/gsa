c4b817dc9e5486e73d637b599076a9f777afb24a
Björn Ricks <bjoern.ricks@greenbone.net>
2026-07-29 14:07:47 +0200

    Change: Cleanup types for SaveDialog and remove State component usage
    The State component predates the existence of react hooks and is now a
    lot easier to implement with hooks. Also make the intention on how the
    SaveDialog works more obvious in that only the default initial values
    should be updated via the onValueChange function of the render props.
    The values are intended to be controlled outside of the SaveDialog.

--- src/web/components/dialog/SaveDialog.tsx
+++ src/web/components/dialog/SaveDialog.tsx
@@ -11,12 +11,15 @@ import DialogError from 'web/components/dialog/DialogError';
 import SaveDialogFooter from 'web/components/dialog/SaveDialogFooter';
 import ErrorBoundary from 'web/components/error/ErrorBoundary';
 import useTranslation from 'web/hooks/useTranslation';
-import State from 'web/utils/State';
 
-interface SaveDialogRenderProps<TValues> {
+interface ValueChangeFunction<TValues> {
+  (value: TValues[keyof TValues], name?: string): void; // name is optional for now to support compatibility with uncontrolled components
+}
+
+interface SaveDialogRenderProps<TValues, TDefaultValues> {
   currentStep: number;
-  values: TValues;
-  onValueChange: (value: TValues[keyof TValues], name?: string) => void; // name is optional for now to support compatibility with uncontrolled components
+  values: TValues & TDefaultValues;
+  onValueChange: ValueChangeFunction<TDefaultValues>;
 }
 
 interface SaveDialogProps<TValues, TDefaultValues> {
@@ -24,19 +27,40 @@ interface SaveDialogProps<TValues, TDefaultValues> {
   children:
     | React.ReactNode
     | ((
-        props: SaveDialogRenderProps<TValues & TDefaultValues>,
+        props: SaveDialogRenderProps<TValues, TDefaultValues>,
       ) => React.ReactNode);
-  defaultValues?: TDefaultValues; // default values for uncontrolled values
+  defaultValues?: TDefaultValues; // default values for uncontrolled values which are updated via the onValueChange function of the render props
   error?: string; // for errors controlled from parent (onErrorClose must be used if set)
   multiStep?: number; // number of steps for multi-step dialogs
   title: string;
-  values?: TValues; // should be used for controlled values
+  values?: TValues; // should be used for controlled values handles outside of the SaveDialog. They are expected to be updated via separate handler functions.
   width?: string; // width of the dialog, default is '40vw'
   onClose?: () => void; // function to call when dialog is closed
   onError?: (error: Error) => void; // function to call when an error occurs
   onErrorClose?: () => void; // function to call when error dialog is closed
   onSave?: (state: TValues & TDefaultValues) => Promise<void> | void; // function to call when save button is clicked
 }
+
+const useDialogState = <TInitialValues,>(
+  initialValues?: TInitialValues,
+): [TInitialValues, ValueChangeFunction<TInitialValues>] => {
+  const [state, setState] = useState<TInitialValues>(
+    initialValues ?? ({} as TInitialValues),
+  );
+
+  const onValueChange = (
+    value: TInitialValues[keyof TInitialValues],
+    name?: string,
+  ) => {
+    if (isDefined(name)) {
+      setState(prevState => ({
+        ...prevState,
+        [name]: value,
+      }));
+    }
+  };
+  return [state, onValueChange];
+};
 
 const SaveDialog = <TValues, TDefaultValues = {}>({
   buttonTitle,
@@ -100,50 +124,42 @@ const SaveDialog = <TValues, TDefaultValues = {}>({
     }
   };
 
+  const [state, onValueChange] = useDialogState<TDefaultValues>(defaultValues);
+  const childValues = {...state, ...values} as TValues & TDefaultValues;
   return (
-    <State {...defaultValues}>
-      {({state, onValueChange}) => {
-        const childValues = {...state, ...values};
-        return (
-          <Dialog
-            footer={
-              <SaveDialogFooter
-                buttonTitle={buttonTitle}
-                currentStep={currentStep}
-                handleSaveClick={() => handleSaveClick(childValues)}
-                isLoading={isLoading}
-                multiStep={multiStep}
-                nextDisabled={nextDisabled}
-                prevDisabled={prevDisabled}
-                setCurrentStep={setCurrentStep}
-                onClose={onClose}
-              />
-            }
-            title={title}
-            width={width}
-            onClose={onClose}
-          >
-            <DialogContent>
-              {stateError && (
-                <DialogError
-                  error={stateError}
-                  onCloseClick={handleErrorClose}
-                />
-              )}
-              <ErrorBoundary message={_('An error occurred in this dialog.')}>
-                {isFunction(children)
-                  ? children({
-                      currentStep,
-                      values: childValues,
-                      onValueChange,
-                    })
-                  : children}
-              </ErrorBoundary>
-            </DialogContent>
-          </Dialog>
-        );
-      }}
-    </State>
+    <Dialog
+      footer={
+        <SaveDialogFooter
+          buttonTitle={buttonTitle}
+          currentStep={currentStep}
+          handleSaveClick={() => handleSaveClick(childValues)}
+          isLoading={isLoading}
+          multiStep={multiStep}
+          nextDisabled={nextDisabled}
+          prevDisabled={prevDisabled}
+          setCurrentStep={setCurrentStep}
+          onClose={onClose}
+        />
+      }
+      title={title}
+      width={width}
+      onClose={onClose}
+    >
+      <DialogContent>
+        {stateError && (
+          <DialogError error={stateError} onCloseClick={handleErrorClose} />
+        )}
+        <ErrorBoundary message={_('An error occurred in this dialog.')}>
+          {isFunction(children)
+            ? children({
+                currentStep,
+                values: childValues,
+                onValueChange,
+              })
+            : children}
+        </ErrorBoundary>
+      </DialogContent>
+    </Dialog>
   );
 };
 
