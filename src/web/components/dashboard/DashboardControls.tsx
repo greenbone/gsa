d7c39d5ad50a85b4bf8e0881496431c06f13e2e5
Björn Ricks <bjoern.ricks@greenbone.net>
2026-08-04 12:41:05 +0200

    Change: Refactor DisplayComponent to a function component
    Function components are easier to read and to understand.

--- src/web/components/dashboard/DashboardControls.tsx
+++ src/web/components/dashboard/DashboardControls.tsx
@@ -3,10 +3,9 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import React from 'react';
+import {useState} from 'react';
 import {connect} from 'react-redux';
 import type Gmp from 'gmp/gmp';
-import {type TranslateFunc} from 'gmp/locale';
 import {isDefined} from 'gmp/utils/identity';
 import {
   type DisplayRegistry,
@@ -23,167 +22,128 @@ import FormGroup from 'web/components/form/FormGroup';
 import Select from 'web/components/form/Select';
 import {NewIcon, ResetIcon} from 'web/components/icon';
 import IconDivider from 'web/components/layout/IconDivider';
-import type {I18n} from 'web/hooks/useTranslation';
+import useTranslation from 'web/hooks/useTranslation';
 import {
   resetSettings,
   saveSettings,
 } from 'web/store/dashboard/settings/actions';
 import getDashboardSettings from 'web/store/dashboard/settings/selectors';
 import compose from 'web/utils/compose';
-import PropTypes from 'web/utils/prop-types';
 import withGmp from 'web/utils/withGmp';
-import withTranslation from 'web/utils/withTranslation';
 
-export type OnNewDisplay = (
+export type OnNewDisplayFunc = (
   settings: DashboardSettings | undefined,
   dashboardId: string,
   displayId: string,
 ) => void;
 
 interface DashboardControlsProps {
-  _: TranslateFunc;
   canAdd: boolean;
   dashboardId: string;
   displayRegistry?: DisplayRegistry;
   displayIds?: string[];
-  i18n: I18n;
   settings?: DashboardSettings;
-  onNewDisplay: OnNewDisplay;
+  onNewDisplay: OnNewDisplayFunc;
   onResetClick: (dashboardId: string) => void;
-}
-
-interface DashboardControlsState {
-  showNewDialog: boolean;
 }
 
 interface NewDisplayValues {
   displayId: string;
 }
 
-export class DashboardControls extends React.Component<
-  DashboardControlsProps,
-  DashboardControlsState
-> {
-  static propTypes = {
-    canAdd: PropTypes.bool.isRequired,
-    dashboardId: PropTypes.id.isRequired,
-    displayRegistry: PropTypes.object,
-    displayIds: PropTypes.arrayOf(PropTypes.string),
-    i18n: PropTypes.object.isRequired,
-    settings: PropTypes.object,
-    onNewDisplay: PropTypes.func.isRequired,
-    onResetClick: PropTypes.func.isRequired,
-    _: PropTypes.func.isRequired,
-  };
+export const DashboardControls = ({
+  canAdd,
+  dashboardId,
+  displayIds = [],
+  displayRegistry,
+  settings,
+  onNewDisplay,
+  onResetClick,
+}: DashboardControlsProps) => {
+  const [_] = useTranslation();
+  const [showNewDialog, setShowNewDialog] = useState(false);
 
-  constructor(props: DashboardControlsProps) {
-    super(props);
-
-    this.state = {
-      showNewDialog: false,
-    };
-
-    this.handleNewClick = this.handleNewClick.bind(this);
-    this.handleResetClick = this.handleResetClick.bind(this);
-    this.handleNewDialogClose = this.handleNewDialogClose.bind(this);
-    this.handleNewDisplay = this.handleNewDisplay.bind(this);
-  }
-
-  handleResetClick() {
-    const {dashboardId, onResetClick} = this.props;
-
+  const handleResetClick = () => {
     if (isDefined(onResetClick)) {
       onResetClick(dashboardId);
     }
-  }
+  };
 
-  handleNewClick() {
-    this.setState({showNewDialog: true});
-  }
+  const handleNewClick = () => {
+    setShowNewDialog(true);
+  };
 
-  closeNewDialog() {
-    this.setState({showNewDialog: false});
-  }
+  const closeNewDialog = () => {
+    setShowNewDialog(false);
+  };
 
-  handleNewDialogClose() {
-    this.closeNewDialog();
-  }
+  const handleNewDialogClose = () => {
+    closeNewDialog();
+  };
 
-  handleNewDisplay({displayId}: NewDisplayValues) {
-    const {dashboardId, settings, onNewDisplay} = this.props;
-
+  const handleNewDisplay = ({displayId}: NewDisplayValues) => {
     if (isDefined(onNewDisplay)) {
-      this.closeNewDialog();
+      closeNewDialog();
 
       onNewDisplay(settings, dashboardId, displayId);
     }
-  }
+  };
 
-  render() {
-    const {_} = this.props;
+  const displays = displayIds
+    .map(displayId => getDisplay(displayId, displayRegistry))
+    .filter(isDefined);
+  const firstDisplay = displays[0];
+  const displayItems = displays.map(display => ({
+    label: `${display.title}`,
+    value: display.component.displayId,
+  }));
 
-    const {showNewDialog} = this.state;
-    const {canAdd, displayIds = [], displayRegistry} = this.props;
-
-    const displays = displayIds
-      .map(displayId => getDisplay(displayId, displayRegistry))
-      .filter(isDefined);
-    const firstDisplay = displays[0];
-    const displayItems = displays.map(display => ({
-      label: `${display.title}`,
-      value: display.component.displayId,
-    }));
-    return (
-      <>
-        <IconDivider>
-          <NewIcon
-            active={canAdd}
-            data-testid="add-dashboard-display"
-            title={
-              canAdd
-                ? _('Add new Dashboard Display')
-                : _('Dashboard limit reached')
-            }
-            onClick={canAdd ? this.handleNewClick : undefined}
-          />
-          <ResetIcon
-            data-testid="reset-dashboard"
-            title={_('Reset to Defaults')}
-            onClick={this.handleResetClick}
-          />
-        </IconDivider>
-        {showNewDialog && isDefined(firstDisplay) && (
-          <SaveDialog<{}, NewDisplayValues>
-            buttonTitle={_('Add')}
-            defaultValues={{
-              displayId: firstDisplay.component.displayId,
-            }}
-            title={_('Add new Dashboard Display')}
-            width="660px"
-            onClose={this.handleNewDialogClose}
-            onSave={this.handleNewDisplay}
-          >
-            {({values, onValueChange}) => (
-              <FormGroup title={_('Choose Display')}>
-                <Select
-                  items={displayItems}
-                  name="displayId"
-                  value={values.displayId}
-                  width="auto"
-                  onChange={onValueChange}
-                />
-              </FormGroup>
-            )}
-          </SaveDialog>
-        )}
-      </>
-    );
-  }
-}
-
-export const TranslatedDashboardControls = withTranslation(
-  DashboardControls as React.ComponentType<DashboardControlsProps>,
-);
+  return (
+    <>
+      <IconDivider>
+        <NewIcon
+          active={canAdd}
+          data-testid="add-dashboard-display"
+          title={
+            canAdd
+              ? _('Add new Dashboard Display')
+              : _('Dashboard limit reached')
+          }
+          onClick={canAdd ? handleNewClick : undefined}
+        />
+        <ResetIcon
+          data-testid="reset-dashboard"
+          title={_('Reset to Defaults')}
+          onClick={handleResetClick}
+        />
+      </IconDivider>
+      {showNewDialog && isDefined(firstDisplay) && (
+        <SaveDialog<{}, NewDisplayValues>
+          buttonTitle={_('Add')}
+          defaultValues={{
+            displayId: firstDisplay.component.displayId,
+          }}
+          title={_('Add new Dashboard Display')}
+          width="660px"
+          onClose={handleNewDialogClose}
+          onSave={handleNewDisplay}
+        >
+          {({values, onValueChange}) => (
+            <FormGroup title={_('Choose Display')}>
+              <Select
+                items={displayItems}
+                name="displayId"
+                value={values.displayId}
+                width="auto"
+                onChange={onValueChange}
+              />
+            </FormGroup>
+          )}
+        </SaveDialog>
+      )}
+    </>
+  );
+};
 
 const mapStateToProps = (
   rootState: unknown,
@@ -226,7 +186,6 @@ const mapDispatchToProps = (
 });
 
 export default compose(
-  withTranslation,
   withGmp,
   connect(mapStateToProps, mapDispatchToProps as never),
 )(DashboardControls as never);
