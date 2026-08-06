43b139ba2782e5010dc49c04e8cd86303a7143b6
Björn Ricks <bjoern.ricks@greenbone.net>
2026-08-06 14:12:10 +0200

    Change: Refactor FilterSelection to a function component
    Function components are easier to read and to understand.

--- src/web/components/dashboard/display/FilterSelection.tsx
+++ src/web/components/dashboard/display/FilterSelection.tsx
@@ -3,169 +3,108 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import React from 'react';
-import {connect} from 'react-redux';
-import type Gmp from 'gmp/gmp';
+import {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import type Filter from 'gmp/models/filter';
-import {hasValue, isDefined} from 'gmp/utils/identity';
+import type FilterType from 'gmp/models/filter/filter-type';
+import {isDefined} from 'gmp/utils/identity';
 import SaveDialog from 'web/components/dialog/SaveDialog';
 import FormGroup from 'web/components/form/FormGroup';
 import Select from 'web/components/form/Select';
+import useGmp from 'web/hooks/useGmp';
+import useTranslation from 'web/hooks/useTranslation';
 import {loadEntities, selector} from 'web/store/entities/filters';
-import compose from 'web/utils/compose';
 import {
   type RenderSelectItemProps,
   renderSelectItems,
   UNSET_VALUE,
 } from 'web/utils/Render';
-import withGmp from 'web/utils/withGmp';
-import withTranslation from 'web/utils/withTranslation';
 
 interface FilterSelectionRenderProps {
   filter?: Filter;
   selectFilter: () => void;
 }
 
-interface StateProps {
-  filters?: Filter[];
-}
-
-interface DispatchProps {
-  loadFilters: () => void;
-}
-
-interface TranslationProps {
-  _: (text: string) => string;
-}
-
-interface FilterSelectionProps
-  extends StateProps, DispatchProps, TranslationProps {
+interface FilterSelectionProps {
   children: (props: FilterSelectionRenderProps) => React.ReactNode;
   filterId?: string;
-  filtersFilter: Filter;
-  onFilterIdChanged?: (filterId: string | undefined) => void;
+  filtersFilter: FilterType;
+  onFilterIdChanged?: (filterId?: string) => void;
 }
 
-interface FilterSelectionState {
-  showDialog: boolean;
-}
+const FilterSelection = ({
+  children,
+  filterId,
+  filtersFilter,
+  onFilterIdChanged,
+}: FilterSelectionProps) => {
+  const [_] = useTranslation();
+  const gmp = useGmp();
+  const dispatch = useDispatch();
+  const [showDialog, setShowDialog] = useState(false);
 
-class FilterSelection extends React.Component<
-  FilterSelectionProps,
-  FilterSelectionState
-> {
-  constructor(props: FilterSelectionProps) {
-    super(props);
+  const filters = useSelector<unknown, Filter[]>(state => {
+    if (!isDefined(filtersFilter)) {
+      return [];
+    }
+    return selector(state).getEntities(filtersFilter) ?? [];
+  });
 
-    this.state = {
-      showDialog: false,
-    };
+  useEffect(() => {
+    // @ts-expect-error redux thunk action
+    dispatch(loadEntities(gmp)(filtersFilter));
+  }, [dispatch, filtersFilter, gmp]);
 
-    this.handleCloseDialog = this.handleCloseDialog.bind(this);
-    this.handleOpenDialog = this.handleOpenDialog.bind(this);
-    this.handleSaveDialog = this.handleSaveDialog.bind(this);
-  }
+  const closeDialog = () => {
+    setShowDialog(false);
+  };
 
-  componentDidMount() {
-    this.props.loadFilters();
-  }
-
-  closeDialog() {
-    this.setState({showDialog: false});
-  }
-
-  handleCloseDialog() {
-    this.closeDialog();
-  }
-
-  handleOpenDialog() {
-    this.setState({showDialog: true});
-  }
-
-  handleSaveDialog({filterId = UNSET_VALUE}: {filterId?: string}) {
-    const {onFilterIdChanged} = this.props;
-
-    this.closeDialog();
+  const handleSaveDialog = ({filterId = UNSET_VALUE}: {filterId?: string}) => {
+    closeDialog();
 
     if (isDefined(onFilterIdChanged)) {
       onFilterIdChanged(filterId === UNSET_VALUE ? undefined : filterId);
     }
-  }
-
-  render() {
-    const {_} = this.props;
-
-    const {children, filters = [], filterId} = this.props;
-    const {showDialog} = this.state;
-    const filter = isDefined(filterId)
-      ? filters.find(f => f.id === filterId)
-      : undefined;
-
-    return (
-      <>
-        {children({
-          filter,
-          selectFilter: this.handleOpenDialog,
-        })}
-        {showDialog && (
-          <SaveDialog
-            buttonTitle={_('Select')}
-            defaultValues={{
-              filterId: isDefined(filterId) ? filterId : UNSET_VALUE,
-            }}
-            title={_('Select Filter')}
-            width="500px"
-            onClose={this.handleCloseDialog}
-            onSave={this.handleSaveDialog}
-          >
-            {({values, onValueChange}) => (
-              <FormGroup title={_('Filter')}>
-                <Select
-                  items={renderSelectItems(
-                    filters as RenderSelectItemProps[],
-                    UNSET_VALUE,
-                  )}
-                  name="filterId"
-                  value={values.filterId}
-                  onChange={onValueChange}
-                />
-              </FormGroup>
-            )}
-          </SaveDialog>
-        )}
-      </>
-    );
-  }
-}
-
-const mapStateToProps = (
-  state: unknown,
-  {filtersFilter}: {filtersFilter: Filter},
-): StateProps => {
-  if (!isDefined(filtersFilter)) {
-    return {
-      filters: [],
-    };
-  }
-
-  const filterSelector = selector(state);
-  const filters: Filter[] = filterSelector.getEntities(filtersFilter);
-
-  return {
-    filters: hasValue(filters) ? filters : [],
   };
+
+  const filter = isDefined(filterId)
+    ? filters.find(f => f.id === filterId)
+    : undefined;
+
+  return (
+    <>
+      {children({
+        filter,
+        selectFilter: () => setShowDialog(true),
+      })}
+      {showDialog && (
+        <SaveDialog
+          buttonTitle={_('Select')}
+          defaultValues={{
+            filterId: isDefined(filterId) ? filterId : UNSET_VALUE,
+          }}
+          title={_('Select Filter')}
+          width="500px"
+          onClose={closeDialog}
+          onSave={handleSaveDialog}
+        >
+          {({values, onValueChange}) => (
+            <FormGroup title={_('Filter')}>
+              <Select
+                items={renderSelectItems(
+                  filters as RenderSelectItemProps[],
+                  UNSET_VALUE,
+                )}
+                name="filterId"
+                value={values.filterId}
+                onChange={onValueChange}
+              />
+            </FormGroup>
+          )}
+        </SaveDialog>
+      )}
+    </>
+  );
 };
 
-const mapDispatchToProps = (
-  dispatch: unknown,
-  {gmp, filtersFilter}: {gmp: Gmp; filtersFilter: Filter},
-): DispatchProps => ({
-  // @ts-expect-error dispatch is a redux thunk
-  loadFilters: () => dispatch(loadEntities(gmp)(filtersFilter)),
-});
-
-export default compose(
-  withTranslation,
-  withGmp,
-  connect(mapStateToProps, mapDispatchToProps),
-)(FilterSelection);
+export default FilterSelection;
