056b56ba49a762b83a4339467f67c7c1d23edae9
Björn Ricks <bjoern.ricks@greenbone.net>
2026-08-12 14:23:45 +0200

    Change: Use TypeScript for dashboard DataTableDisplay
    Also improve and extends the tests for the component.

---
+++ src/web/components/dashboard/display/DataTableDisplay.tsx
@@ -0,0 +1,73 @@
+/* SPDX-FileCopyrightText: 2024 Greenbone AG
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import {isFunction} from 'gmp/utils/identity';
+import DataDisplay, {
+  type State,
+  type DataDisplayProps,
+} from 'web/components/dashboard/display/DataDisplay';
+import DataTable, {
+  type DataTableProps,
+} from 'web/components/dashboard/display/DataTable';
+
+type DataTableDisplayRenderProps<TTransformedData> =
+  DataTableProps<TTransformedData>;
+
+interface DataTableDisplayProps<
+  TData,
+  TState extends State = State,
+  TTransformedData = TData,
+  TTransformProps = Record<string, unknown>,
+> extends Omit<
+  DataDisplayProps<TData, TState, TTransformedData, TTransformProps>,
+  'children'
+> {
+  dataTitles: string[];
+  children?:
+    | ((
+        props: DataTableDisplayRenderProps<TTransformedData>,
+      ) => React.ReactNode)
+    | unknown;
+}
+
+const DataTableDisplay = <
+  TData,
+  TState extends State = State,
+  TTransformedData = TData,
+  TTransformProps = Record<string, unknown>,
+>({
+  children,
+  dataRow,
+  dataTitles,
+  ...props
+}: DataTableDisplayProps<TData, TState, TTransformedData, TTransformProps>) => (
+  <DataDisplay<
+    TData,
+    DataDisplayProps<TData, TState, TTransformedData, TTransformProps>,
+    TState,
+    TTransformedData,
+    TTransformProps
+  >
+    {...props}
+    dataRow={dataRow}
+    dataTitles={dataTitles}
+    showSvgDownload={false}
+    showToggleLegend={false}
+  >
+    {({data}) =>
+      isFunction(children) ? (
+        children({
+          data,
+          dataRow,
+          dataTitles,
+        })
+      ) : (
+        <DataTable data={data} dataRow={dataRow} dataTitles={dataTitles} />
+      )
+    }
+  </DataDisplay>
+);
+
+export default DataTableDisplay;
