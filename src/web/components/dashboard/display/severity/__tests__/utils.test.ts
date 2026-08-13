8fe99048bf12fd4ba8db0845d784c9471efdad03
Björn Ricks <bjoern.ricks@greenbone.net>
2026-08-13 12:47:05 +0200

    test: Convert dashboard SeverityDisplay untils tests to TypeScript

--- src/web/components/dashboard/display/severity/__tests__/utils.test.ts
+++ src/web/components/dashboard/display/severity/__tests__/utils.test.ts
@@ -10,8 +10,8 @@ import {CRITICAL_VALUE, LOG_VALUE} from 'web/utils/severity';
 
 describe('filterValueToFilterTerms', () => {
   test('should return two filter terms when start and end are provided', () => {
-    const start = LOG_VALUE;
-    const end = CRITICAL_VALUE;
+    const start = String(LOG_VALUE);
+    const end = String(CRITICAL_VALUE);
     const result = filterValueToFilterTerms({start, end});
     expect(result).toHaveLength(2);
     expect(result[0]).toEqual(FilterTerm.fromString(`severity>${start}`));
@@ -19,16 +19,33 @@ describe('filterValueToFilterTerms', () => {
   });
 
   test('should return equal filter term when log is requested', () => {
-    const start = LOG_VALUE;
+    const start = String(LOG_VALUE);
     const result = filterValueToFilterTerms({start});
     expect(result).toHaveLength(1);
     expect(result[0]).toEqual(FilterTerm.fromString(`severity=${start}`));
   });
 
+  test('should return equal filter term for negative severity values', () => {
+    const start = '-1';
+    const result = filterValueToFilterTerms({start});
+    expect(result).toEqual([FilterTerm.fromString(`severity=${start}`)]);
+  });
+
+  test('should return equal filter term for invalid start values', () => {
+    const start = 'invalid';
+    const result = filterValueToFilterTerms({start});
+    expect(result).toEqual([FilterTerm.fromString(`severity=${start}`)]);
+  });
+
   test('should return a single greater filter term when critical is requested', () => {
-    const start = CRITICAL_VALUE;
+    const start = String(CRITICAL_VALUE);
     const result = filterValueToFilterTerms({start});
     expect(result).toHaveLength(1);
     expect(result[0]).toEqual(FilterTerm.fromString(`severity>${start}`));
+  });
+
+  test('should return an empty array when start is undefined', () => {
+    const result = filterValueToFilterTerms({});
+    expect(result).toEqual([]);
   });
 });
