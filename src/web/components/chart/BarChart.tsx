1fa21ac2d05458e57953ff274ac803a220adcb8f
Björn Ricks <bjoern.ricks@greenbone.net>
2026-08-14 14:12:45 +0200

    Change: Convert BarChar into a function component
    Function components are easier to understand and to maintain.

--- src/web/components/chart/BarChart.tsx
+++ src/web/components/chart/BarChart.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {scaleBand, scaleLinear} from 'd3-scale';
 import styled from 'styled-components';
 import {type ToString} from 'gmp/types';
@@ -17,7 +17,6 @@ import Legend, {
 import Svg from 'web/components/chart/base/Svg';
 import ToolTip from 'web/components/chart/base/Tooltip';
 import {MENU_PLACEHOLDER_WIDTH} from 'web/components/chart/utils/Constants';
-import {shouldUpdate} from 'web/components/chart/utils/Update';
 import Layout from 'web/components/layout/Layout';
 
 export interface BarChartDataPoint extends LegendData {
@@ -36,10 +35,6 @@ export interface BarChartProps<TData extends BarChartDataPoint> {
   yLabel?: string;
   onDataClick?: (dataPoint: TData) => void;
   onLegendItemClick?: (dataPoint: TData) => void;
-}
-
-interface BarChartState {
-  width: number;
 }
 
 const StyledLayout = styled(Layout)`
@@ -69,183 +64,139 @@ const tickFormat = (val: number | string | Date) => {
   return valStr;
 };
 
-class BarChart<TData extends BarChartDataPoint> extends React.Component<
-  BarChartProps<TData>,
-  BarChartState
-> {
-  legendRef: LegendRef;
+const getWidth = (width: number, legend: HTMLElement | null): number => {
+  width -= MENU_PLACEHOLDER_WIDTH;
 
-  constructor(props: BarChartProps<TData>) {
-    super(props);
-
-    this.legendRef = React.createRef();
-
-    this.state = {
-      width: this.getWidth(),
-    };
+  if (legend !== null) {
+    width -= legend.getBoundingClientRect().width + LEGEND_MARGIN;
   }
 
-  shouldComponentUpdate(
-    nextProps: BarChartProps<TData>,
-    nextState: BarChartState,
-  ) {
-    return (
-      shouldUpdate(nextProps, this.props) ||
-      nextState.width !== this.state.width
+  return Math.max(width, MIN_WIDTH);
+};
+
+const BarChart = <TData extends BarChartDataPoint>({
+  data = [],
+  showLegend = true,
+  height,
+  xLabel = '',
+  yLabel = '',
+  horizontal = false,
+  svgRef,
+  onDataClick,
+  onLegendItemClick,
+  width,
+}: BarChartProps<TData>) => {
+  const legendRef: LegendRef = useRef(null);
+  const [chartWidth, setChartWidth] = useState(() => getWidth(width, null));
+
+  useEffect(() => {
+    const nextWidth = getWidth(width, legendRef.current);
+    setChartWidth(currentWidth =>
+      currentWidth === nextWidth ? currentWidth : nextWidth,
     );
+  }, [data, showLegend, width]);
+
+  const xValues = data.map(d => String(d.x));
+  const yValues = data.map(d => d.y);
+  const yMax = Math.max(...yValues);
+
+  const maxLabelLength = Math.max(
+    ...xValues.map(val => String(val).length),
+    MAX_LABEL_LENGTH,
+  );
+
+  // adjust left margin for label length on horizontal bars
+  // 4px for each letter is just a randomly chosen value
+  const marginLeft = horizontal
+    ? margin.left + Math.min(MAX_LABEL_LENGTH, maxLabelLength) * 4
+    : margin.left;
+
+  const maxWidth = chartWidth - marginLeft - margin.right;
+  let maxHeight = height - margin.top - margin.bottom;
+
+  if (isDefined(xLabel)) {
+    // adjust height for x axis label
+    maxHeight = maxHeight - LABEL_HEIGHT;
   }
 
-  componentDidMount() {
-    this.update();
-  }
+  const xScale = scaleBand<string>()
+    .rangeRound(horizontal ? [maxHeight, 0] : [0, maxWidth])
+    .domain(xValues)
+    .padding(0.125);
 
-  componentDidUpdate() {
-    this.update();
-  }
+  const yScale = scaleLinear()
+    .range(horizontal ? [0, maxWidth] : [maxHeight, 0])
+    .domain([0, Math.abs(yMax) > 0 ? yMax : 10])
 
-  update() {
-    const width = this.getWidth();
-    if (width !== this.state.width) {
-      this.setState({width});
-    }
-  }
-
-  getWidth() {
-    let {width} = this.props;
-    const {current: legend} = this.legendRef;
-
-    width = width - MENU_PLACEHOLDER_WIDTH;
-
-    if (legend !== null) {
-      const {width: legendWidth} = legend.getBoundingClientRect();
-      width = width - legendWidth - LEGEND_MARGIN;
-    }
-
-    if (width < MIN_WIDTH) {
-      width = MIN_WIDTH;
-    }
-
-    return width;
-  }
-
-  render() {
-    const {
-      data = [],
-      showLegend = true,
-      height,
-      xLabel = '',
-      yLabel = '',
-      horizontal = false,
-      svgRef,
-      onDataClick,
-      onLegendItemClick,
-    } = this.props;
-    const {width} = this.state;
-
-    const xValues = data.map(d => String(d.x));
-    const yValues = data.map(d => d.y);
-    const yMax = Math.max(...yValues);
-
-    const maxLabelLength = Math.max(
-      ...xValues.map(val => String(val).length),
-      MAX_LABEL_LENGTH,
-    );
-
-    // adjust left margin for label length on horizontal bars
-    // 4px for each letter is just a randomly chosen value
-    const marginLeft = horizontal
-      ? margin.left + Math.min(MAX_LABEL_LENGTH, maxLabelLength) * 4
-      : margin.left;
-
-    const maxWidth = width - marginLeft - margin.right;
-    let maxHeight = height - margin.top - margin.bottom;
-
-    if (isDefined(xLabel)) {
-      // adjust height for x axis label
-      maxHeight = maxHeight - LABEL_HEIGHT;
-    }
-
-    const xScale = scaleBand<string>()
-      .rangeRound(horizontal ? [maxHeight, 0] : [0, maxWidth])
-      .domain(xValues)
-      .padding(0.125);
-
-    const yScale = scaleLinear()
-      .range(horizontal ? [0, maxWidth] : [maxHeight, 0])
-      .domain([0, Math.abs(yMax) > 0 ? yMax : 10])
-
-      /*
+    /*
         nice seems to round first and last value.
         see https://github.com/d3/d3-scale/blob/master/README.md#continuous_nice
         the old version did call nice(10) which isn't possible with vx at the moment.
       */
-      .nice();
+    .nice();
 
-    const tickValues = horizontal ? yScale.ticks(10) : xScale.domain();
-    const numTicks = tickValues.length;
+  const tickValues = horizontal ? yScale.ticks(10) : xScale.domain();
+  const numTicks = tickValues.length;
 
-    const hideTickLabels = maxWidth / numTicks < MIN_TICK_WIDTH;
-    return (
-      <StyledLayout align={['start', 'start']}>
-        <Svg ref={svgRef} height={height} width={width}>
-          <Group left={marginLeft} top={margin.top}>
-            <Axis
-              dataTestId="bar-chart-y-axis"
-              label={String(yLabel)}
-              left={0}
-              numTicks={10}
-              orientation="left"
-              scale={horizontal ? xScale : yScale}
-              tickFormat={horizontal ? tickFormat : undefined}
-              top={0}
-            />
-            <Axis
-              dataTestId="bar-chart-x-axis"
-              hideTickLabels={hideTickLabels}
-              label={String(xLabel)}
-              orientation="bottom"
-              scale={horizontal ? yScale : xScale}
-              tickValues={tickValues}
-              top={maxHeight}
-            />
-            {data.map(d => (
-              <ToolTip key={horizontal ? d.y : String(d.x)} content={d.toolTip}>
-                {({targetRef, hide, show}) => (
-                  <Group
-                    onClick={
-                      isDefined(onDataClick) ? () => onDataClick(d) : undefined
-                    }
-                  >
-                    <rect
-                      ref={targetRef as React.Ref<SVGRectElement>}
-                      fill={String(d.color)}
-                      height={
-                        horizontal
-                          ? xScale.bandwidth()
-                          : maxHeight - yScale(d.y)
-                      }
-                      width={horizontal ? yScale(d.y) : xScale.bandwidth()}
-                      x={horizontal ? 1 : xScale(String(d.x))}
-                      y={horizontal ? xScale(String(d.x)) : yScale(d.y)}
-                      onMouseEnter={show}
-                      onMouseLeave={hide}
-                    />
-                  </Group>
-                )}
-              </ToolTip>
-            ))}
-          </Group>
-        </Svg>
-        {showLegend && data.length > 0 && (
-          <Legend<TData>
-            data={data}
-            legendRef={this.legendRef}
-            onItemClick={onLegendItemClick}
+  const hideTickLabels = maxWidth / numTicks < MIN_TICK_WIDTH;
+  return (
+    <StyledLayout align={['start', 'start']}>
+      <Svg ref={svgRef} height={height} width={chartWidth}>
+        <Group left={marginLeft} top={margin.top}>
+          <Axis
+            dataTestId="bar-chart-y-axis"
+            label={String(yLabel)}
+            left={0}
+            numTicks={10}
+            orientation="left"
+            scale={horizontal ? xScale : yScale}
+            tickFormat={horizontal ? tickFormat : undefined}
+            top={0}
           />
-        )}
-      </StyledLayout>
-    );
-  }
-}
+          <Axis
+            dataTestId="bar-chart-x-axis"
+            hideTickLabels={hideTickLabels}
+            label={String(xLabel)}
+            orientation="bottom"
+            scale={horizontal ? yScale : xScale}
+            tickValues={tickValues}
+            top={maxHeight}
+          />
+          {data.map(d => (
+            <ToolTip key={horizontal ? d.y : String(d.x)} content={d.toolTip}>
+              {({targetRef, hide, show}) => (
+                <Group
+                  onClick={
+                    isDefined(onDataClick) ? () => onDataClick(d) : undefined
+                  }
+                >
+                  <rect
+                    ref={targetRef as React.Ref<SVGRectElement>}
+                    fill={String(d.color)}
+                    height={
+                      horizontal ? xScale.bandwidth() : maxHeight - yScale(d.y)
+                    }
+                    width={horizontal ? yScale(d.y) : xScale.bandwidth()}
+                    x={horizontal ? 1 : xScale(String(d.x))}
+                    y={horizontal ? xScale(String(d.x)) : yScale(d.y)}
+                    onMouseEnter={show}
+                    onMouseLeave={hide}
+                  />
+                </Group>
+              )}
+            </ToolTip>
+          ))}
+        </Group>
+      </Svg>
+      {showLegend && data.length > 0 && (
+        <Legend<TData>
+          data={data}
+          legendRef={legendRef}
+          onItemClick={onLegendItemClick}
+        />
+      )}
+    </StyledLayout>
+  );
+};
 
 export default BarChart;
