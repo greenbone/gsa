82393cf65560c392aba82b1a1ede3588d1633f4e
Björn Ricks <bjoern.ricks@greenbone.net>
2026-09-18 13:50:41 +0200

    Change: Make data for StatusDisplay generic
    Allow to pass data with additional properties to StatusDisplay.

--- src/web/components/dashboard/display/status/StatusDisplay.tsx
+++ src/web/components/dashboard/display/status/StatusDisplay.tsx
@@ -8,6 +8,7 @@ import {type FilterType} from 'gmp/models/filter';
 import FilterTerm from 'gmp/models/filter/filter-term';
 import QueryFilter from 'gmp/models/filter/query-filter';
 import {isDefined} from 'gmp/utils/identity';
+import {isEmpty} from 'gmp/utils/string';
 import DonutChart, {type DonutChartData} from 'web/components/chart/DonutChart';
 import DataDisplay, {
   type DataDisplayProps,
@@ -18,52 +19,60 @@ interface StatusData extends DonutChartData {
   filterValue?: string;
 }
 
-interface StatusDisplayProps<TData = unknown> extends DataDisplayProps<
+interface StatusDisplayProps<
   TData,
-  StatusData[]
-> {
+  TTransformData extends Array<StatusData> = StatusData[],
+> extends DataDisplayProps<TData, TTransformData> {
   filter?: FilterType;
   filterTerm?: string;
   onFilterChanged?: (filter: FilterType) => void;
 }
 
-const StatusDisplay = <TData,>({
+const StatusDisplay = <
+  TData,
+  TTransformData extends Array<StatusData> = StatusData[],
+>({
   filter,
   filterTerm = 'status',
   onFilterChanged,
   ...props
-}: StatusDisplayProps<TData>) => {
+}: StatusDisplayProps<TData, TTransformData>) => {
   const handleDataClick = useCallback(
-    ({filterValue}: StatusData) => {
-      if (isDefined(filterValue) && isDefined(onFilterChanged)) {
-        const statusTerm = FilterTerm.fromString(
-          `${filterTerm}="${filterValue}"`,
-        );
-
-        if (isDefined(filter) && filter.hasTerm(statusTerm)) {
-          return;
-        }
-
-        const statusFilter = QueryFilter.fromTerm(statusTerm);
-        const newFilter = isDefined(filter)
-          ? filter.copy().and(statusFilter)
-          : statusFilter;
-
-        onFilterChanged(newFilter);
+    ({filterValue}: TTransformData[number]) => {
+      if (isEmpty(filterValue) || !isDefined(onFilterChanged)) {
+        return;
       }
+      const statusTerm = FilterTerm.fromString(
+        `${filterTerm}="${filterValue}"`,
+      );
+
+      if (isDefined(filter) && filter.hasTerm(statusTerm)) {
+        return;
+      }
+
+      const statusFilter = QueryFilter.fromTerm(statusTerm);
+      const newFilter = isDefined(filter)
+        ? filter.copy().and(statusFilter)
+        : statusFilter;
+
+      onFilterChanged(newFilter);
     },
     [filter, filterTerm, onFilterChanged],
   );
   return (
-    <DataDisplay<TData, StatusDisplayProps<TData>, StatusData[]>
+    <DataDisplay<
+      TData,
+      StatusDisplayProps<TData, TTransformData>,
+      TTransformData
+    >
       {...props}
       filter={filter}
       icons={DataDisplayIcons}
       initialState={{}}
     >
-      {({width, height, data: tdata, svgRef, state}) => (
-        <DonutChart<StatusData>
-          data={tdata}
+      {({width, height, data, svgRef, state}) => (
+        <DonutChart<TTransformData[number]>
+          data={data}
           height={height}
           showLegend={state.showLegend}
           svgRef={svgRef}
